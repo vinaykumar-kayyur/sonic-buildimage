62d12a62a749510b08f11565b0e014b2698726e2
Guohan Lu <gulv@microsoft.com>
2016-09-23 21:46:42 +0000

    add saiserver docker for mlnx sn2700 platform

---
+++ dockers/docker-saiserver-mlnx/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function clean_up {
+    service rsyslog stop
+    exit
+}
+
+start_mlnx()
+{
+    mkdir -p /dev/sxdevs
+
+    [ -e /dev/sxdevs/sxcdev ] || mknod /dev/sxdevs/sxcdev c 231 193
+}
+
+trap clean_up SIGTERM SIGKILL
+
+service rsyslog start
+
+start_mlnx
+
+/usr/bin/saiserver -p /etc/sai/profile.ini -f /etc/sai/portmap.ini
