d94148edec0996b80b875b5e146685db653dbd4a
Pavel Shirshov <pavelsh@microsoft.com>
2016-10-14 13:16:10 -0700

    Use a submodule for quagga repo

--- src/quagga/build.sh
+++ src/quagga/build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash -x
 
-mkdir quagga
-
-wget -O quagga_0.99.24.1.tar.gz https://github.com/Azure/sonic-quagga/archive/debian/0.99.24.1.tar.gz
-tar -xzf quagga_0.99.24.1.tar.gz -C quagga --strip 1
-
-ls -lrt
-
-cd quagga
+cd sonic-quagga
 ls -lrt
 
 sudo chmod a+x debian/rules
