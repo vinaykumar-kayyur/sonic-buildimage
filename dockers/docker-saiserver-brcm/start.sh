e3d0e1be9d917057cc492ea9d7090244cb281b05
Ubuntu <overnitary@UbuntuWestUS.hnkijaxchqtujhhfrsjw2ppy4a.dx.internal.cloudapp.net>
2016-10-25 22:54:07 +0000

    Add docker configuration for docker-saiserver-brcm

---
+++ dockers/docker-saiserver-brcm/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+function clean_up {
+    service rsyslog stop
+}
+
+start_mlnx()
+{
+    [ -e /dev/sxdevs/sxcdev ] || ( mkdir -p /dev/sxdevs && mknod /dev/sxdevs/sxcdev c 231 193 )
+}
+
+start_bcm()
+{
+    [ -e /dev/linux-bcm-knet ] || mknod /dev/linux-bcm-knet c 122 0
+    [ -e /dev/linux-user-bde ] || mknod /dev/linux-user-bde c 126 0
+    [ -e /dev/linux-kernel-bde ] || mknod /dev/linux-kernel-bde c 127 0
+}
+
+trap clean_up SIGTERM SIGKILL
+
+service rsyslog start
+
+start_bcm
+
+/usr/bin/saiserver -p /etc/sai/profile.ini -f /etc/sai/portmap.ini
