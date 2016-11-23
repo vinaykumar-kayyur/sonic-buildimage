97c561f558b4a6ef22aaad46e4cdc9c328ad5d29
Guohan Lu <gulv@microsoft.com>
2016-11-23 03:21:38 +0000

    update to 3.5.2-8

--- src/python3.5/build.sh
+++ src/python3.5/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PYTHON_VER=3.5.2
-PYTHON_DEB_VER=7
+PYTHON_DEB_VER=8
 
 wget -N http://http.debian.net/debian/pool/main/p/python3.5/python3.5_${PYTHON_VER}.orig.tar.xz
 wget -N http://http.debian.net/debian/pool/main/p/python3.5/python3.5_${PYTHON_VER}-${PYTHON_DEB_VER}.debian.tar.xz
