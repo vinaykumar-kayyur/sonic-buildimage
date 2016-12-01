24183b66fbb39608f6cfdb804f698f3b3021ec21
marian-pritsak <marianp@mellanox.com>
2016-12-01 20:16:49 +0200

    Add recipe for Mellanox hw management

--- platform/mellanox/rules.mk
+++ platform/mellanox/rules.mk
@@ -2,6 +2,7 @@ include $(PLATFORM_PATH)/sdk.mk
 include $(PLATFORM_PATH)/fw.mk
 include $(PLATFORM_PATH)/mft.mk
 include $(PLATFORM_PATH)/mlnx-sai.mk
+include $(PLATFORM_PATH)/hw-management.mk
 include $(PLATFORM_PATH)/docker-syncd-mlnx.mk
 
 SONIC_ALL += $(SONIC_GENERIC) $(DOCKER_SYNCD_MLNX) $(DOCKER_ORCHAGENT) \
