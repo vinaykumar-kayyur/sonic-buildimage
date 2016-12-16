759be24d2762c2332dc52785303bdd2f5a0f9699
Denys Haryachyy <Denys.Haryachyy@caviumnetworks.com>
2016-12-16 18:23:43 +0200

    Cavium support in the new build infrastructure

---
+++ platform/cavium/docker-syncd-cavm.mk
@@ -0,0 +1,7 @@
+# docker image for cavium syncd
+
+DOCKER_SYNCD_CAVM = docker-syncd-cavm.gz
+$(DOCKER_SYNCD_CAVM)_PATH = $(PLATFORM_PATH)/docker-syncd-cavm
+$(DOCKER_SYNCD_CAVM)_DEPENDS += $(SYNCD)
+$(DOCKER_SYNCD_CAVM)_LOAD_DOCKERS += $(DOCKER_BASE)
+SONIC_DOCKER_IMAGES += $(DOCKER_SYNCD_CAVM)
