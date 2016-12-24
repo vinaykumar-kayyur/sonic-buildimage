f6ed5c91ef16414d0702796cf600b706a117a717
Guohan Lu <gulv@microsoft.com>
2016-12-24 21:45:55 +0000

    add download url for broadcom sai/sdk deb packages

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,10 +1,12 @@
-BRCM_SAI = libsaibcm_2.0.3.7~20161214010054.112_amd64.deb
-$(BRCM_SAI)_PATH = $(PLATFORM_PATH)/sdk
-BRCM_SAI_DEV = libsaibcm-dev_2.0.3.7~20161214010054.112_amd64.deb
-$(BRCM_SAI_DEV)_PATH = $(PLATFORM_PATH)/sdk
+BRCM_SAI = libsaibcm_2.0.3.7_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_2.0.3.7_amd64.deb?sv=2015-04-05&sr=b&sig=3S9pY5Allql4fguipFdilJ%2BzP%2Ff4dvUFe3mNY3uhCIc%3D&se=2030-09-02T21%3A43%3A38Z&sp=r"
+
+BRCM_SAI_DEV = libsaibcm-dev_2.0.3.7_amd64.deb
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_2.0.3.7_amd64.deb?sv=2015-04-05&sr=b&sig=ztk12uK%2BN1LpCqsZM35YBs8m90OCPWMsjj%2FnY3IcJ7g%3D&se=2030-09-02T21%3A44%3A18Z&sp=r"
+
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 
-SONIC_COPY_DEBS += $(BRCM_SAI)
+SONIC_ONLINE_DEBS += $(BRCM_SAI)
 
 $(BRCM_SAI)_DEPENDS += $(BRCM_OPENNSL)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
--- platform/broadcom/sdk.mk
+++ platform/broadcom/sdk.mk
@@ -1,4 +1,14 @@
-BRCM_OPENNSL = libopennsl_3.2.1.5+0~20161214000201.54~1.gbpb514c3_amd64.deb
-$(BRCM_OPENNSL)_PATH = $(PLATFORM_PATH)/sdk
+BRCM_OPENNSL = libopennsl_3.2.1.5_amd64.deb
+$(BRCM_OPENNSL)_URL = "https://sonicstorage.blob.core.windows.net/packages/libopennsl_3.2.1.5_amd64.deb?sv=2015-04-05&sr=b&sig=qm6%2BkiGuRGsFKwZcUz6yEtbgbbwQuhxEr0chLM7qJEQ%3D&se=2030-09-02T21%3A41%3A11Z&sp=r"
 
-SONIC_COPY_DEBS += $(BRCM_OPENNSL)
+BRCM_OPENNSL_KERNEL = opennsl-modules-3.16.0-4-amd64_3.2.1.5_amd64.deb
+$(BRCM_OPENNSL_KERNEL)_URL = "https://sonicstorage.blob.core.windows.net/packages/opennsl-modules-3.16.0-4-amd64_3.2.1.5_amd64.deb?sv=2015-04-05&sr=b&sig=c8hO5PQpvod7IX3aYOiyvFB9rICxDgGiFF5g3GDHx84%3D&se=2030-09-02T21%3A42%3A34Z&sp=r"
+
+BCMCMD = bcmcmd
+$(BCMCMD)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmcmd?sv=2015-04-05&sr=b&sig=X3bFApmsNFmcnWM9mSGRxBugPcg%2FgJCHh5hhSuV1M2c%3D&se=2030-08-23T14%3A41%3A56Z&sp=r"
+
+DSSERVE = dsserve
+$(DSSERVE)_URL = "https://sonicstorage.blob.core.windows.net/packages/dsserve?sv=2015-04-05&sr=b&sig=aMlnRA%2FXZNmHPgmOj%2FNMJMYLWyvva1QrN4HcsVXvqKA%3D&se=2030-08-23T14%3A42%3A32Z&sp=r"
+
+SONIC_ONLINE_DEBS += $(BRCM_OPENNSL) $(BRCM_OPENNSL_KERNEL)
+SONIC_ONLINE_FILES += $(BCMCMD) $(DSSERVE)
