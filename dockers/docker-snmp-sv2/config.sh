dc361cfd7f0bb29be66faf89e36b7005eb928d71
Taoyu Li <taoyl@microsoft.com>
2017-01-19 03:19:14 +0000

    Minor fix

--- dockers/docker-snmp-sv2/config.sh
+++ dockers/docker-snmp-sv2/config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #sysDescription is currently mounted, uncomment this line and provide version.yml instead if use copying instead of mounting
-#sonic-cfggen -m /etc/sonic/minigraph.xml -v /etc/sonic/version.yml -t /etc/swss/snmp/sysDescription.j2 >/etc/ssw/sysDescription
+#sonic-cfggen -m /etc/sonic/minigraph.xml -y /etc/sonic/version.yml -t /etc/swss/snmp/sysDescription.j2 >/etc/ssw/sysDescription
 
 mkdir -p /etc/snmp
 
-sonic-cfggen -m /etc/sonic/minigraph.xml -v /etc/sonic/snmp.yml -t /etc/swss/snmp/snmpd.conf.j2 >/etc/snmp/snmpd.conf
+sonic-cfggen -m /etc/sonic/minigraph.xml -y /etc/sonic/snmp.yml -t /etc/swss/snmp/snmpd.conf.j2 >/etc/snmp/snmpd.conf
 
 hwsku=`sonic-cfggen -m /etc/sonic/minigraph.xml -v minigraph_hwsku`
 /bin/cp -rf /usr/share/sonic/$hwsku/alias_map.json /etc/snmp/
--- dockers/docker-teamd/config.sh
+++ dockers/docker-teamd/config.sh
@@ -3,6 +3,6 @@
 mkdir -p /etc/teamd
 
 for pc in `sonic-cfggen -m /etc/sonic/minigraph.xml --var-keys minigraph_portchannel_interfaces`; do
-  sonic-cfggen -m /etc/sonic/minigraph.xml -a '{"pc":"'$pc'"}' -t teamd.j2 >/etc/teamd/$pc.conf
+  sonic-cfggen -m /etc/sonic/minigraph.xml -a '{"pc":"'$pc'"}' -t /etc/swss/teamd/teamd.j2 >/etc/teamd/$pc.conf
 done
 
