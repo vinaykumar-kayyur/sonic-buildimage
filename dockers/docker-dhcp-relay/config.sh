feba6aefe4efbdf68bba46e2fa5662aa3d2fcacd
Joe LeVeque <jolevequ@microsoft.com>
2017-03-02 22:29:04 +0000

    Check if device requires DHCP relay upon docker start

--- dockers/docker-dhcp-relay/config.sh
+++ dockers/docker-dhcp-relay/config.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+MINIGRAPH_HOSTNAME=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_hostname"`
+DEVICE_ROLE=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_devices['$MINIGRAPH_HOSTNAME']['type']"`
+
+if [ $DEVICE_ROLE != "ToRRouter" ]; then
+    echo "Device does not require DHCP relay. docker-dhcp-relay exiting..."
+    exit 1
+fi
+
 sonic-cfggen -m /etc/sonic/minigraph.xml -t /usr/share/sonic/templates/isc-dhcp-relay.j2 > /etc/default/isc-dhcp-relay
 
