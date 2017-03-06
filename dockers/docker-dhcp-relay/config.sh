0fdd3c8e9a38f070421339b2e2896b8d83b24a69
Joe LeVeque <jolevequ@microsoft.com>
2017-03-06 20:39:45 +0000

    Remove unnecessary call to sonic-cfggen

--- dockers/docker-dhcp-relay/config.sh
+++ dockers/docker-dhcp-relay/config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-MINIGRAPH_HOSTNAME=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_hostname"`
-DEVICE_ROLE=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_devices['$MINIGRAPH_HOSTNAME']['type']"`
+DEVICE_ROLE=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_devices[minigraph_hostname]['type']"`
 
 if [ $DEVICE_ROLE != "ToRRouter" ]; then
     echo "Device does not require DHCP relay. docker-dhcp-relay exiting..."
