fc31d8302e229a6c5862c1c7722541a22abd104a
Joe LeVeque <jolevequ@microsoft.com>
2017-03-20 23:15:26 +0000

    Replace legacy rc.local with device-config service
     - device-config service will run only once on the first boot
     - This service installs platform drivers (what rc.local was doing), and
       also disables services that are not needed by that device based on
       the device's configuration (currently dhcp_relay and teamd)
     - rc.local use is discouraged in the modern era of system/service
       managers

---
+++ files/image_config/platform/device-config.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# device-config.sh
+#
+# Script to perform tasks which configure device upon first boot
+# To be run on first boot by device-config.service
+#
+
+# Install platform-dependent packages
+echo "Installing platform-dependent packages..."
+
+PLATFORM=`/usr/bin/sonic-cfggen -v platform`
+
+if [ -n $PLATFORM ]; then
+    echo "SONiC platform: $PLATFORM"
+
+    cp /usr/share/sonic/device/$platform/minigraph.xml /etc/sonic/
+
+    if [ -d /host/platform/$platform ]; then
+        dpkg -i /host/platform/$platform/*.deb
+    fi
+else
+    echo "SONiC platform unknown. Could not install platform-dependent packages."
+fi
+
+# Disable DHCP relay service if device does not require it
+DEVICE_ROLE=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_devices[minigraph_hostname]['type']"`
+
+if [ $DEVICE_ROLE != "ToRRouter" ]; then
+    echo "Device does not require DHCP relay. Disabling DHCP relay service..."
+    systemctl disable dhcp_relay.service
+fi
+
+
+# Disable teamd service if device does not require it
+NUM_PORTCHANNELS=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_portchannels.keys() | count"`
+if [ $NUM_PORTCHANNELS -eq 0 ]; then
+    echo "Device does not require teamd. Disabling teamd service..."
+    systemctl disable teamd.service
+fi
+
