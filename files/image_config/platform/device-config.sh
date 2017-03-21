4bd76f32717e0aaa3e629534a74646edc4fb7ed7
Joe LeVeque <jolevequ@microsoft.com>
2017-03-21 19:25:19 +0000

    Service runs after updategraph service, so only copy default minigraph if we didn't download one

--- files/image_config/platform/device-config.sh
+++ files/image_config/platform/device-config.sh
@@ -6,18 +6,19 @@
 # To be run on first boot by device-config.service
 #
 
-# Install platform-dependent packages
-echo "Installing platform-dependent packages..."
-
 PLATFORM=`sonic-cfggen -v platform`
 
 if [ -n $PLATFORM ]; then
     echo "SONiC platform: $PLATFORM"
 
-    cp /usr/share/sonic/device/$platform/minigraph.xml /etc/sonic/
+    if [ ! -f /etc/sonic/minigraph.xml ]; then
+        echo "Copying default minigraph for $PLATFORM to /etc/sonic/"
+        cp /usr/share/sonic/device/$PLATFORM/minigraph.xml /etc/sonic/
+    fi
 
-    if [ -d /host/platform/$platform ]; then
-        dpkg -i /host/platform/$platform/*.deb
+    if [ -d /host/platform/$PLATFORM ]; then
+        echo "Installing platform-dependent packages for $PLATFORM..."
+        dpkg -i /host/platform/$PLATFORM/*.deb
     fi
 else
     echo "SONiC platform unknown. Could not install platform-dependent packages."
@@ -34,6 +35,7 @@ fi
 
 # Disable teamd service if device does not require it
 NUM_PORTCHANNELS=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_portchannels.keys() | count"`
+
 if [ $NUM_PORTCHANNELS -eq 0 ]; then
     echo "Device does not require teamd. Disabling teamd service..."
     systemctl disable teamd.service
