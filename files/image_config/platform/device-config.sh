ca2d1c290aacd4ef57c43bd3c7b0a46ae4806cc1
Joe LeVeque <jolevequ@microsoft.com>
2017-03-22 00:34:58 +0000

    Break into two services, platform-init (which only runs on first boot) and device-config (which runs on every boot)

--- files/image_config/platform/device-config.sh
+++ files/image_config/platform/device-config.sh
@@ -2,32 +2,17 @@
 #
 # device-config.sh
 #
-# Script to perform tasks which configure device upon first boot
-# To be run on first boot by device-config.service
+# Script to perform tasks which configure device based on its role, etc.
+# To be run by device-config.service
 #
-
-PLATFORM=`sonic-cfggen -v platform`
-
-if [ -n $PLATFORM ]; then
-    echo "SONiC platform: $PLATFORM"
-
-    if [ ! -f /etc/sonic/minigraph.xml ]; then
-        echo "Copying default minigraph for $PLATFORM to /etc/sonic/"
-        cp /usr/share/sonic/device/$PLATFORM/minigraph.xml /etc/sonic/
-    fi
-
-    if [ -d /host/platform/$PLATFORM ]; then
-        echo "Installing platform-dependent packages for $PLATFORM..."
-        dpkg -i /host/platform/$PLATFORM/*.deb
-    fi
-else
-    echo "SONiC platform unknown. Could not install platform-dependent packages."
-fi
 
 # Disable DHCP relay service if device does not require it
 DEVICE_ROLE=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_devices[minigraph_hostname]['type']"`
 
-if [ $DEVICE_ROLE != "ToRRouter" ]; then
+if [ $DEVICE_ROLE == "ToRRouter" ]; then
+    echo "Device requires DHCP relay. Enabling DHCP relay service..."
+    systemctl enable dhcp_relay.service
+else
     echo "Device does not require DHCP relay. Disabling DHCP relay service..."
     systemctl disable dhcp_relay.service
 fi
@@ -36,7 +21,10 @@ fi
 # Disable teamd service if device does not require it
 NUM_PORTCHANNELS=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_portchannels.keys() | count"`
 
-if [ $NUM_PORTCHANNELS -eq 0 ]; then
+if [ $NUM_PORTCHANNELS -gt 0 ]; then
+    echo "Device requires teamd. Enabling teamd service..."
+    systemctl enable teamd.service
+else
     echo "Device does not require teamd. Disabling teamd service..."
     systemctl disable teamd.service
 fi
---
+++ files/image_config/platform/platform-init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# platform-init.sh
+#
+# Script to perform tasks which configure platform upon first boot
+# To be run by platform-initservice if it detects first boot
+#
+
+PLATFORM=`sonic-cfggen -v platform`
+
+if [ -n $PLATFORM ]; then
+    echo "SONiC platform: $PLATFORM"
+
+    echo "Copying default minigraph for $PLATFORM to /etc/sonic/"
+    cp /usr/share/sonic/device/$PLATFORM/minigraph.xml /etc/sonic/
+
+    if [ -d /host/platform/$PLATFORM ]; then
+        echo "Installing platform-dependent packages for $PLATFORM..."
+        dpkg -i /host/platform/$PLATFORM/*.deb
+    fi
+else
+    echo "SONiC platform unknown. Could not install platform-dependent packages."
+fi
+
