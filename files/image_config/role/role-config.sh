d4e52b8bdff699c76b1437e45af99228f119080b
Joe LeVeque <jolevequ@microsoft.com>
2017-03-22 22:50:27 +0000

    Revamp role-config
     - Move role-config service and script in to role/ directory
     - role-config now writes or deletes files to/from /etc/sonic/role/
       directory
     - Services which are role-dependent use 'ConditionPathExists=' to
       determine whether or not to start

--- files/image_config/role/role-config.sh
+++ files/image_config/role/role-config.sh
@@ -7,25 +7,27 @@
 #
 
 # Disable DHCP relay service if device does not require it
+DHCP_RELAY_SERVICE_START_FILE=/etc/sonic/role/dhcp_relay
 DEVICE_ROLE=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_devices[minigraph_hostname]['type']"`
 
 if [ $DEVICE_ROLE == "ToRRouter" ]; then
-    echo "Device requires DHCP relay. Enabling DHCP relay service..."
-    systemctl enable dhcp_relay.service
+    echo "Device requires DHCP relay service. Ensuring start file exists..."
+    touch $DHCP_RELAY_SERVICE_START_FILE
 else
-    echo "Device does not require DHCP relay. Disabling DHCP relay service..."
-    systemctl disable dhcp_relay.service
+    echo "Device does not require DHCP relay service. Deleting start file..."
+    rm -f $DHCP_RELAY_SERVICE_START_FILE
 fi
 
 
 # Disable teamd service if device does not require it
+TEAMD_SERVICE_START_FILE=/etc/sonic/role/teamd
 NUM_PORTCHANNELS=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "minigraph_portchannels.keys() | count"`
 
 if [ $NUM_PORTCHANNELS -gt 0 ]; then
-    echo "Device requires teamd. Enabling teamd service..."
-    systemctl enable teamd.service
+    echo "Device requires teamd service. Ensuring start file exists..."
+    touch $TEAMD_SERVICE_START_FILE
 else
-    echo "Device does not require teamd. Disabling teamd service..."
-    systemctl disable teamd.service
+    echo "Device does not require teamd service. Deleting start file..."
+    rm -f $TEAMD_SERVICE_START_FILE
 fi
 
