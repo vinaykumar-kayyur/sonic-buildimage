884ffd868eb543fe9b5861eae133e605b314e092
Pavel Shirshov <pavelsh@microsoft.com>
2017-03-23 17:01:17 -0700

    Update make files for rpc

--- platform/broadcom/docker-syncd-brcm-rpc.mk
+++ platform/broadcom/docker-syncd-brcm-rpc.mk
@@ -1,4 +1,4 @@
-# docker image for brcm syncd
+# docker image for brcm syncd with rpc
 
 DOCKER_SYNCD_BRCM_RPC = docker-syncd-brcm-rpc.gz
 $(DOCKER_SYNCD_BRCM_RPC)_PATH = $(PLATFORM_PATH)/docker-syncd-brcm-rpc
@@ -6,12 +6,3 @@ $(DOCKER_SYNCD_BRCM_RPC)_DEPENDS += $(SYNCD) $(LIBTHRIFT)
 $(DOCKER_SYNCD_BRCM_RPC)_FILES += $(DSSERVE) $(BCMCMD)
 $(DOCKER_SYNCD_BRCM_RPC)_LOAD_DOCKERS += $(DOCKER_SYNCD_BRCM)
 SONIC_DOCKER_IMAGES += $(DOCKER_SYNCD_BRCM_RPC)
-
-$(DOCKER_SYNCD_BRCM_RPC)_CONTAINER_NAME = syncd
-$(DOCKER_SYNCD_BRCM_RPC)_RUN_OPT += --net=host --privileged -t
-$(DOCKER_SYNCD_BRCM_RPC)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
-$(DOCKER_SYNCD_BRCM_RPC)_RUN_OPT += -v /var/run/docker-syncd:/var/run/sswsyncd
-$(DOCKER_SYNCD_BRCM_RPC)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
-
-$(DOCKER_SYNCD_BRCM)_BASE_IMAGE_FILES += bcmcmd:/usr/bin/bcmcmd
-
--- platform/mellanox/docker-syncd-mlnx-rpc.mk
+++ platform/mellanox/docker-syncd-mlnx-rpc.mk
@@ -1,4 +1,4 @@
-# docker image for mlnx syncd
+# docker image for mlnx syncd with rpc
 
 DOCKER_SYNCD_MLNX_RPC = docker-syncd-mlnx-rpc.gz
 $(DOCKER_SYNCD_MLNX_RPC)_PATH = $(PLATFORM_PATH)/docker-syncd-mlnx-rpc
@@ -6,8 +6,3 @@ $(DOCKER_SYNCD_MLNX_RPC)_DEPENDS += $(SYNCD) $(LIBTHRIFT)
 $(DOCKER_SYNCD_MLNX_RPC)_FILES += $(MLNX_FW)
 $(DOCKER_SYNCD_MLNX_RPC)_LOAD_DOCKERS += $(DOCKER_SYNCD_MLNX)
 SONIC_DOCKER_IMAGES += $(DOCKER_SYNCD_MLNX_RPC)
-
-$(DOCKER_SYNCD_MLNX_RPC)_CONTAINER_NAME = syncd
-$(DOCKER_SYNCD_MLNX_RPC)_RUN_OPT += --net=host --privileged -t
-$(DOCKER_SYNCD_MLNX_RPC)_RUN_OPT += -v /host/machine.conf:/etc/machine.conf
-$(DOCKER_SYNCD_MLNX_RPC)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
