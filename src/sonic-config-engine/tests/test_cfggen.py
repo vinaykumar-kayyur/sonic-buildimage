2f80407a18351eefe7208e6e37371625f40d5ccf
Taoyu Li <taoyl@microsoft.com>
2017-03-29 22:14:55 +0000

    Update config engine tests

--- src/sonic-config-engine/tests/test_cfggen.py
+++ src/sonic-config-engine/tests/test_cfggen.py
@@ -9,6 +9,8 @@ class TestCfgGen(TestCase):
         self.script_file = os.path.join(self.test_dir, '..', 'sonic-cfggen')
         self.sample_graph = os.path.join(self.test_dir, 'sample_graph.xml')
         self.sample_graph_t0 = os.path.join(self.test_dir, 't0-sample-graph.xml')
+        self.sample_graph_simple = os.path.join(self.test_dir, 'simple-sample-graph.xml')
+        self.port_config = os.path.join(self.test_dir, 't0-sample-port-config.ini')
 
     def run_script(self, argument):
         print '\n    Running sonic-cfggen ' + argument
@@ -59,4 +61,29 @@ class TestCfgGen(TestCase):
         argument = '-m "' + self.sample_graph_t0 + '" -v minigraph_acls'
         output = self.run_script(argument)
         self.assertEqual(output.strip(), "{'DataAcl': ['Ethernet112', 'Ethernet116', 'Ethernet120', 'Ethernet124']}")
+
+    def test_minigraph_interfaces(self):
+        argument = '-m "' + self.sample_graph_simple + '" -p  "' + self.port_config + '" -v minigraph_interfaces'
+        output = self.run_script(argument)
+        self.assertEqual(output.strip(), "[{'subnet': IPv4Network('10.0.0.58/31'), 'peer_addr': IPv4Address('10.0.0.59'), 'addr': IPv4Address('10.0.0.58'), 'mask': IPv4Address('255.255.255.254'), 'attachto': 'Ethernet0', 'prefixlen': 31}, {'subnet': IPv6Network('fc00::74/126'), 'peer_addr': IPv6Address('fc00::76'), 'addr': IPv6Address('fc00::75'), 'mask': '126', 'attachto': 'Ethernet0', 'prefixlen': 126}]")
+        
+    def test_minigraph_vlans(self):
+        argument = '-m "' + self.sample_graph_simple + '" -p  "' + self.port_config + '" -v minigraph_vlans'
+        output = self.run_script(argument)
+        self.assertEqual(output.strip(), "{'Vlan1000': {'name': 'Vlan1000', 'members': ['Ethernet8'], 'vlanid': '1000'}}")
+
+    def test_minigraph_vlan_interfaces(self):
+        argument = '-m "' + self.sample_graph_simple + '" -p  "' + self.port_config + '" -v minigraph_vlan_interfaces'
+        output = self.run_script(argument)
+        self.assertEqual(output.strip(), "[{'prefixlen': 27, 'subnet': IPv4Network('192.168.0.0/27'), 'mask': IPv4Address('255.255.255.224'), 'addr': IPv4Address('192.168.0.1'), 'attachto': 'Vlan1000'}]")
+
+    def test_minigraph_portchannels(self):
+        argument = '-m "' + self.sample_graph_simple + '" -p  "' + self.port_config + '" -v minigraph_portchannels'
+        output = self.run_script(argument)
+        self.assertEqual(output.strip(), "{'PortChannel01': {'name': 'PortChannel01', 'members': ['Ethernet4']}}")
+
+    def test_minigraph_portchannels(self):
+        argument = '-m "' + self.sample_graph_simple + '" -p  "' + self.port_config + '" -v minigraph_portchannel_interfaces'
+        output = self.run_script(argument)
+        self.assertEqual(output.strip(), "[{'subnet': IPv4Network('10.0.0.56/31'), 'peer_addr': IPv4Address('10.0.0.57'), 'addr': IPv4Address('10.0.0.56'), 'mask': IPv4Address('255.255.255.254'), 'attachto': 'PortChannel01', 'prefixlen': 31}, {'subnet': IPv6Network('fc00::70/126'), 'peer_addr': IPv6Address('fc00::72'), 'addr': IPv6Address('fc00::71'), 'mask': '126', 'attachto': 'PortChannel01', 'prefixlen': 126}]")
 
