dc5a2178f2a1c691abd7db8e486146b950deae83
Nadiya.Stetskovych <Nadiya.Stetskovych@cavium.com>
2017-04-03 22:51:04 +0300

    Allow dpkg -i for cavm_xpnet
    Signed-off-by: Nadiya.Stetskovych <Nadiya.Stetskovych@cavium.com>

--- platform/cavium/cavm-xpnet.mk
+++ platform/cavium/cavm-xpnet.mk
@@ -1,4 +1,4 @@
-CAVM_SAI_URL = https://github.com/XPliant/OpenXPS/raw/7704afc78cc1ce3b4e2aca6659afa9dc171ba874/SAI
+CAVM_SAI_URL = https://github.com/XPliant/OpenXPS/raw/18cd5fb556b6262943c0af70375e2631ac3fe08d/SAI
 
 CAVM_XPNET_DEB = xp80-Pcie-Endpoint.deb
 $(CAVM_XPNET_DEB)_URL = $(CAVM_SAI_URL)/netdev/$(CAVM_XPNET_DEB)
