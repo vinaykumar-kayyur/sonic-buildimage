25748b66a19292b5ec800ebc4c541f45b5867b92
Pavel Shirshov <pavelsh@microsoft.com>
2017-05-10 17:48:17 -0700

    Add less and vim in docker-base. Add strace in docker_base_dbg

--- rules/docker-base.mk
+++ rules/docker-base.mk
@@ -5,10 +5,10 @@ $(DOCKER_BASE)_PATH = $(DOCKERS_PATH)/docker-base
 
 ifeq ($(SONIC_CONFIG_DEBUG),y)
 GDB = gdb
-VIM = vim
+STRACE = strace
 OPENSSH = openssh-client
 SSHPASS = sshpass
-$(DOCKER_BASE)_DBG_PACKAGES += $(GDB) $(VIM) $(OPENSSH) $(SSHPASS)
+$(DOCKER_BASE)_DBG_PACKAGES += $(GDB) $(STRACE) $(OPENSSH) $(SSHPASS)
 endif
 
 SONIC_DOCKER_IMAGES += $(DOCKER_BASE)
