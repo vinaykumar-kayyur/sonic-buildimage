e8fed239993ea75108309c99e9fbfb2269ca54b8
Pavel Shirshov <pavelsh@microsoft.com>
2017-05-26 15:06:07 -0700

    Lowercase for centec

--- platform/centec/docker-ptf-centec.mk
+++ platform/centec/docker-ptf-centec.mk
@@ -1,6 +1,6 @@
 # docker image for docker-ptf-centec
 
-DOCKER_PTF_CENTEC = docker-ptf-CENTEC.gz
+DOCKER_PTF_CENTEC = docker-ptf-centec.gz
 $(DOCKER_PTF_CENTEC)_PATH = $(DOCKERS_PATH)/docker-ptf-saithrift
 $(DOCKER_PTF_CENTEC)_DEPENDS += $(PYTHON_SAITHRIFT_CENTEC)
 $(DOCKER_PTF_CENTEC)_LOAD_DOCKERS += $(DOCKER_PTF)
