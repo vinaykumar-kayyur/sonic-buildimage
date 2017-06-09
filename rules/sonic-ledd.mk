49b4e28f5dcca27431a08b24399e4e2d7743a186
Joe LeVeque <jolevequ@microsoft.com>
2017-06-09 21:19:48 +0000

    Fix build

--- rules/sonic-ledd.mk
+++ rules/sonic-ledd.mk
@@ -1,10 +1,5 @@
 # sonic-ledd (SONiC Front-panel LED control daemon) Debian package
 
-SONIC_LEDD_VERSION = 1.0
-SONIC_LEDD_VERSION_FULL = $(SONIC_LEDD_VERSION)-1
-
-export SONIC_LEDD_VERSION SONIC_LEDD_VERSION_FULL
-
-SONIC_LEDD = python-sonic-ledd_$(SONIC_LEDD_VERSION_FULL)_all.deb
+SONIC_LEDD = python-sonic-ledd_1.0-1_all.deb
 $(SONIC_LEDD)_SRC_PATH = $(SRC_PATH)/sonic-platform-daemons/sonic-ledd
-SONIC_MAKE_DEBS += $(SONIC_LEDD)
+SONIC_PYTHON_STDEB_DEBS += $(SONIC_LEDD)
