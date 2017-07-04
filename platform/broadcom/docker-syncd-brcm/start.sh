d527836993217843105befbacf7c4ad623e83526
Polly Hsu <polly_hsu@accton.com>
2017-07-04 12:42:24 +0800

    Update start.sh

--- platform/broadcom/docker-syncd-brcm/start.sh
+++ platform/broadcom/docker-syncd-brcm/start.sh
@@ -8,21 +8,24 @@ supervisorctl start rsyslogd
 
 supervisorctl start syncd
 
-# Wait until syncd has created the socket for bcmcmd to connect to
-while true; do
-    if [ -e /var/run/sswsyncd/sswsyncd.socket ]; then
-        break
-    fi
-    sleep 1
-done
+# Function: wait until syncd has created the socket for bcmcmd to connect to
+wait_syncd() {
+    while true; do
+        if [ -e /var/run/sswsyncd/sswsyncd.socket ]; then
+            break
+        fi
+        sleep 1
+    done
+}
 
 # If this platform has an initialization file for the Broadcom LED microprocessor, load it
 if [ -r ${PLATFORM_DIR}/led_proc_init.soc ]; then
+    wait_syncd
     /usr/bin/bcmcmd -t 60 "rcload ${PLATFORM_DIR}/led_proc_init.soc"
 fi
 
 # If this platform has an initialization file for pre-emphasis, load it
 if [ -r ${PLATFORM_DIR}/preemphasis_init.soc ]; then
+    wait_syncd
     /usr/bin/bcmcmd -t 60 "rcload ${PLATFORM_DIR}/preemphasis_init.soc"
 fi
-
