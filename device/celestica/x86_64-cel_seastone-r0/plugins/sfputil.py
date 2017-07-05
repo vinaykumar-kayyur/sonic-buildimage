f2e7083ff610641012daf176cde3501819d4865b
Abhisit Sangjan <asang@celestica.com>
2017-07-05 11:08:16 +0700

    [Seastone]: Add a new supported device and platform, Celestica Seastone-DX010. Switch Vendor: Celestica Switch SKU : Seastone-DX010 ASIC Vendor: Broadcom Swich ASIC: Tomahawk Port Configuration: 32x100G SONiC Image: SONiC-ONIE-Broadcom
    Signed-off-by: asang@celestica.com

---
+++ device/celestica/x86_64-cel_seastone-r0/plugins/sfputil.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+try:
+    from sonic_sfp.sfputilbase import sfputilbase
+except ImportError, e:
+    raise ImportError (str(e) + "- required module not found")
+
+
+class sfputil(sfputilbase):
+    """Platform specific sfputil class"""
+
+    port_start = 1
+    port_end = 32
+    ports_in_block = 33
+
+    port_to_eeprom_mapping = {}
+    port_to_i2c_mapping = {
+        1: 26,
+        2: 27,
+        3: 28,
+        4: 29,
+        5: 30,
+        6: 31,
+        7: 32,
+        8: 33,
+        9: 34,
+        10: 35,
+        11: 36,
+        12: 37,
+        13: 38,
+        14: 39,
+        15: 40,
+        16: 41,
+        17: 42,
+        18: 43,
+        19: 44,
+        20: 45,
+        21: 46,
+        22: 47,
+        23: 48,
+        24: 49,
+        25: 50,
+        26: 51,
+        27: 52,
+        28: 53,
+        29: 47,
+        30: 48,
+        31: 49,
+        32: 50
+    }
+
+    _qsfp_ports = range(0, ports_in_block + 1)
+
+    def __init__(self, port_num):
+        # Override port_to_eeprom_mapping for class initialization
+        eeprom_path = '/sys/bus/i2c/devices/i2c-{0}/{0}-0050/eeprom'
+        for x in range(1, self.port_end + 1):
+            port_eeprom_path = eeprom_path.format(self.port_to_i2c_mapping[x])
+            self.port_to_eeprom_mapping[x] = port_eeprom_path
+        sfputilbase.__init__(self, port_num)
