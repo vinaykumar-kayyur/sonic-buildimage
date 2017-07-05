e8d4fe0f4b9a1c0c351ed765aa1ff2c90ba06159
Pradchaya P <pphuchar@celestica.com>
2017-07-05 18:47:02 +0700

    Fix inccorect i2c to qsfp port mapping.

--- device/celestica/x86_64-cel_seastone-r0/plugins/sfputil.py
+++ device/celestica/x86_64-cel_seastone-r0/plugins/sfputil.py
@@ -9,52 +9,17 @@ except ImportError, e:
 class sfputil(sfputilbase):
     """Platform specific sfputil class"""
 
-    port_start = 1
-    port_end = 32
-    ports_in_block = 33
+    port_start = 0
+    port_end = 31
+    ports_in_block = 32
 
     port_to_eeprom_mapping = {}
-    port_to_i2c_mapping = {
-        1: 26,
-        2: 27,
-        3: 28,
-        4: 29,
-        5: 30,
-        6: 31,
-        7: 32,
-        8: 33,
-        9: 34,
-        10: 35,
-        11: 36,
-        12: 37,
-        13: 38,
-        14: 39,
-        15: 40,
-        16: 41,
-        17: 42,
-        18: 43,
-        19: 44,
-        20: 45,
-        21: 46,
-        22: 47,
-        23: 48,
-        24: 49,
-        25: 50,
-        26: 51,
-        27: 52,
-        28: 53,
-        29: 54,
-        30: 55,
-        31: 56,
-        32: 57
-    }
 
     _qsfp_ports = range(0, ports_in_block + 1)
 
     def __init__(self, port_num):
         # Override port_to_eeprom_mapping for class initialization
         eeprom_path = '/sys/bus/i2c/devices/i2c-{0}/{0}-0050/eeprom'
-        for x in range(1, self.port_end + 1):
-            port_eeprom_path = eeprom_path.format(self.port_to_i2c_mapping[x])
-            self.port_to_eeprom_mapping[x] = port_eeprom_path
+        for x in range(self.port_start, self.port_end + 1):
+            self.port_to_eeprom_mapping[x] = eeprom_path.format( x + 26 )
         sfputilbase.__init__(self, port_num)
