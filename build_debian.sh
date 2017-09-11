332929b555c61f617b800e0f7b7cccee86d8b73b
Ravi Jonnadula <rjonnadula@linkedin.com>
2017-09-11 12:50:04 -0700

    Framework to plugin Organization specific scripts

--- build_debian.sh
+++ build_debian.sh
@@ -308,8 +308,7 @@ if [ -f sonic_debian_extension.sh ]; then
     ./sonic_debian_extension.sh $FILESYSTEM_ROOT $PLATFORM_DIR
 fi
 
-# Setting up configurations & scripts for AAA Extensions 
-## Orignaization specific extensions such as Configuration & Scripts for features like AAA, ZTP...
+## Orgnaization specific extensions such as Configuration & Scripts for features like AAA, ZTP...
 if [ "${enable_organization_extensions}" = "y" ]; then
    if [ -f files/build_templates/organization_extensions.sh ]; then
       sudo chmod 755 files/build_templates/organization_extensions.sh 
