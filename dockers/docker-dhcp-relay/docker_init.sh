57101fbb77e04b9abdb57c2eefb4a9656109b888
Joe LeVeque <jolevequ@microsoft.com>
2017-09-12 23:45:15 +0000

    Modify to work with Taoyu's minigraph/DB changes (#942)

--- dockers/docker-dhcp-relay/docker_init.sh
+++ dockers/docker-dhcp-relay/docker_init.sh
@@ -4,6 +4,10 @@
 mkdir -p /etc/supervisor/conf.d/
 sonic-cfggen -m /etc/sonic/minigraph.xml -t /usr/share/sonic/templates/docker-dhcp-relay.supervisord.conf.j2 > /etc/supervisor/conf.d/docker-dhcp-relay.supervisord.conf
 
+# Generate the script that waits for all interfaces to come up and make it executable
+sonic-cfggen -d -t /usr/share/sonic/templates/wait_for_intf.sh.j2 > /usr/bin/wait_for_intf.sh
+chmod +x /usr/bin/wait_for_intf.sh
+
 # The docker container should start this script as PID 1, so now that supervisord is
 # properly configured, we exec supervisord so that it runs as PID 1 for the
 # duration of the container's lifetime
--- dockers/docker-dhcp-relay/start.sh
+++ dockers/docker-dhcp-relay/start.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-function wait_until_iface_exists
-{
-    IFACE=$1
-
-    echo "Waiting for interface ${IFACE}..."
-
-    # Wait for the interface to come up (i.e., 'ip link show' returns 0)
-    until ip link show $IFACE > /dev/null 2>&1; do
-        sleep 1
-    done
-
-    echo "Interface ${IFACE} is created"
-}
-
 # Remove stale rsyslog PID file if it exists
 rm -f /var/run/rsyslogd.pid
 
@@ -21,8 +7,6 @@ rm -f /var/run/rsyslogd.pid
 supervisorctl start rsyslogd
 
 # Wait for all interfaces to come up before starting the DHCP relay agent(s)
-sonic-cfggen -d -t /usr/share/sonic/templates/wait_for_intf.sh.j2 > /usr/bin/wait_for_intf.sh
-chmod +x /usr/bin/wait_for_intf.sh
 /usr/bin/wait_for_intf.sh
 
 # Start the DHCP relay agent(s)
