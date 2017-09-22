a46bae2529021a6cf7500df9ec81d8e49c152484
Shu0T1an ChenG <shuche@microsoft.com>
2017-09-22 11:47:07 -0700

    [Broadcom]: Update SAI version to 3.0.3.2-3
    Update Seastone config.bcm files

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.0.3.2-2_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-2_amd64.deb?sv=2015-04-05&sr=b&sig=A9GjUgI9%2B7kRQHsCFqH07iRQLhdkwSH5tvSXepb3b%2BA%3D&se=2031-05-30T22%3A47%3A47Z&sp=r"
+BRCM_SAI = libsaibcm_3.0.3.2-3_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.0.3.2-3_amd64.deb?sv=2015-04-05&sr=b&sig=N8PItR5KMlFJUQICeghzwSkiuqdBNBVTYX6svqR3wcg%3D&se=2031-06-01T18%3A46%3A00Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-2_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.0.3.2-3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-2_amd64.deb?sv=2015-04-05&sr=b&sig=lDIwnDNHIA0WKc5ggTA4x3pVmBIM%2Bdib0hR7H1qyTe0%3D&se=2031-05-30T22%3A48%3A07Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.0.3.2-3_amd64.deb?sv=2015-04-05&sr=b&sig=wJ%2BKxm3CzBpCnkB4Zv70wlWbt9cTEOupPrVTcJe8CGM%3D&se=2031-06-01T18%3A46%3A28Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
