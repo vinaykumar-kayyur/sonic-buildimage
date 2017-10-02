5c8068662954b3f1e412ebe7b151722c17818989
Joe LeVeque <jolevequ@microsoft.com>
2017-10-02 23:18:38 +0000

    Add radvd.conf.j2 template; Run radvd in foreground; Rename supervisor config file

--- dockers/docker-router-advertiser/start.sh
+++ dockers/docker-router-advertiser/start.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Generate /etc/radvd.conf config file
+sonic-cfggen -d -t /usr/share/sonic/templates/radvd.conf.j2 > /etc/radvd.conf
+
 rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
