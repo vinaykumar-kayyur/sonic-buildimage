154d0334eafcb629a63f313aab4faee579be6bdb
Yonatan Piasetzky <yonatanp@mellanox.com>
2017-10-04 18:25:44 +0300

    updated docker sonic p4 with scripts and config. restored p4c-bm to allow compiling json in make

--- platform/p4/rules.mk
+++ platform/p4/rules.mk
@@ -1,7 +1,7 @@
 # include $(PLATFORM_PATH)/p4-softswitch.mk
-# include $(PLATFORM_PATH)/tenjin.mk
-# include $(PLATFORM_PATH)/p4-hlir.mk
-# include $(PLATFORM_PATH)/p4c-bm.mk
+include $(PLATFORM_PATH)/tenjin.mk
+include $(PLATFORM_PATH)/p4-hlir.mk
+include $(PLATFORM_PATH)/p4c-bm.mk
 # include $(PLATFORM_PATH)/p4-sai-bm.mk
 include $(PLATFORM_PATH)/p4-bmv.mk
 include $(PLATFORM_PATH)/p4-switch.mk
