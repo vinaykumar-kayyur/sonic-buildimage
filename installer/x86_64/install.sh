c66bef7be3ffba018deb0014b284eb5723c966d2
Polly Hsu <polly_hsu@accton.com>
2017-11-15 17:14:38 +0800

    Update install.sh

--- installer/x86_64/install.sh
+++ installer/x86_64/install.sh
@@ -455,7 +455,9 @@ if [ "$install_env" = "onie" ]; then
     cp /etc/machine.conf $demo_mnt
 
     # Store installation log in target file system
-    ${onie_bin} onie-support $demo_mnt/$image_dir
+    rm -f $onie_initrd_tmp/tmp/onie-support*.tar.bz2
+    ${onie_bin} onie-support /tmp
+    mv $onie_initrd_tmp/tmp/onie-support*.tar.bz2 $demo_mnt/$image_dir/
 
     if [ "$firmware" = "uefi" ] ; then
         demo_install_uefi_grub "$demo_mnt" "$blk_dev"
