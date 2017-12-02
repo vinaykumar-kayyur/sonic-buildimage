fcd1bb6c0361bd31ae663f08063a948e742c4105
Joe LeVeque <jolevequ@microsoft.com>
2017-12-02 01:07:46 +0000

    Don't start caclmgrd on any Arista platform (including 7050)

--- files/image_config/caclmgrd/caclmgrd-start.sh
+++ files/image_config/caclmgrd/caclmgrd-start.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-PLATFORM=$(sonic-cfggen -v "platform")
-
-# If this is an Arista platform newer than the 7050, do not start caclmgrd.
-# Newer Arista devices will use their own service ACL manager instead.
-if [ "$(echo $PLATFORM | grep -v "7050" | grep -c "arista")" -gt 0 ]; then
+# Only start control plance ACL manager daemon if not an Arista platform.
+# Arista devices will use their own service ACL manager daemon(s) instead.
+if [ "$(sonic-cfggen -v "platform" | grep -c "arista")" -gt 0 ]; then
     echo "Not starting caclmgrd - unsupported platform"
     exit 0
 fi
