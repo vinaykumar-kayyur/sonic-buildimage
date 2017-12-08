e276ff81932387b8ab5a9b674a9d1625b9c1612d
Qi Luo <qiluo-msft@users.noreply.github.com>
2017-12-08 02:27:14 +0000

    Remove unused dependencies
    Signed-off-by: Qi Luo <qiluo-msft@users.noreply.github.com>

--- rules/docker-snmp-sv2.mk
+++ rules/docker-snmp-sv2.mk
@@ -3,9 +3,7 @@
 DOCKER_SNMP_SV2 = docker-snmp-sv2.gz
 $(DOCKER_SNMP_SV2)_PATH = $(DOCKERS_PATH)/docker-snmp-sv2
 ## TODO: remove LIBPY3_DEV if we can get pip3 directly
-## TODO: replace SONIC_UTILS with appropriate target once 
-#  SONiC Python modules are moved to their own package
-$(DOCKER_SNMP_SV2)_DEPENDS += $(SNMP) $(SNMPD) $(PY3) $(LIBPY3_DEV) $(SONIC_UTILS)
+$(DOCKER_SNMP_SV2)_DEPENDS += $(SNMP) $(SNMPD) $(PY3) $(LIBPY3_DEV)
 $(DOCKER_SNMP_SV2)_PYTHON_WHEELS += $(ASYNCSNMP_PY3)
 $(DOCKER_SNMP_SV2)_LOAD_DOCKERS += $(DOCKER_CONFIG_ENGINE)
 SONIC_DOCKER_IMAGES += $(DOCKER_SNMP_SV2)
