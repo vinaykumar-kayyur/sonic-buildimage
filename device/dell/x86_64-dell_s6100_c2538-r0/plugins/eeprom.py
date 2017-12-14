c7ab742435ab5fca000382aadc268b020f16309e
Joe LeVeque <jolevequ@microsoft.com>
2017-12-14 22:54:02 +0000

    [Dell S6100]: EEPROM plugin overrides serial_number_str() to return service tag instead of serial number

--- device/dell/x86_64-dell_s6100_c2538-r0/plugins/eeprom.py
+++ device/dell/x86_64-dell_s6100_c2538-r0/plugins/eeprom.py
@@ -21,3 +21,17 @@ class board(eeprom_tlvinfo.TlvInfoDecoder):
         self.eeprom_path = "/sys/class/i2c-adapter/i2c-2/2-0050/eeprom"
         super(board, self).__init__(self.eeprom_path, 0, '', True)
 
+    def serial_number_str(self, e):
+        """Return Service Tag instead of serial number"""
+
+        if self._TLV_HDR_ENABLED:
+            tlv_no_header = e[self._TLV_INFO_HDR_LEN:] if self._TLV_HDR_ENABLED else e
+
+        (is_valid, t) = self.get_tlv_index(tlv_no_header, self._TLV_CODE_SERVICE_TAG)
+
+        if not is_valid:
+            return "Bad service tag"
+
+        t = tlv_no_header[t:]
+
+        return t[2:2 + ord(t[1])]
