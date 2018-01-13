96d7b69b370fe2e98887909ab7726f76bf58c8ee
Joe LeVeque <jolevequ@microsoft.com>
2018-01-13 01:56:40 +0000

    Add comment to rules/sonic-utilities.mk re: runtime dependencies

--- rules/sonic-utilities.mk
+++ rules/sonic-utilities.mk
@@ -1,4 +1,14 @@
 # sonic utilities package
+#
+# NOTE: sonic-config-engine is a build-time dependency of sonic-utilities
+# due to unit tests which are run during the build. However,
+# sonic-platform-common and swsssdk are runtime dependencies, and should be
+# added here also. However, the current build system assumes all runtime
+# dependencies are .deb packages.
+#
+# TODO: Create a way to specify both .deb and .whl runtime dependencies
+#       then add the aforementioned runtime dependencies here.
+#
 
 SONIC_UTILS = python-sonic-utilities_1.2-1_all.deb
 $(SONIC_UTILS)_SRC_PATH = $(SRC_PATH)/sonic-utilities
