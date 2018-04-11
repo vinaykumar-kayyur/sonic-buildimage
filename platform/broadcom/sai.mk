b6c8895b877ee13d1329c4e4bfa7ad9a8d759753
Ying Xie <ying.xie@microsoft.com>
2018-04-11 21:36:44 +0000

    [Broadcom SAI] upgrade Broadcom SAI to version 3.1.3.4-9
    Includes configuration files for following devices:
    
    - Quanta 1X1B-32X
    - Dell Z9264F
    - Inventec D7054Q28B and D7032Q28B

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.1.3.4-7_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.1.3.4-7_amd64.deb?sv=2015-04-05&sr=b&sig=elzOgHCA3G8oKKMfWcbFa%2BvQzAh727mtYJnnVOzVJtY%3D&se=2155-02-07T23%3A37%3A54Z&sp=r"
+BRCM_SAI = libsaibcm_3.1.3.4-9_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm_3.1.3.4-9_amd64.deb?sv=2015-04-05&sr=b&sig=J2TrGxJuOEKXNPOw%2B00%2BL8LLbAmvg9NQtqb73HPsEUY%3D&se=2031-12-19T21%3A33%3A18Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-7_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.1.3.4-9_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.1.3.4-7_amd64.deb?sv=2015-04-05&sr=b&sig=rysxdbCA%2BaqBgDnxztdRA2ixiME3ypqRvzyEds8hLw4%3D&se=2155-02-07T23%3A38%3A39Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/libsaibcm-dev_3.1.3.4-9_amd64.deb?sv=2015-04-05&sr=b&sig=mEIYvBBUKIFwUpGvL6%2FOd6c2Wl0jS6rO9ZizOiPu1fA%3D&se=2031-12-19T21%3A32%3A42Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI) $(BRCM_SAI_DEV)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
