f393c4ce0b8f3033ce5220a601bbee3195c34f9a
wirut <wgetbumr@celestica.com>
2018-06-13 13:48:03 +0800

    add exeption handler for psuutil

--- device/celestica/x86_64-cel_seastone-r0/plugins/psuutil.py
+++ device/celestica/x86_64-cel_seastone-r0/plugins/psuutil.py
@@ -5,6 +5,7 @@
 
 import os
 import logging
+import sys
 
 try:
     from sonic_psu.psu_base import PsuBase
@@ -19,30 +20,33 @@ class PsuUtil(PsuBase):
         PsuBase.__init__(self)
         # DX010 PSU pin mapping
         self.psu = [
-            {'base':216},           # Reserved
+            {'base': self.get_gpio_base()},
             {'abs':27, 'power':22},
             {'abs':28, 'power':25}
         ]
-        self.psu[0]['base'] = self.get_gpio_base()
 
     def get_gpio_base(self):
         sys_gpio_dir = "/sys/class/gpio"
-        for r in os.listdir(sys_gpio_dir):
-            if "gpiochip" in r:
-                return int(r[8:],10)
+        try:
+            for r in os.listdir(sys_gpio_dir):
+                if "gpiochip" in r:
+                    return int(r[8:],10)
+        except Exception as error:
+            logging.error("Unable to get gpio base")
+            sys.exit(0)
 
 
     def init_psu_gpio(self, pinnum):
         # export pin, input as default
         gpio_base = self.psu[0]['base']
         export_file = "/sys/class/gpio/export"
-        direction_file = '/sys/class/gpio/gpio' + str(gpio_base+pinnum) + '/direction' 
 
         try:
             with open(export_file, 'w') as fd:
                 fd.write(str(gpio_base+pinnum))
         except Exception as error:
             logging.error("Unable to export gpio ", str(gpio_base+pinnum))
+            sys.exit(0)
 
 
     # Get a psu status and presence
@@ -53,7 +57,7 @@ class PsuUtil(PsuBase):
 
         gpio_dir = sys_gpio_dir + '/gpio' + str(gpio_base+pinnum)
         gpio_file = gpio_dir + "/value"
-        
+
         # init gpio 
         if (not os.path.isdir(gpio_dir)):
             self.init_psu_gpio(pinnum)
@@ -63,6 +67,7 @@ class PsuUtil(PsuBase):
                 retval = fd.read()
         except Exception as error:
             logging.error("Unable to open ", gpio_file, "file !")
+            sys.exit(0)
 
         retval = retval.rstrip('\r\n')
         return retval
