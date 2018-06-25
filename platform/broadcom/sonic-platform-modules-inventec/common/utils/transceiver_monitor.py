4ddf83d84a84f583843948c01fc581c6ebcb0c71
Blake Huang <huang.blake@inventec.com>
2018-06-25 08:53:43 +0800

    remove license

--- platform/broadcom/sonic-platform-modules-inventec/common/utils/transceiver_monitor.py
+++ platform/broadcom/sonic-platform-modules-inventec/common/utils/transceiver_monitor.py
@@ -1,21 +1,5 @@
 #!/usr/bin/env python
 #
-# Copyright (C) 2018 Inventec, Inc.
-# 
-# Editor: James Huang ( Huang.James@inventec.com )
-#  
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 Usage: %(scriptName)s [options] command object
@@ -62,6 +46,7 @@ transceiver_type_dict = {
                           "FTLF8536P4BCL": "SR",
                           "FCBG125SD1C05": "SR",
                           "FCBG125SD1C30": "SR",
+                          "FCBG125SD1C03": "SR",
                           "FCBG410QB1C03-1E": "SR4",
                           "FCBG4100QB1C030-1E": "SR4",
                           "885350163": "SR4",
