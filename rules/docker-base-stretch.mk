024879eba29eb71932f1ea7899eb54a80d45ca8e
Joe LeVeque <jolevequ@microsoft.com>
2018-07-16 17:42:53 +0000

    Build a Docker base image based on Debian Stretch

---
+++ rules/docker-base-stretch.mk
@@ -0,0 +1,17 @@
+# Docker base image (based on Debian Stretch)
+
+DOCKER_BASE_STRETCH = docker-base-stretch.gz
+$(DOCKER_BASE_STRETCH)_PATH = $(DOCKERS_PATH)/docker-base-stretch
+$(DOCKER_BASE_STRETCH)_DEPENDS += $(SUPERVISOR)
+$(DOCKER_BASE_STRETCH)_DEPENDS += $(SOCAT)
+
+ifeq ($(SONIC_INSTALL_DEBUG_TOOLS),y)
+GDB = gdb
+VIM = vim
+OPENSSH = openssh-client
+SSHPASS = sshpass
+STRACE = strace
+$(DOCKER_BASE_STRETCH)_DBG_PACKAGES += $(GDB) $(VIM) $(OPENSSH) $(SSHPASS) $(STRACE)
+endif
+
+SONIC_DOCKER_IMAGES += $(DOCKER_BASE_STRETCH)
