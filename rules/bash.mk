58cf9aa6e9e312c9ebdb89e4bf4acf27c7506821
Rodny Molina <rmolina@linkedin.com>
2018-07-26 23:30:59 -0700

    Fixing minor issue during code-merge
    Signed-off-by: Rodny Molina <rmolina@linkedin.com>

--- rules/bash.mk
+++ rules/bash.mk
@@ -6,11 +6,11 @@
 # completed.
 
 # Bash major release-number corresponding to Debian-8 (Jessie)
-BASH_VERSION = 4.3
-# Bash complete release-number. This image contains all 4.3 fixes  up to path '42'.
-BASH_VERSION_FULL = $(BASH_VERSION)-14
+BASH_VERSION_MAJOR = 4.3
+# Bash complete release-number. This image contains all 4.3 fixes  up to patch '42'.
+BASH_VERSION_FULL = $(BASH_VERSION_MAJOR)-14
 
-export BASH_VERSION BASH_VERSION_FULL
+export BASH_VERSION_MAJOR BASH_VERSION_FULL
 
 BASH = bash_$(BASH_VERSION_FULL)_amd64.deb
 $(BASH)_SRC_PATH = $(SRC_PATH)/bash
