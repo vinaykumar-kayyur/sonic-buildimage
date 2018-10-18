0ca4d2ae27bda0a7c169e2ad78495668633792b3
Zhenggen Xu <zxu@linkedin.com>
2018-10-18 15:12:49 -0700

    Restore neighbor table to kernel during system warm-reboot
    Added a service: "restore_neighbors" to restore neighbor table into
    kernel during system warm reboot. The service is started by supervisord
    in swss docker when the docker is started.
    
    In case system warm reboot is enabled, it will try to restore the neighbor
    table from appDB into kernel through netlink API calls and update the neighbor
    table by sending arp/ns requests to all neighbor entries, then it sets the
    stateDB flag for neighsyncd to continue the reconciliation process.
    
    -- Added tcpdump python-scapy debian package into orchagent and vs dockers.
    -- Added python module: pyroute2 netifaces into orchagent and vc dockers.
    -- Workarounded tcpdump issue in the vs docker
    
    Signed-off-by: Zhenggen Xu <zxu@linkedin.com>

--- rules/scripts.mk
+++ rules/scripts.mk
@@ -5,7 +5,11 @@ $(ARP_UPDATE_SCRIPT)_PATH = files/scripts
 CONFIGDB_LOAD_SCRIPT = configdb-load.sh
 $(CONFIGDB_LOAD_SCRIPT)_PATH = files/scripts
 
+RESTORE_NEIGHS_SCRIPT = restore_neighbors.py
+$(RESTORE_NEIGHS_SCRIPT)_PATH = files/scripts
+
 SONIC_COPY_FILES += $(CONFIGDB_LOAD_SCRIPT) \
-                    $(ARP_UPDATE_SCRIPT)
+                    $(ARP_UPDATE_SCRIPT) \
+                    $(RESTORE_NEIGHS_SCRIPT) 
 
 
