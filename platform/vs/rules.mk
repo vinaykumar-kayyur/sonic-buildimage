05ae651d1f8de3f59ae6f672d19318254dd89b56
Guohan Lu <gulv@microsoft.com>
2018-11-17 09:35:15 +0000

    build sonic-vs.bin
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- platform/vs/rules.mk
+++ platform/vs/rules.mk
@@ -1,4 +1,7 @@
 include $(PLATFORM_PATH)/syncd-vs.mk
 include $(PLATFORM_PATH)/docker-sonic-vs.mk
+include $(PLATFORM_PATH)/docker-syncd-vs.mk
+include $(PLATFORM_PATH)/docker-orchagent-vs.mk
+include $(PLATFORM_PATH)/one-image.mk
 
-SONIC_ALL += $(DOCKER_SONIC_VS)
+SONIC_ALL += $(SONIC_ONE_IMAGE) $(DOCKER_SONIC_VS)
