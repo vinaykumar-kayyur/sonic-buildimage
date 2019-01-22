d8353ed3312edfda4c15d231db58ae23ac32a3bc
dzhang <d.zhang@alibaba-inc.com>
2019-01-21 18:07:39 -0800

    update redis sock and pid for global access

--- dockers/docker-database/create_single_redis_config.sh
+++ dockers/docker-database/create_single_redis_config.sh
@@ -4,11 +4,10 @@ name=$1
 port=$2
 mkdir -p /etc/$name
 mkdir -p /var/lib/$name
-mkdir -p /var/run/$name
 cp /etc/redis/redis.conf /etc/$name/$name.conf
 r1="s/port 6379/port $port/;"
-r2="s%^unixsocket .*%unixsocket /var/run/$name/$name.sock%;"
+r2="s%^unixsocket .*%unixsocket /var/run/redis/$name.sock%;"
 r3="s%dir .*%dir /var/lib/$name%;"
-r4="s%pidfile .*%pidfile /var/run/$name/$name.pid%;"
+r4="s%pidfile .*%pidfile /var/run/redis/$name.pid%;"
 sed -ri "$r1$r2$r3$r4" /etc/$name/$name.conf
 cp /etc/init.d/redis-server /etc/init.d/$name
