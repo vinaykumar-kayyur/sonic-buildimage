b4e92da88e544effe6175345beca9e04050a225b
richard28530 <richard28530@gmail.com>
2019-02-15 09:54:02 +0800

    [timezone] use -n for non-zero length judgement

--- files/image_config/timezone/timezone-config.sh
+++ files/image_config/timezone/timezone-config.sh
@@ -3,7 +3,7 @@
 CURRENT_TIMEZONE=`cat /etc/timezone`
 TIMEZONE=`sonic-cfggen -d -v DEVICE_METADATA[\'localhost\'][\'timezone\']`
 
-if [ ! $TIMEZONE == "" ]
+if [ -n $TIMEZONE ]
 then
     sudo bash -c "echo $TIMEZONE > /etc/timezone"
     sudo bash -c "dpkg-reconfigure -f noninteractive tzdata"
