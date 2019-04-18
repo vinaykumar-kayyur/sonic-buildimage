b4b30fd88fe27d8bd1a117d9c07e7859c8baf010
Renuka Manavalan <remanava@microsoft.com>
2019-04-18 16:22:40 +0000

    Changes per review comments: 1) Dropped LIBSAIREDIS_DBG from database, teamd, router-advertiser, telemetry, and platform-monitor docker*.mk files from _DBG_DEPENDS list 2) W.r.t docker make for syncd, moved DEPENDS from template to specific makefile and let the template has stuff that is applicable to all.

--- platform/mellanox/docker-syncd-mlnx.mk
+++ platform/mellanox/docker-syncd-mlnx.mk
@@ -2,3 +2,10 @@
 
 DOCKER_SYNCD_PLATFORM_CODE = mlnx
 include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
+
+$(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD) $(PYTHON_SDK_API)
+$(DOCKER_SYNCD_BASE)_PYTHON_DEBS += $(MLNX_SFPD)
+
+
+$(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
+
