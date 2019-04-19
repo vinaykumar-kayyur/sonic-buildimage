0db6c0f00143cba305122795655605b42e6532d2
Renuka Manavalan <remanava@microsoft.com>
2019-04-19 20:38:33 +0000

    Fixed a copy/paste bug

--- platform/barefoot/docker-syncd-bfn.mk
+++ platform/barefoot/docker-syncd-bfn.mk
@@ -4,7 +4,6 @@ DOCKER_SYNCD_PLATFORM_CODE = bfn
 include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD)
-$(DOCKER_SYNCD_BASE)_PYTHON_DEBS += $(MLNX_SFPD)
 
 $(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
                                $(LIBSWSSCOMMON_DBG) \
