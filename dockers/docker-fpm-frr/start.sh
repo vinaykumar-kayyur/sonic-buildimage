7a9b9997008db1dea182a632fb7019f5ced5e6b0
Guohan Lu <gulv@microsoft.com>
2019-05-08 21:18:42 +0000

    support both integrated mode and separated configuration mode

--- dockers/docker-fpm-frr/start.sh
+++ dockers/docker-fpm-frr/start.sh
@@ -4,11 +4,13 @@ mkdir -p /etc/frr
 
 CONFIG_TYPE=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["docker_routing_config_mode"]'`
 
-if [ -z "$CONFIG_TYPE" ] || [ "$CONFIG_TYPE" == "unified" ]; then
-    sonic-cfggen -d -y /etc/sonic/deployment_id_asn_map.yml -t /usr/share/sonic/templates/frr.conf.j2 >/etc/frr/frr.conf
-elif [ "$CONFIG_TYPE" == "separated" ]; then
+if [ -z "$CONFIG_TYPE" ] || [ "$CONFIG_TYPE" == "separated" ]; then
     sonic-cfggen -d -y /etc/sonic/deployment_id_asn_map.yml -t /usr/share/sonic/templates/bgpd.conf.j2 > /etc/frr/bgpd.conf
     sonic-cfggen -d -t /usr/share/sonic/templates/zebra.conf.j2 > /etc/frr/zebra.conf
+    touch /etc/frr/vtysh.conf
+elif [ "$CONFIG_TYPE" == "unified" ]; then
+    sonic-cfggen -d -y /etc/sonic/deployment_id_asn_map.yml -t /usr/share/sonic/templates/frr.conf.j2 >/etc/frr/frr.conf
+    echo "service integrated-vtysh-config" > /etc/frr/vtysh.conf
 fi
 
 sonic-cfggen -d -t /usr/share/sonic/templates/isolate.j2 > /usr/sbin/bgp-isolate
@@ -24,12 +26,16 @@ echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
 
 rm -f /var/run/rsyslogd.pid
 
-supervisorctl start bgpcfgd
-
 supervisorctl start rsyslogd
+
+supervisorctl start bgpcfgd
 
 # Start Quagga processes
 supervisorctl start zebra
 supervisorctl start bgpd
+
+if [ "$CONFIG_TYPE" == "unified" ]; then
+    supervisorctl start vtysh_b
+fi
 
 supervisorctl start fpmsyncd
