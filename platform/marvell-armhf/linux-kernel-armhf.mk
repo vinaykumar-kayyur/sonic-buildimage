9bd11e01d3378a1d0dfabba877a7aa066c209aef
Antony Rheneus <arheneus@marvell.com>
2019-06-07 16:05:01 +0530

    Added Custome Kernel version

--- platform/marvell-armhf/linux-kernel-armhf.mk
+++ platform/marvell-armhf/linux-kernel-armhf.mk
@@ -1,8 +1,8 @@
 # linux kernel package for marvell armhf
 
-KVERSION_armhf = 4.4.8
+KVERSION = 4.4.8
 
 
-LINUX_KERNEL_armhf = linux-image-4.4.8_4.4.8-4_armhf.deb
-$(LINUX_KERNEL_armhf)_PATH = /sonic
-SONIC_COPY_DEBS += $(LINUX_KERNEL_armhf)
+LINUX_KERNEL = linux-image-4.4.8_4.4.8-4_armhf.deb
+$(LINUX_KERNEL)_PATH = /sonic
+SONIC_COPY_DEBS += $(LINUX_KERNEL)
