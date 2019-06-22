3d804a49d7dfe65eefd3c21585264a6aa9329df6
Partha Dutta <partha.dutta@broadcom.com>
2019-06-21 23:55:09 -0700

    Merged changes 70007e6d2ba3a4c0b371cd693ccc63e0a8906e77..00d4fcfed6a759e40d7b92120ea0ee1f08300fc6
    00d4fcfed6a759e40d7b92120ea0ee1f08300fc6 Modified environemnt variables

--- dockers/docker-sonic-mgmt-framework/rest-server.sh
+++ dockers/docker-sonic-mgmt-framework/rest-server.sh
@@ -2,6 +2,11 @@
 
 REST_SERVER_ARGS=" -ui /rest_ui -port=80 -logtostderr"
 
+#export LIBYANG_EXTENSIONS_PLUGINS_DIR 
+export CVL_SCHEMA_PATH=/usr/sbin/schema
+export LIBYANG_EXTENSIONS_PLUGINS_DIR=/usr/sbin/lib
+export LIBYANG_USER_TYPES_PLUGINS_DIR=/usr/sbin/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PATH:/usr/sbin/lib
+
 exec /usr/sbin/rest_server ${REST_SERVER_ARGS}
-
-
+#while [ true ]; do sleep 1; done
