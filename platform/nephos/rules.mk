f6d1f01e6671f1ac186dae366a8bb4b7b1710857
Simon Ji <simon.ji@nephosinc.com>
2019-06-24 00:45:22 -0700

    1. upload sdk source code to support local compile and linux kernel 4.9.0-9 2. add warm_verifier tool to support warm reboot for nephos platform

--- platform/nephos/rules.mk
+++ platform/nephos/rules.mk
@@ -1,5 +1,5 @@
-include $(PLATFORM_PATH)/sdk.mk
 include $(PLATFORM_PATH)/sai.mk
+include $(PLATFORM_PATH)/nephos-modules.mk
 include $(PLATFORM_PATH)/platform-modules-ingrasys.mk
 include $(PLATFORM_PATH)/platform-modules-accton.mk
 include $(PLATFORM_PATH)/platform-modules-cig.mk
@@ -13,6 +13,10 @@ include $(PLATFORM_PATH)/docker-ptf-nephos.mk
 
 NPX_DIAG = npx_diag
 $(NPX_DIAG)_URL = "https://github.com/NephosInc/SONiC/raw/master/sdk/npx_diag"
+
+WARM_VERIFIER = warm-verifier
+$(WARM_VERIFIER)_URL = "https://github.com/NephosInc/SONiC/raw/master/sai/warm-verifier"
+
 
 DSSERVE = dsserve
 $(DSSERVE)_URL = "https://sonicstorage.blob.core.windows.net/packages/20170518/dsserve?sv=2015-04-05&sr=b&sig=gyNbgSL%2FvpMXDdpboVkIJcTKMRdGgEaOR9OukHhEsu8%3D&se=2030-03-31T23%3A06%3A35Z&sp=r"
