ae830cb9f77b9655b6a8976777e16858941646a0
Wei Bai <baiwei0427@users.noreply.github.com>
2019-07-01 16:42:59 +0800

    Add test for port channel and specified VNI cases

--- src/sonic-config-engine/tests/test_j2files.py
+++ src/sonic-config-engine/tests/test_j2files.py
@@ -18,6 +18,8 @@ class TestJ2Files(TestCase):
         self.mlnx_port_config = os.path.join(self.test_dir, 'sample-port-config-mlnx.ini')
         self.dell6100_t0_minigraph = os.path.join(self.test_dir, 'sample-dell-6100-t0-minigraph.xml')
         self.t2_chassis_fe_minigraph = os.path.join(self.test_dir, 't2-chassis-fe-graph.xml')
+        self.t2_chassis_fe_vni_minigraph = os.path.join(self.test_dir, 't2-chassis-fe-graph-vni.xml')
+        self.t2_chassis_fe_pc_minigraph = os.path.join(self.test_dir, 't2-chassis-fe-graph-pc.xml')
         self.t2_chassis_fe_port_config = os.path.join(self.test_dir, 't2-chassis-fe-port-config.ini')
         self.output_file = os.path.join(self.test_dir, 'output')
 
@@ -86,6 +88,20 @@ class TestJ2Files(TestCase):
         argument = '-m ' + self.t2_chassis_fe_minigraph + ' -p ' + self.t2_chassis_fe_port_config + ' -t ' + conf_template + ' > ' + self.output_file
         self.run_script(argument)
         self.assertTrue(filecmp.cmp(os.path.join(self.test_dir, 'sample_output', 't2-chassis-fe-zebra.conf'), self.output_file))
+
+    # Test zebra.conf in FRR docker for a T2 chassis frontend (fe) switch with port channel interfaces
+    def test_t2_chassis_fe_pc_zebra_frr(self):
+        conf_template = os.path.join(self.test_dir, '..', '..', '..', 'dockers', 'docker-fpm-frr', 'zebra.conf.j2')
+        argument = '-m ' + self.t2_chassis_fe_pc_minigraph + ' -p ' + self.t2_chassis_fe_port_config + ' -t ' + conf_template + ' > ' + self.output_file
+        self.run_script(argument)
+        self.assertTrue(filecmp.cmp(os.path.join(self.test_dir, 'sample_output', 't2-chassis-fe-pc-zebra.conf'), self.output_file))        
+
+    # Test zebra.conf in FRR docker for a T2 chassis frontend (fe) switch with specified VNI
+    def test_t2_chassis_fe_pc_zebra_frr(self):
+        conf_template = os.path.join(self.test_dir, '..', '..', '..', 'dockers', 'docker-fpm-frr', 'zebra.conf.j2')
+        argument = '-m ' + self.t2_chassis_fe_vni_minigraph + ' -p ' + self.t2_chassis_fe_port_config + ' -t ' + conf_template + ' > ' + self.output_file
+        self.run_script(argument)
+        self.assertTrue(filecmp.cmp(os.path.join(self.test_dir, 'sample_output', 't2-chassis-fe-vni-zebra.conf'), self.output_file))        
 
     # Test bgpd.conf in FRR docker for a T2 chassis frontend (fe)
     def test_t2_chassis_frontend_bgpd_frr(self):
