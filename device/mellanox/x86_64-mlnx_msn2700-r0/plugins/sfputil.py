213595f15ec74cd7c846c54018b4b528ac2e3444
Stephen Sun <stephens@mellanox.com>
2019-07-05 14:10:52 +0300

    fix comments

--- device/mellanox/x86_64-mlnx_msn2700-r0/plugins/sfputil.py
+++ device/mellanox/x86_64-mlnx_msn2700-r0/plugins/sfputil.py
@@ -10,8 +10,10 @@ try:
 except ImportError as e:
     raise ImportError("%s - required module not found" % str(e))
 
-#sfp supports dom
+# sfp supports dom
 XCVR_DOM_CAPABILITY_DOM_SUPPORT_BIT = 0x40
+# I2C page size for sfp
+SFP_I2C_PAGE_SIZE = 256
 
 # parameters for DB connection 
 REDIS_HOSTNAME = "localhost"
@@ -517,9 +519,9 @@ class SfpUtil(SfpUtilBase):
             transceiver_dom_info_dict['tx4bias'] = dom_channel_monitor_data['data']['TX4Bias']['value']
 
         else:
-            offset = 256
+            offset = SFP_I2C_PAGE_SIZE
 
-            eeprom_raw = ['0'] * 256
+            eeprom_raw = ['0'] * SFP_I2C_PAGE_SIZE
             eeprom_raw[XCVR_DOM_CAPABILITY_OFFSET : XCVR_DOM_CAPABILITY_OFFSET + XCVR_DOM_CAPABILITY_WIDTH] = \
                 self._read_eeprom_specific_bytes_via_ethtool(port_num, XCVR_DOM_CAPABILITY_OFFSET, XCVR_DOM_CAPABILITY_WIDTH)
             sfp_obj = sff8472InterfaceId()
@@ -529,7 +531,7 @@ class SfpUtil(SfpUtilBase):
             if not dom_supported:
                 return transceiver_dom_info_dict
 
-            eeprom_domraw = self._read_eeprom_specific_bytes_via_ethtool(port_num, offset, 256)
+            eeprom_domraw = self._read_eeprom_specific_bytes_via_ethtool(port_num, offset, SFP_I2C_PAGE_SIZE)
             if eeprom_domraw is None:
                 return transceiver_dom_info_dict
 
