823dd0aca85068466a22700fc5bfc511baf8a733
leo lin <leo.yt.lin@ufispace.com>
2019-07-08 16:52:08 +0800

    [barefoot] change bf platform deb file name

--- platform/barefoot/bfn-platform-ingrasys.mk
+++ platform/barefoot/bfn-platform-ingrasys.mk
@@ -1,6 +1,6 @@
 ifdef BLDENV
 BFN_INGRASYS_PLATFORM = bfnplatform-ingrasys_8.8.1_docker_amd64.deb
-$(BFN_INGRASYS_PLATFORM)_URL = "https://github.com/Ingrasys-sonic/packages/raw/master/lib/bfnplatform-ingrasys_8.8.1_amd64.deb"
+$(BFN_INGRASYS_PLATFORM)_URL = "https://github.com/Ingrasys-sonic/packages/raw/master/lib/bfnplatform-ingrasys_master_amd64.deb"
 endif
 
 SONIC_ONLINE_DEBS += $(BFN_INGRASYS_PLATFORM) # $(BFN_SAI_DEV)
