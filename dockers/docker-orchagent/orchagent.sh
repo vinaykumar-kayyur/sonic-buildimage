vinaykumar-kayyur/sonic-buildimage

commit 50fe458592c21e1c05b6e9d433cb555fc1a9b2eb

[build]: SONiC buildimage ARM arch support (#2980)
ARM Architecture support in SONIC

make configure platform=[ASIC_VENDOR_ARCH] PLATFORM_ARCH=[ARM_ARCH]
SONIC_ARCH: default amd64
armhf - arm32bit
arm64 - arm64bit

Signed-off-by: Antony Rheneus <arheneus@marvell.com>

dockers/docker-orchagent/orchagent.sh
@@ -29,6 +29,9 @@ elif [ "$platform" == "barefoot" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 elif [ "$platform" == "vs" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
+else
+    MAC_ADDRESS=`sonic-cfggen -d -v 'DEVICE_METADATA.localhost.mac'`
+    ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 fi
 
 exec /usr/bin/orchagent ${ORCHAGENT_ARGS}

commit 56f976cacc81c981301388d5b56c7d35045cc8ff

[mellanox]: fix orchagent start args for mellanox
Bug is introduced in #2980. Mellanox does not support setting
MAC on switch create.

dockers/docker-orchagent/orchagent.sh
@@ -29,6 +29,8 @@ elif [ "$platform" == "barefoot" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
 elif [ "$platform" == "vs" ]; then
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"
+elif [ "$platform" == "mellanox" ]; then
+    ORCHAGENT_ARGS+=""
 else
     MAC_ADDRESS=`sonic-cfggen -d -v 'DEVICE_METADATA.localhost.mac'`
     ORCHAGENT_ARGS+="-m $MAC_ADDRESS"