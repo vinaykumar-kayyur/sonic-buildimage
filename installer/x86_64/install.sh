e974bf93ee408f12156d71094c68bdaafe3166d9
Guohan Lu <gulv@microsoft.com>
2019-08-04 07:21:04 +0000

    [build]: enable docker in ram option for small disk device
    when device disk is small, do not unzip dockerfs.tar.gz on disk.
    keep the tar file on the disk, unzip to tmpfs in the initrd phase.
    
    Signed-off-by: Guohan Lu <gulv@microsoft.com>

--- installer/x86_64/install.sh
+++ installer/x86_64/install.sh
@@ -481,13 +481,18 @@ fi
 # Decompress the file for the file system directly to the partition
 unzip -o $ONIE_INSTALLER_PAYLOAD -x "$FILESYSTEM_DOCKERFS" -d $demo_mnt/$image_dir
 
-if [ "$install_env" = "onie" ]; then
-    TAR_EXTRA_OPTION="--numeric-owner"
+# when disk is small, keep dockerfs.tar.gz in disk, expand it into ramfs during initrd
+if [ x"$DOCKER_IN_RAM" = x"y" ]; then
+    unzip -o $ONIE_INSTALLER_PAYLOAD "$FILESYSTEM_DOCKERFS" -d $demo_mnt/$image_dir
 else
-    TAR_EXTRA_OPTION="--numeric-owner --warning=no-timestamp"
+    if [ "$install_env" = "onie" ]; then
+        TAR_EXTRA_OPTION="--numeric-owner"
+    else
+        TAR_EXTRA_OPTION="--numeric-owner --warning=no-timestamp"
+    fi
+    mkdir -p $demo_mnt/$image_dir/$DOCKERFS_DIR
+    unzip -op $ONIE_INSTALLER_PAYLOAD "$FILESYSTEM_DOCKERFS" | tar xz $TAR_EXTRA_OPTION -f - -C $demo_mnt/$image_dir/$DOCKERFS_DIR
 fi
-mkdir -p $demo_mnt/$image_dir/$DOCKERFS_DIR
-unzip -op $ONIE_INSTALLER_PAYLOAD "$FILESYSTEM_DOCKERFS" | tar xz $TAR_EXTRA_OPTION -f - -C $demo_mnt/$image_dir/$DOCKERFS_DIR
 
 if [ "$install_env" = "onie" ]; then
     # Store machine description in target file system
