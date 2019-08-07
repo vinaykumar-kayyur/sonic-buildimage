1621bdb7e360f656f2a6b2bca3980d9a8904920d
dzhang <d.zhang@alibaba-inc.com>
2019-08-07 10:10:40 -0700

    three config file, default one in imaage, running config and user defined config

--- dockers/docker-database/create_single_redis_config.sh
+++ dockers/docker-database/create_single_redis_config.sh
@@ -2,11 +2,12 @@
 
 name=$1
 port=$2
+sock=$3
 mkdir -p /etc/$name
 mkdir -p /var/lib/$name
 cp /etc/redis/redis.conf /etc/$name/$name.conf
 r1="s/port 6379/port $port/;"
-r2="s%^unixsocket .*%unixsocket /var/run/redis/$name.sock%;"
+r2="s%^unixsocket .*%unixsocket $sock%;"
 r3="s%dir .*%dir /var/lib/$name%;"
 r4="s%pidfile .*%pidfile /var/run/redis/$name.pid%;"
 sed -ri "$r1$r2$r3$r4" /etc/$name/$name.conf
--- dockers/docker-database/start.sh
+++ dockers/docker-database/start.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+mkdir -p /var/run/redis/
+if [ -f /etc/sonic/database_config.json ]; then
+    cp /etc/sonic/database_config.json /var/run/redis/
+else
+    cp /usr/share/sonic/config/database_config.json /var/run/redis/
+fi
+
 # first, create all redis configuration files
 create_all_redis_config
 
 # then, generate all redis server supervisord configuration file
-sonic-cfggen -j /etc/sonic/database_config.json -t /usr/share/sonic/templates/supervisord_database.conf.j2 > /etc/supervisor/conf.d/supervisord_database.conf
+sonic-cfggen -j /var/run/redis/database_config.json -t /usr/share/sonic/templates/supervisord_database.conf.j2 > /etc/supervisor/conf.d/supervisord_database.conf
 
 # finally, reread supervisord config files and start new added redis servers only
 supervisorctl reread
