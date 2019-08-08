5f7cbecbe418f1420dc5769ce63958f082ae80ef
Stephen Sun <stephens@mellanox.com>
2019-08-08 14:02:59 +0300

    [sonic_platform.chassis]improve reboot-cause support

--- platform/mellanox/mlnx-platform-api/sonic_platform/chassis.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/chassis.py
@@ -36,9 +36,12 @@ HWMGMT_SYSTEM_ROOT = '/var/run/hw-management/system/'
 REBOOT_CAUSE_ROOT = HWMGMT_SYSTEM_ROOT
 
 REBOOT_CAUSE_POWER_LOSS_FILE = 'reset_main_pwr_fail'
+REBOOT_CAUSE_AUX_POWER_LOSS_FILE = 'reset_aux_pwr_or_ref'
 REBOOT_CAUSE_THERMAL_OVERLOAD_ASIC_FILE = 'reset_asic_thermal'
 REBOOT_CAUSE_WATCHDOG_FILE = 'reset_hotswap_or_wd'
 REBOOT_CAUSE_MLNX_FIRMWARE_RESET = 'reset_fw_reset'
+REBOOT_CAUSE_LONG_PB = 'reset_long_pb'
+REBOOT_CAUSE_SHORT_PB = 'reset_short_pb'
 
 REBOOT_CAUSE_FILE_LENGTH = 1
 
@@ -261,6 +264,8 @@ class Chassis(ChassisBase):
         minor_cause = ''
         if self._verify_reboot_cause(REBOOT_CAUSE_POWER_LOSS_FILE):
             major_cause = self.REBOOT_CAUSE_POWER_LOSS
+        elif self._verify_reboot_cause(REBOOT_CAUSE_AUX_POWER_LOSS_FILE):
+            major_cause = self.REBOOT_CAUSE_POWER_LOSS
         elif self._verify_reboot_cause(REBOOT_CAUSE_THERMAL_OVERLOAD_ASIC_FILE):
             major_cause = self.REBOOT_CAUSE_THERMAL_OVERLOAD_ASIC
         elif self._verify_reboot_cause(REBOOT_CAUSE_WATCHDOG_FILE):
@@ -269,6 +274,10 @@ class Chassis(ChassisBase):
             major_cause = self.REBOOT_CAUSE_HARDWARE_OTHER
             if self._verify_reboot_cause(REBOOT_CAUSE_MLNX_FIRMWARE_RESET):
                 minor_cause = "Reset by ASIC firmware"
+            elif self._verify_reboot_cause(REBOOT_CAUSE_LONG_PB):
+                minor_cause = "Reset by long press on power button"
+            elif self._verify_reboot_cause(REBOOT_CAUSE_SHORT_PB):
+                minor_cause = "Reset by short press on power button"
             else:
                 major_cause = self.REBOOT_CAUSE_NON_HARDWARE
 
