0013e63b44adb3f0540da276f810a1af063ffefd
sun-siyuan <53020722+sun-siyuan@users.noreply.github.com>
2019-08-09 19:30:47 -0700

    Update slave.mk
    fix SONIC_ENABLE_KDUMP

--- slave.mk
+++ slave.mk
@@ -649,7 +649,7 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_INSTALLERS)) : $(TARGET_PATH)/% : \
 	export platform_common_py2_wheel_path="$(addprefix $(PYTHON_WHEELS_PATH)/,$(SONIC_PLATFORM_COMMON_PY2))"
 	export redis_dump_load_py2_wheel_path="$(addprefix $(PYTHON_WHEELS_PATH)/,$(REDIS_DUMP_LOAD_PY2))"
 	export install_debug_image="$(INSTALL_DEBUG_TOOLS)"
-	export sonic_kdump_enable="$(SONIC_KDUMP_ENABLE)"
+	export sonic_kdump_enable="$(SONIC_ENABLE_KDUMP)"
 
 	$(foreach docker, $($*_DOCKERS),\
 		export docker_image="$(docker)"
