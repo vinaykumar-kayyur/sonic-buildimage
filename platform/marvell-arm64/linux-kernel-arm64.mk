0742a86bf48111c5b364896c07b723f6a00b9ff0
Antony Rheneus <arheneus@marvell.com>
2019-09-09 15:15:35 +0530

    [Makefile] Marvell arm64 kernel update

--- platform/marvell-arm64/linux-kernel-arm64.mk
+++ platform/marvell-arm64/linux-kernel-arm64.mk
@@ -1,9 +1,9 @@
 # linux kernel package for marvell arm64
 
-KVERSION = 4.4.120
+KVERSION = 4.9.168
 
 
-LINUX_KERNEL = linux-image-4.4.120-arm64.deb
+LINUX_KERNEL = linux-image-4.9.168-arm64.deb
 export LINUX_KERNEL
 
 $(LINUX_KERNEL)_SRC_PATH = $(PLATFORM_PATH)/linux
