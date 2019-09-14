b94dbf9c596d0b891ef9a922f738ebc19fff4394
padmanarayana <PADMANABHAN_N@DELL.COM>
2019-09-13 22:10:23 -0400

    Update host-sflow version and remove pid=host

--- rules/docker-sflow.mk
+++ rules/docker-sflow.mk
@@ -29,7 +29,6 @@ $(DOCKER_SFLOW)_CONTAINER_NAME = sflow
 $(DOCKER_SFLOW)_RUN_OPT += --net=host --privileged -t
 $(DOCKER_SFLOW)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_SFLOW)_RUN_OPT += -v /host/warmboot:/var/warmboot
-$(DOCKER_SFLOW)_RUN_OPT += --pid=host
 
 $(DOCKER_SFLOW)_BASE_IMAGE_FILES += psample:/usr/bin/psample
 $(DOCKER_SFLOW)_BASE_IMAGE_FILES += sflowtool:/usr/bin/sflowtool
