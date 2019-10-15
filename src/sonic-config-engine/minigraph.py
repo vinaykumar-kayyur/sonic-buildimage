949baeb78773f8facf53d240adae26a2a239b886
Pradnya Mohite <pramoh@microsoft.com>
2019-10-15 13:30:13 -0700

    resolving comment

--- src/sonic-config-engine/minigraph.py
+++ src/sonic-config-engine/minigraph.py
@@ -729,9 +729,11 @@ def parse_xml(filename, platform=None, port_config_file=None):
     results['NTP_SERVER'] = dict((item, {}) for item in ntp_servers)
     results['TACPLUS_SERVER'] = dict((item, {'priority': '1', 'tcp_port': '49'}) for item in tacacs_servers)
     results['ACL_TABLE'] = acls
-    results['FEATURES'] = {'telemetry': {
-        'status': 'on'
-        }}
+    results['FEATURES'] = {
+        'telemetry': {
+            'status': 'on'
+        }
+    }
 
     # Do not configure the minigraph's mirror session, which is currently unused
     # mirror_sessions = {}
