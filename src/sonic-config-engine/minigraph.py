92e78d8d75761c93aa8b1280327bdd2e53db57fa
Pradnya Mohite <pramoh@microsoft.com>
2019-10-15 12:17:37 -0700

    enable telemetry by default

--- src/sonic-config-engine/minigraph.py
+++ src/sonic-config-engine/minigraph.py
@@ -728,8 +728,10 @@ def parse_xml(filename, platform=None, port_config_file=None):
     results['DHCP_SERVER'] = dict((item, {}) for item in dhcp_servers)
     results['NTP_SERVER'] = dict((item, {}) for item in ntp_servers)
     results['TACPLUS_SERVER'] = dict((item, {'priority': '1', 'tcp_port': '49'}) for item in tacacs_servers)
-
     results['ACL_TABLE'] = acls
+    results['FEATURES'] = {'telemetry': {
+        'status': 'on'
+        }}
 
     # Do not configure the minigraph's mirror session, which is currently unused
     # mirror_sessions = {}
