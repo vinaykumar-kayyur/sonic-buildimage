e1da98eee10f140866fc3b3c3f3fb00eabd9fe9e
pavel-shirshov <pavelsh@microsoft.com>
2019-11-06 15:55:42 -0800

    Update start.sh

--- dockers/docker-fpm-frr/start.sh
+++ dockers/docker-fpm-frr/start.sh
@@ -34,7 +34,7 @@ rm -f /var/run/rsyslogd.pid
 supervisorctl start rsyslogd
 
 # start eoiu pulling, only if configured so
-if [ $(sonic-cfggen -d -v 'WARM_RESTART.bgp.bgp_eoiu') == 'true' ]; then
+if [[ $(sonic-cfggen -d -v 'WARM_RESTART.bgp.bgp_eoiu') == 'true' ]]; then
 		supervisorctl start bgp_eoiu_marker
 fi
 
