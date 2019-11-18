89f2d08d6bf38af2d713abc2d5b3648c1a80f24d
Danny Allen <daall@microsoft.com>
2019-11-18 11:41:17 -0800

    [bcm SAI] Upgrade Broadcom SAI to version 3.5.3.3m-1
    Signed-off-by: Danny Allen <daall@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,9 +1,9 @@
-BRCM_SAI = libsaibcm_3.5.3.1m-25_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/libsaibcm_3.5.3.1m-25_amd64.deb?sv=2015-04-05&sr=b&sig=kLfhfLeBmENmTXVa%2BZ90k2Fq8ZOQ84kxn5%2FzY3KLV2s%3D&se=2033-06-17T23%3A45%3A50Z&sp=r"
+BRCM_SAI = libsaibcm_3.5.3.3m-1_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/libsaibcm_3.5.3.3m-1_amd64.deb?sv=2015-04-05&sr=b&sig=lQe95VWDf1x%2BQJaB4BpyBLYEmShQgRJIOWseFZgG%2BDM%3D&se=2033-07-26T04%3A11%3A58Z&sp=r"
 
-BRCM_SAI_DEV = libsaibcm-dev_3.5.3.1m-25_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.5.3.3m-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/libsaibcm-dev_3.5.3.1m-25_amd64.deb?sv=2015-04-05&sr=b&sig=a%2BvxIrNcVwnp0CBXS%2BTGiGl%2F%2Fl8mo9ZKPkZyroPY0bI%3D&se=2033-06-17T23%3A45%3A21Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/libsaibcm-dev_3.5.3.3m-1_amd64.deb?sv=2015-04-05&sr=b&sig=RwL2o9S5he4JJ3oF2RDvZeBdzfAb%2BFojD4rsEM3kvhI%3D&se=2033-07-26T04%3A10%3A48Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
