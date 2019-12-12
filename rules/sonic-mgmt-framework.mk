52faab4171fd9212a8327523797cc9bbeeb4b872
Anand Kumar Subramanian <anand.subramanian@broadcom.com>
2019-12-11 16:07:51 -0800

    Provided flag to disable compiling mgmt-framework

--- rules/sonic-mgmt-framework.mk
+++ rules/sonic-mgmt-framework.mk
@@ -1,5 +1,7 @@
 # SONiC mgmt-framework package
 
+ifeq ($(ENABLE_MGMT_FRAMEWORK), y)
+
 SONIC_MGMT_FRAMEWORK = sonic-mgmt-framework_1.0-01_amd64.deb
 $(SONIC_MGMT_FRAMEWORK)_SRC_PATH = $(SRC_PATH)/sonic-mgmt-framework
 $(SONIC_MGMT_FRAMEWORK)_DEPENDS = $(LIBYANG_DEV) $(LIBYANG)
@@ -10,3 +12,5 @@ SONIC_MGMT_FRAMEWORK_DBG = sonic-mgmt-framework-dbg_1.0-01_amd64.deb
 $(SONIC_MGMT_FRAMEWORK_DBG)_DEPENDS += $(SONIC_MGMT_FRAMEWORK)
 $(SONIC_MGMT_FRAMEWORK_DBG)_RDEPENDS += $(SONIC_MGMT_FRAMEWORK)
 $(eval $(call add_derived_package,$(SONIC_MGMT_FRAMEWORK),$(SONIC_MGMT_FRAMEWORK_DBG)))
+
+endif
