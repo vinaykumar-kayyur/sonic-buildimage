cf09064e42015d2b500295cfbb6068efe1aee742
Yong Zhao <yozhao@microsoft.com>
2019-12-20 17:42:38 -0800

    Add a copy mechanism to put the monit config file of dhcp_relay container into base image under /etc/monit/conf.d.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- rules/docker-dhcp-relay.mk
+++ rules/docker-dhcp-relay.mk
@@ -26,3 +26,4 @@ $(DOCKER_DHCP_RELAY)_CONTAINER_NAME = dhcp_relay
 $(DOCKER_DHCP_RELAY)_RUN_OPT += --net=host --privileged -t
 $(DOCKER_DHCP_RELAY)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_DHCP_RELAY)_FILES += $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
+$(DOCKER_DHCP_RELAY)_BASE_IMAGE_FILES += monit_dhcprelay:/etc/monit/conf.d
