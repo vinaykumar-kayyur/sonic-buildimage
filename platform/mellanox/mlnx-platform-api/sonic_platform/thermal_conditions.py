af996c760453e010e3880905775175483319cf49
junchao <junchao@mellanox.com>
2019-12-24 11:22:40 +0800

    1. Add base class for FanConditions and PsuConditions; 2. Add default chassis_info to policy file

--- platform/mellanox/mlnx-platform-api/sonic_platform/thermal_conditions.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/thermal_conditions.py
@@ -2,61 +2,62 @@ from sonic_platform_base.sonic_thermal_control.thermal_condition_base import The
 from sonic_platform_base.sonic_thermal_control.thermal_json_object import thermal_json_object
 
 
-@thermal_json_object('fan.any.absence')
-class AnyFanAbsenceCondition(ThermalPolicyConditionBase):
-    def is_match(self, thermal_info_dict):
+class FanCondition(ThermalPolicyConditionBase):
+    def get_fan_info(self, thermal_info_dict):
         from .thermal_infos import FanInfo
         if FanInfo.INFO_NAME in thermal_info_dict and isinstance(thermal_info_dict[FanInfo.INFO_NAME], FanInfo):
-            fan_info_obj = thermal_info_dict[FanInfo.INFO_NAME]
-            return len(fan_info_obj.get_absence_fans()) > 0
-        return False
+            return thermal_info_dict[FanInfo.INFO_NAME]
+        else:
+            return None
+
+
+@thermal_json_object('fan.any.absence')
+class AnyFanAbsenceCondition(FanCondition):
+    def is_match(self, thermal_info_dict):
+        fan_info_obj = self.get_fan_info(thermal_info_dict)
+        return len(fan_info_obj.get_absence_fans()) > 0 if fan_info_obj else False
 
 
 @thermal_json_object('fan.all.absence')
-class AllFanAbsenceCondition(ThermalPolicyConditionBase):
+class AllFanAbsenceCondition(FanCondition):
     def is_match(self, thermal_info_dict):
-        from .thermal_infos import FanInfo
-        if FanInfo.INFO_NAME in thermal_info_dict and isinstance(thermal_info_dict[FanInfo.INFO_NAME], FanInfo):
-            fan_info_obj = thermal_info_dict[FanInfo.INFO_NAME]
-            return len(fan_info_obj.get_presence_fans()) == 0
-        return False
+        fan_info_obj = self.get_fan_info(thermal_info_dict)
+        return len(fan_info_obj.get_presence_fans()) == 0 if fan_info_obj else False
 
 
 @thermal_json_object('fan.all.presence')
-class AllFanPresenceCondition(ThermalPolicyConditionBase):
+class AllFanPresenceCondition(FanCondition):
     def is_match(self, thermal_info_dict):
-        from .thermal_infos import FanInfo
-        if FanInfo.INFO_NAME in thermal_info_dict and isinstance(thermal_info_dict[FanInfo.INFO_NAME], FanInfo):
-            fan_info_obj = thermal_info_dict[FanInfo.INFO_NAME]
-            return len(fan_info_obj.get_absence_fans()) == 0
-        return False
+        fan_info_obj = self.get_fan_info(thermal_info_dict)
+        return len(fan_info_obj.get_absence_fans()) == 0 if fan_info_obj else False
+
+
+class PsuCondition(ThermalPolicyConditionBase):
+    def get_psu_info(self, thermal_info_dict):
+        from .thermal_infos import PsuInfo
+        if PsuInfo.INFO_NAME in thermal_info_dict and isinstance(thermal_info_dict[PsuInfo.INFO_NAME], PsuInfo):
+            return thermal_info_dict[PsuInfo.INFO_NAME]
+        else:
+            return None
 
 
 @thermal_json_object('psu.any.absence')
-class AnyPsuAbsenceCondition(ThermalPolicyConditionBase):
+class AnyPsuAbsenceCondition(PsuCondition):
     def is_match(self, thermal_info_dict):
-        from .thermal_infos import PsuInfo
-        if PsuInfo.INFO_NAME in thermal_info_dict and isinstance(thermal_info_dict[PsuInfo.INFO_NAME], PsuInfo):
-            psu_info_obj = thermal_info_dict[PsuInfo.INFO_NAME]
-            return len(psu_info_obj.get_absence_psus()) > 0
-        return False
+        psu_info_obj = self.get_psu_info(thermal_info_dict)
+        return len(psu_info_obj.get_absence_psus()) > 0 if psu_info_obj else False
 
 
 @thermal_json_object('psu.all.absence')
-class AllPsuAbsenceCondition(ThermalPolicyConditionBase):
+class AllPsuAbsenceCondition(PsuCondition):
     def is_match(self, thermal_info_dict):
-        from .thermal_infos import PsuInfo
-        if PsuInfo.INFO_NAME in thermal_info_dict and isinstance(thermal_info_dict[PsuInfo.INFO_NAME], PsuInfo):
-            psu_info_obj = thermal_info_dict[PsuInfo.INFO_NAME]
-            return len(psu_info_obj.get_presence_psus()) == 0
-        return False
+        psu_info_obj = self.get_psu_info(thermal_info_dict)
+        return len(psu_info_obj.get_presence_psus()) == 0 if psu_info_obj else False
 
 
 @thermal_json_object('psu.all.presence')
-class AllPsuPresenceCondition(ThermalPolicyConditionBase):
+class AllPsuPresenceCondition(PsuCondition):
     def is_match(self, thermal_info_dict):
-        from .thermal_infos import PsuInfo
-        if PsuInfo.INFO_NAME in thermal_info_dict and isinstance(thermal_info_dict[PsuInfo.INFO_NAME], PsuInfo):
-            psu_info_obj = thermal_info_dict[PsuInfo.INFO_NAME]
-            return len(psu_info_obj.get_absence_psus()) == 0
-        return False
+        psu_info_obj = self.get_psu_info(thermal_info_dict)
+        return len(psu_info_obj.get_absence_psus()) == 0 if psu_info_obj else False
+
