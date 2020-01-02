1082c49308e2021b796b0a5a7012d15c20f081ce
SuvarnaMeenakshi <sumeenak@microsoft.com>
2020-01-02 15:28:52 -0800

    [topology.service]: Fix dependency and minor fix in the script file.

--- files/image_config/topology/topology.sh
+++ files/image_config/topology/topology.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script is invoked by topology.service only
-# for multi-asic virtual platform. This expects
-# multiple Database instances and HWKSU information
-# is retrieved from first database instance.
+# for multi-asic virtual platform. For multi-asic platform 
+# multiple Database instances are present 
+# and HWKSU information is retrieved from first database instance.
 #
 
 start() {
@@ -10,14 +10,14 @@ start() {
 	TOPOLOGY_SCRIPT="topology.sh"
 	PLATFORM=`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform -s $DB_FIRST_INSTANCE`
 	HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]' -s $DB_FIRST_INSTANCE`
-        /usr/share/sonic/device/$VS_PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT start
+        /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT start
 }
 stop() {
         DB_FIRST_INSTANCE="/var/run/redis0/redis.sock"
         TOPOLOGY_SCRIPT="topology.sh"
         PLATFORM=`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform -s $DB_FIRST_INSTANCE`
         HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]' -s $DB_FIRST_INSTANCE`
-        /usr/share/sonic/device/$VS_PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT stop 
+        /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT stop 
 }
 
 case "$1" in
