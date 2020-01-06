cfa0c681450b495ba2c64918751655d005541cd1
Ying Xie <ying.xie@microsoft.com>
2020-01-06 22:00:29 +0000

    hard code arch

--- rules/monit.mk
+++ rules/monit.mk
@@ -1,16 +1,14 @@
 # monit package
 
 MONIT_VERSION = 5.20.0-6
-CONFIGURED_ARCH = amd64
 
 export MONIT_VERSION
-export CONFIGURED_ARCH
 
-MONIT = monit_$(MONIT_VERSION)_$(CONFIGURED_ARCH).deb
+MONIT = monit_$(MONIT_VERSION)_amd64.deb
 $(MONIT)_SRC_PATH = $(SRC_PATH)/monit
 SONIC_MAKE_DEBS += $(MONIT)
 
 SONIC_STRETCH_DEBS += $(MONIT)
 
-MONIT_DBG = monit-dbgsym_$(MONIT_VERSION)_$(CONFIGURED_ARCH).deb
+MONIT_DBG = monit-dbgsym_$(MONIT_VERSION)_amd64.deb
 $(eval $(call add_derived_package,$(MONIT),$(MONIT_DBG)))
