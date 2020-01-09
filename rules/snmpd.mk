b83fe6bd29713542fcd955051c9cbb3471b0d0ee
Tamer Ahmed <tamer.ahmed@microsoft.com>
2020-01-08 16:36:38 -0800

    [snmp] Upgrading to snmp v5.8
    Upgraded from snmp v5.7.3 to snmp v5.8. Status of snmp v5.7.3 patches:
    
    0001-SNMP-Stop-spamming-logs-with-statfs-permission-denie.patch - recreated
    0002-at.c-properly-check-return-status-from-realloc.-Than.patch - already integrated and the file moved to agent/mibgroup/mibII/data_access/at_linux.c
    0003-CHANGES-BUG-2743-snmpd-crashes-when-receiving-a-GetN.patch - already integrated
    0004-Disable-SNMPv1.patch                                       - recreated
    0005-Port-OpenSSL-1.1.0-with-support-for-1.0.2.patch            - it is integrated, enabled via autoconfigure after checking the OS capabilities
    0006-From-Jiri-Cervenka-snmpd-Fixed-agentx-crashing-and-or-freezing-on-timeout.patch
                                                                    - already integrated
    0007-Linux-VRF-5.7.3-Support.patch                              - non trivial patch, will for help to port it
    0008-Enable-macro-DEB_BUILD_ARCH_OS-in-order-to-build-ipv.patch - recreated
    
    Also, Added new patch in order to get the new lib compiling for a case when
    SNMPv1 is disabled while SNMPv2 is enabled.
    
    signed-off-by: Tamer Ahmed <tamer.ahmed@microsoft.com>

--- rules/snmpd.mk
+++ rules/snmpd.mk
@@ -1,7 +1,7 @@
 # snmpd package
 
-SNMPD_VERSION = 5.7.3+dfsg
-SNMPD_VERSION_FULL = $(SNMPD_VERSION)-1.5
+SNMPD_VERSION = 5.8+dfsg
+SNMPD_VERSION_FULL = $(SNMPD_VERSION)-2
 
 export SNMPD_VERSION SNMPD_VERSION_FULL
 
@@ -32,11 +32,11 @@ $(eval $(call add_derived_package,$(SNMP),$(SNMP_DBG)))
 SNMPD_DBG = snmpd-dbgsym_$(SNMPD_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(SNMPD),$(SNMPD_DBG)))
 
-LIBSNMP = libsnmp30_$(SNMPD_VERSION_FULL)_$(CONFIGURED_ARCH).deb
+LIBSNMP = libsnmp35_$(SNMPD_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(LIBSNMP)_RDEPENDS += $(LIBSNMP_BASE)
 $(eval $(call add_derived_package,$(LIBSNMP_BASE),$(LIBSNMP)))
 
-LIBSNMP_DBG = libsnmp30-dbg_$(SNMPD_VERSION_FULL)_$(CONFIGURED_ARCH).deb
+LIBSNMP_DBG = libsnmp35-dbg_$(SNMPD_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(LIBSNMP_DBG)_DEPENDS += $(LIBSNMP)
 $(LIBSNMP_DBG)_RDEPENDS += $(LIBSNMP)
 $(eval $(call add_derived_package,$(LIBSNMP_BASE),$(LIBSNMP_DBG)))
@@ -49,11 +49,6 @@ LIBSNMP_PERL = libsnmp-perl_$(SNMPD_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(LIBSNMP_PERL)_DEPENDS += $(LIBSNMP)
 $(LIBSNMP_PERL)_RDEPENDS += $(LIBSNMP)
 $(eval $(call add_derived_package,$(LIBSNMP_BASE),$(LIBSNMP_PERL)))
-
-PYTHON_NETSNMP = python-netsnmp_$(SNMPD_VERSION_FULL)_$(CONFIGURED_ARCH).deb
-$(PYTHON_NETSNMP)_DEPENDS += $(LIBSNMP)
-$(PYTHON_NETSNMP)_RDEPENDS += $(LIBSNMP)
-$(eval $(call add_derived_package,$(LIBSNMP_BASE),$(PYTHON_NETSNMP)))
 
 TKMIB = tkmib_$(SNMPD_VERSION_FULL)_all.deb
 $(TKMIB)_DEPENDS += $(LIBSNMP_PERL)
