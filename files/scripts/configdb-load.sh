c370285fdbc1374fa20bdbd0d19e83cfb43b831f
Yong Zhao <yozhao@microsoft.com>
2020-02-13 14:43:31 -0800

    [sonic-cfggen] Change the configuration file loading orders and first loading the init_cfg.json and then loading config_db.json.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- files/scripts/configdb-load.sh
+++ files/scripts/configdb-load.sh
@@ -7,7 +7,7 @@ done
 
 # If there is a config db dump file, load it
 if [ -r /etc/sonic/config_db.json ]; then
-    sonic-cfggen -j /etc/sonic/config_db.json --write-to-db
+    sonic-cfggen -j /etc/sonic/init_cfg.json -j /etc/sonic/config_db.json --write-to-db
 fi
 
 sonic-db-cli CONFIG_DB SET "CONFIG_DB_INITIALIZED" "1"
