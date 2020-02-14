5e9e642f77f13106bf7ff8354f49fd9cf0019899
Jostar Yang <jostar_yang@accton.com.tw>
2020-02-14 11:32:29 +0800

    Add checking sysfs use 0x57 or 0x56

--- device/accton/x86_64-accton_as9716_32d-r0/plugins/eeprom.py
+++ device/accton/x86_64-accton_as9716_32d-r0/plugins/eeprom.py
@@ -24,5 +24,11 @@ def eeprom_check():
 class board(eeprom_tlvinfo.TlvInfoDecoder):
     _TLV_INFO_MAX_LEN = 256
     def __init__(self, name, path, cpld_root, ro):
-        self.eeprom_path = "/sys/bus/i2c/devices/0-0056/eeprom"
+        ret=eeprom_check()
+        if ret==1:
+            self.eeprom_path = "/sys/bus/i2c/devices/0-0057/eeprom"
+        else:
+            self.eeprom_path = "/sys/bus/i2c/devices/0-0056/eeprom"
+
         super(board, self).__init__(self.eeprom_path, 0, '', True)
+
