55410e5fb3611889662258d954c40a8604250ed3
Prince <prince.sunny@microsoft.com>
2020-02-19 00:42:59 +0000

    Addressed comments, added log message

--- platform/vs/docker-sonic-vs/orchagent.sh
+++ platform/vs/docker-sonic-vs/orchagent.sh
@@ -6,9 +6,10 @@ else
     export platform=$fake_platform
 fi
 
-MAC_ADDRESS=`sonic-cfggen -d -v 'DEVICE_METADATA.localhost.mac'`
+MAC_ADDRESS=$(sonic-cfggen -d -v 'DEVICE_METADATA.localhost.mac')
 if [ "$MAC_ADDRESS" == "None" ] || [ -z "$MAC_ADDRESS" ]; then
-    MAC_ADDRESS=`ip link show eth0 | grep ether | awk '{print $2}'`
+    MAC_ADDRESS=$(ip link show eth0 | grep ether | awk '{print $2}')
+    logger "Mac address not found in Device Metadata, Falling back to eth0"
 fi
 
 # Create a folder for SwSS record files
