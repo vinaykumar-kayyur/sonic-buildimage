4828153e3eec1de82b8deb2b7a45fb0f3a7c2083
Yong Zhao <yozhao@microsoft.com>
2020-03-02 11:36:09 -0800

    [sonic_cfggen] Add a logic to determine whether the files config_db.json and init_cfg.json are existing. Otherwise sonic-cfggen will only load one of them if it does exist.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- files/scripts/configdb-load.sh
+++ files/scripts/configdb-load.sh
@@ -5,9 +5,14 @@ until [[ $(redis-cli ping | grep -c PONG) -gt 0 ]]; do
   sleep 1;
 done
 
-# If there is a config db dump file, load it
-if [ -r /etc/sonic/config_db.json ]; then
+# If there are init_cfg.json and config_db.json dump files, load them. Otherwise,
+# loading one of them if it exists.
+if [[ -r /etc/sonic/init_cfg.json ]] && [[ -r /etc/sonic/config_db.json ]]; then
     sonic-cfggen -j /etc/sonic/init_cfg.json -j /etc/sonic/config_db.json --write-to-db
+elif [ -r /etc/sonic/init_cfg.json ]; then
+    sonic-cfggen -j /etc/sonic/init_cfg.json --write-to-db
+elif [ -r /etc/sonic/config_db.json ]; then
+    sonic-cfggen -j /etc/sonic/config_db.json --write-to-db
 fi
 
 sonic-db-cli CONFIG_DB SET "CONFIG_DB_INITIALIZED" "1"
