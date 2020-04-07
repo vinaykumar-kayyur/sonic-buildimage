8cd7fb93b9e3d65ec2ba131bb21db93c8a5b1b5b
Sumukha Tumkur Vani <stumkurv@microsoft.com>
2020-04-07 05:33:18 +0000

    Fixes to restapi code

--- dockers/docker-sonic-restapi/start.sh
+++ dockers/docker-sonic-restapi/start.sh
@@ -7,4 +7,14 @@ rm -f /var/run/rsyslogd.pid
 
 supervisorctl start rsyslogd
 
-supervisorctl start restapi
+SERVER_CERT=/etc/sonic/certificates/restapiserver.crt
+SERVER_KEY=/etc/sonic/certificates/restapiserver.key
+CLIENT_CERT=/etc/sonic/certificates/restapiclient.crt
+
+while [ : ]
+do
+    if [[ -f $SERVER_CERT && -f $SERVER_KEY && -f $CLIENT_CERT ]]; then
+        supervisorctl start restapi
+    fi
+    sleep 5s
+done
