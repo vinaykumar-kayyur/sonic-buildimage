9d4d57a0bb4bed752179a189bdab3baff2cf728a
Renuka Manavalan <remanava@microsoft.com>
2020-04-10 00:06:34 +0000

    Update per review comments 1) Added new args with options 2) Handle PORT possible being empty 3) Exhibit new behavior only if both version & platform are given.

--- push_docker.sh
+++ push_docker.sh
@@ -1,30 +1,33 @@
+#! /bin/bash
+
+sonic_version=""
+sonic_platform=""
+
+while getopts ":v:p:" opt
+do
+    case ${opt} in
+        v ) # SONiC image version
+            sonic_version=${OPTARG}
+            ;;
+        p ) # Platform info
+            sonic_platform=${OPTARG}
+            ;;
+        \? ) echo "\
+Usage: [-v <version> ] [ -p <platform> ] <DOCKER_IMAGE_FILE> <REGISTRY_SERVER> <REGISTRY_PORT> \
+<REGISTRY_USERNAME> <REGISTRY_PASSWD> [<DOCKER_IMAGE_TAG>]" 
+            ;;
+    esac
+done
+
+shift $((OPTIND -1))
+
 DOCKER_IMAGE_FILE=$1
 REGISTRY_SERVER=$2
 REGISTRY_PORT=$3
 REGISTRY_USERNAME=$4
 REGISTRY_PASSWD=$5
-DOCKER_IMAGE_TAG=""
-PLATFORM=""
-SONIC_VERSION=""
-
-shift 5
-
-if [ "$#" != "0" ]
-then
-    DOCKER_IMAGE_TAG=$1
-    shift
-fi
-
-if [ "$#" != "0" ]
-then
-    PLATFORM=$1
-    shift
-fi
-
-if [ "$#" != "0" ]
-then
-    SONIC_VERSION=$1
-fi
+DOCKER_IMAGE_TAG="$6"
+REGISTRY_SERVER_WITH_PORT=${REGISTRY_SERVER}${REGISTRY_PORT:+:$REGISTRY_PORT}
 
 push_it() {
     # $1 - Given image name
@@ -43,35 +46,33 @@ set -e
 echo "Loading image ${DOCKER_IMAGE_FILE}"
 docker load < ${DOCKER_IMAGE_FILE}
 
-## Fetch the Jenkins build number if inside it
-[ ${BUILD_NUMBER} ] || {
-    echo "No BUILD_NUMBER found, setting to 0."
-    BUILD_NUMBER="0"
-}
-
 ## Login the docker image registry server
 ## Note: user name and password are passed from command line
-docker login -u ${REGISTRY_USERNAME} -p "${REGISTRY_PASSWD}" ${REGISTRY_SERVER}:${REGISTRY_PORT}
+docker login -u ${REGISTRY_USERNAME} -p "${REGISTRY_PASSWD}" ${REGISTRY_SERVER_WITH_PORT}
 
 ## Get Docker image name
 docker_image_name=$(basename ${DOCKER_IMAGE_FILE} | cut -d. -f1)
-remote_image_name=${REGISTRY_SERVER}:${REGISTRY_PORT}/${docker_image_name}
+remote_image_name=${REGISTRY_SERVER_WITH_PORT}/${docker_image_name}
 
 [ -z "${DOCKER_IMAGE_TAG}" ] || {
     push_it ${docker_image_name} ${remote_image_name}:${DOCKER_IMAGE_TAG}
 }
 
-[ -n "${SONIC_VERSION}" ] || {
-    # If SONIC_VERSION is not given, tag it with build number.
+if [ -n "${sonic_version}" ] && [ -n "${sonic_platform}" ]
+then
+    remote_image_name=${REGISTRY_SERVER_WITH_PORT}/sonic-dockers/${sonic_platform}/${docker_image_name}:${sonic_version}
+    push_it ${docker_image_name} ${remote_image_name}
+else
+    ## Fetch the Jenkins build number if inside it
+    [ ${BUILD_NUMBER} ] || {
+        echo "No BUILD_NUMBER found, setting to 0."
+        BUILD_NUMBER="0"
+    }
+
     timestamp="$(date -u +%Y%m%d)"
     build_version="${timestamp}.bld-${BUILD_NUMBER}"
     push_it ${docker_image_name} ${remote_image_name}:${build_version}
-}
-
-[ -z "${SONIC_VERSION}" ] || {
-    remote_image_name=${REGISTRY_SERVER}/sonic-dockers/${PLATFORM}/${docker_image_name}:${SONIC_VERSION}
-    push_it ${docker_image_name} ${remote_image_name}
-}
+fi
 
 docker rmi $docker_image_name || true
 echo "Job completed"
