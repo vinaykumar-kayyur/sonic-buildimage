6a8db7e2bf06984b27ba2ce7839f9fed4710705e
Srideep Devireddy <srideep_devireddy@dell.com>
2020-04-21 00:56:46 -0700

    [DellEMC] s5232f console and sfputil fix Remove hardcoding 115K baudrate for console Enhance sfputil api as per spec

--- device/dell/x86_64-dellemc_s5232f_c3538-r0/plugins/sfputil.py
+++ device/dell/x86_64-dellemc_s5232f_c3538-r0/plugins/sfputil.py
@@ -213,8 +213,11 @@ class SfpUtil(SfpUtilBase):
 
         return True
 
-    def get_transceiver_change_event(self):
+    def get_transceiver_change_event(self, timeout=0):
         port_dict = {}
+        sleep_time_ms = 500  # Poll interval, in milliseconds
+        sleep_time = sleep_time_ms / 1000.0
+        elapsed_time_ms = 0
         while True:
             for port_num in range(self.port_start, (self.port_end + 1)):
                 presence = self.get_presence(port_num)
@@ -227,6 +230,12 @@ class SfpUtil(SfpUtilBase):
                     port_dict[port_num] = '0'
 
                 if(len(port_dict) > 0):
-                    return True, port_dict
+                    break
 
-            time.sleep(0.5)
+            if len(port_dict) > 0:
+                break
+            if timeout != 0:
+                elapsed_time_ms += sleep_time_ms
+                if elapsed_time_ms > timeout:
+                    break
+            time.sleep(sleep_time)
