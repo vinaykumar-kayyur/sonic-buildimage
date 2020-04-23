e7e50138bb7494c2ba39d7480f7a2c9709e8dd89
zegan <zegan@microsoft.com>
2020-04-23 23:02:55 +0800

    Fix : Duplicate build wpasupplicant
    Signed-off-by: zegan <zegan@microsoft.com>

--- rules/wpasupplicant.mk
+++ rules/wpasupplicant.mk
@@ -8,12 +8,8 @@ WPASUPPLICANT = wpasupplicant_$(WPASUPPLICANT_VERSION)_$(CONFIGURED_ARCH).deb
 $(WPASUPPLICANT)_SRC_PATH = $(SRC_PATH)/wpasupplicant
 SONIC_MAKE_DEBS += $(WPASUPPLICANT)
 
-WPASUPPLICANT_DBG = wpasupplicant_$(WPASUPPLICANT_VERSION)_$(CONFIGURED_ARCH).deb
-$(eval $(call add_derived_package,$(WPASUPPLICANT),$(WPASUPPLICANT_DBG)))
-
 WPASUPPLICANT_DBG = wpasupplicant-dbgsym_$(WPASUPPLICANT_VERSION)_$(CONFIGURED_ARCH).deb
 $(eval $(call add_derived_package,$(WPASUPPLICANT),$(WPASUPPLICANT_DBG)))
-
 
 # The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
 # are archived into debug one image to facilitate debugging.
