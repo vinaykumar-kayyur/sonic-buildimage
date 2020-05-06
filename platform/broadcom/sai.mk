b0899a4a36350212a7cccaf5fd4762632fac7b2b
Mahesh Maddikayala <samaddik@microsoft.com>
2020-05-06 21:31:23 +0000

    missed updating BRCM_SAI variable, fixed it

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,6 +1,6 @@
-BRCM_SAI = libsaibcm_3.7.3.3-4_amd64.deb
+BRCM_SAI = libsaibcm_3.7.4.2_amd64.deb
 $(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.4.2_amd64.deb?sv=2015-04-05&sr=b&sig=sxAe6FwisgN2YRTJsjA8t6hDxGrReguhluh4xnQrbQ4%3D&se=2034-01-13T18%3A01%3A24Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_3.7.3.3-4_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_3.7.4.2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 $(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.4.2_amd64.deb?sv=2015-04-05&sr=b&sig=3v%2B7qSNXqbwFqAGgExS4PY9tDpcNiR6pi9xBEQYXv4g%3D&se=2034-01-13T18%3A06%3A39Z&sp=r"
 
