4cb4ca9e777a77c73ec781a262764f8320571086
Qi Luo <qiluo-msft@users.noreply.github.com>
2020-05-08 19:48:02 +0000

    Fix test code

--- src/sonic-config-engine/tests/test_cfggen.py
+++ src/sonic-config-engine/tests/test_cfggen.py
@@ -200,7 +200,7 @@ class TestCfgGen(TestCase):
     def test_minigraph_port_rs(self):
         argument = '-m "' + self.sample_graph_t0 + '" -p "' + self.port_config + '" -v "PORT[\'Ethernet124\']"'
         output = self.run_script(argument)
-        self.assertEqual(output.strip(), "{'lanes': '25,26,27,28', 'description': 'Servers0:eth0', 'pfc_asym': 'off', 'mtu': '9100', 'alias': 'fortyGigE0/4', 'admin_status': 'up', 'speed': '100000'}")
+        self.assertEqual(output.strip(), "{'lanes': '101,102,103,104', 'fec': 'rs', 'pfc_asym': 'off', 'mtu': '9100', 'alias': 'fortyGigE0/124', 'admin_status': 'up', 'speed': '100000', 'description': 'ARISTA04T1:Ethernet1/1'}")
 
     def test_minigraph_bgp(self):
         argument = '-m "' + self.sample_graph_bgp_speaker + '" -p "' + self.port_config + '" -v "BGP_NEIGHBOR[\'10.0.0.59\']"'
