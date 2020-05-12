2c95fa71a24018fc1a57c83b06a8291411c0340b
Ying Xie <ying.xie@microsoft.com>
2020-05-12 14:31:09 +0000

    fix typo

--- files/image_config/ntp/ntp-config.sh
+++ files/image_config/ntp/ntp-config.sh
@@ -2,7 +2,7 @@
 
 reboot_type='cold'
 
-function get_databaes_reboot_type()
+function get_database_reboot_type()
 {
     SYSTEM_WARM_START=`/usr/bin/redis-cli -n 6 hget "WARM_RESTART_ENABLE_TABLE|system" enable`
     SYSTEM_FAST_START=`/usr/bin/redis-cli -n 6 get "FAST_REBOOT|system"`
@@ -16,7 +16,7 @@ function get_databaes_reboot_type()
 
 sonic-cfggen -d -t /usr/share/sonic/templates/ntp.conf.j2 >/etc/ntp.conf
 
-get_databaes_reboot_type
+get_database_reboot_type
 if [[ x"${reboot_type}" == x"cold" ]]; then
     echo "Enabling NTP long jump for reboot type ${reboot_type} ..."
     sed -i "s/NTPD_OPTS='-x'/NTPD_OPTS='-g'/" /etc/default/ntp
