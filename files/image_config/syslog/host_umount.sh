7f1b98c71a4d24f1a0d278bdcc99c5fe3b7833c5
drk <Devi_rk@dell.com>
2020-05-14 10:27:59 +0530

    host_unmount_fix_review_changes

--- files/image_config/syslog/host_umount.sh
+++ files/image_config/syslog/host_umount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#This script is invoked at the closure of syslog socket during reboot
-#This will stop journal services, unmount /var/log and delete loop device 
-#associated to /host to ensure proper unmount of /host
+# This script is invoked at the closure of syslog socket during reboot
+# This will stop journal services, unmount /var/log and delete loop device 
+# associated to /host to ensure proper unmount of /host
 
 journal_stop() {
     systemctl stop systemd-journald.service
@@ -14,17 +14,18 @@ delete_loop_device() {
     umount /var/log
     if [[ $? -ne 0 ]]
     then
-	exit 0
+        exit 0
     fi
     losetup -d /dev/loop1
 }
 
 case "$1" in
     journal_stop|delete_loop_device)
-	$1
-	;;
+        $1
+        ;;
     *)
-	echo "Usage: $0 {journal_stop|delete_loop_device}"
-	;;
+        echo "Usage: $0 {journal_stop|delete_loop_device}" >&2
+        exit 1
+        ;;
 esac
 
