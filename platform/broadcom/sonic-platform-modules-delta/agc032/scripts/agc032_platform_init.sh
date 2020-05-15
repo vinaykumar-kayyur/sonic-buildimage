de8f8bae4efc9a55cc43739cbaac595ed98aa45b
zoe-kuan <ZOE.KUAN@deltaww.com>
2020-05-15 09:35:23 +0800

    [platform]: Add a new supported platform, Delta-agc032
    Switch Vendor: Delta
    Switch SKU: Delta-agc032
    CPU: BROADWELL-DE
    ASIC Vendor: Broadcom
    Switch ASIC: Tomahawk3, BCM56980
    Port Configuration: 32x400G + 2x10G
    
    - What I did
    Add a new Delta platform Delta-agc032.
    
    - How I did it
    Add files by following SONiC Porting Guide.
    
    - How to verify it
    1. decode-syseeprom
    2. sensors
    3. psuutil
    4. sfputil
    5. show interface status
    6. bcmcmd
    
    - Description for the changelog
    Initial commit

---
+++ platform/broadcom/sonic-platform-modules-delta/agc032/scripts/agc032_platform_init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#platform init script for Delta agc032
+
+#fan speed monitol start
+/usr/share/sonic/device/x86_64-delta_agc032-r0/fancontrol.service /usr/share/sonic/device/x86_64-delta_agc032-r0/fancontrol &
+
+#Get syseeprom information
+echo 0 |sudo tee /sys/devices/platform/delta-agc032-cpupld.0/cpu_i2c_mux_sel
+decode-syseeprom
+echo 1 |sudo tee /sys/devices/platform/delta-agc032-cpupld.0/cpu_i2c_mux_sel
+
+exit 0
+
