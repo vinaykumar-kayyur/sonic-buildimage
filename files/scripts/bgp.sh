3e0986bb9e8d6dc4962acc9f5fe20cac067ef319
heidi.ou@alibaba-inc.com <heidi.ou@alibaba-inc.com>
2020-05-20 10:48:37 -0700

    BGP warm reboot script to service, also handle fast boot as indicated by flag saved in StateDB

--- files/scripts/bgp.sh
+++ files/scripts/bgp.sh
@@ -31,14 +31,26 @@ function validate_restore_count()
     fi
 }
 
+function check_fast_boot ()
+{
+		if [[ $($SONIC_DB_CLI STATE_DB GET "FAST_REBOOT|system") == "1" ]]; then
+				FAST_BOOT = "true"
+		else
+				FAST_BOOT = "false"
+		fi
+}
+
 start() {
     debug "Starting ${SERVICE} service..."
 
     check_warm_boot
     validate_restore_count
 
+		check_fast_boot
+		
     debug "Warm boot flag: ${SERVICE} ${WARM_BOOT}."
-
+    debug "Fast boot flag: ${SERVICE} ${Fast_BOOT}."
+		
     # start service docker
     /usr/bin/${SERVICE}.sh start
     debug "Started ${SERVICE} service..."
@@ -46,7 +58,6 @@ start() {
 }
 
 attach() {
-    startPeerService
     /usr/bin/${SERVICE}.sh attach
 }
 
@@ -54,10 +65,12 @@ stop() {
     debug "Stopping ${SERVICE} service..."
 
     check_warm_boot
+    check_fast_boot
     debug "Warm boot flag: ${SERVICE} ${WARM_BOOT}."
+    debug "Fast boot flag: ${SERVICE} ${FAST_BOOT}."
 
 		# Kill bgpd to start the bgp graceful restart procedure
-		if [[ x"$WARM_BOOT" == x"true" ]]; then
+		if [[ x"$WARM_BOOT" == x"true" ]] || [[ x"$FAST_BOOT" == x"true" ]]; then
 				debug "Kill zebra first"
 				/usr/bin/docker exec -i bgp pkill -9 zebra
 				/usr/bin/docker exec -i bgp pkill -9 bgpd || [ $? == 1 ]
