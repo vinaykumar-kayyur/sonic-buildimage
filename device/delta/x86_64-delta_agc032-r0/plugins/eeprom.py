e04b721e641e0314e762a14e3e76a8b98f468b4a
zoe-kuan <ZOE.KUAN@deltaww.com>
2020-05-22 11:45:49 +0800

    Fix inconsistent indentation.
    Signed-off-by: zoe-kuan <ZOE.KUAN@deltaww.com>

--- device/delta/x86_64-delta_agc032-r0/plugins/eeprom.py
+++ device/delta/x86_64-delta_agc032-r0/plugins/eeprom.py
@@ -8,5 +8,5 @@ except ImportError, e:
 class board(eeprom_tlvinfo.TlvInfoDecoder):
     _TLV_INFO_MAX_LEN = 256
     def __init__(self, name, path, cpld_root, ro):
-	self.eeprom_path = "/sys/devices/pci0000:00/0000:00:1f.3/i2c-0/0-0053/eeprom"
-	super(board, self).__init__(self.eeprom_path, 0, '', True)
+        self.eeprom_path = "/sys/devices/pci0000:00/0000:00:1f.3/i2c-0/0-0053/eeprom"
+        super(board, self).__init__(self.eeprom_path, 0, '', True)
