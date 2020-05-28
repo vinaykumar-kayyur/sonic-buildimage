9f8bfaab472d9f863ec78cc0849d37d6458f0a91
Qi Luo <qiluo-msft@users.noreply.github.com>
2020-05-28 00:28:28 +0000

    Install redis_dump_load python3 wheel in host image

---
+++ rules/redis-dump-load-py3.mk
@@ -0,0 +1,6 @@
+# redis_dump_load python3 wheel
+
+REDIS_DUMP_LOAD_PY3 = redis_dump_load-1.1-py3-none-any.whl
+$(REDIS_DUMP_LOAD_PY3)_SRC_PATH = $(SRC_PATH)/redis-dump-load
+$(REDIS_DUMP_LOAD_PY3)_PYTHON_VERSION = 3
+SONIC_PYTHON_WHEELS += $(REDIS_DUMP_LOAD_PY3)
