5cfe03285dd1473c8819204ce372f280db344b9a
taocy <taocy2@centecnetworks.com>
2020-06-05 14:51:06 +0000

    move tsingma SoC driver to initramfs as ko

--- platform/centec-arm64/linux-kernel-arm64.mk
+++ platform/centec-arm64/linux-kernel-arm64.mk
@@ -1,6 +1,6 @@
 # linux kernel package for centec arm64
 
-LINUX_KERNEL_DTB_INITRAMFS = linux-dtb-initramfs.deb
+LINUX_KERNEL_DTB_INITRAMFS = linux-dtb-initramfs_1.0.deb
 $(LINUX_KERNEL_DTB_INITRAMFS)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/kernel/$(LINUX_KERNEL_DTB_INITRAMFS)
 SONIC_ONLINE_DEBS += $(LINUX_KERNEL_DTB_INITRAMFS)
 SONIC_STRETCH_DEBS += $(LINUX_KERNEL_DTB_INITRAMFS)
