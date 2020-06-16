a9e6c71f4e082be676280e01c280fe452d6ceb8f
Judy Joseph <judy.joseph@microsoft.com>
2020-06-15 19:10:40 -0700

    Support for connecting to DB in namespace via IP:port ( using docker bridge network ) for applications in multi-asic platform.

--- dockers/docker-database/docker-database-init.sh
+++ dockers/docker-database/docker-database-init.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+# For linux host namespace, in both single and multi ASIC platform use the loopback interface
+# For other namespaces, use eth0 interface which is connected to the docker0 bridge in the host.
+if [[ $NAMESPACE_ID == "" ]]
+then
+    INTFC=lo
+else
+    INTFC=eth0
+    LOOPBACK_IP=127.0.0.1
+fi
+local_ip=$(ip -4 -o addr show $INTFC | awk '{print $4}' | cut -d'/' -f1)
+export HOST_IP=$local_ip
+
 REDIS_DIR=/var/run/redis$NAMESPACE_ID
 mkdir -p $REDIS_DIR/sonic-db
 
@@ -22,6 +34,9 @@ then
 fi
 
 # generate all redis server supervisord configuration file
-sonic-cfggen -j /var/run/redis/sonic-db/database_config.json -t /usr/share/sonic/templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
+# Pass the Loopback IP explicitly in case of namespaces other than the linux host namespace.
+# This is needed as there are applications who hardcoded host=loopback IP in Connector classes.
+# Redis server will then bind on multiple IP addresses viz <loopback ip> , <eth0 ip>
+sonic-cfggen -j /var/run/redis/sonic-db/database_config.json -a "{\"LOOPBACK_IP\":\"$LOOPBACK_IP\"}" -t /usr/share/sonic/templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
 
 exec /usr/bin/supervisord
