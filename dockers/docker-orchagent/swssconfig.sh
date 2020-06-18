c79e906f81f7ffa0967bee3d056ac6d34e024ec0
Tamer Ahmed <tamer.ahmed@microsoft.com>
2020-06-17 17:10:56 -0700

    [fast-reboot] Back up FDB/ARP/Default routes
    FDB/ARP/Devault routes files are deleted after swssconfig. This
    make debugging/validation of device conversion hard. This PR
    save those files in order to facilitate device conversion.
    
    signed-off-by: Tamer Ahmed <tamer.ahmed@microsoft.com>

--- dockers/docker-orchagent/swssconfig.sh
+++ dockers/docker-orchagent/swssconfig.sh
@@ -8,19 +8,19 @@ function fast_reboot {
       if [[ -f /fdb.json ]];
       then
         swssconfig /fdb.json
-        rm -f /fdb.json
+        mv /fdb.json /fdb.json.1
       fi
 
       if [[ -f /arp.json ]];
       then
         swssconfig /arp.json
-        rm -f /arp.json
+        mv /arp.json /arp.json.1
       fi
 
       if [[ -f /default_routes.json ]];
       then
         swssconfig /default_routes.json
-        rm -f /default_routes.json
+        mv /default_routes.json /default_routes.json.1
       fi
 
       ;;
