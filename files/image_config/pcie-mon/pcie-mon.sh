a6fa28591a7585b5236be33bc6f1111c693fcdb0
sujinmkang <sujkang@microsoft.com>
2020-06-18 16:47:12 -0700

    Clean up

--- files/image_config/pcie-mon/pcie-mon.sh
+++ files/image_config/pcie-mon/pcie-mon.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 ## Check the platform PCIe device presence and status
 
-VERBOSE=no
+VERBOSE="no"
 RESULTS="PCIe Device Checking All Test"
-PASS="PASSED"
-TEMP_PCIE_MON_F="/tmp/pcie-mon.txt"
-STATUS=1
+PCIE_CHK_CMD=`sudo pcieutil pcie-check |grep "$RESULTS"`
+EXPECTED="PCIe Device Checking All Test ----------->>> PASSED"
 MAX_RESCAN=15
 
 function debug()
@@ -16,42 +15,19 @@ function debug()
     fi
 }
 
-function check_pcie_devices()
-{
-    show platform pcieinfo -c > $TEMP_PCIE_MON_F
-    while read -r line;
-    do
-       debug "$line"
-       if [[ $line =~ $RESULTS ]]; then
-           if [[ $line =~ $PASS ]]; then
-               STATUS=0 
-           fi
-        fi
-    done < $TEMP_PCIE_MON_F
-    rm $TEMP_PCIE_MON_F
-    if [ $STATUS = 0 ]; then
-        debug "PCIe check passed"
-        return 0
-    fi
-    debug "PCIe check failed"
-    return 1
-}
-
 function check_and_rescan_pcie_devices()
 {
-    i=0
-    while [ $i -lt $MAX_RESCAN ];
+    for i in $(seq 1 1 $MAX_RESCAN)
     do
-        ret=check_pcie_devices
-        if [ $ret -eq 0]
-            exit 0
-        fi
-        debug "PCIe RESCAN"
-        echo 1 > /sys/bus/pci/rescan
-        let "i+=1"
-        sleep 1
-    done
-    exit 1
+        if [ "$PCIE_CHK_CMD" = "$EXPECTED" ]; then
+            debug "PCIe check passed"
+            exit
+        else
+            debug "PCIe RESCAN"
+            echo 1 > /sys/bus/pci/rescan
+         fi
+         sleep 1
+     done
 }
 
 check_and_rescan_pcie_devices
