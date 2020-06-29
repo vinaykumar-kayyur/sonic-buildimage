7ef34de0f59f8877035e037b1d46e31f4cb03078
paavaanan <paavaanan_t_n@dell.com>
2020-06-29 08:27:02 -0400

    DellEMC S6100 disable pericom/xlinx chipset

--- platform/broadcom/sonic-platform-modules-dell/s6100/scripts/s6100_platform.sh
+++ platform/broadcom/sonic-platform-modules-dell/s6100/scripts/s6100_platform.sh
@@ -26,6 +26,18 @@ if [[ "$1" == "init" ]]; then
     modprobe nvram
     systemctl start s6100-reboot-cause.service
 
+    # Disable Xilinx/pericom chipset
+    bios_version=$(dmidecode -s system-version | cut -d "-" -f2)
+
+    if [[ $bios_version -gt 5 ]] ; then
+        # Pericom is disabled in BIOS. Disable only Xilinx
+        echo 1 > /sys/bus/pci/devices/0000:02:00.0/remove
+    else
+        # Disable pericom/xilinx
+        echo 1 > /sys/bus/pci/devices/0000:02:00.0/remove
+        echo 1 > /sys/bus/pci/devices/0000:08:00.0/remove
+    fi
+
     # Disable Watchdog Timer
     if [[ -e /usr/local/bin/platform_watchdog_disable.sh ]]; then
         /usr/local/bin/platform_watchdog_disable.sh
