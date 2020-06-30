51533a774c71215d518724e71d4194aa85008663
taocy <taocy@centecnetworks.com>
2020-06-30 13:14:52 +0000

    update SAI to v1.6.3 for centec arm64 arch

--- platform/centec-arm64/sai.mk
+++ platform/centec-arm64/sai.mk
@@ -1,8 +1,9 @@
 # Centec SAI
 
-export CENTEC_SAI_VERSION = 1.6.1-1.0
+export CENTEC_SAI_VERSION = 1.6.3-1
 export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
 $(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI)
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
 SONIC_STRETCH_DEBS += $(CENTEC_SAI)
+
