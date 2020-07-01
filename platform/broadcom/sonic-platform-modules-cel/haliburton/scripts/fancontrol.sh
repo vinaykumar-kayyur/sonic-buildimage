a5d529dfbf2fc4b0d2808c53f57a10f07afc5996
Wirut Getbamrung <wgetbum@icloud.com>
2020-07-01 13:15:05 +0700

    [platform/cel]: fix indent

--- platform/broadcom/sonic-platform-modules-cel/haliburton/scripts/fancontrol.sh
+++ platform/broadcom/sonic-platform-modules-cel/haliburton/scripts/fancontrol.sh
@@ -26,53 +26,53 @@ test -x $DAEMON || exit 0
 
 for i in 1 2 3
 do
-	j=$i
-	[ $i -eq 3 ] && j=4
-	FANFAULT=$(cat ${DEVPATH}/fan${j}_fault)
+    j=$i
+    [ $i -eq 3 ] && j=4
+    FANFAULT=$(cat ${DEVPATH}/fan${j}_fault)
   [ $FANFAULT = 1 ] && continue
-	FANDIR=$(cat ${PLATFORMPATH}/fan${i}_dir)
+    FANDIR=$(cat ${PLATFORMPATH}/fan${i}_dir)
 done
 CONF=${MAIN_CONF}-${FANDIR}
 
 case "$1" in
   start)
-  	if [ -f $CONF ] ; then
-		if $DAEMON --check $CONF 1>/dev/null 2>/dev/null ; then
-			log_daemon_msg "Starting $DESC" "$NAME\n"
-			start-stop-daemon --start --quiet --pidfile $PIDFILE --startas $DAEMON $CONF
-			log_end_msg $?
-		else
-			log_failure_msg "Not starting fancontrol, broken configuration file; please re-run pwmconfig."
-		fi
-	else
-		if [ "$VERBOSE" != no ]; then
-			log_warning_msg "Not starting fancontrol; run pwmconfig first."
-		fi
-	fi
-	;;
+    if [ -f $CONF ] ; then
+        if $DAEMON --check $CONF 1>/dev/null 2>/dev/null ; then
+            log_daemon_msg "Starting $DESC" "$NAME\n"
+            start-stop-daemon --start --quiet --pidfile $PIDFILE --startas $DAEMON $CONF
+            log_end_msg $?
+        else
+            log_failure_msg "Not starting fancontrol, broken configuration file; please re-run pwmconfig."
+        fi
+    else
+        if [ "$VERBOSE" != no ]; then
+            log_warning_msg "Not starting fancontrol; run pwmconfig first."
+        fi
+    fi
+    ;;
   stop)
-		log_daemon_msg "Stopping $DESC" "$NAME"
-		start-stop-daemon --stop --quiet --pidfile $PIDFILE --oknodo --startas $DAEMON $CONF
-		rm -f $PIDFILE
-		log_end_msg $?
-	;;
+        log_daemon_msg "Stopping $DESC" "$NAME"
+        start-stop-daemon --stop --quiet --pidfile $PIDFILE --oknodo --startas $DAEMON $CONF
+        rm -f $PIDFILE
+        log_end_msg $?
+    ;;
   restart)
-  	$0 stop
-		sleep 3
-		$0 start
-	;;
+    $0 stop
+        sleep 3
+        $0 start
+    ;;
   force-reload)
-	if start-stop-daemon --stop --test --quiet --pidfile $PIDFILE --startas $DAEMON $CONF ; then
-		$0 restart
-	fi
-	;;
+    if start-stop-daemon --stop --test --quiet --pidfile $PIDFILE --startas $DAEMON $CONF ; then
+        $0 restart
+    fi
+    ;;
   status)
-		status_of_proc $DAEMON $NAME $CONF && exit 0 || exit $?
-	;;
+        status_of_proc $DAEMON $NAME $CONF && exit 0 || exit $?
+    ;;
   *)
-	log_success_msg "Usage: /etc/init.d/fancontrol {start|stop|restart|force-reload|status}"
-	exit 1
-	;;
+    log_success_msg "Usage: /etc/init.d/fancontrol {start|stop|restart|force-reload|status}"
+    exit 1
+    ;;
 esac
 
 exit 0
