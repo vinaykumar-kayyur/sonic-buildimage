6464b802a387a89d6ba36d8a604fbe69209e7195
Wirut Getbamrung <wgetbum@icloud.com>
2020-07-03 11:54:49 +0700

    [platform/cel]: fix invalid indentation

--- platform/broadcom/sonic-platform-modules-cel/haliburton/scripts/fancontrol.sh
+++ platform/broadcom/sonic-platform-modules-cel/haliburton/scripts/fancontrol.sh
@@ -20,24 +20,23 @@ NAME="fancontrol"
 PIDFILE=/var/run/fancontrol.pid
 PLATFORMPATH=/sys/devices/platform/e1031.smc
 MAIN_CONF=/usr/share/sonic/device/x86_64-cel_e1031-r0/fancontrol
-DEVPATH=/sys/devices/pci0000:00/0000:00:13.0/i2c-0/i2c-8/i2c-23/23-004d 
+DEVPATH=/sys/devices/pci0000:00/0000:00:13.0/i2c-0/i2c-8/i2c-23/23-004d
 
 test -x $DAEMON || exit 0
 
-for i in 1 2 3
-do
+for i in 1 2 3; do
     j=$i
     [ $i -eq 3 ] && j=4
     FANFAULT=$(cat ${DEVPATH}/fan${j}_fault)
-  [ $FANFAULT = 1 ] && continue
+    [ $FANFAULT = 1 ] && continue
     FANDIR=$(cat ${PLATFORMPATH}/fan${i}_dir)
 done
 CONF=${MAIN_CONF}-${FANDIR}
 
 case "$1" in
-  start)
-    if [ -f $CONF ] ; then
-        if $DAEMON --check $CONF 1>/dev/null 2>/dev/null ; then
+start)
+    if [ -f $CONF ]; then
+        if $DAEMON --check $CONF 1>/dev/null 2>/dev/null; then
             log_daemon_msg "Starting $DESC" "$NAME\n"
             start-stop-daemon --start --quiet --pidfile $PIDFILE --startas $DAEMON $CONF
             log_end_msg $?
@@ -50,26 +49,26 @@ case "$1" in
         fi
     fi
     ;;
-  stop)
-        log_daemon_msg "Stopping $DESC" "$NAME"
-        start-stop-daemon --stop --quiet --pidfile $PIDFILE --oknodo --startas $DAEMON $CONF
-        rm -f $PIDFILE
-        log_end_msg $?
+stop)
+    log_daemon_msg "Stopping $DESC" "$NAME"
+    start-stop-daemon --stop --quiet --pidfile $PIDFILE --oknodo --startas $DAEMON $CONF
+    rm -f $PIDFILE
+    log_end_msg $?
     ;;
-  restart)
+restart)
     $0 stop
-        sleep 3
-        $0 start
+    sleep 3
+    $0 start
     ;;
-  force-reload)
-    if start-stop-daemon --stop --test --quiet --pidfile $PIDFILE --startas $DAEMON $CONF ; then
+force-reload)
+    if start-stop-daemon --stop --test --quiet --pidfile $PIDFILE --startas $DAEMON $CONF; then
         $0 restart
     fi
     ;;
-  status)
-        status_of_proc $DAEMON $NAME $CONF && exit 0 || exit $?
+status)
+    status_of_proc $DAEMON $NAME $CONF && exit 0 || exit $?
     ;;
-  *)
+*)
     log_success_msg "Usage: /etc/init.d/fancontrol {start|stop|restart|force-reload|status}"
     exit 1
     ;;
