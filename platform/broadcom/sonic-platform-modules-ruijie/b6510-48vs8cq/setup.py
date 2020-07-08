9d7be7ca41889c3ca9a41a6a3bda4fc1f733061c
tim-rj <sonic_rd@ruijie.com.cn>
2020-07-08 14:38:41 +0800

    [platform/ruijie] Add new platform api support for ruijie b6510
    Signed-off-by: tim-rj <sonic_rd@ruijie.com.cn>

---
+++ platform/broadcom/sonic-platform-modules-ruijie/b6510-48vs8cq/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='sonic-platform',
+    version='1.0',
+    description='SONiC platform API implementation on RUIJIE B6510 Platforms',
+    license='Apache 2.0',
+    author='SONiC Team',
+    author_email='sonic_rd@ruijie.com.cn',
+    url='',
+    maintainer='RUIJIE B6510',
+    maintainer_email='',
+    packages=[
+        'sonic_platform',
+        'plat_hal',
+        'rjutil',
+        'eepromutil'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    keywords='sonic SONiC platform PLATFORM',
+)
+
