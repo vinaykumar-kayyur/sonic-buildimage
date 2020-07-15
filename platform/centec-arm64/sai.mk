7da1f1b8d7c57c21a931328adb5662712ba5215a
Gu Xianghong <gxh2001757@163.com>
2020-07-15 03:32:44 +0000

    remove ramfs from dtb package; remove unused variable

--- platform/centec-arm64/sai.mk
+++ platform/centec-arm64/sai.mk
@@ -5,5 +5,4 @@ export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
 $(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI)
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
-SONIC_STRETCH_DEBS += $(CENTEC_SAI)
 
