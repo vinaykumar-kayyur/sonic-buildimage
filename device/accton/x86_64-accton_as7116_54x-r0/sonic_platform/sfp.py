61b8739cc161206af6e69fe37fc11d57bee08a27
Joe LeVeque <jolevequ@microsoft.com>
2020-07-20 23:07:28 +0000

    Migrate from sonic_daemon_base to sonic_py_common

--- device/accton/x86_64-accton_as7116_54x-r0/sonic_platform/sfp.py
+++ device/accton/x86_64-accton_as7116_54x-r0/sonic_platform/sfp.py
@@ -18,7 +18,7 @@ try:
     from sonic_platform_base.sonic_sfp.sff8436 import sff8436Dom
     from sonic_platform_base.sonic_sfp.sff8436 import sff8436InterfaceId
     from sonic_platform_base.sonic_sfp.sfputilhelper import SfpUtilHelper
-    from sonic_daemon_base.daemon_base import Logger
+    from sonic_py_common.logger import Logger
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
--- platform/mellanox/mlnx-platform-api/sonic_platform/chassis.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/chassis.py
@@ -13,7 +13,7 @@ try:
     from sonic_platform_base.component_base import ComponentBase
     from sonic_device_util import get_machine_info
     from sonic_device_util import get_platform_info
-    from sonic_daemon_base.daemon_base import Logger
+    from sonic_py_common.logger import Logger
     from os import listdir
     from os.path import isfile, join
     import sys
