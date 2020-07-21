30489fae66f544e0efbedb29982c88534c1eb677
Wirut Getbamrung <wgetbum@icloud.com>
2020-07-21 11:49:25 +0700

    [device/celestica-seastone2]: clean uncode component api code

--- device/celestica/x86_64-cel_seastone_2-r0/sonic_platform/component.py
+++ device/celestica/x86_64-cel_seastone_2-r0/sonic_platform/component.py
@@ -8,10 +8,6 @@
 #
 #############################################################################
 
-import json
-import os.path
-import inspect
-
 try:
     from sonic_platform_base.component_base import ComponentBase
     from common import Common
@@ -30,7 +26,6 @@ class Component(ComponentBase):
         self._index = component_index
         self._config = conf
         self._api_common = Common(conf=self._config)
-        self._name = self.get_name()
 
     def get_name(self):
         """
@@ -62,54 +57,3 @@ class Component(ComponentBase):
         default = 'N/A'
         config = self._config["get_firmware_version"]
         return self._api_common.get_output(self._index, config, default)
-
-    def get_available_firmware_version(self, image_path):
-        """
-        Retrieves the available firmware version of the component
-        Note: the firmware version will be read from image
-        Args:
-            image_path: A string, path to firmware image
-        Returns:
-            A string containing the available firmware version of the component
-        """
-        raise NotImplementedError
-
-    def get_firmware_update_notification(self, image_path):
-        """
-        Retrieves a notification on what should be done in order to complete
-        the component firmware update
-        Args:
-            image_path: A string, path to firmware image
-        Returns:
-            A string containing the component firmware update notification if required.
-            By default 'None' value will be used, which indicates that no actions are required
-        """
-        return None
-
-    def install_firmware(self, image_path):
-        """
-        Installs firmware to the component
-        This API performs firmware installation only: this may/may not be the same as firmware update.
-        In case platform component requires some extra steps (apart from calling Low Level Utility)
-        to load the installed firmware (e.g, reboot, power cycle, etc.) - this must be done manually by user
-        Note: in case immediate actions are required to complete the component firmware update
-        (e.g., reboot, power cycle, etc.) - will be done automatically by API and no return value provided
-        Args:
-            image_path: A string, path to firmware image
-        Returns:
-            A boolean, True if install was successful, False if not
-        """
-        raise NotImplementedError
-
-    def update_firmware(self, image_path):
-        """
-        Updates firmware of the component
-        This API performs firmware update: it assumes firmware installation and loading in a single call.
-        In case platform component requires some extra steps (apart from calling Low Level Utility)
-        to load the installed firmware (e.g, reboot, power cycle, etc.) - this will be done automatically by API
-        Args:
-            image_path: A string, path to firmware image
-        Raises:
-            RuntimeError: update failed
-        """
-        raise NotImplementedError
