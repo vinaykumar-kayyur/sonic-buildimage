a4a507df578050faca298cc4b8cea06065e50e79
Volodymyr Boyko <volodymyrx.boiko@intel.com>
2020-07-20 19:05:41 -0700

    [barefoot][platform] increase init timeout in eeprom.py
    Signed-off-by: Volodymyr Boyko <volodymyrx.boiko@intel.com>
    
    https://github.com/Azure/sonic-buildimage/pull/5006

--- platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/eeprom.py
+++ platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/eeprom.py
@@ -65,7 +65,7 @@ EEPROM_SYMLINK = "/var/run/platform/eeprom/syseeprom"
 EEPROM_STATUS = "/var/run/platform/eeprom/status"
 
 class Eeprom(eeprom_tlvinfo.TlvInfoDecoder):
-    RETRIES = 3
+    RETRIES = 35
 
     def __init__(self):
 
@@ -89,8 +89,10 @@ class Eeprom(eeprom_tlvinfo.TlvInfoDecoder):
         super(Eeprom, self).__init__(self.eeprom_path, 0, EEPROM_STATUS, True)
 
         for attempt in range(self.RETRIES):
-            if self.eeprom_init() or (attempt + 1 >= self.RETRIES):
+            if self.eeprom_init():
                 break
+            if attempt + 1 == self.RETRIES:
+                raise RuntimeError("eeprom.py: Initialization failed")
             time.sleep(1)
 
     def eeprom_init(self):
