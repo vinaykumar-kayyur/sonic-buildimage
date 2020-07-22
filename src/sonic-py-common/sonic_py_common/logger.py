d65f6839035938e6d344fbcfe716f6ebb384285e
Joe LeVeque <jolevequ@microsoft.com>
2020-07-22 03:45:01 +0000

    Add ability to control minimum log priority, set default to NOTICE

--- src/sonic-py-common/sonic_py_common/logger.py
+++ src/sonic-py-common/sonic_py_common/logger.py
@@ -8,6 +8,13 @@ Logging functionality for SONiC Python applications
 FACILITY_USER = syslog.LOG_USER
 FACILITY_DAEMON = syslog.LOG_DAEMON
 
+PRIORITY_ERROR = syslog.LOG_ERR
+PRIORITY_WARNING = syslog.LOG_WARNING
+PRIORITY_NOTICE = syslog.LOG_NOTICE
+PRIORITY_INFO = syslog.LOG_INFO
+PRIORITY_DEBUG = syslog.LOG_DEBUG
+
+
 class Logger(object):
     """
     Logger class for SONiC Python applications
@@ -22,8 +29,14 @@ class Logger(object):
                             logoption=(syslog.LOG_PID | syslog.LOG_NDELAY),
                             facility=log_facility)
 
+        # Set the default minimum log priority to PRIORITY_NOTICE
+        self.set_min_log_priority(PRIORITY_NOTICE)
+
     def __del__(self):
         self.syslog.closelog()
+
+    def set_min_log_priority(priority):
+        self.syslog.setlogmask(self.syslog.LOG_UPTO(priority))
 
     def log_error(self, msg, also_print_to_console=False):
         self.syslog.syslog(self.syslog.LOG_ERR, msg)
