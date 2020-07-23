461a56cb75374d971a142b79b042a5884d67eea4
Joe LeVeque <jolevequ@microsoft.com>
2020-07-23 21:32:49 +0000

    Add 'LOG_' prefix to facilities and priorities

--- src/sonic-py-common/sonic_py_common/logger.py
+++ src/sonic-py-common/sonic_py_common/logger.py
@@ -10,16 +10,16 @@ class Logger(object):
     """
     Logger class for SONiC Python applications
     """
-    FACILITY_USER = syslog.LOG_USER
-    FACILITY_DAEMON = syslog.LOG_DAEMON
+    LOG_FACILITY_USER = syslog.LOG_USER
+    LOG_FACILITY_DAEMON = syslog.LOG_DAEMON
 
-    PRIORITY_ERROR = syslog.LOG_ERR
-    PRIORITY_WARNING = syslog.LOG_WARNING
-    PRIORITY_NOTICE = syslog.LOG_NOTICE
-    PRIORITY_INFO = syslog.LOG_INFO
-    PRIORITY_DEBUG = syslog.LOG_DEBUG
+    LOG_PRIORITY_ERROR = syslog.LOG_ERR
+    LOG_PRIORITY_WARNING = syslog.LOG_WARNING
+    LOG_PRIORITY_NOTICE = syslog.LOG_NOTICE
+    LOG_PRIORITY_INFO = syslog.LOG_INFO
+    LOG_PRIORITY_DEBUG = syslog.LOG_DEBUG
 
-    def __init__(self, log_identifier=None, log_facility=FACILITY_USER):
+    def __init__(self, log_identifier=None, log_facility=LOG_FACILITY_USER):
         self.syslog = syslog
 
         if not log_identifier:
@@ -29,8 +29,8 @@ class Logger(object):
                             logoption=(syslog.LOG_PID | syslog.LOG_NDELAY),
                             facility=log_facility)
 
-        # Set the default minimum log priority to PRIORITY_NOTICE
-        self.set_min_log_priority(self.PRIORITY_NOTICE)
+        # Set the default minimum log priority to LOG_PRIORITY_NOTICE
+        self.set_min_log_priority(self.LOG_PRIORITY_NOTICE)
 
     def __del__(self):
         self.syslog.closelog()
@@ -51,33 +51,33 @@ class Logger(object):
 
     def set_min_log_priority_error(self):
         """
-        Convenience function to set minimum log priority to PRIORITY_ERROR
+        Convenience function to set minimum log priority to LOG_PRIORITY_ERROR
         """
-        self.set_min_log_priority(self.PRIORITY_ERROR)
+        self.set_min_log_priority(self.LOG_PRIORITY_ERROR)
 
     def set_min_log_priority_warning(self):
         """
-        Convenience function to set minimum log priority to PRIORITY_WARNING
+        Convenience function to set minimum log priority to LOG_PRIORITY_WARNING
         """
-        self.set_min_log_priority(self.PRIORITY_WARNING)
+        self.set_min_log_priority(self.LOG_PRIORITY_WARNING)
 
     def set_min_log_priority_notice(self):
         """
-        Convenience function to set minimum log priority to PRIORITY_NOTICE
+        Convenience function to set minimum log priority to LOG_PRIORITY_NOTICE
         """
-        self.set_min_log_priority(self.PRIORITY_NOTICE)
+        self.set_min_log_priority(self.LOG_PRIORITY_NOTICE)
 
     def set_min_log_priority_info(self):
         """
-        Convenience function to set minimum log priority to PRIORITY_INFO
+        Convenience function to set minimum log priority to LOG_PRIORITY_INFO
         """
-        self.set_min_log_priority(self.PRIORITY_INFO)
+        self.set_min_log_priority(self.LOG_PRIORITY_INFO)
 
     def set_min_log_priority_debug(self):
         """
-        Convenience function to set minimum log priority to PRIORITY_DEBUG
+        Convenience function to set minimum log priority to LOG_PRIORITY_DEBUG
         """
-        self.set_min_log_priority(self.PRIORITY_DEBUG)
+        self.set_min_log_priority(self.LOG_PRIORITY_DEBUG)
 
     #
     # Methods for logging messages
@@ -90,16 +90,16 @@ class Logger(object):
             print(msg)
 
     def log_error(self, msg, also_print_to_console=False):
-        self.log(self.PRIORITY_ERROR, msg, also_print_to_console)
+        self.log(self.LOG_PRIORITY_ERROR, msg, also_print_to_console)
 
     def log_warning(self, msg, also_print_to_console=False):
-        self.log(self.PRIORITY_WARNING, msg, also_print_to_console)
+        self.log(self.LOG_PRIORITY_WARNING, msg, also_print_to_console)
 
     def log_notice(self, msg, also_print_to_console=False):
-        self.log(self.PRIORITY_NOTICE, msg, also_print_to_console)
+        self.log(self.LOG_PRIORITY_NOTICE, msg, also_print_to_console)
 
     def log_info(self, msg, also_print_to_console=False):
-        self.log(self.PRIORITY_INFO, msg, also_print_to_console)
+        self.log(self.LOG_PRIORITY_INFO, msg, also_print_to_console)
 
     def log_debug(self, msg, also_print_to_console=False):
-        self.log(self.PRIORITY_DEBUG, msg, also_print_to_console)
+        self.log(self.LOG_PRIORITY_DEBUG, msg, also_print_to_console)
