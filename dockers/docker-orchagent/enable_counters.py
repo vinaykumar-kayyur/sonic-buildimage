cfb5cca3203deab1920a764fcea99a150ecb8542
Mykola Faryma <mykolaf@mellanox.com>
2020-07-27 17:56:21 +0300

    [enable counters] provide initial rates parameters
    Signed-off-by: Mykola Faryma <mykolaf@mellanox.com>

--- dockers/docker-orchagent/enable_counters.py
+++ dockers/docker-orchagent/enable_counters.py
@@ -3,10 +3,23 @@
 import swsssdk
 import time
 
+DEFAULT_SMOOTH_INTERVAL = '10'
+DEFAULT_ALPHA = '0.18'
+
 def enable_counter_group(db, name):
     info = {}
     info['FLEX_COUNTER_STATUS'] = 'enable'
     db.mod_entry("FLEX_COUNTER_TABLE", name, info)
+
+def enable_rates():
+    # set the default interval for rates (N) as 10s, alpha is 2/(N+1)
+    counters_db = swsssdk.SonicV2Connector()
+    counters_db.connect('COUNTERS_DB')
+    counters_db.set('COUNTERS_DB', 'RATES:PORT', 'PORT_SMOOTH_INTERVAL', DEFAULT_SMOOTH_INTERVAL)
+    counters_db.set('COUNTERS_DB', 'RATES:PORT', 'PORT_ALPHA', DEFAULT_ALPHA)
+    counters_db.set('COUNTERS_DB', 'RATES:RIF', 'RIF_SMOOTH_INTERVAL', DEFAULT_SMOOTH_INTERVAL)
+    counters_db.set('COUNTERS_DB', 'RATES:RIF', 'RIF_ALPHA', DEFAULT_ALPHA)
+
 
 def enable_counters():
     db = swsssdk.ConfigDBConnector()
@@ -18,6 +31,7 @@ def enable_counters():
     enable_counter_group(db, 'PG_WATERMARK')
     enable_counter_group(db, 'QUEUE_WATERMARK')
     enable_counter_group(db, 'BUFFER_POOL_WATERMARK')
+    enable_rates()
 
 def get_uptime():
     with open('/proc/uptime') as fp:
@@ -36,3 +50,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
