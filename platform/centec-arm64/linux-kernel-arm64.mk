0d94c63b9026cda5326be9fa5d6a0a99b64e1ac7
shil <shil@centecnetworks.com>
2020-08-03 07:56:40 +0000

    centec arm64 support compile tsingma e530 dtb

--- platform/centec-arm64/linux-kernel-arm64.mk
+++
@@ -1,5 +0,0 @@
-# linux kernel package for centec arm64
-
-LINUX_KERNEL_DTB = linux-dtb_1.0.0-1_arm64.deb
-$(LINUX_KERNEL_DTB)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/kernel/$(LINUX_KERNEL_DTB)
-SONIC_ONLINE_DEBS += $(LINUX_KERNEL_DTB)
