866c0d352cc7b294042e26693ff38e0f6098985a
junchao <junchao@mellanox.com>
2020-08-04 09:22:16 +0800

    Fix LGTM alert

--- src/system-health/health_checker/utils.py
+++ src/system-health/health_checker/utils.py
@@ -5,7 +5,7 @@ def run_command(command):
     try:
         process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
         return process.communicate()[0].encode('utf-8')
-    except:
+    except Exception:
         return None
 
 
