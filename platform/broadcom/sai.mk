cf3f6eefe1abdbd89b4e43066c6c8aac42a742d4
Gen-Hwa Chiang <gechiang@microsoft.com>
2020-08-05 01:24:10 +0000

    Moving BRCM SAI from 3.7.5.1-1 to 3.7.5.1-2 to pick up the stubbed SAI API that were present in the header but missing in SAI implementation that cause link error during build

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_3.7.5.1-3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/master/libsaibcm_3.7.5.1-3_amd64.deb?sv=2015-04-05&sr=b&sig=jEHMQEFOVpIxsCh0GUuHXY5EB%2BNAygbLpAxz9Is6jIg%3D&se=2034-04-13T22%3A29%3A13Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_3.7.5.1-3_amd64.deb
+BRCM_SAI = libsaibcm_3.7.5.1-2_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/master/libsaibcm_3.7.5.1-2_amd64.deb?sv=2015-04-05&sr=b&sig=Ir7VDgFPOs4mU0k%2Fu76shUBpysh46Q1Btt0i%2FdhM0Ig%3D&se=2034-04-14T01%3A19%3A11Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_3.7.5.1-2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/master/libsaibcm-dev_3.7.5.1-3_amd64.deb?sv=2015-04-05&sr=b&sig=c%2BAd3wMESlwFqRMy%2F1X3kPApuxA62JeX0n1ItWY%2BQgY%3D&se=2034-04-13T22%3A30%3A12Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/master/libsaibcm-dev_3.7.5.1-2_amd64.deb?sv=2015-04-05&sr=b&sig=sBiXxx2vncp0GLHIPx4IyiJQBqFV%2BGWUmdr7ccrnsT4%3D&se=2034-04-14T01%3A18%3A39Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
