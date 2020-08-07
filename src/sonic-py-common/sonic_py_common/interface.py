11ca58df4cee8e4b2177f91c8a45c1542b949b48
Judy Joseph <judy.joseph@microsoft.com>
2020-08-06 17:14:05 -0700

    Updates to the function names.

--- src/sonic-py-common/sonic_py_common/interface.py
+++ src/sonic-py-common/sonic_py_common/interface.py
@@ -12,31 +12,31 @@ SONIC_INTERFACE_PREFIXES = {
   "Ethernet-Backplane":"Ethernet-BP"
 }
 
-def fp_interface_prefix():
+def front_panel_prefix():
     """
     Retrieves the SONIC front panel interface name prefix.
     """
     return SONIC_INTERFACE_PREFIXES["Ethernet-FrontPanel"]
 
-def bp_interface_prefix():
+def backplane_prefix():
     """
     Retrieves the SONIC backplane interface name prefix.
     """
     return SONIC_INTERFACE_PREFIXES["Ethernet-Backplane"]
 
-def portchannel_interface_prefix():
+def portchannel_prefix():
     """
     Retrieves the SONIC PortChannel interface name prefix.
     """
     return SONIC_INTERFACE_PREFIXES["PortChannel"]
 
-def vlan_interface_prefix():
+def vlan_prefix():
     """
     Retrieves the SONIC Vlan interface name prefix.
     """
     return SONIC_INTERFACE_PREFIXES["Vlan"]
 
-def lo_interface_prefix():
+def loopback_prefix():
     """
     Retrieves the SONIC Loopback interface name prefix.
     """
