22e6a23e7603b5a9318a2a4c4209a10226b5ccb7
Joe LeVeque <jolevequ@microsoft.com>
2020-08-11 08:34:34 +0000

    [build] Build sonic-ledd as a Python Wheel package; Also build Python 3 package

--- rules/sonic-ledd.mk
+++ rules/sonic-ledd.mk
@@ -1,6 +1,7 @@
 # sonic-ledd (SONiC Front-panel LED control daemon) Debian package
 
-SONIC_LEDD = python-sonic-ledd_1.1-1_all.deb
+SONIC_LEDD = sonic_ledd-1.1-py3-none-any.whl
 $(SONIC_LEDD)_SRC_PATH = $(SRC_PATH)/sonic-platform-daemons/sonic-ledd
-$(SONIC_LEDD)_WHEEL_DEPENDS = $(SONIC_PY_COMMON_PY2)
-SONIC_PYTHON_STDEB_DEBS += $(SONIC_LEDD)
+$(SONIC_LEDD)_DEPENDS = $(SONIC_PY_COMMON_PY3)
+$(SONIC_LEDD)_PYTHON_VERSION = 3
+SONIC_PYTHON_WHEELS += $(SONIC_LEDD)
