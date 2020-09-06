b6518b428a45d34769218a2b7681294b393879ef
Judy Joseph <judy.joseph@microsoft.com>
2020-09-06 12:36:05 -0700

    Brodcom SAI 3.7.5.2

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_3.7.5.1-3_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.5.1-3_amd64.deb?sv=2015-04-05&sr=b&sig=jKyO230pW7whAMsTPZeUvcCjfE7sFin5JKzdvKswgKQ%3D&se=2034-04-19T15%3A59%3A16Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_3.7.5.1-3_amd64.deb
+BRCM_SAI = libsaibcm_3.7.5.2_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm_3.7.5.2_amd64.deb?sv=2015-04-05&sr=b&sig=QKJ2MvpYP%2BFPmJZXaNZ4M%2B1Bmk5qVTENoJsmnJgcvmo%3D&se=2034-05-16T19%3A17%3A32Z&sp=r"
+BRCM_SAI_DEV = libsaibcm-dev_3.7.5.2_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.5.1-3_amd64.deb?sv=2015-04-05&sr=b&sig=eqVrbb2kbr%2Bz4B8OeyJ2mchjOL70Og9W0demES3uCF0%3D&se=2034-04-19T16%3A00%3A02Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.7/libsaibcm-dev_3.7.5.2_amd64.deb?sv=2015-04-05&sr=b&sig=%2BKKzHd1MNBSCewOZZjQcuAdjkFoR3Ppzl5gXMNGSTWM%3D&se=2034-05-16T19%3A17%3A08Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
