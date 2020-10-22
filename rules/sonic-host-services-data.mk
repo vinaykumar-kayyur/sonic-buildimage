fe17d096e1b47f70cd79526f2b60a3df472ba1ee
Joe LeVeque <jolevequ@microsoft.com>
2020-10-22 01:28:39 +0000

    Delete trailing whitespace

--- rules/sonic-host-services-data.mk
+++ rules/sonic-host-services-data.mk
@@ -1,4 +1,4 @@
-# SONiC host services data package 
+# SONiC host services data package
 
 SONIC_HOST_SERVICES_DATA = sonic-host-services-data_1.0-1_all.deb
 $(SONIC_HOST_SERVICES_DATA)_SRC_PATH = $(SRC_PATH)/sonic-host-services-data
