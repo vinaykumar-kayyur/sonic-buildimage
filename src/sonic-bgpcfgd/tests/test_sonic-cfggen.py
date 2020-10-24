7b8e67ef2964764c374b1e4c6d06ea6ab19d2acb
Pavel Shirshov <pavel.contrib@gmail.com>
2020-10-24 00:16:39 -0700

    Fix sonic-cfggen tests

--- src/sonic-bgpcfgd/tests/test_sonic-cfggen.py
+++ src/sonic-bgpcfgd/tests/test_sonic-cfggen.py
@@ -15,11 +15,11 @@ def run_test(name, template_path, json_path, match_path):
     template_path = os.path.join(TEMPLATE_PATH, template_path)
     json_path = os.path.join(DATA_PATH, json_path)
     cfggen = os.path.abspath("../sonic-config-engine/sonic-cfggen")
-    command = [cfggen, "-T", TEMPLATE_PATH, "-t", template_path, "-y", json_path]
+    command = ["python2", cfggen, "-T", TEMPLATE_PATH, "-t", template_path, "-y", json_path]
     p = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = p.communicate()
     assert p.returncode == 0, "sonic-cfggen for %s test returned %d code. stderr='%s'" % (name, p.returncode, stderr)
-    raw_generated_result = stdout
+    raw_generated_result = stdout.decode("ascii")
     assert "None" not in raw_generated_result, "Test %s" % name
     canonical_generated_result = ConfigMgr.to_canonical(raw_generated_result)
     match_path = os.path.join(DATA_PATH, match_path)
