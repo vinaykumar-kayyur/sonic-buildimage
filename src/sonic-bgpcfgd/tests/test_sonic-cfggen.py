c320f3daa26830e5d09dd8293c8cb04b9cac7bf1
Pavel Shirshov <pavel.contrib@gmail.com>
2020-10-25 01:38:29 -0700

    Try another attempt to run python2 code from python3 script

--- src/sonic-bgpcfgd/tests/test_sonic-cfggen.py
+++ src/sonic-bgpcfgd/tests/test_sonic-cfggen.py
@@ -16,7 +16,7 @@ def run_test(name, template_path, json_path, match_path):
     json_path = os.path.join(DATA_PATH, json_path)
     cfggen = os.path.abspath("../sonic-config-engine/sonic-cfggen")
     command = ['/usr/bin/python2.7', cfggen, "-T", TEMPLATE_PATH, "-t", template_path, "-y", json_path]
-    p = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env={"PYTHONPATH": "."})
     stdout, stderr = p.communicate()
     assert p.returncode == 0, "sonic-cfggen for %s test returned %d code. stderr='%s'" % (name, p.returncode, stderr)
     raw_generated_result = stdout.decode("ascii")
