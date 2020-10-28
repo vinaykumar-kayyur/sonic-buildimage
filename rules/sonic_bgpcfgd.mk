60fcc440de8086a5e4fd11af360624a00b56a204
Pavel Shirshov <pavel.contrib@gmail.com>
2020-10-28 12:39:06 -0700

    Install python-swsscommon3

--- rules/sonic_bgpcfgd.mk
+++ rules/sonic_bgpcfgd.mk
@@ -6,6 +6,6 @@ $(SONIC_BGPCFGD)_SRC_PATH = $(SRC_PATH)/sonic-bgpcfgd
 # of sonic-config-engine and bgpcfgd explicitly calls sonic-cfggen
 # as part of its unit tests.
 # TODO: Refactor unit tests so that these dependencies are not needed
-$(SONIC_BGPCFGD)_DEPENDS += $(SONIC_PY_COMMON_PY2)
+$(SONIC_BGPCFGD)_DEPENDS += $(SONIC_PY_COMMON_PY2) $(PYTHON3_SWSSCOMMON)
 $(SONIC_BGPCFGD)_PYTHON_VERSION = 3
 SONIC_PYTHON_WHEELS += $(SONIC_BGPCFGD)
