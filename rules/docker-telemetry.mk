e1bad8bb30341e0873d110b2ba5f966757004e31
Eric Seifert <seiferteric@gmail.com>
2020-11-05 14:14:40 -0800

    Mount /etc to /host_etc and symlink passwd and group files to host for group lookups for authorization
    Signed-off-by: Eric Seifert <seiferteric@gmail.com>

--- rules/docker-telemetry.mk
+++ rules/docker-telemetry.mk
@@ -27,6 +27,7 @@ $(DOCKER_TELEMETRY)_CONTAINER_NAME = telemetry
 $(DOCKER_TELEMETRY)_RUN_OPT += --privileged -t
 $(DOCKER_TELEMETRY)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
 $(DOCKER_TELEMETRY)_RUN_OPT += -v /var/run/dbus:/var/run/dbus:rw
+$(DOCKER_TELEMETRY)_RUN_OPT += -v /etc:/host_etc:ro
 
 $(DOCKER_TELEMETRY)_FILES += $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)
 $(DOCKER_TELEMETRY)_BASE_IMAGE_FILES += monit_telemetry:/etc/monit/conf.d
