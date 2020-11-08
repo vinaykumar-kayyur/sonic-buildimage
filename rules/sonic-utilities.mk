52fbe9542edf74c7bc4630e4a19955be7571c896
Joe LeVeque <jolevequ@microsoft.com>
2020-11-08 01:09:28 +0000

    Fix typo

--- rules/sonic-utilities.mk
+++ rules/sonic-utilities.mk
@@ -21,6 +21,6 @@ $(SONIC_UTILITIES_PY2)_DEBS_DEPENDS = $(LIBYANG) \
                                       $(LIBYANG_PY2) \
                                       $(LIBYANG_PY3) \
                                       $(LIBSWSSCOMMON) \
-                                      $(PYTHON2_SWSSCOMMON) \
+                                      $(PYTHON_SWSSCOMMON) \
                                       $(PYTHON3_SWSSCOMMON)
 SONIC_PYTHON_WHEELS += $(SONIC_UTILITIES_PY2)
