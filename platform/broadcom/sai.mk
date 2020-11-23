40481bc4686c0e8a21e4d15ea98e239b157e81f3
Ubuntu <vineetmittal@vineetmittal-vm-01.lytjgi3u21sebb4m2cgpzrwfde.cx.internal.cloudapp.net>
2020-11-23 23:38:42 +0000

    syncd buster related changes.

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,8 +1,8 @@
-BRCM_SAI = libsaibcm_4.2.1.5-4_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/libsaibcm_4.2.1.5-4_amd64.deb?sv=2015-04-05&sr=b&sig=dHfQf95FxgLBlnrGJwv6pIkAYAc6mOGbz9vvUMfxFvg%3D&se=2034-07-28T05%3A53%3A21Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.2.1.5-4_amd64.deb
+BRCM_SAI = libsaibcm_4.2.1.5_amd64-buster.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/libsaibcm_4.2.1.5_amd64-buster.deb?sv=2019-12-12&st=2020-11-24T21%3A45%3A37Z&se=2021-11-25T21%3A45%3A00Z&sr=b&sp=r&sig=6we05VowbABC4VhxUOQlZdnfohRgqpOMUxYdL9O4sX0%3D"
+BRCM_SAI_DEV = libsaibcm-dev_4.2.1.5_amd64-buster.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/libsaibcm-dev_4.2.1.5-4_amd64.deb?sv=2015-04-05&sr=b&sig=n98j842RZy%2BwQh4CtOku7jz6wgV%2BusbTl9NkeRndBrE%3D&se=2037-01-13T05%3A54%3A34Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/libsaibcm-dev_4.2.1.5_amd64-buster.deb?sv=2019-12-12&st=2020-11-24T21%3A46%3A53Z&se=2021-11-25T21%3A46%3A00Z&sr=b&sp=r&sig=efXrwkOa1EVVecQCYslN%2BMTXhxWj4RuwKm06Uh%2B5M3I%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
