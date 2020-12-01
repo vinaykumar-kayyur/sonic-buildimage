4c09bd6c2c848402da5e32de9993821e6ae35719
Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>
2020-11-30 22:09:42 -0800

    Install systemd-coredump in SONiC to be used as the core dump utility
    Signed-off-by: Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>

--- files/scripts/core_cleanup.py
+++ files/scripts/core_cleanup.py
@@ -1,43 +1,80 @@
-#!/usr/bin/env python3
-
-import os
-from collections import defaultdict
-from datetime import datetime
-
+#!/usr/bin/python3
+import argparse
+from argparse import RawTextHelpFormatter
+from stat import ST_CTIME
+import os, time
+from datetime import datetime, timedelta
 from sonic_py_common.logger import Logger
+import sys
 
+CORE_FILE_DIR=r'/var/core'
+DEFAULT_INTERVAL=0
+DEFAULT_MAX_DUMPS=4
 SYSLOG_IDENTIFIER = 'core_cleanup.py'
-CORE_FILE_DIR = '/var/core/'
-MAX_CORE_FILES = 4
+
+def exe_name(filename):
+    try:
+        return filename.split('.')[1]
+    except:
+        return filename
+
+def corefile_cleanup(input_dir, max_dumps, logger):
+    core_files = (os.path.join(input_dir, fn) for fn in os.listdir(input_dir))
+    core_file_info = ((os.stat(path), path) for path in core_files)
+    core_file_detailed_info = ((stat[ST_CTIME], path) for stat, path in core_file_info)
+    # Groups core files based on the executable program name to identify duplicate core files
+    exe_groups = dict()
+    for cdate, path in core_file_detailed_info:
+        if exe_groups.get(exe_name(os.path.basename(path))) is None:
+            exe_groups[exe_name(os.path.basename(path))] = [(cdate, path)]
+        else:
+            exe_groups.get(exe_name(os.path.basename(path))).append((cdate, path))
+
+    for exe, core_files in exe_groups.items():
+        if len(core_files) > 1:
+            idx = 0
+            last_idx = len(core_files) - 1
+            for cdate, path in sorted(core_files, reverse=True):
+                if idx >= (max_dumps - 1) and idx != last_idx:
+                    logger.log_info('Deleting the core file {}'.format(path))
+                    try:
+                        os.remove(path)
+                    except FileNotFoundError:
+                        pass
+                    except Exception as e:
+                        logger.log_error('Exception [{}] while deleting the core file {}'.format(e, path))
+                idx += 1
 
 def main():
     logger = Logger(SYSLOG_IDENTIFIER)
     logger.set_min_log_priority_info()
 
-    if os.getuid() != 0:
-        logger.log_error('Root required to clean up core files')
-        return
+    # Add allowed arguments
+    parser = argparse.ArgumentParser(description="Core file garbage collector:\n\n"
+                                                 "Duplicate core files generated for the same exectable program are deleted.\n"
+                                                 "Only the oldest and n-1 latest duplicate core files are saved, rest of them are deleted.",
+                                     formatter_class=RawTextHelpFormatter)
+
+    # Directory to clean-up
+    parser.add_argument("-d", "--input-dir",
+                        metavar='DIR', default=CORE_FILE_DIR,
+                        help="core files directory to cleanup")
+
+    # Maximum number of duplicate core files stored
+    parser.add_argument("-n", "--max-dumps", type=int,
+                        default=DEFAULT_MAX_DUMPS, choices=range(1, 11),
+                        help='maximum number of core files saved for a given executable program. Default: 4')
+
+    # Parse command arguments
+    options = parser.parse_args()
+
+    if not os.path.isdir(options.input_dir):
+        logger.log_error("Error! Directory {} does not exist".format(options.input_dir))
+        sys.exit(1)
 
     logger.log_info('Cleaning up core files')
-    core_files = [f for f in os.listdir(CORE_FILE_DIR) if os.path.isfile(os.path.join(CORE_FILE_DIR, f))]
-
-    core_files_by_process = defaultdict(list)
-    for f in core_files:
-        process = f.split('.')[0]
-        curr_files = core_files_by_process[process]
-        curr_files.append(f)
-
-        if len(curr_files) > MAX_CORE_FILES:
-            curr_files.sort(reverse = True, key = lambda x: datetime.utcfromtimestamp(int(x.split('.')[1])))
-            oldest_core = curr_files[MAX_CORE_FILES]
-            logger.log_info('Deleting {}'.format(oldest_core))
-            try:
-                os.remove(os.path.join(CORE_FILE_DIR, oldest_core))
-            except:
-                logger.log_error('Unexpected error occured trying to delete {}'.format(oldest_core))
-            core_files_by_process[process] = curr_files[0:MAX_CORE_FILES]
-
+    corefile_cleanup(options.input_dir, options.max_dumps, logger)
     logger.log_info('Finished cleaning up core files')
 
-if __name__ == '__main__':
+if __name__== "__main__":
     main()
