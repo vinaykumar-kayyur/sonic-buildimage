f09e695d653eab0b3100db9ed03f11fb82b6455a
Stephen Sun <stephens@nvidia.com>
2020-12-08 10:26:28 +0800

    buffermgrd: provide the peripheral_table.json only if it exists
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- dockers/docker-orchagent/buffermgrd.sh
+++ dockers/docker-orchagent/buffermgrd.sh
@@ -3,7 +3,11 @@
 BUFFER_CALCULATION_MODE=$(redis-cli -n 4 hget "DEVICE_METADATA|localhost" buffer_model)
 
 if [ "$BUFFER_CALCULATION_MODE" == "dynamic" ]; then
-    BUFFERMGRD_ARGS="-a /etc/sonic/asic_table.json -p /etc/sonic/peripheral_table.json"
+    if [ -f /etc/sonic/peripheral_table.json ]; then
+        BUFFERMGRD_ARGS="-a /etc/sonic/asic_table.json -p /etc/sonic/peripheral_table.json"
+    else
+        BUFFERMGRD_ARGS="-a /etc/sonic/asic_table.json"
+    fi
 else
     # Should we use the fallback MAC in case it is not found in Device.Metadata
     BUFFERMGRD_ARGS="-l /usr/share/sonic/hwsku/pg_profile_lookup.ini"
--- platform/vs/docker-sonic-vs/buffermgrd.sh
+++ platform/vs/docker-sonic-vs/buffermgrd.sh
@@ -4,7 +4,7 @@ BUFFER_CALCULATION_MODE=$(redis-cli -n 4 hget "DEVICE_METADATA|localhost" buffer
 export ASIC_VENDOR=vs
 
 if [ "$BUFFER_CALCULATION_MODE" == "dynamic" ]; then
-    BUFFERMGRD_ARGS="-a /etc/sonic/asic_table.json -p /etc/sonic/peripheral_table.json"
+    BUFFERMGRD_ARGS="-a /etc/sonic/asic_table.json"
 else
     # Should we use the fallback MAC in case it is not found in Device.Metadata
     BUFFERMGRD_ARGS="-l /usr/share/sonic/hwsku/pg_profile_lookup.ini"
