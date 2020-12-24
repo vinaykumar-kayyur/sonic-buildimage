805e3233cfebdd46bfa02100e16ff4bf10613f9a
Jostar Yang <jostar_yang@accton.com.tw>
2020-12-24 18:25:30 +0800

    [as7312-54x] Set fan direction to follow rule

--- device/accton/x86_64-accton_as7312_54x-r0/sonic_platform/fan.py
+++ device/accton/x86_64-accton_as7312_54x-r0/sonic_platform/fan.py
@@ -61,17 +61,17 @@ class Fan(FanBase):
         if not self.is_psu_fan:
             dir_str = "{}{}{}".format(CPLD_I2C_PATH, self.fan_tray_index, '_direction')
             val=self._api_helper.read_txt_file(dir_str)
-            if val==0:
-                direction=self.FAN_DIRECTION_EXHAUST
-            else: 
+            if val==0: #B2F
                 direction=self.FAN_DIRECTION_INTAKE
+            else: #F2B
+                direction=self.FAN_DIRECTION_EXHAUST
         else: #For PSU
             dir_str = "{}{}".format(self.psu_hwmon_path,'psu_hwmon_path')
             val=self._api_helper.read_txt_file(dir_str)
             if val=='F2B':
-                direction=self.FAN_DIRECTION_INTAKE
-            else:
                 direction=self.FAN_DIRECTION_EXHAUST
+            else:
+                direction=self.FAN_DIRECTION_INTAKE
 
         return direction
 
