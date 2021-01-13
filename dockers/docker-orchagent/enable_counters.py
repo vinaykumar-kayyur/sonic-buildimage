e9de3b60701ddecb65ddfc6716ca5c961639ae24
Andriy Yurkiv <ayurkiv@nvidia.com>
2021-01-14 01:52:37 +0200

    Enable SAI_INGRESS_PRIORITY_GROUP_STAT_DROPPED_PACKETS counter by default
    Signed-off-by: Andriy Yurkiv <ayurkiv@nvidia.com>

--- dockers/docker-orchagent/enable_counters.py
+++ dockers/docker-orchagent/enable_counters.py
@@ -19,6 +19,7 @@ def enable_counters():
     enable_counter_group(db, 'QUEUE_WATERMARK')
     enable_counter_group(db, 'BUFFER_POOL_WATERMARK')
     enable_counter_group(db, 'PORT_BUFFER_DROP')
+    enable_counter_group(db, 'PG_STAT_DROP')
 
 def get_uptime():
     with open('/proc/uptime') as fp:
