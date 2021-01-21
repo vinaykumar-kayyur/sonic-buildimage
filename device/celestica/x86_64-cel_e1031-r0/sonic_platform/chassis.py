bb5edc66dacd560224a50da9bce4fb70310fbb44
Wirut Getbamrung <wgetbum@icloud.com>
2021-01-21 20:33:31 +0700

    [device/celestica-e1031]: add thermalctld api support

--- device/celestica/x86_64-cel_e1031-r0/sonic_platform/chassis.py
+++ device/celestica/x86_64-cel_e1031-r0/sonic_platform/chassis.py
@@ -20,6 +20,7 @@ try:
     from sonic_platform.thermal import Thermal
     from sonic_platform.sfp import Sfp
     from sonic_platform.eeprom import Tlv
+    from common import Common
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
@@ -40,41 +41,63 @@ class Chassis(ChassisBase):
 
     def __init__(self):
         ChassisBase.__init__(self)
-        self.config_data = {}
+        self._api_common = Common()
+        self.sfp_module_initialized = False
+        self.__initialize_eeprom()
+        self.is_host = self._api_common.is_host()
+
+        if not self.is_host:
+            self.__initialize_fan()
+            self.__initialize_psu()
+            self.__initialize_thermals()
+        else:
+            self.__initialize_components()
+
+        self._reboot_cause_path = HOST_REBOOT_CAUSE_PATH if self._api_common.is_host(
+        ) else PMON_REBOOT_CAUSE_PATH
+
+    def __initialize_sfp(self):
+        from sonic_platform.sfp import Sfp
+        for index in range(0, NUM_SFP):
+            sfp = Sfp(index)
+            self._sfp_list.append(sfp)
+        self.sfp_module_initialized = True
+
+    def __initialize_psu(self):
+        from sonic_platform.psu import Psu
+        for index in range(0, NUM_PSU):
+            psu = Psu(index)
+            self._psu_list.append(psu)
+
+    def __initialize_fan(self):
+        from sonic_platform.fan import Fan
         for fant_index in range(0, NUM_FAN_TRAY):
             for fan_index in range(0, NUM_FAN):
                 fan = Fan(fant_index, fan_index)
                 self._fan_list.append(fan)
-        for index in range(0, NUM_PSU):
-            psu = Psu(index)
-            self._psu_list.append(psu)
+
+    def __initialize_thermals(self):
+        from sonic_platform.thermal import Thermal
+        airflow = self.__get_air_flow()
         for index in range(0, NUM_THERMAL):
-            thermal = Thermal(index)
+            thermal = Thermal(index, airflow)
             self._thermal_list.append(thermal)
-        # sfp index start from 1
-        self._sfp_list.append(None)
-        for index in range(1, NUM_SFP+1):
-            sfp = Sfp(index)
-            self._sfp_list.append(sfp)
+
+    def __initialize_eeprom(self):
+        from sonic_platform.eeprom import Tlv
+        self._eeprom = Tlv()
+
+    def __initialize_components(self):
+        from sonic_platform.component import Component
         for index in range(0, NUM_COMPONENT):
             component = Component(index)
             self._component_list.append(component)
-        self._reboot_cause_path = HOST_REBOOT_CAUSE_PATH if self.__is_host(
-        ) else PMON_REBOOT_CAUSE_PATH
 
-        self._eeprom = Tlv()
-
-    def __is_host(self):
-        return os.system(HOST_CHK_CMD) == 0
-
-    def __read_txt_file(self, file_path):
-        try:
-            with open(file_path, 'r') as fd:
-                data = fd.read()
-                return data.strip()
-        except IOError:
-            pass
-        return None
+    def __get_air_flow(self):
+        air_flow_path = '/usr/share/sonic/device/{}/fan_airflow'.format(
+            self._api_common.platform) if self.is_host else '/usr/share/sonic/platform/fan_airflow'
+        air_flow = self._api_common.read_txt_file(air_flow_path)
+        return air_flow or 'B2F'
 
     def get_base_mac(self):
         """
@@ -85,7 +108,7 @@ class Chassis(ChassisBase):
         """
         return self._eeprom.get_mac()
 
-    def get_serial(self):
+    def get_serial_number(self):
         """
         Retrieves the hardware serial number for the chassis
         Returns:
@@ -116,8 +139,9 @@ class Chassis(ChassisBase):
         """
         description = 'None'
         reboot_cause = self.REBOOT_CAUSE_HARDWARE_OTHER
-        hw_reboot_cause = self._component_list[0].get_register_value(RESET_REGISTER)
-        sw_reboot_cause = self.__read_txt_file(
+        hw_reboot_cause = self._component_list[0].get_register_value(
+            RESET_REGISTER)
+        sw_reboot_cause = self._api_common.read_txt_file(
             self._reboot_cause_path) or "Unknown"
 
         if hw_reboot_cause == "0x55":
@@ -127,8 +151,12 @@ class Chassis(ChassisBase):
             reboot_cause = self.REBOOT_CAUSE_POWER_LOSS
         elif hw_reboot_cause == "0x33":
             reboot_cause = self.REBOOT_CAUSE_WATCHDOG
+        elif hw_reboot_cause == "0x88":
+            reboot_cause = self.REBOOT_CAUSE_THERMAL_OVERLOAD_CPU
+        elif hw_reboot_cause == "0x99":
+            reboot_cause = self.REBOOT_CAUSE_THERMAL_OVERLOAD_ASIC
         else:
-            reboot_cause = self.REBOOT_CAUSE_HARDWARE_OTHER
+            reboot_cause = self.REBOOT_CAUSE_NON_HARDWARE
             description = 'Unknown reason'
 
         return (reboot_cause, description)
@@ -145,3 +173,101 @@ class Chassis(ChassisBase):
             self._watchdog = Watchdog()
 
         return self._watchdog
+
+    ##############################################################
+    ######################## SFP methods #########################
+    ##############################################################
+
+    def get_num_sfps(self):
+        """
+        Retrieves the number of sfps available on this chassis
+        Returns:
+            An integer, the number of sfps available on this chassis
+        """
+        if not self.sfp_module_initialized:
+            self.__initialize_sfp()
+
+        return len(self._sfp_list)
+
+    def get_all_sfps(self):
+        """
+        Retrieves all sfps available on this chassis
+        Returns:
+            A list of objects derived from SfpBase representing all sfps
+            available on this chassis
+        """
+        if not self.sfp_module_initialized:
+            self.__initialize_sfp()
+
+        return self._sfp_list
+
+    def get_sfp(self, index):
+        """
+        Retrieves sfp represented by (1-based) index <index>
+        Args:
+            index: An integer, the index (1-based) of the sfp to retrieve.
+            The index should be the sequence of a physical port in a chassis,
+            starting from 1.
+            For example, 1 for Ethernet0, 2 for Ethernet4 and so on.
+        Returns:
+            An object dervied from SfpBase representing the specified sfp
+        """
+        sfp = None
+        if not self.sfp_module_initialized:
+            self.__initialize_sfp()
+
+        try:
+            # The index will start from 1
+            sfp = self._sfp_list[index-1]
+        except IndexError:
+            sys.stderr.write("SFP index {} out of range (1-{})\n".format(
+                             index, len(self._sfp_list)))
+        return sfp
+
+    ##############################################################
+    ###################### Device methods ########################
+    ##############################################################
+
+    def get_name(self):
+        """
+        Retrieves the name of the device
+            Returns:
+            string: The name of the device
+        """
+        return self._api_common.hwsku
+
+    def get_presence(self):
+        """
+        Retrieves the presence of the PSU
+        Returns:
+            bool: True if PSU is present, False if not
+        """
+        return True
+
+    def get_model(self):
+        """
+        Retrieves the model number (or part number) of the device
+        Returns:
+            string: Model/part number of device
+        """
+        return self._eeprom.get_serial()
+
+    def get_serial(self):
+        """
+        Retrieves the serial number of the device
+        Returns:
+            string: Serial number of device
+        """
+        return self.get_serial_number()
+
+    def get_status(self):
+        """
+        Retrieves the operational status of the device
+        Returns:
+            A boolean value, True if device is operating properly, False if not
+        """
+        return True
+
+        def get_thermal_manager(self):
+            from .thermal_manager import ThermalManager
+            return ThermalManager
