ccc60bbb975300dffbfe768a7bc22e12e90287ba
Mahesh Maddikayala <samaddik@microsoft.com>
2021-01-23 02:24:16 +0000

    fix the libsaibcm version

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,6 +1,6 @@
-BRCM_SAI = libsaibcm_4.2.1.5-10_amd64.deb
+BRCM_SAI = libsaibcm_4.3.1.10-1_amd64.deb
 $(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_4.3.0.10_amd64.deb?sv=2015-04-05&sr=b&sig=yKic6%2FwJ2mHM6n4lXrR32yqEsLt01EUzqxIuOAvV7uQ%3D&se=2022-01-21T23%3A13%3A54Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_4.2.1.5-10_amd64.deb
+BRCM_SAI_DEV = libsaibcm-dev_4.3.1.10-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 $(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.0.10_amd64.deb?sv=2015-04-05&sr=b&sig=IAx7bmd0mgQL8DyF345jQOQHRxwx9wen2AyWdacfmw4%3D&se=2022-01-21T23%3A15%3A30Z&sp=r"
 
