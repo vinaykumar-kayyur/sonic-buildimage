9cd562b5b7795f32b800025f8bf69c1a02c3deba
Tamer Ahmed <taahme@microsoft.com>
2021-01-26 01:43:10 +0000

    Merged PR 4021269: [BCM SAI] update BCM SAI to 4.2.2.4 With SAI v1.7.1
    [BCM SAI] update BCM SAI to 4.2.2.4 With SAI v1.7.1
    
    signed-off-by: Tamer Ahmed <tamer.ahmed@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -4,9 +4,9 @@ BRCM_SAI_DEV = libsaibcm-dev_4.3.3.4_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 $(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.4_amd64.deb?sv=2015-04-05&sr=b&sig=im7%2Fqp2dnoQTaPhY3Jv%2BHV0eZ4mPu27BT%2FbcF%2BQxGyQ%3D&se=2034-12-10T05%3A40%3A26Z&sp=r"
 
-BRCM_SAI_DBG = libsaibcm-dbg_3.5.2.3_amd64.deb
+BRCM_SAI_DBG = libsaibcm-dbg_4.2.2.4_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DBG)))
-$(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/libsaibcm-dbg_3.5.2.3_amd64.deb?sv=2015-04-05&sr=b&sig=02XfkVdPH4RF85%2B0LG%2BFA5g22tUgEXKIlF2IpZLflIk%3D&se=2033-04-16T16%3A32%3A54Z&sp=r"
+$(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/buster/libsaibcm-dbg_4.2.2.4_amd64.deb?sv=2015-04-05&sr=b&sig=6NLCDzW3WYs5Yui4grSEFxNbY9XoK7Dv3I4xRv1w4AY%3D&se=2048-06-11T17%3A24%3A27Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
