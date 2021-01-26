102020fb90cd980ec8ac48a130ec1744d170102a
Aravind Mani <aravind_m1@dell.com>
2021-01-26 19:18:11 +0530

    Address comments

--- src/system-health/health_checker/hardware_checker.py
+++ src/system-health/health_checker/hardware_checker.py
@@ -11,10 +11,10 @@ class HardwareChecker(HealthChecker):
     import sonic_platform.platform
     ASIC_TEMPERATURE_KEY = []
     thermal_list = sonic_platform.platform.Platform().get_chassis().get_all_thermals()
-    for i in range(0,len(thermal_list)):
-         thermal_name=thermal_list[i].get_name()
-         if ("ASIC" in thermal_name):
-             ASIC_TEMPERATURE_KEY.append('TEMPERATURE_INFO|'+ thermal_name)
+    for i in range(0, len(thermal_list)):
+         thermal_name = thermal_list[i].get_name()
+         if "ASIC" in thermal_name:
+             ASIC_TEMPERATURE_KEY.append('TEMPERATURE_INFO|' + thermal_name)
 
     FAN_TABLE_NAME = 'FAN_INFO'
     PSU_TABLE_NAME = 'PSU_INFO'
@@ -42,32 +42,32 @@ class HardwareChecker(HealthChecker):
         if config.ignore_devices and 'asic' in config.ignore_devices:
             return
 
-        for i in range(0,len(HardwareChecker.ASIC_TEMPERATURE_KEY)):
+        for i in range(0, len(HardwareChecker.ASIC_TEMPERATURE_KEY)):
             temperature = self._db.get(self._db.STATE_DB, HardwareChecker.ASIC_TEMPERATURE_KEY[i],
                                                           'temperature')
             temperature_threshold = self._db.get(self._db.STATE_DB, HardwareChecker.ASIC_TEMPERATURE_KEY[i],
                                                           'high_threshold')
             asic_name = HardwareChecker.ASIC_TEMPERATURE_KEY[i][17:]
             if not temperature:
-                self.set_object_not_ok('ASIC',asic_name,
-                        ('Failed to get {} temperature').format(asic_name))
+                self.set_object_not_ok('ASIC', asic_name,
+                        'Failed to get {} temperature'.format(asic_name))
             elif not temperature_threshold:
-                self.set_object_not_ok('ASIC',asic_name,
-                        ('Failed to get {} temperature threshold').format(asic_name))
+                self.set_object_not_ok('ASIC', asic_name,
+                        'Failed to get {} temperature threshold'.format(asic_name))
             else:
                 try:
                     temperature = float(temperature)
                     temperature_threshold = float(temperature_threshold)
                     if temperature > temperature_threshold:
                         self.set_object_not_ok('ASIC', asic_name,
-                                               ('{} temperature is too hot, temperature={}, threshold={}'.format(
-                                                asic_name,temperature,temperature_threshold)))
+                                               '{} temperature is too hot, temperature={}, threshold={}'.format(
+                                                asic_name,temperature,temperature_threshold))
                     else:
                         self.set_object_ok('ASIC', asic_name)
                 except ValueError as e:
                     self.set_object_not_ok('ASIC', asic_name,
-                                           ('Invalid {} temperature data, temperature={}, threshold={}'.format(
-                                            asic_name,temperature,temperature_threshold)))
+                                           'Invalid {} temperature data, temperature={}, threshold={}'.format(
+                                            asic_name,temperature,temperature_threshold))
 
     def _check_fan_status(self, config):
         """
