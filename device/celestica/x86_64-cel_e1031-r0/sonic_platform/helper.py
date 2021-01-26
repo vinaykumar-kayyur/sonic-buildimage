3ecdfce0db108619a016d317d06f30ef4ae9abae
Wirut Getbamrung <wgetbum@icloud.com>
2021-01-26 15:37:16 +0700

    [device/celestica]: fix lgtm

--- device/celestica/x86_64-cel_e1031-r0/sonic_platform/helper.py
+++ device/celestica/x86_64-cel_e1031-r0/sonic_platform/helper.py
@@ -26,7 +26,7 @@ class APIHelper():
             mm.seek(int(offset))
             read_data_stream = mm.read(4)
             result = struct.unpack('I', read_data_stream)
-        except:
+        except Exception:
             status = False
         return status, result
 
@@ -39,14 +39,14 @@ class APIHelper():
             raw_data, err = p.communicate()
             if err == '':
                 result = raw_data.strip()
-        except:
+        except Exception:
             status = False
         return status, result
 
     def run_interactive_command(self, cmd):
         try:
             os.system(cmd)
-        except:
+        except Exception:
             return False
         return True
 
@@ -93,7 +93,7 @@ class APIHelper():
                 result = raw_data.strip()
             else:
                 status = False
-        except:
+        except Exception:
             status = False
         return status, result
 
@@ -111,7 +111,7 @@ class APIHelper():
                 result = raw_data.strip()
             else:
                 status = False
-        except:
+        except Exception:
             status = False
         return status, result
 
@@ -128,6 +128,6 @@ class APIHelper():
                 result = raw_data.strip()
             else:
                 status = False
-        except:
+        except Exception:
             status = False
         return status, result
