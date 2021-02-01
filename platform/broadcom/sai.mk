aece9628e713c4b576fa84df0e768887f2918a0e
Ying Xie <ying.xie@microsoft.com>
2021-02-01 19:06:44 +0000

    [bcm sai] update SAI 4.3.0.10-3 debug package
    Signed-off-by: Ying Xie <ying.xie@microsoft.com>

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -4,9 +4,9 @@ BRCM_SAI_DEV = libsaibcm-dev_4.3.3.4_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
 $(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dev_4.3.3.4_amd64.deb?sv=2015-04-05&sr=b&sig=im7%2Fqp2dnoQTaPhY3Jv%2BHV0eZ4mPu27BT%2FbcF%2BQxGyQ%3D&se=2034-12-10T05%3A40%3A26Z&sp=r"
 
-BRCM_SAI_DBG = libsaibcm-dbg_4.2.2.4_amd64.deb
+BRCM_SAI_DBG = libsaibcm-dbg_4.3.0.10-3_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DBG)))
-$(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.2/master/buster/libsaibcm-dbg_4.2.2.4_amd64.deb?sv=2015-04-05&sr=b&sig=6NLCDzW3WYs5Yui4grSEFxNbY9XoK7Dv3I4xRv1w4AY%3D&se=2048-06-11T17%3A24%3A27Z&sp=r"
+$(BRCM_SAI_DBG)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dbg_4.3.0.10-3_amd64.deb?sv=2019-10-10&st=2021-02-01T18%3A50%3A19Z&se=2036-02-02T18%3A50%3A00Z&sr=b&sp=r&sig=fpeFJ0AQeViX%2Bq4XZjxryEF0au2JtilumteRsZkXR6Y%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
