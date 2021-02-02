5f33ba942c036822854641e56da8b0cea2bba391
Shilong Liu <shilongliu@microsoft.com>
2021-02-02 14:35:33 +0800

    change directory
    Signed-off-by: Shilong Liu <shilongliu@microsoft.com>

--- slave.mk
+++ slave.mk
@@ -254,6 +254,7 @@ $(info "INCLUDE_NAT"                     : "$(INCLUDE_NAT)")
 $(info "INCLUDE_KUBERNETES"              : "$(INCLUDE_KUBERNETES)")
 $(info "TELEMETRY_WRITABLE"              : "$(TELEMETRY_WRITABLE)")
 $(info "PDDF_SUPPORT"                    : "$(PDDF_SUPPORT)")
+$(info "SONIC_DPKG_CACHE_METHOD"         : "$(SONIC_DPKG_CACHE_METHOD)")
 $(info )
 else
 $(info SONiC Build System for $(CONFIGURED_PLATFORM):$(CONFIGURED_ARCH))
