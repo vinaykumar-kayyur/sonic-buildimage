866af718bea1edecad9f7efa3ee429edc27c0bf4
Jostar Yang <jostar_yang@accton.com.tw>
2021-02-03 11:46:21 +0800

    Move to check eeprom to postins

--- platform/broadcom/sonic-platform-modules-accton/as9716-32d/utils/pddf_post_driver_install.sh
+++ platform/broadcom/sonic-platform-modules-accton/as9716-32d/utils/pddf_post_driver_install.sh
@@ -20,15 +20,4 @@ ir3570_check()
 
 ir3570_check
 
-# Steps to check syseeprom i2c address
-use_57_eeprom=false
-i2cget -y -f 0 0x57 0x0 &>/dev/null
-if [ $? -eq 0 ]; then
-    use_57_eeprom=true
-fi
-echo "The board is using 0x57 address: $use_57_eeprom"
-
-if $use_57_eeprom ; then
-    # syseeprom is at the i2c address 0x57. Change the .json file
-    sed -i 's@"topo_info": {"parent_bus": "0x0", "dev_addr": "0x56", "dev_type": "24c02"},@"topo_info": {"parent_bus": "0x0", "dev_addr": "0x57", "dev_type": "24c02"},@g' ./pddf-device.json
-fi
+echo "AS9716 post PDDF driver install completed"
