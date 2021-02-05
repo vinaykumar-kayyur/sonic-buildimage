88cc4bb61941a33c732c5865e54eea7fb03e44c5
Carl Keene <keene@nokia.com>
2021-02-05 11:11:32 -0500

    [Platform][ixs7215]: Updates and Improvements supporting platform API tests
            1) addition of platform.json supporting platform api tests
            2) addition of pcie.yaml supporting pcied
            3) Addition of Real fan drawer support
    	4) Removal of python2 wheel with support in place for python3
            5) Enumeration fix for PSU supporting eeprom access
            6) supporting changes platform api tests
    
    (cherry picked from commit 89d87571bfc2d596d1f11ac1edad5de384b43b4e)

--- platform/marvell-armhf/sonic-platform-nokia/7215/sonic_platform/sfp.py
+++ platform/marvell-armhf/sonic-platform-nokia/7215/sonic_platform/sfp.py
@@ -917,3 +917,23 @@ class Sfp(SfpBase):
             A boolean value, True if device is operating properly, False if not
         """
         return self.get_presence()
+
+    def is_replaceable(self):
+        """
+        Indicate whether this device is replaceable.
+        Returns:
+            bool: True if it is replaceable.
+        """
+
+        if self.sfp_type == "SFP":
+            return True
+        else:
+            return False
+        
+    def get_position_in_parent(self):
+        """
+        Retrieves 1-based relative physical position in parent device
+        Returns:
+            integer: The 1-based relative physical position in parent device
+        """
+        return self.index
