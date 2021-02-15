6ca858e7ca4433790c58deb636733fe03c51eb33
SuvarnaMeenakshi <sumeenak@microsoft.com>
2021-02-15 11:13:07 -0800

    [vs][topology]: Add a function to get hwsku from config_db or minigraph.xml as per review comment.
    Signed-off-by: SuvarnaMeenakshi <sumeenak@microsoft.com>

--- files/image_config/topology/topology.sh
+++ files/image_config/topology/topology.sh
@@ -1,45 +1,49 @@
 #!/bin/bash
 # This script is invoked by topology.service only
-# for multi-asic virtual platform. For multi-asic platform 
-# multiple Database instances are present 
+# for multi-asic virtual platform. For multi-asic platform
+# multiple Database instances are present
 # and HWKSU information is retrieved from first database instance.
 #
+
+get_hwsku() {
+    # Get HWSKU from config_db. If HWSKU is not available in config_db
+    # get HWSKU from minigraph.xml if minigraph file exists.
+    HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]' 2>&1`
+    if [[ $? -ne 0 || $HWSKU == "" ]]; then
+            if [[ -f "/etc/sonic/minigraph.xml" ]]; then
+                HWSKU=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "DEVICE_METADATA['localhost']['hwsku']" 2>&1`
+                if [[ $? -ne 0 || $HWSKU == "" ]]; then
+                    HWSKU=""
+                fi
+            else
+                HWSKU=""
+            fi
+    fi
+    echo "${HWSKU}"
+}
 
 start() {
     TOPOLOGY_SCRIPT="topology.sh"
     PLATFORM=`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform`
-    HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]' 2>&1`
-    if [[ $? -ne 0 || HWSKU == "" ]]; then
-            if [[ -f "/etc/sonic/minigraph.xml" ]]; then
-                HWSKU=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "DEVICE_METADATA['localhost']['hwsku']" 2>&1`
-                if [[ $? -ne 0 || HWSKU == "" ]]; then
-                    echo "Failed to get HWSKU"
-                    exit 1 
-                fi
-            else
-                echo "Failed to get HWSKU"
-                exit 1
-            fi
+    HWSKU=`get_hwsku`
+    if [[ $HWSKU != "" ]]; then
+        /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT start
+    else
+        echo "Failed to get HWSKU"
+        exit 1
     fi
-    /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT start
 }
+
 stop() {
     TOPOLOGY_SCRIPT="topology.sh"
     PLATFORM=`sonic-cfggen -H -v DEVICE_METADATA.localhost.platform`
-    HWSKU=`sonic-cfggen -d -v 'DEVICE_METADATA["localhost"]["hwsku"]' 2>&1`
-    if [[ $? -ne 0 || HWSKU == "" ]]; then
-            if [[ -f "/etc/sonic/minigraph.xml" ]]; then
-                HWSKU=`sonic-cfggen -m /etc/sonic/minigraph.xml -v "DEVICE_METADATA['localhost']['hwsku']" 2>&1`
-                if [[ $? -ne 0 || HWSKU == "" ]]; then
-                    echo "Failed to get HWSKU"
-                    exit 1
-                fi
-            else
-                echo "Failed to get HWSKU"
-                exit 1
-            fi
+    HWSKU=`get_hwsku`
+    if [[ $HWSKU != "" ]]; then
+        /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT stop
+    else
+        echo "Failed to get HWSKU"
+        exit 1
     fi
-    /usr/share/sonic/device/$PLATFORM/$HWSKU/$TOPOLOGY_SCRIPT stop
 }
 
 # read SONiC immutable variables
