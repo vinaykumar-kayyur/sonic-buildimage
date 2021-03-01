3790af2195b2e98d5bdbfb515937e32757977aeb
junchao <junchao@nvidia.com>
2021-03-01 15:30:50 +0800

    Initialize PSU API on both host and docker side

--- platform/mellanox/mlnx-platform-api/sonic_platform/platform.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/platform.py
@@ -16,17 +16,16 @@ except ImportError as e:
 class Platform(PlatformBase):
     def __init__(self):
         PlatformBase.__init__(self)
+        self._chassis = Chassis()
+        self._chassis.initialize_psu()
+        self._chassis.initialize_eeprom()
         if self._is_host():
-            self._chassis = Chassis()
             self._chassis.initialize_components()
             self._chassis.initizalize_system_led()
-            self._chassis.initialize_eeprom()
         else:
-            self._chassis = Chassis()
-            self._chassis.initialize_psu()
             self._chassis.initialize_fan()
-            self._chassis.initialize_eeprom()
             self._chassis.initialize_thermals()
+        
 
     def _is_host(self):
         """
