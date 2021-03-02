03257ab8407e5acebb2c5cb300b599606c555325
st <Santhosh_Kumar_T@dell.com>
2021-03-02 12:08:06 +0530

    log output change

--- platform/broadcom/sonic-platform-modules-dell/s6100/scripts/s6100_ssd_mon.sh
+++ platform/broadcom/sonic-platform-modules-dell/s6100/scripts/s6100_ssd_mon.sh
@@ -7,6 +7,6 @@ iSMART_CMD=`$iSMART $iSMART_OPTIONS`
 GPIO_STATUS=$(echo "$iSMART_CMD" | grep GPIO | rev | cut -d' ' -f 2 | rev)
 
 if [ $GPIO_STATUS != "0x01" ];then
-    logger -p user.crit -t DELL_S6100_SSD_MON "The SSD on this unit has deteriorated and does not support reboot.\
+    logger -p user.crit -t DELL_S6100_SSD_MON "The SSD on this unit does not support reboot.\
         Please perform a warm/fast-reboot instead."
 fi
