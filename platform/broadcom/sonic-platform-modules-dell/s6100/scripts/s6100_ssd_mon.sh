1cdb7a7d4d104f162b483eeeaa5bbb4b765fd9b9
Santhosh Kumar T <53558409+santhosh-kt@users.noreply.github.com>
2021-03-04 10:52:48 +0530

    changing syslog messages

--- platform/broadcom/sonic-platform-modules-dell/s6100/scripts/s6100_ssd_mon.sh
+++ platform/broadcom/sonic-platform-modules-dell/s6100/scripts/s6100_ssd_mon.sh
@@ -7,6 +7,6 @@ iSMART_CMD=`$iSMART $iSMART_OPTIONS`
 GPIO_STATUS=$(echo "$iSMART_CMD" | grep GPIO | rev | cut -d' ' -f 2 | rev)
 
 if [ $GPIO_STATUS != "0x01" ];then
-    logger -p user.crit -t DELL_S6100_SSD_MON "The SSD on this unit does not support reboot.\
-        Please perform a warm/fast-reboot instead."
+    logger -p user.crit -t DELL_S6100_SSD_MON "The SSD on this unit is faulty and does not support cold reboot."
+    logger -p user.crit -t DELL_S6100_SSD_MON "If a reboot is required, please perform a soft-/fast-/warm-reboot"
 fi
