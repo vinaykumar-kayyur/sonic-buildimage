734d07e93bc6d7ebbf098bdf539275acf49f40d7
doni@celestica.com <doni@celestica.com>
2021-03-08 17:32:14 +0800

    add max timeout check,if exceed the max timeout return error

--- device/celestica/x86_64-cel_seastone-r0/sonic_platform/watchdog.py
+++ device/celestica/x86_64-cel_seastone-r0/sonic_platform/watchdog.py
@@ -133,6 +133,8 @@ class Watchdog(WatchdogBase):
         ret = WDT_COMMON_ERROR
         if seconds < 0:
             return ret
+        if seconds > 16779:
+            return ret
 
         try:
             if self.timeout != seconds:
