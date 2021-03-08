6dd29d410054aaeeb67fe0e908fbdb7dd55a9f54
Judy Joseph <judy.joseph@microsoft.com>
2021-03-08 14:05:48 -0800

    Update to the latest version of SAI for DNX

--- platform/broadcom/sai-dnx.mk
+++ platform/broadcom/sai-dnx.mk
@@ -1,8 +1,8 @@
-BRCM_DNX_SAI = libsaibcm-dnx_4.3.0.13-1_amd64.deb
-$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dnx_4.3.0.13-1_amd64.deb?sv=2015-04-05&sr=b&sig=8UlGRZ1Fx%2B2%2BcWUK8zL%2F1FCQKEA%2FBay2tV3gF%2B4F9DU%3D&se=2034-10-31T04%3A53%3A51Z&sp=r"
-BRCM_DNX_SAI_DEV = libsaibcm-dnx-dev_4.3.0.13-1_amd64.deb
+BRCM_DNX_SAI = libsaibcm_dnx_4.3.3.1-1_amd64.deb
+$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_dnx_4.3.3.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=6dfTLPsvg%2BtK9u23Z%2BEeqQWj3GCiBvlv8lbwEwv5k7U%3D&se=2034-11-15T22%3A02%3A40Z&sp=r"
+BRCM_DNX_SAI_DEV = libsaibcm_dnx-dev_4.3.3.1-1_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_DNX_SAI),$(BRCM_DNX_SAI_DEV)))
-$(BRCM_DNX_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm-dnx-dev_4.3.0.13-1_amd64.deb?sv=2015-04-05&sr=b&sig=OoJwgS%2FHyV2vPJOgeABjr1eeCa12vi8cNCASSMl%2Bt5w%3D&se=2034-10-31T04%3A54%3A31Z&sp=r"
+$(BRCM_DNX_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/4.3/master/libsaibcm_dnx-dev_4.3.3.1-1_amd64.deb?sv=2015-04-05&sr=b&sig=0FpubRB0zxgWhSovC5maX8w5pH83vE8WjPml40KWt24%3D&se=2034-11-15T22%3A03%3A43Z&sp=r"
 
 SONIC_ONLINE_DEBS += $(BRCM_DNX_SAI)
 $(BRCM_DNX_SAI_DEV)_DEPENDS += $(BRCM_DNX_SAI)
