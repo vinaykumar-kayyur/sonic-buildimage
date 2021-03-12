e807a43a3cd5f66390ef856017b4d21286b27bc8
sandycelestica <47624974+sandycelestica@users.noreply.github.com>
2021-03-12 19:20:33 +0800

    Add reload_udev.sh
    This file is used to trigger the udev rules when the first time sonic been installed and boot under E1031

---
+++ platform/broadcom/sonic-platform-modules-cel/haliburton/script/reload_udev.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+UDEV_DIR=/etc/udev/rules.d/50-ttyUSB-C0.rules
+TTYUSB_DIR=/dev/ttyUSB
+
+if [ -f "$UDEV_DIR" ]; then
+        for i in {0..95}
+        do
+                ttydev=$TTYUSB_DIR$i
+                if [ -c "$ttydev" ]; then
+                        echo "$ttydev"
+                        udevadm trigger -c add $ttydev
+                fi
+        done
+fi
