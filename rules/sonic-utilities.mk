13b15917f56717fc1c43ef496d2fabe3686d0339
Guohan Lu <lguohan@gmail.com>
2021-03-12 21:28:44 -0800

    [build]: add platform-common as build dep for sonic-utilities
    Signed-off-by: Guohan Lu <lguohan@gmail.com>

--- rules/sonic-utilities.mk
+++ rules/sonic-utilities.mk
@@ -5,6 +5,7 @@ $(SONIC_UTILITIES_PY3)_SRC_PATH = $(SRC_PATH)/sonic-utilities
 $(SONIC_UTILITIES_PY3)_PYTHON_VERSION = 3
 $(SONIC_UTILITIES_PY3)_DEPENDS += $(SONIC_PY_COMMON_PY3) \
                                   $(SWSSSDK_PY3) \
+                                  $(SONIC_PLATFORM_COMMON_PY3) \
                                   $(SONIC_CONFIG_ENGINE_PY3) \
                                   $(SONIC_PLATFORM_COMMON_PY3) \
                                   $(SONIC_YANG_MGMT_PY3) \
