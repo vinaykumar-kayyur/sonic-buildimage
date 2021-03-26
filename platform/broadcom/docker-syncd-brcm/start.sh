0d4fa332ef94629067634221effdaaec23554a98
Gen-Hwa Chiang <gechiang@microsoft.com>
2021-03-26 05:45:19 +0000

    7260cx3 DualToR config.bcm support based on DualToR setting in device metadata at boot time

--- platform/broadcom/docker-syncd-brcm/start.sh
+++ platform/broadcom/docker-syncd-brcm/start.sh
@@ -8,11 +8,22 @@ SYNCD_SOCKET_FILE=/var/run/sswsyncd/sswsyncd.socket
 rm -f ${SYNCD_SOCKET_FILE}
 
 mkdir -p /etc/sai.d/
+rm /etc/sai.d/*
+
+# There are two ways to specify the contents of the SAI_INIT_CONFIG_FILE and they are mutually exclusive
+# via current method (sai.profile.j2) or new method (append.config.j2)
+# If delta is large, use sai.profile.j2 which basically require the user to select which config file to use
+# If delta is small, use append.config.j2 where additional SAI INIT config properties are added
+#   based on specific device metadata requirement
+#   in this case sai.profile should have been modified to use the path /etc/sai.d/sai.config
 
 # Create/Copy the sai.profile to /etc/sai.d/sai.profile
 if [ -f $HWSKU_DIR/sai.profile.j2 ]; then
     sonic-cfggen -d -t $HWSKU_DIR/sai.profile.j2 > /etc/sai.d/sai.profile
 else
+    if [ -f $HWSKU_DIR/append.config.j2 ]; then
+        sonic-cfggen -d -t $HWSKU_DIR/append.config.j2 > /etc/sai.d/sai.config
+    fi
     if [ -f $HWSKU_DIR/sai.profile ]; then
         cp $HWSKU_DIR/sai.profile /etc/sai.d/sai.profile
     fi
