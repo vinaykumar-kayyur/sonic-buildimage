afc847388c869b84f1dd4a424d761b77a2b4a6a0
Stepan Blyschak <stepanb@nvidia.com>
2021-03-26 11:29:34 +0200

    [finalize-warmboot] fix bad substitution
    Signed-off-by: Stepan Blyschak <stepanb@nvidia.com>

--- files/image_config/warmboot-finalizer/finalize-warmboot.sh
+++ files/image_config/warmboot-finalizer/finalize-warmboot.sh
@@ -14,7 +14,7 @@ declare -A RECONCILE_COMPONENTS=( \
 
 for reconcile_file in $(find /etc/sonic/ -iname '*_reconcile' -type f); do
     file_basename=$(basename $reconcile_file)
-    docker_container_name=${$file_basename%_reconcile}
+    docker_container_name=${file_basename%_reconcile}
     RECONCILE_COMPONENTS[$docker_container_name]=$(cat $reconcile_file)
 done
 
