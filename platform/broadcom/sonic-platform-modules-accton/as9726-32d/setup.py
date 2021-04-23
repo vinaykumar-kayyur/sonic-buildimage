468f3bcd14f2e4b29e6ee1918c5974d837c7f828
michael_shih <michael_shih@edge-core.com>
2021-04-23 15:55:52 +0800

    [Platform] Accton add to support as9726-32d platform.

---
+++ platform/broadcom/sonic-platform-modules-accton/as9726-32d/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup
+os.listdir
+
+setup(
+    name='as9726_32d',
+    version='1.0',
+    description='Module to initialize Accton AS9726_32D platforms',
+
+    packages=['as9726_32d'],
+    package_dir={'as9726_32d': 'as9726-32d/classes'},
+)
