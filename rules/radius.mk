vinaykumar-kayyur/sonic-buildimage

commit ec9101f9c556e71ac10d686fc0f5c0f8c58bec10

RADIUS Management User Authentication Feature (#7284)
Why I did it
HLD: https://github.com/Azure/SONiC/blob/master/doc/aaa/radius_authentication.md
CLI: In a separate PR.

How I did it
How to verify it
UT: src/sonic-host-services/tests/hostcfgd/hostcfgd_radius_test.py

rules/radius.mk
@@ -1,0 +1,24 @@
+# libpam-radius-auth packages
+
+PAM_RADIUS_VERSION = 1.4.1-1
+
+export PAM_RADIUS_VERSION
+
+LIBPAM_RADIUS = libpam-radius-auth_$(PAM_RADIUS_VERSION)_amd64.deb
+$(LIBPAM_RADIUS)_SRC_PATH = $(SRC_PATH)/radius/pam
+SONIC_MAKE_DEBS += $(LIBPAM_RADIUS)
+
+SONIC_STRETCH_DEBS += $(LIBPAM_RADIUS)
+
+# libnss-radius packages
+
+NSS_RADIUS_VERSION = 1.0.1-1
+
+export NSS_RADIUS_VERSION
+
+LIBNSS_RADIUS = libnss-radius_$(NSS_RADIUS_VERSION)_amd64.deb
+$(LIBNSS_RADIUS)_SRC_PATH = $(SRC_PATH)/radius/nss
+SONIC_MAKE_DEBS += $(LIBNSS_RADIUS)
+
+SONIC_STRETCH_DEBS += $(LIBNSS_RADIUS)
+

src/radius/nss/Makefile
@@ -1,0 +1,23 @@
+.ONESHELL:
+SHELL = /bin/bash
+.SHELLFLAGS += -e
+
+MAIN_TARGET = libnss-radius_$(NSS_RADIUS_VERSION)_amd64.deb
+
+$(addprefix $(DEST)/, $(MAIN_TARGET)): $(DEST)/% :
+	pushd ./libnss-radius
+
+	make clean
+	-rm -rf debian
+	-rm -rf patches
+	cp -r ../debian .
+	cp -r ../patches .
+
+	# Apply patch (if any)
+
+	dpkg-buildpackage -rfakeroot -b -us -uc
+	popd
+
+	mv $(DERIVED_TARGETS) $* $(DEST)/
+
+$(addprefix $(DEST)/, $(DERIVED_TARGETS)): $(DEST)/% : $(DEST)/$(MAIN_TARGET)

src/radius/pam/Makefile
@@ -1,0 +1,43 @@
+.ONESHELL:
+SHELL = /bin/bash
+.SHELLFLAGS += -e
+
+MAIN_TARGET = libpam-radius-auth_$(PAM_RADIUS_VERSION)_amd64.deb
+
+$(addprefix $(DEST)/, $(MAIN_TARGET)): $(DEST)/% :
+	# Build freeradius-server v1.1.x
+	pushd freeradius
+	-rm -rf freeradius-server
+	git clone https://github.com/FreeRADIUS/freeradius-server.git
+	popd
+	pushd freeradius/freeradius-server
+	git checkout -f 5f715dba4d2dbdb268bf60fcc656352274930941
+	# Apply patch
+	patch -p1 < ../patches/freeradius_configure.patch
+	patch -p1 < ../patches/freeradius_2007-04-06.patch
+	patch -p1 < ../patches/freeradius_libeap_deprecated_openssl_1_0.patch
+	./configure --disable-static --enable-libtool-lock
+	make
+	popd
+
+	# Obtain pam_radius
+	-rm -rf ./pam_radius
+	git clone https://github.com/FreeRADIUS/pam_radius.git
+	pushd ./pam_radius
+	# This is the latest commit with Radius MPL support
+	git checkout -f 149c25df84cf5cd0e9addd9346699a9ca8fdddd2
+
+	cp -r ../debian .
+	cp -r ../patches .
+
+	# Apply patch
+	git apply patches/0001-chap-support.patch
+	git apply patches/0002-peap-mschapv2-support.patch
+	git apply patches/0003-nas-ip-address-config.patch
+
+	dpkg-buildpackage -rfakeroot -b -us -uc -nc
+	popd
+
+	mv $(DERIVED_TARGETS) $* $(DEST)/
+
+$(addprefix $(DEST)/, $(DERIVED_TARGETS)): $(DEST)/% : $(DEST)/$(MAIN_TARGET)

src/radius/nss/libnss-radius/Makefile
@@ -1,0 +1,59 @@
+#######################################################################
+#
+# Copyright 2019 Broadcom. All rights reserved.
+# The term "Broadcom" refers to Broadcom Inc. and/or its subsidiaries.
+#
+#######################################################################
+
+#
+# Makefile for libnss-radius
+#
+
+TARGETS = libnss_radius.so.2 cache_radius
+COMMON_INCLUDE = nss_radius_common.h
+COMMON_SOURCE = nss_radius_common.c
+LIBNSS_SOURCE = nss_radius.c $(COMMON_SOURCE)
+CACHE_SOURCE = cache_radius.c $(COMMON_SOURCE)
+
+# For now place the multiarch flag here
+# Eventually this needs to be move to the debian packaging
+#moduledir = $(prefix)/lib/x86_64-linux-gnu
+moduledir = $(prefix)/lib/$(DEB_HOST_MULTIARCH)
+
+
+all: $(TARGETS)
+
+libnss_radius.so.2: $(LIBNSS_SOURCE) $(COMMON_INCLUDE)
+	$(CC) $(CFLAGS) $(LDFLAGS) -fPIC -Wall -shared -o libnss_radius.so.2 \
+		-Wl,-soname,libnss_radius.so.2 -Wl,--version-script=libnss_radius_vs.txt $(LIBNSS_SOURCE)
+
+cache_radius: $(CACHE_SOURCE) $(COMMON_INCLUDE)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o cache_radius $(CACHE_SOURCE)
+
+clean:
+	-rm -f $(TARGETS)
+	-rm -f test_nss_radius test_cache_radius
+
+install: libnss_radius.so.2 cache_radius
+	install -m 0644 -D libnss_radius.so.2 \
+		$(DESTDIR)$(moduledir)/libnss_radius.so.2
+	install -m 0755 -D cache_radius \
+		$(DESTDIR)$(prefix)/usr/sbin/cache_radius
+	install -m 0755 -d $(DESTDIR)$(prefix)/etc/pam_radius_auth.d/
+
+distclean: clean
+
+uninstall:
+	-rm -f $(DESTDIR)$(moduledir)/libnss_radius.so.2
+	-rm -f $(DESTDIR)$(prefix)/usr/sbin/cache_radius
+
+test: test_nss_radius.c $(LIBNSS_SOURCE) $(CACHE_SOURCE) \
+		$(COMMON_SOURCE) $(COMMON_INCLUDE)
+	$(CC) $(CFLAGS) $(LDFLAGS) -g -DTEST_RADIUS_NSS -o test_nss_radius \
+		$(LIBNSS_SOURCE) test_nss_radius.c
+	$(CC) $(CFLAGS) $(LDFLAGS) -g -DTEST_RADIUS_NSS -o test_cache_radius \
+		$(CACHE_SOURCE)
+	
+
+.PHONY: all install clean distclean uninstall test
+

commit 00e49f11432af60b648830a7e12b45aa74edb313

[build]: fix build break on armhf for radius
regression introduced in #7284

Signed-off-by: Guohan Lu <lguohan@gmail.com>

rules/radius.mk
@@ -4,7 +4,7 @@ PAM_RADIUS_VERSION = 1.4.1-1
 
 export PAM_RADIUS_VERSION
 
-LIBPAM_RADIUS = libpam-radius-auth_$(PAM_RADIUS_VERSION)_amd64.deb
+LIBPAM_RADIUS = libpam-radius-auth_$(PAM_RADIUS_VERSION)_$(CONFIGURED_ARCH).deb
 $(LIBPAM_RADIUS)_SRC_PATH = $(SRC_PATH)/radius/pam
 SONIC_MAKE_DEBS += $(LIBPAM_RADIUS)
 
@@ -16,7 +16,7 @@ NSS_RADIUS_VERSION = 1.0.1-1
 
 export NSS_RADIUS_VERSION
 
-LIBNSS_RADIUS = libnss-radius_$(NSS_RADIUS_VERSION)_amd64.deb
+LIBNSS_RADIUS = libnss-radius_$(NSS_RADIUS_VERSION)_$(CONFIGURED_ARCH).deb
 $(LIBNSS_RADIUS)_SRC_PATH = $(SRC_PATH)/radius/nss
 SONIC_MAKE_DEBS += $(LIBNSS_RADIUS)
 

src/radius/nss/Makefile
@@ -2,7 +2,7 @@
 SHELL = /bin/bash
 .SHELLFLAGS += -e
 
-MAIN_TARGET = libnss-radius_$(NSS_RADIUS_VERSION)_amd64.deb
+MAIN_TARGET = libnss-radius_$(NSS_RADIUS_VERSION)_$(CONFIGURED_ARCH).deb
 
 $(addprefix $(DEST)/, $(MAIN_TARGET)): $(DEST)/% :
 	pushd ./libnss-radius

src/radius/pam/Makefile
@@ -2,7 +2,7 @@
 SHELL = /bin/bash
 .SHELLFLAGS += -e
 
-MAIN_TARGET = libpam-radius-auth_$(PAM_RADIUS_VERSION)_amd64.deb
+MAIN_TARGET = libpam-radius-auth_$(PAM_RADIUS_VERSION)_$(CONFIGURED_ARCH).deb
 
 $(addprefix $(DEST)/, $(MAIN_TARGET)): $(DEST)/% :
 	# Build freeradius-server v1.1.x