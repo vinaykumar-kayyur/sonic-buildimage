661507d8800913b1cb8c0128353dba1bcf8f0109
Stephen Sun <stephens@nvidia.com>
2021-04-26 02:51:35 +0000

    Support handling the error status of SFP modules
    1. Update get_change_event
       - Return event in a bitmap format in case there is an error
       - Return an extra map indexed by sfp_error in case there is a Mellanox-specific error
       - Indicate whether the error is a blocking error
    2. Expose the error status of SFP modules to CLI
       - A platform API calls SDK API to fetch the error status
         - Display the SFP module state: plugged, unplugged and plugged with error.
         - The SFP error state is displayed only if the SFP module state is plugged with error
       - The CLI will call platform API and provide user-friendly output
    3. Redirect stderr to /dev/null in order to eliminate errors in case SFP module is not plugged
    4. Beautify the code, making all the state/error/descriptions defined in a uniform place
    
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- platform/mellanox/mlnx-platform-api/tests/test_sfp_event.py
+++ platform/mellanox/mlnx-platform-api/tests/test_sfp_event.py
@@ -8,6 +8,8 @@ test_path = os.path.dirname(os.path.abspath(__file__))
 modules_path = os.path.dirname(test_path)
 sys.path.insert(0, modules_path)
 
+from sonic_platform_base.sfp_base import SfpBase
+
 class TestSfpEvent(object):
     @classmethod
     def setup_class(cls):
@@ -16,21 +18,29 @@ class TestSfpEvent(object):
 
     def test_check_sfp_status(self):
         from sonic_platform.sfp_event import SDK_SFP_STATE_IN, SDK_SFP_STATE_OUT, SDK_SFP_STATE_ERR
-        from sonic_platform.sfp_event import STATUS_PLUGIN, STATUS_PLUGOUT
-        from sonic_platform.sfp_event import sdk_sfp_err_type_dict
+        from sonic_platform.sfp_event import SDK_ERRORS_TO_ERROR_BITS, SDK_ERRORS_TO_DESCRIPTION, SDK_SFP_BLOCKING_ERRORS
 
-        self.executor(SDK_SFP_STATE_IN, None, STATUS_PLUGIN)
-        self.executor(SDK_SFP_STATE_OUT, None, STATUS_PLUGOUT)
-        for error_type, error_status in sdk_sfp_err_type_dict.items():
-            self.executor(SDK_SFP_STATE_ERR, error_type, error_status)
+        self.executor(SDK_SFP_STATE_IN, None, SfpBase.SFP_STATUS_BIT_INSERTED)
+        self.executor(SDK_SFP_STATE_OUT, None, SfpBase.SFP_STATUS_BIT_REMOVED)
+        for error_type, error_status in SDK_ERRORS_TO_ERROR_BITS.items():
+            description = SDK_ERRORS_TO_DESCRIPTION.get(error_type)
+            if error_type in SDK_SFP_BLOCKING_ERRORS:
+                error_status |= SfpBase.SFP_ERROR_BIT_BLOCKING
+            error_status |= SfpBase.SFP_STATUS_BIT_INSERTED
+            self.executor(SDK_SFP_STATE_ERR, error_type, error_status, description)
 
-    def executor(self, mock_module_state, mock_error_type, expect_status):
+    def executor(self, mock_module_state, mock_error_type, expect_status, description=None):
         from sonic_platform.sfp_event import sfp_event
 
         event = sfp_event()
         event.on_pmpe = MagicMock(return_value=(True, [0,1], mock_module_state, mock_error_type))
         port_change = {}
-        found = event.check_sfp_status(port_change, 0)
+        error_dict = {}
+        found = event.check_sfp_status(port_change, error_dict, 0)
         assert found
-        assert 1 in port_change and port_change[1] == expect_status
-        assert 2 in port_change and port_change[2] == expect_status
+        expect_status_str = str(expect_status)
+        assert 1 in port_change and port_change[1] == expect_status_str
+        assert 2 in port_change and port_change[2] == expect_status_str
+        if description:
+            assert 1 in error_dict and error_dict[1] == description
+            assert 2 in error_dict and error_dict[2] == description
