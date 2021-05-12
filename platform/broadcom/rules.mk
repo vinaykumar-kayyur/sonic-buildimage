98f6f7000b545a663d3d4b20e00fdfa36bf90107
Judy Joseph <jujoseph@microsoft.com>
2021-05-12 14:45:07 -0700

    Adding new sonic-buildimage targets for building the sonic-buildimage-dnx images

--- platform/broadcom/rules.mk
+++ platform/broadcom/rules.mk
@@ -30,7 +30,7 @@ $(DSSERVE)_URL = "https://sonicstorage.blob.core.windows.net/packages/20190307/d
 
 SONIC_ONLINE_FILES += $(BCMCMD) $(DSSERVE)
 
-SONIC_ALL += $(SONIC_ONE_IMAGE) $(SONIC_ONE_ABOOT_IMAGE) \
+SONIC_ALL += $(SONIC_ONE_IMAGE) $(SONIC_ONE_ABOOT_IMAGE) $(SONIC_ONE_DNX_IMAGE) $(SONIC_ONE_DNX_ABOOT_IMAGE) \
              $(DOCKER_FPM)
 
 # Inject brcm sai into syncd
