1296e953e08a437ef1b2bbbe712fef727348c4bd
spenugondaa <srinadh.penugondaa@dell.com>
2021-06-09 19:45:31 +0000

    docker eventd infra cleanup
    Signed-off-by: spenugondaa <srinadh.penugondaa@dell.com>

--- dockers/docker-eventd/docker-init.sh
+++ dockers/docker-eventd/docker-init.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 sonic-db-cli EVENT_DB config set  notify-keyspace-events AKE
-sed -i '/username=/d' /etc/supervisor/supervisord.conf
-sed -i '/password=/d' /etc/supervisor/supervisord.conf
 
 cp /var/evprofile/default.json /etc/evprofile/default.json
 
