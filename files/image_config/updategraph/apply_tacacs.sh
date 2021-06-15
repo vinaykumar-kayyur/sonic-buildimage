6b099c72cb79ca215be63415808722decf4a8f44
Renuka Manavalan <remanava@microsoft.com>
2021-06-15 15:49:32 +0000

    Added missed define.

--- files/image_config/updategraph/apply_tacacs.sh
+++ files/image_config/updategraph/apply_tacacs.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -e
 
+TACACS_JSON_BACKUP=tacacs.json
+
 if [ -r /etc/sonic/old_config/${TACACS_JSON_BACKUP} ]; then
     sonic-cfggen -j /etc/sonic/old_config/${TACACS_JSON_BACKUP} --write-to-db
     echo "Applied tacacs config from /etc/sonic/old_config/${TACACS_JSON_BACKUP}"
