743ac26619e8a3899d5da7752ad5da6bfff62f66
Sumukha Tumkur Vani <Sumukha.TumkurVani@microsoft.com>
2021-06-15 21:21:13 +0000

    Merged PR 4647015: [ACMS] Redesign start script
    1. Handle multiple bootstrap certificates. Choose the latest one and keep trying with other certificates if the current one fails
    2. Modify the config file from start script instead of another script
    
    Scenarios tested:
    1. One good bootstrap cert -> bootstraps as before
    2. One bad boostrap cert -> tries to bootstrap > fails > keeps trying over and over every 1min
    3. Add one good bootstrap cert while scenario 2 is running -> Adds the newly added bootstrap cert to the list of bootstrap certs and tries it based on "latest" file logic and bootstraps successfully
    4. Four good bootstrap certs -> Tries the bootstrap certs in the "latest" file order and bootstraps

--- dockers/docker-acms/dSMS_config_modifier.py
+++ dockers/docker-acms/dSMS_config_modifier.py
@@ -3,40 +3,18 @@
 This script will determine the correct dSMS region to configure ACMS and update the config file
 It will also update the ACMS config file with the bootstrap cert path
 '''
-import os, sys, subprocess, time, re, fnmatch
-from datetime import datetime, timedelta
+import os, re, sys
 from sonic_py_common import logger
 from swsscommon import swsscommon
 
-# Bootstrap cert
-bootstrap_cert = ""
-# Bootstrap cert file pattern
-bootstrap_cert_pattern = "sonic_acms_bootstrap-*.pfx"
-# SONiC credentials directory
-sonic_creds_dir = "/etc/sonic/credentials/"
-# dSMS config file
-dsms_conf = "/var/opt/msft/client/dsms.conf"
 # ACMS config file
 acms_conf = "/var/opt/msft/client/acms_secrets.ini"
-# ACMS config file original
-acms_conf_ori = "/acms/acms_secrets.ini"
-# Acceptable time difference between now and next ACMS poll event for meddling with ACMS config file
-time_margin = timedelta(seconds=1800)
-# Sleep timer for bootstrapping
-bootstrap_alarm = 30
 
 # Redis DB information
 REDIS_TIMEOUT_MS = 0
 
 sonic_logger = logger.Logger()
 sonic_logger.set_min_log_priority_info()
-
-def get_device_region():
-    config_db = swsscommon.DBConnector("CONFIG_DB", REDIS_TIMEOUT_MS, True)
-    device_metadata = swsscommon.Table(config_db, swsscommon.CFG_DEVICE_METADATA_TABLE_NAME)
-    (status, tuples) = device_metadata.get("localhost")
-    localhost = dict(tuples)
-    return localhost['region']
 
 def get_device_cloudtype():
     config_db = swsscommon.DBConnector("CONFIG_DB", REDIS_TIMEOUT_MS, True)
@@ -45,8 +23,10 @@ def get_device_cloudtype():
     localhost = dict(tuples)
     return localhost['cloudtype']
 
-def get_device_region_from_bootstrap_cert():
-    region = bootstrap_cert.split('-')[1].split('.')[0]
+def get_device_region_from_bootstrap_cert(path_to_bootstrap_cert):
+    # /etc/sonic/credentials/sonic_acms_bootstrap-uswestcentral.pfx
+    cert = os.path.basename(path_to_bootstrap_cert)
+    region = cert.split('-')[1].split('.')[0]
     return region
 
 def update_dsms_url(conf_file, new_url):
@@ -76,45 +56,23 @@ def fix_endpoint_for_cloud(cloud):
     else:
         sonic_logger.log_error("CloudType "+cloud+" not listed!")
         return False
-    sonic_logger.log_info("dSMS endpoint for "+cloud+" is "+url_pattern)
+    sonic_logger.log_info("dSMS_config_modifier: fix_endpoint_for_cloud: dSMS endpoint for "+cloud+" is "+url_pattern)
     # Change URL pattern in the ACMS config file
     update_dsms_url(acms_conf, url_pattern)
-    # Change URL pattern in the original ACMS config file 
-    update_dsms_url(acms_conf_ori, url_pattern)
     return True
 
-def update_config():
+def update_config(path_to_bootstrap_cert):
     '''
     Determine the correct dSMS endpoint and update the ACMS config file
     '''
-    region = get_device_region_from_bootstrap_cert()
+    region = get_device_region_from_bootstrap_cert(path_to_bootstrap_cert)
     acms_conf_file = open(acms_conf, "r+")
     contents = acms_conf_file.read()
-    if region not in contents:
-        new_contents = re.sub("region", region, contents)
-        new_contents = re.sub("BootstrapCert=.*\n*", "BootstrapCert="+sonic_creds_dir+bootstrap_cert+"\n", new_contents)
-        sonic_logger.log_info("Updated config: "+new_contents)
-        acms_conf_file.seek(0)
-        acms_conf_file.write(new_contents)
-        sonic_logger.log_debug("dSMS endpoint set to "+region)
+    new_contents = re.sub("region", region, contents)
+    new_contents = re.sub("BootstrapCert=.*\n*", "BootstrapCert="+path_to_bootstrap_cert+"\n", new_contents)
+    sonic_logger.log_info("dSMS_config_modifier: update_config: Updated config: "+new_contents)
+    acms_conf_file.seek(0)
+    acms_conf_file.write(new_contents)
+    sonic_logger.log_debug("dSMS_config_modifier: update_config: dSMS endpoint set to "+region)
     acms_conf_file.close()
-
-if __name__ == '__main__':
-    # Sleep until bootstrap cert is available
-    bootstrap = False
-    while bootstrap == False:
-        for file_t in os.listdir(sonic_creds_dir):
-            if fnmatch.fnmatch(file_t, bootstrap_cert_pattern):
-                bootstrap_cert = file_t
-                sonic_logger.log_info("Bootstrap cert found: "+file_t)
-                bootstrap = True
-                cloud = get_device_cloudtype()
-                if cloud.lower() != "Public".lower():
-                    if not fix_endpoint_for_cloud(cloud):
-                        sonic_logger.log_error("Fixing endpoint for cloudtype "+cloud+" failed!")
-                        sys.exit(1)
-                update_config()
-                sonic_logger.log_info("ACMS Config update complete")
-                sys.exit(0)
-        sonic_logger.log_info("Waiting for bootstrap cert")
-        time.sleep(60)
+    return
---
+++ dockers/docker-acms/start.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+import os, subprocess, time, shutil, sys, re
+import dSMS_config_modifier
+from sonic_py_common import logger
+
+sonic_logger = logger.Logger()
+
+# Default ACMS directory
+ACMS_DIR_PATH = "/var/opt/msft/acms"
+# Default dSMS client directory
+CLIENT_DIR_PATH = "/var/opt/msft/client"
+# Original ACMS config
+ACMS_CONFIG_PATH = "/acms/acms_secrets.ini"
+# Original ACMS.ini
+ACMS_INI_PATH = "/acms/acms.ini"
+# Bootstrap status file
+DSMS_CONF = "/var/opt/msft/client/dsms.conf"
+# SONiC credentials directory
+SONIC_CREDS_PATH = "/etc/sonic/credentials/"
+# SONiC ACMS bootstrap certificate naming pattern
+SONIC_ACMS_BOOTSTRAP_PATTERN = "sonic_acms_bootstrap-.*\.pfx"
+
+WAIT_TIME_FOR_BOOTSTRAP_CERT = 60
+
+def exec_cmd(cmd):
+    process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdoutdata, stderrdata = process.communicate()
+    return process.returncode, stdoutdata, stderrdata
+
+
+def get_bootstrap_cert_paths():
+    # Returns list of bootstrap certs sorted based on last modified time
+    files = [f for f in os.listdir(SONIC_CREDS_PATH) if re.match(SONIC_ACMS_BOOTSTRAP_PATTERN, f)]
+    bootstrap_certs = []
+    for f in files:
+        path = os.path.join(SONIC_CREDS_PATH, f)
+        tup = (path, os.path.getmtime(path))
+        bootstrap_certs.append(tup)
+
+    bootstrap_certs.sort(key = lambda x: x[1], reverse = True)
+    return bootstrap_certs
+
+
+def get_bootstrap_status():
+    sonic_logger.log_info("start: check_bootstrap_status: Checking bootstrap status")
+    if os.path.isfile(DSMS_CONF):
+        dsms_conf_file = open(DSMS_CONF, "r")
+        text = dsms_conf_file.read()
+        dsms_conf_file.close()
+        if "HasBootstrapped=yes" in text:
+            return True
+    else:
+        sonic_logger.log_info("start: check_bootstrap_status: "+DSMS_CONF+" file not found!")
+    return False
+
+
+def update_acms_config(bootstrap_cert):
+    cloud = dSMS_config_modifier.get_device_cloudtype()
+    if cloud.lower() != "Public".lower():
+        if not dSMS_config_modifier.fix_endpoint_for_cloud(cloud):
+            sonic_logger.log_error("start: update_acms_config: Fixing endpoint for cloudtype "+cloud+" failed!")
+            sys.exit(1)
+    dSMS_config_modifier.update_config(bootstrap_cert)
+    sonic_logger.log_info("start: update_acms_config: ACMS Config update complete")  
+    return
+
+
+def main():
+    sonic_logger.set_min_log_priority_info()
+
+    if os.path.exists('/var/run/rsyslogd.pid'):
+        os.remove('/var/run/rsyslogd.pid')
+    rc, stdoutdata, stderrdata = exec_cmd("supervisorctl start rsyslogd")
+    if rc != 0:
+        sonic_logger.log_error("start: main: rsyslogd start failed!")
+
+    if os.path.isdir(ACMS_DIR_PATH):
+        sonic_logger.log_info("start: main: "+ACMS_DIR_PATH+" exists. Deleting it")
+        shutil.rmtree(ACMS_DIR_PATH)
+    os.makedirs(ACMS_DIR_PATH)
+    sonic_logger.log_info("start: main: "+ACMS_DIR_PATH+" has been created")
+    shutil.copy2(ACMS_INI_PATH, ACMS_DIR_PATH)
+    dest = os.path.join(ACMS_DIR_PATH, "acms.ini")
+    if os.path.isfile(dest):
+        sonic_logger.log_info("start: main: acms.ini copied to "+ACMS_DIR_PATH)  
+    else:
+        sonic_logger.log_error("start: main: acms.ini could not be copied to "+ACMS_DIR_PATH)
+        sys.exit(1)
+
+    if os.path.isdir(CLIENT_DIR_PATH):
+        sonic_logger.log_info("start: main: "+CLIENT_DIR_PATH+" exists. Deleting it")
+        shutil.rmtree(CLIENT_DIR_PATH)
+    os.makedirs(CLIENT_DIR_PATH)
+    sonic_logger.log_info("start: main: "+CLIENT_DIR_PATH+" has been created")
+
+    ctr = 0
+    # Bootstrap ACMS
+    while(get_bootstrap_status() == False):
+        # Fresh copy of acms_secrets.ini
+        shutil.copy2(ACMS_CONFIG_PATH, CLIENT_DIR_PATH)
+        dest = os.path.join(CLIENT_DIR_PATH, "acms_secrets.ini")
+        if os.path.isfile(dest):
+            sonic_logger.log_info("start: main: acms_secrets.ini copied to "+CLIENT_DIR_PATH)
+        else:
+            sonic_logger.log_error("start: main: acms_secrets.ini could not be copied to "+CLIENT_DIR_PATH)
+            sys.exit(1)
+
+        # Remove stale dsms.conf    
+        if os.path.isfile(DSMS_CONF):
+            os.remove(DSMS_CONF)
+        boostrap_certs = get_bootstrap_cert_paths()
+        if len(boostrap_certs) > 0:
+            if ctr < len(boostrap_certs):
+                curr_bootstrap_cert = boostrap_certs[ctr][0]
+            else:
+                curr_bootstrap_cert = boostrap_certs[0][0]
+                ctr = 0
+            sonic_logger.log_info("start: main: Trying to bootstrap with "+curr_bootstrap_cert)
+            update_acms_config(curr_bootstrap_cert)
+            exec_cmd("/usr/bin/acms -Bootstrap -Dependant client -BaseDirPath /var/opt/msft/")
+            # Choose a different bootstrap cert for next bootstrap attempt
+            ctr += 1
+            ctr = ctr % len(boostrap_certs)
+        else:
+            sonic_logger.log_info("start: main: Waiting for bootstrap cert")
+        time.sleep(WAIT_TIME_FOR_BOOTSTRAP_CERT)
+
+    sonic_logger.log_info("start: main: ACMS bootstrapping complete")
+
+    # Start ACMS
+    rc, stdoutdata, stderrdata = exec_cmd("supervisorctl start acms")
+    if rc == 0:
+        sonic_logger.log_info("start: main: ACMS has been started")
+    else:
+        sonic_logger.log_error("start: main: Starting ACMS failed!")   
+
+
+if __name__ == "__main__":
+    main()
