11c22f36b22174534926af742107c2a2ba4fcec5
Kwan <kwangsuk_kim@dell.com>
2021-06-16 09:37:07 -0700

    Update start.sh

--- dockers/docker-sonic-mgmt-framework/start.sh
+++ dockers/docker-sonic-mgmt-framework/start.sh
@@ -2,3 +2,7 @@
 
 mkdir -p /var/sonic
 echo "# Config files managed by sonic-config-engine" > /var/sonic/config_status
+
+## TODO: symbolic links will be removed when AAA improvements get merged
+ln -sf /host_etc/passwd /etc/passwd	
+ln -sf /host_etc/group /etc/group
