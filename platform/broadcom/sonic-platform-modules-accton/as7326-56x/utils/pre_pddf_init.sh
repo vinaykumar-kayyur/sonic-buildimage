1bd9eafc23c06c8eae7c387da4d61e0dc47f1f23
Jostar Yang <jostar_yang@accton.com.tw>
2021-06-25 16:59:56 +0800

    Add -i to sed cmd

--- platform/broadcom/sonic-platform-modules-accton/as7326-56x/utils/pre_pddf_init.sh
+++ platform/broadcom/sonic-platform-modules-accton/as7326-56x/utils/pre_pddf_init.sh
@@ -15,7 +15,7 @@ if $use_57_eeprom ; then
     # syseeprom is at the i2c address 0x57. Change the PDDF JSON file
     if [ -f /usr/share/sonic/device/x86_64-accton_as7326_56x-r0/pddf_support ] && \
        [ -f /usr/share/sonic/device/x86_64-accton_as7326_56x-r0/pddf/pddf-device.json ]; then
-        sed 's@"topo_info": {"parent_bus": "0x0", "dev_addr": "0x56", "dev_type": "24c04"},@\
+        sed -i 's@"topo_info": {"parent_bus": "0x0", "dev_addr": "0x56", "dev_type": "24c04"},@\
             "topo_info": {"parent_bus": "0x0", "dev_addr": "0x57", "dev_type": "24c02"},@g' \
             /usr/share/sonic/device/x86_64-accton_as7326_56x-r0/pddf/pddf-device.json
         sync
