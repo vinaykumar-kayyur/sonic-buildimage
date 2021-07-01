f6b7fb03b8668476d344840ea1954dc86028416f
shilongliu <shilongliu@microsoft.com>
2021-07-01 08:10:47 +0000

    draft

--- scripts/prepare_docker_buildinfo.sh
+++ scripts/prepare_docker_buildinfo.sh
@@ -22,6 +22,8 @@ if [ -z "$DISTRO" ]; then
     [ -z "$DISTRO" ] && DISTRO=jessie
 fi
 
+DISTRO=$DISTRO scripts/version_control_image.sh $DOCKERFILE
+
 DOCKERFILE_PRE_SCRIPT='# Auto-Generated for buildinfo 
 COPY ["buildinfo", "/usr/local/share/buildinfo"]
 RUN dpkg -i /usr/local/share/buildinfo/sonic-build-hooks_1.0_all.deb
---
+++ scripts/version_control_image.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# parse parameters & setup vars
+parse_config(){
+    if [ -f /usr/local/share/buildinfo/scripts/buildinfo_base.sh ];then
+        # inside slave docker
+        . /usr/local/share/buildinfo/scripts/buildinfo_base.sh
+        version_file=$VERSION_PATH/versions-image
+        new_version_file=$BUILD_VERSION_PATH/versions-image
+    else
+        # outside slave docker
+        version_file=files/build/versions/default/versions-image
+        new_version_file=target/versions/build/build-sonic-slave-$DISTRO/versions-image
+    fi
+    dir=`dirname $new_version_file`
+    mkdir -p $dir
+    touch $new_version_file
+}
+
+main(){
+    parse_config
+    docker_file=$1
+
+    image_tag=`grep "^FROM " $docker_file | awk '{print$2}'`
+    image=`echo $image_tag | cut -f1 -d:`
+    tag=`echo $image_tag | cut -f2 -d:`
+    # if docker image load from local build, exit.
+    if [ -f target/${image_tag}.gz ]; then
+        exit 0
+    fi
+    if [[ ",$SONIC_VERSION_CONTROL_COMPONENTS," == *,all,* ]] || [[ ",$SONIC_VERSION_CONTROL_COMPONENTS," == *,docker,* ]]; then
+        if [ ! -f $version_file ];then
+            echo "Failed to access $version_file"
+            exit 1
+	fi
+	hash_value=`grep $image_tag $version_file  | awk -F== '{print$2}'`
+        if [ -z $hash_value ];then
+            echo "Failed to verify the docker image: $image, the hash value is not specified"
+	    exit 1
+	fi
+	oldstr=${image_tag//\//\\/}
+	newstr=`echo $oldstr | awk -F: '{print$1}'`@$hash_value
+	sed -i "s/$oldstr/$newstr/" $docker_file
+    fi
+    hash_value_latest=`docker pull $image_tag | grep Digest | awk '{print$2}'`
+
+    echo -e "$image_tag==$hash_value_latest\n" >> $new_version_file
+    echo "" >> $new_version_file
+}
+
+main "$@"
