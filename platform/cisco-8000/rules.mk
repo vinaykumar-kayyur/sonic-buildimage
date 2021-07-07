4e1059e4a55b3b8e2d761bd707c26f1205b7fae6
Venkat Garigipati <venkatg@cisco.com>
2021-07-07 15:38:58 -0700

    update platform/cisco-8000/rules.mk to reflect the updated path & include file
    Signed-off-by: Venkat Garigipati <venkatg@cisco.com>

--- platform/cisco-8000/rules.mk
+++ platform/cisco-8000/rules.mk
@@ -2,7 +2,7 @@
 ## Top level platform rules.mk
 ##############################################################
 CISCO_PLATFORM_BRANCH := cisco-8000/master
-CISCO_PLATFORM_URL := https://$(GITUSER):$(GITTOKEN)@Cisco-sonic-8000/platform-cisco-8000.git
+CISCO_PLATFORM_URL := https://$(GITUSER):$(GITTOKEN)@github.com/Cisco-8000-sonic/platform-cisco-8000.git
 CISCO_PLATFORM_SRC=$(PLATFORM_PATH)/src
 CISCO_8000_SHA1 := f224715
 
@@ -10,5 +10,5 @@ $(shell if [ ! -d $(CISCO_PLATFORM_SRC) ]; then git clone -b $(CISCO_PLATFORM_BR
 $(shell if [ -d $(CISCO_PLATFORM_SRC) ]; then cd $(CISCO_PLATFORM_SRC) && git checkout $(CISCO_8000_SHA1))
 $(shell if [ -d $(CISCO_PLATFORM_SRC) ]; then $(CISCO_PLATFORM_SRC)/platform_setup.sh; fi)
 
-include $(CISCO_PLATFORM_SRC)/rules.dep
+include $(CISCO_PLATFORM_SRC)/rules.mk
 
