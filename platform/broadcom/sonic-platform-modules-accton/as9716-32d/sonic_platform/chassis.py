823e8c5bba8dfc5b675199303f28416cfb1e80f3
Sean Wu <sean_wu@edge-core.com>
2021-07-08 06:45:27 +0000

    [AS9716-32D] Support system health monitoring
    1. Implement SysLED on chassis.
    2. Add config to system health monitor.
    
    Signed-off-by: Sean Wu <sean_wu@edge-core.com>

--- platform/broadcom/sonic-platform-modules-accton/as9716-32d/sonic_platform/chassis.py
+++ platform/broadcom/sonic-platform-modules-accton/as9716-32d/sonic_platform/chassis.py
@@ -18,6 +18,8 @@ class Chassis(PddfChassis):
     PDDF Platform-specific Chassis class
     """
 
+    SYSLED_DEV_NAME = "DIAG_LED"
+
     def __init__(self, pddf_data=None, pddf_plugin_data=None):
         PddfChassis.__init__(self, pddf_data, pddf_plugin_data)
 
@@ -59,3 +61,12 @@ class Chassis(PddfChassis):
             return True, change_dict
         else:
             return True, change_dict
+
+    def initizalize_system_led(self):
+        return
+
+    def get_status_led(self):
+        return self.get_system_led(self.SYSLED_DEV_NAME)
+
+    def set_status_led(self, color):
+        return self.set_system_led(self.SYSLED_DEV_NAME, color)
