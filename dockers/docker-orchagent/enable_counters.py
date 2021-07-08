535c8d2bfdd70d619fdd755d653762769b816cee
noaOrMlnx <58519608+noaOrMlnx@users.noreply.github.com>
2021-07-09 00:18:09 +0300

    Update enable_counters.py
    Change the RIF enabling to be Mellanox specific

--- dockers/docker-orchagent/enable_counters.py
+++ dockers/docker-orchagent/enable_counters.py
@@ -12,13 +12,14 @@ def enable_counters():
     db = swsssdk.ConfigDBConnector()
     db.connect()
     enable_counter_group(db, 'PORT')
-    enable_counter_group(db, 'RIF')
     enable_counter_group(db, 'QUEUE')
     enable_counter_group(db, 'PFCWD')
     enable_counter_group(db, 'PG_WATERMARK')
     enable_counter_group(db, 'QUEUE_WATERMARK')
     enable_counter_group(db, 'BUFFER_POOL_WATERMARK')
     enable_counter_group(db, 'PORT_BUFFER_DROP')
+    if 'mlnx' in db.get_table('DEVICE_METADATA')['localhost'].get('platform'):
+        enable_counter_group(db, 'RIF')
 
 def get_uptime():
     with open('/proc/uptime') as fp:
