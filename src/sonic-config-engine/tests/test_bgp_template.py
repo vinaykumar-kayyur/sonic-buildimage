14c76e5522a19e049e9dc7304d32cfd599d3e780
jika <jika@contoso.com>
2021-07-27 07:40:04 +0000

    Rewrite bgp template tests with frr

--- src/sonic-config-engine/tests/test_bgp_template.py
+++ src/sonic-config-engine/tests/test_bgp_template.py
@@ -1,47 +1,61 @@
 import filecmp
 import os
 import subprocess
-import json
-import shutil
 
 from unittest import TestCase
+import tests.common_utils as utils
 
 class TestBgpTemplate(TestCase):
     def setUp(self):
         self.test_dir = os.path.dirname(os.path.realpath(__file__))
         self.minigraph_dir = os.path.join(self.test_dir, 'minigraph')
         self.script_file = os.path.join(self.test_dir, '..', 'sonic-cfggen')
-        self.m0_minigraph = os.path.join(self.minigraph_dir, 'MWH01-0100-0202-01M0.xml')
-        self.mc0_minigraph = os.path.join(self.minigraph_dir, 'MWH01-0100-0202-01MC0.xml')
-        self.hlx_port_config = os.path.join(self.test_dir, 'hlx-port-config.ini')
-        self.deployment_id_asn_map = os.path.join(self.test_dir, 'deployment_id_asn_map.yml')
-        self.device_metadata = os.path.join(self.test_dir, 'device_metadata.json')
         self.output_file = os.path.join(self.test_dir, 'output')
 
-    def run_script(self, argument):
-        print 'CMD: sonic-cfggen ' + argument
-        return subprocess.check_output(self.script_file + ' ' + argument, shell=True)
-    
-    def run_diff(self, file1, file2):
-        return subprocess.check_output('diff -u {} {} || true'.format(file1, file2), shell=True)
+        self.m0_minigraph = os.path.join(self.minigraph_dir, 'MWH01-0100-0202-01M0.xml')
+        self.mc0_minigraph = os.path.join(self.minigraph_dir, 'MWH01-0100-0202-01MC0.xml')
 
-    def run_bgpd_test(self, minigraph, portconfig, valid_result_file):
-        conf_template = os.path.join(self.test_dir, '..', '..', '..', 'dockers', 'docker-fpm-quagga', 'bgpd.conf.j2')
-        argument = '-m ' + minigraph + ' -p ' + portconfig + ' -t ' + conf_template + ' -y ' + self.deployment_id_asn_map + ' -j ' + self.device_metadata + ' > ' + self.output_file
+        self.hlx_port_config = os.path.join(self.test_dir, 'hlx-port-config.ini')
+
+        self.deployment_id_asn_map = os.path.join(self.test_dir, 'deployment_id_asn_map.yml')
+        self.device_metadata = os.path.join(self.test_dir, 'device_metadata.json')
+
+    def run_script(self, argument):
+        print('CMD: sonic-cfggen ' + argument)
+        output = subprocess.check_output(self.script_file + ' ' + argument, shell=True)
+
+        if utils.PY3x:
+            output = output.decode()
+
+        return output
+
+    def run_diff(self, file1, file2):
+        output = subprocess.check_output('diff -u {} {} || true'.format(file1, file2), shell=True)
+
+        if utils.PY3x:
+            output = output.decode()
+
+        return output
+
+    def run_bgpd_template_test(self, minigraph, port_config, valid_result_file):
+        template_dir = os.path.join(self.test_dir, '..', '..', '..', 'dockers', 'docker-fpm-frr', "frr")
+        conf_template = os.path.join(template_dir, 'bgpd', 'bgpd.conf.j2')
+        constants = os.path.join(self.test_dir, '..', '..', '..', 'files', 'image_config', 'constants', 'constants.yml')
+        argument = '-m {} -p {} -y {} -t {} -T {} > {}'.format(minigraph, port_config, constants, conf_template, template_dir, self.output_file)
         self.run_script(argument)
-        original_filename = os.path.join(self.test_dir, 'sample_output', valid_result_file)
-        r = filecmp.cmp(original_filename, self.output_file)
-        diff_output = self.run_diff(original_filename, self.output_file) if not r else ""
-        self.assertTrue(r, "Diff:\n" + diff_output)
+
+        expected = os.path.join(self.test_dir, 'sample_output', valid_result_file)
+        self.assertTrue(filecmp.cmp(expected, self.output_file), self.run_diff(expected, self.output_file))
 
     def test_bgpd_m0(self):
-        return self.run_bgpd_test(self.m0_minigraph, self.hlx_port_config, 'MWH01-0100-0202-01M0.bgpd.conf')
+        self.run_bgpd_template_test(self.m0_minigraph, self.hlx_port_config, 'MWH01-0100-0202-01M0.bgpd.conf')
 
     def test_bgpd_mc0(self):
-        return self.run_bgpd_test(self.mc0_minigraph, self.hlx_port_config, 'MWH01-0100-0202-01MC0.bgpd.conf')
+        self.run_bgpd_template_test(self.mc0_minigraph, self.hlx_port_config, 'MWH01-0100-0202-01MC0.bgpd.conf')
 
     def tearDown(self):
         try:
+            pass
             os.remove(self.output_file)
         except OSError:
             pass
