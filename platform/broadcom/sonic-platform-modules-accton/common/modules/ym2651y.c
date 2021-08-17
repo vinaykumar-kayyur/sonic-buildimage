51a6390687bb6f401dfaa7bf45d10ab505871f19
Jostar Yang <jostar_yang@accton.com.tw>
2021-08-17 17:06:04 +0800

    Modify psu_serial_numn to psu_mfr_serial

--- platform/broadcom/sonic-platform-modules-accton/common/modules/ym2651y.c
+++ platform/broadcom/sonic-platform-modules-accton/common/modules/ym2651y.c
@@ -42,7 +42,7 @@ enum chips {
 	YM2651,
 	YM2401,
 	YM2851,
-        YPEB1200AM
+    YPEB1200AM
 };
 
 /* Each client has this additional data
@@ -66,9 +66,9 @@ struct ym2651y_data {
     u16  fan_duty_cycle[2];  /* Register value */
     u8   fan_dir[4];     /* Register value */
     u8   pmbus_revision; /* Register value */
-    u8   serial_num[20]; /* Register value */
+    u8   mfr_serial[21]; /* Register value */
     u8   mfr_id[10];     /* Register value */
-    u8   mfr_model[10];  /* Register value */
+    u8   mfr_model[16];  /* Register value */
     u8   mfr_revsion[3]; /* Register value */
     u16  mfr_vin_min;    /* Register value */
     u16  mfr_vin_max;    /* Register value */
@@ -114,10 +114,10 @@ enum ym2651y_sysfs_attributes {
     PSU_FAN1_SPEED,
     PSU_FAN1_DUTY_CYCLE,
     PSU_PMBUS_REVISION,
-    PSU_SERIAL_NUM,
     PSU_MFR_ID,
     PSU_MFR_MODEL,
     PSU_MFR_REVISION,
+    PSU_MFR_SERIAL,
     PSU_MFR_VIN_MIN,
     PSU_MFR_VIN_MAX,
     PSU_MFR_VOUT_MIN,
@@ -143,10 +143,10 @@ static SENSOR_DEVICE_ATTR(psu_fan1_speed_rpm, S_IRUGO, show_linear, NULL, PSU_FA
 static SENSOR_DEVICE_ATTR(psu_fan1_duty_cycle_percentage, S_IWUSR | S_IRUGO, show_linear, set_fan_duty_cycle, PSU_FAN1_DUTY_CYCLE);
 static SENSOR_DEVICE_ATTR(psu_fan_dir,     S_IRUGO, show_ascii,     NULL, PSU_FAN_DIRECTION);
 static SENSOR_DEVICE_ATTR(psu_pmbus_revision, S_IRUGO, show_byte,   NULL, PSU_PMBUS_REVISION);
-static SENSOR_DEVICE_ATTR(psu_serial_num,         S_IRUGO, show_ascii,  NULL, PSU_SERIAL_NUM);
 static SENSOR_DEVICE_ATTR(psu_mfr_id,         S_IRUGO, show_ascii,  NULL, PSU_MFR_ID);
 static SENSOR_DEVICE_ATTR(psu_mfr_model,      S_IRUGO, show_ascii,  NULL, PSU_MFR_MODEL);
 static SENSOR_DEVICE_ATTR(psu_mfr_revision,    S_IRUGO, show_ascii, NULL, PSU_MFR_REVISION);
+static SENSOR_DEVICE_ATTR(psu_mfr_serial,     S_IRUGO, show_ascii,  NULL, PSU_MFR_SERIAL);
 static SENSOR_DEVICE_ATTR(psu_mfr_vin_min,    S_IRUGO, show_linear, NULL, PSU_MFR_VIN_MIN);
 static SENSOR_DEVICE_ATTR(psu_mfr_vin_max,    S_IRUGO, show_linear, NULL, PSU_MFR_VIN_MAX);
 static SENSOR_DEVICE_ATTR(psu_mfr_vout_min,   S_IRUGO, show_linear, NULL, PSU_MFR_VOUT_MIN);
@@ -178,10 +178,10 @@ static struct attribute *ym2651y_attributes[] = {
     &sensor_dev_attr_psu_fan1_duty_cycle_percentage.dev_attr.attr,
     &sensor_dev_attr_psu_fan_dir.dev_attr.attr,
     &sensor_dev_attr_psu_pmbus_revision.dev_attr.attr,
-    &sensor_dev_attr_psu_serial_num.dev_attr.attr,
     &sensor_dev_attr_psu_mfr_id.dev_attr.attr,
     &sensor_dev_attr_psu_mfr_model.dev_attr.attr,
     &sensor_dev_attr_psu_mfr_revision.dev_attr.attr,
+    &sensor_dev_attr_psu_mfr_serial.dev_attr.attr,
     &sensor_dev_attr_psu_mfr_vin_min.dev_attr.attr,
     &sensor_dev_attr_psu_mfr_vin_max.dev_attr.attr,
     &sensor_dev_attr_psu_mfr_pout_max.dev_attr.attr,
@@ -366,17 +366,17 @@ static ssize_t show_ascii(struct device *dev, struct device_attribute *da,
         }
         ptr = data->fan_dir;
         break;
-    case PSU_SERIAL_NUM: /* psu_serial_num */
-        ptr = data->serial_num;
+    case PSU_MFR_SERIAL: /* psu_mfr_serial */
+        ptr = data->mfr_serial+1; /* The first byte is the count byte of string. */
         break;
     case PSU_MFR_ID: /* psu_mfr_id */
-        ptr = data->mfr_id;
+        ptr = data->mfr_id+1; /* The first byte is the count byte of string. */
         break;
     case PSU_MFR_MODEL: /* psu_mfr_model */
-        ptr = data->mfr_model;
+        ptr = data->mfr_model+1; /* The first byte is the count byte of string. */
         break;
     case PSU_MFR_REVISION: /* psu_mfr_revision */
-        ptr = data->mfr_revsion;
+        ptr = data->mfr_revsion+1;
         break;
     default:
         return 0;
@@ -568,8 +568,8 @@ static struct ym2651y_data *ym2651y_update_device(struct device *dev)
 
     if (time_after(jiffies, data->last_updated + HZ + HZ / 2)
             || !data->valid) {
-        int i, status;
-        u8 command;
+        int i, status, length;
+        u8 command, buf;
         u8 fan_dir[5] = {0};
         struct reg_data_byte regs_byte[] = { {0x19, &data->capability},
             {0x20, &data->vout_mode},
@@ -606,6 +606,7 @@ static struct ym2651y_data *ym2651y_update_device(struct device *dev)
                 dev_dbg(&client->dev, "reg %d, err %d\n",
                         regs_byte[i].reg, status);
                 *(regs_byte[i].value) = 0;
+                goto exit;
             }
             else {
                 *(regs_byte[i].value) = status;
@@ -621,6 +622,7 @@ static struct ym2651y_data *ym2651y_update_device(struct device *dev)
                 dev_dbg(&client->dev, "reg %d, err %d\n",
                         regs_word[i].reg, status);
                 *(regs_word[i].value) = 0;
+                goto exit;
             }
             else {
                 *(regs_word[i].value) = status;
@@ -633,19 +635,28 @@ static struct ym2651y_data *ym2651y_update_device(struct device *dev)
 
         if (status < 0) {
             dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+            goto exit;
         }
 
         strncpy(data->fan_dir, fan_dir+1, ARRAY_SIZE(data->fan_dir)-1);
         data->fan_dir[ARRAY_SIZE(data->fan_dir)-1] = '\0';
 
-        /* Read serial_num */
+        /* Read mfr_serial */
         command = 0x9e;
-        status = ym2651y_read_block(client, command, data->serial_num,
-                                    ARRAY_SIZE(data->serial_num)-1);
-        data->serial_num[ARRAY_SIZE(data->serial_num)-1] = '\0';
-
-        if (status < 0)
+        length  = 1;
+        /* Read first byte to determine the length of data */
+        status = ym2651y_read_block(client, command, &buf, length);
+        if (status < 0) {
             dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+            goto exit;
+        }        
+        status = ym2651y_read_block(client, command, data->mfr_serial, buf+1);
+        data->mfr_serial[buf+1] = '\0';
+        if (status < 0)
+        {
+            dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+            goto exit;
+        }
 
         /* Read mfr_id */
         command = 0x99;
@@ -654,16 +665,28 @@ static struct ym2651y_data *ym2651y_update_device(struct device *dev)
         data->mfr_id[ARRAY_SIZE(data->mfr_id)-1] = '\0';
 
         if (status < 0)
+        {
             dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+            goto exit;
+        }
 
         /* Read mfr_model */
         command = 0x9a;
-        status = ym2651y_read_block(client, command, data->mfr_model,
-                                    ARRAY_SIZE(data->mfr_model)-1);
-        data->mfr_model[ARRAY_SIZE(data->mfr_model)-1] = '\0';
+        length  = 1;
+        /* Read first byte to determine the length of data */
+        status = ym2651y_read_block(client, command, &buf, length);
+        if (status < 0) {
+            dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+            goto exit;
+        }
+        status = ym2651y_read_block(client, command, data->mfr_model, buf+1);
+        data->mfr_model[buf+1] = '\0';
 
         if (status < 0)
+        {
             dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+            goto exit;
+        }
 
         /* Read mfr_revsion */
         command = 0x9b;
@@ -672,12 +695,16 @@ static struct ym2651y_data *ym2651y_update_device(struct device *dev)
         data->mfr_revsion[ARRAY_SIZE(data->mfr_revsion)-1] = '\0';
 
         if (status < 0)
+        {
             dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+            goto exit;
+        }
 
         data->last_updated = jiffies;
         data->valid = 1;
     }
 
+exit:
     mutex_unlock(&data->update_lock);
 
     return data;
