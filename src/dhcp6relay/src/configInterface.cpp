f7b7d4ae6d61ddac0cc4a8be9f842e8ea96189c3
kellyyeh <kellyyeh@microsoft.com>
2021-08-18 22:52:35 +0000

    Updated option parsing

--- src/dhcp6relay/src/configInterface.cpp
+++ src/dhcp6relay/src/configInterface.cpp
@@ -131,15 +131,10 @@ void processRelayNotification(std::deque<swss::KeyOpFieldsValuesTuple> &entries,
                 syslog(LOG_DEBUG, "key: %s, Operation: %s, f: %s, v: %s", vlan.c_str(), operation.c_str(), f.c_str(), v.c_str());
             }
             if(f == "dhcpv6_option|rfc6939_support") {
-                std::stringstream ss(v);
-                while (ss.good()) {
-                    std::string substr;
-                    getline(ss, substr, ',');
-                    if(substr == "true")
-                        intf.is_option_79 = true;
-                    else if(substr == "false")
-                        intf.is_option_79 = false;
-                }
+                if(v == "true")
+                    intf.is_option_79 = true;
+                else if(v == "false")
+                    intf.is_option_79 = false;
             }
         }
         vlans->push_back(intf);
