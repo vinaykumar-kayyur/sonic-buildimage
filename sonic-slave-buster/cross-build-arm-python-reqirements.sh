eba55b6409a944f1987e114aaa8a5fe50fe40558
gregshpit <gregshpit@gmail.com>
2021-09-12 10:48:43 +0300

    Update cross-build-arm-python-reqirements.sh
    Added support for both armhf and arm64 cross-build platform using $PY_PLAT environment variable.

--- sonic-slave-buster/cross-build-arm-python-reqirements.sh
+++ sonic-slave-buster/cross-build-arm-python-reqirements.sh
@@ -9,24 +9,24 @@ rm -rf $PRE_BUILT_TARGET_DIR && mkdir $PRE_BUILT_TARGET_DIR
 for pkg in "${pkgs[@]}"
 do
     rm -rf wheel-download
-    mkdir -p wheel-download/sources && pip3 download --platform linux_armv7l --no-deps --dest wheel-download $pkg
+    mkdir -p wheel-download/sources && pip3 download --platform $PY_PLAT --no-deps --dest wheel-download $pkg
     count=`ls -1 wheel-download/*.gz  2>/dev/null | wc -l`
     if [ $count != 0 ]
     then 
         tar xvf  wheel-download/*.gz -C wheel-download/sources
-        pushd wheel-download/sources/* && CC=$CC LDSHARED="$CC -shared" python3 -u -c "import setuptools, tokenize;__file__='setup.py';f=getattr(tokenize, 'open', open)(__file__);code=f.read().replace('\ri\n', '\n');f.close();exec(compile(code, __file__, 'exec'))" --verbose bdist_wheel --plat-name linux_armv7l -d ../../../$PRE_BUILT_TARGET_DIR && popd
+        pushd wheel-download/sources/* && CC=$CC LDSHARED="$CC -shared" python3 -u -c "import setuptools, tokenize;__file__='setup.py';f=getattr(tokenize, 'open', open)(__file__);code=f.read().replace('\ri\n', '\n');f.close();exec(compile(code, __file__, 'exec'))" --verbose bdist_wheel --plat-name $PY_PLAT -d ../../../$PRE_BUILT_TARGET_DIR && popd
     else
         cp wheel-download/*.whl $PRE_BUILT_TARGET_DIR
     fi 
 
     rm -rf wheel-download
 
-    mkdir -p wheel-download/sources && pip download --platform linux_armv7l --no-deps --dest wheel-download $pkg
+    mkdir -p wheel-download/sources && pip download --platform $PY_PLAT --no-deps --dest wheel-download $pkg
     count=`ls -1 wheel-download/*.gz  2>/dev/null | wc -l`
     if [ $count != 0 ]
     then 
         tar xvf  wheel-download/*.gz -C wheel-download/sources
-        pushd wheel-download/sources/* && CC=$CC LDSHARED="$CC -shared" python -u -c "import setuptools, tokenize;__file__='setup.py';f=getattr(tokenize, 'open', open)(__file__);code=f.read().replace('\ri\n', '\n');f.close();exec(compile(code, __file__, 'exec'))" --verbose bdist_wheel --plat-name linux_armv7l -d ../../../$PRE_BUILT_TARGET_DIR && popd
+        pushd wheel-download/sources/* && CC=$CC LDSHARED="$CC -shared" python -u -c "import setuptools, tokenize;__file__='setup.py';f=getattr(tokenize, 'open', open)(__file__);code=f.read().replace('\ri\n', '\n');f.close();exec(compile(code, __file__, 'exec'))" --verbose bdist_wheel --plat-name $PY_PLAT -d ../../../$PRE_BUILT_TARGET_DIR && popd
     else
         cp wheel-download/*.whl $PRE_BUILT_TARGET_DIR/
     fi 
