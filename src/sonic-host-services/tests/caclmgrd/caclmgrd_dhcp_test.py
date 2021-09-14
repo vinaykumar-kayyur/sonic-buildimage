1744d6bf40b261608b6a828f29664e760071e3c6
Tianrong Zhang <trzhang@microsoft.com>
2021-09-14 16:54:04 -0700

    update

--- src/sonic-host-services/tests/caclmgrd/caclmgrd_dhcp_test.py
+++ src/sonic-host-services/tests/caclmgrd/caclmgrd_dhcp_test.py
@@ -5,13 +5,9 @@ from swsscommon import swsscommon
 from parameterized import parameterized
 from sonic_py_common.general import load_module_from_source
 from unittest import TestCase, mock
-#from pyfakefs.fake_filesystem_unittest import patchfs
 
 from .test_dhcp_vectors import CACLMGRD_DHCP_TEST_VECTOR
 from tests.common.mock_configdb import MockConfigDb
-
-
-#DBCONFIG_PATH = '/var/run/redis/sonic-db/database_config.json'
 
 
 swsscommon.ConfigDBConnector = MockConfigDb
@@ -28,11 +24,7 @@ class TestCaclmgrdDhcp(TestCase):
         Test caclmgrd dhcp
     """
     @parameterized.expand(CACLMGRD_DHCP_TEST_VECTOR)
-#    @patchfs
     def test_caclmgrd_dhcp(self, test_name, test_data):
-#        if not os.path.exists(DBCONFIG_PATH):
-#            fs.create_file(DBCONFIG_PATH) # fake database_config.json
-
         MockConfigDb.set_config_db(test_data["config_db"])
 
         with mock.patch("caclmgrd.subprocess") as mocked_subprocess:
