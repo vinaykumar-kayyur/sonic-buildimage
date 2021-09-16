7139fe2d78f4711e7d6ed59ae2f21cc4e8bb8a1f
Anand Mehra (anamehra) <anamehra@cisco.com>
2021-09-16 15:56:22 -0700

    Added HTTP server support (#1383)
    This commit introduces HTTP server for Supervisor Card. HTTP will be used by Line Cards to download Sonic image
    file from Supervisor card. This may also be used to download configuration files or any other data file from Supervisor.
    
    HTTP server is enabled by a systemd service, http-server.service and runs on Supervisor only when
    /etc/sonic/http_config.yaml file is present.
    
    On Supervisor Card, the platform shall provide HTTP configuration via devices/chassis.yaml file.
    Following HTTP configuration may be provided by chassis.yaml:
    
    http-config:
        http_server_dir: <http server home dir path>
        http_server_ip: <ip>
        http_server_port: <port>
        start_http_server: 'yes'
    
    On Line Card, chassis.yaml shall only contain:
    http-config:
        http_server_ip: 127.1.1.1
    
    http_server_dir:         Optional; HTTP home directory, default: /var/www
    http_server_ip:        Required; IP to map http-server hostname used by HTTP Server.
                                      This should be the internal IP address used by the internal chassis network.
    http_server_port:    Optional; Port to bind, default: 8000.
    start_http_server:   Optional; If 'yes' or '1', start HTTP server on the card.
    
    chassis.yaml file is parsed by a new service, config-chassis.service. This service parses the http-config
    from chassis.yaml to set up pre-requisites for http-server.service. config-chassis.service is made generic
    to be used for other chassis-based configurations in the future.
    
    config-chassis.service does the following two operations for HTTP server based on http-config block:
    
    If start_http_server is 'yes' or '1', it creates /etc/sonic/http_config.yaml file with the HTTP configuration
    provided.
    Create the http_server_ip to http-server hostname mapping in /etc/hosts. Line Cards may also provide
    http-config with this entry only to create hostname mapping during boot.
    
    Signed-off-by: Anand Mehra <anamehra@cisco.com>

---
+++ files/image_config/config-chassis/config-chassis.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# 
+###########################################################################
+# config-chassis.py
+# 
+# Chassis preconfiguration service.
+# This module provides preconfiguration service for chassis based system
+# based on data provided by
+# /use/share/sonic/devices/<platform>/chassis.yaml file
+#
+###########################################################################
+# Copyright (c) 2021 Cisco Systems, Inc. and its affiliates.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###########################################################################
+
+import argparse
+import logging
+import os.path
+import subprocess
+import yaml
+from pathlib import Path
+
+ETC_HOSTS_FILE = '/etc/hosts'
+HTTP_CONFIG_FILE = '/etc/sonic/http_config.yaml'
+
+class ChassisConfig():
+    def __init__(self, args, **kw):
+        self.chassis_conf = None
+        self.http_config = None
+        if args.config_file:
+            chassis_yaml = args.config_file
+        else:
+            # get chassis.yaml path based on platform type
+            self.get_platform()
+            chassis_yaml=os.path.join('/usr/share/sonic/device', self.platform, 'chassis.yaml')
+
+        if os.path.isfile(chassis_yaml):
+            with open(chassis_yaml) as f:
+                self.chassis_conf = yaml.safe_load(f)
+
+            # Extract http config
+            self.http_config = self.chassis_conf.get('http_config')
+        else:
+            logger.info('chassis config file {} not present - skip config'.format(chassis_yaml))
+
+    def get_platform(self):
+        ''' Get the platform type from DEVICE_METADATA '''
+        SONIC_CFGGEN_PATH = '/usr/local/bin/sonic-cfggen'
+        PLATFORM_KEY = 'DEVICE_METADATA.localhost.platform'
+
+        proc = subprocess.Popen([SONIC_CFGGEN_PATH, '-H', '-v', PLATFORM_KEY],
+                            stdout=subprocess.PIPE,
+                            shell=False,
+                            stderr=subprocess.STDOUT)
+        outp = proc.communicate()[0]
+        proc.wait()
+        self.platform = outp.decode().rstrip('\n')
+
+    def config_http(self):
+        ''' Setup HTTP server configiguration.
+            if http_config is not none:
+              if start_http_server is 'yes' or '1',
+                 create HTTP_CONFIG_FILE file with the config. 
+                 This file is a pre-requisite to run HTTP server service.
+              if http_server_ip is not None, Set 'http-server' hostname with provided IP.
+                HTTP server shall bind to this hostname.'''
+        if self.http_config:
+            start_http_server = self.http_config.get('start_http_server')
+            if start_http_server == 'yes' or 'start_http_server' == '1':
+                # Create HTTP_CONFIG_FILE with http_config, generally on the Supervisor card.
+                logger.info('start_http_server is set, creating {}'.format(HTTP_CONFIG_FILE))
+                with open(HTTP_CONFIG_FILE, 'w') as f:
+                    yaml.dump(self.http_config, f)
+            http_server_ip = self.http_config.get('http_server_ip')
+            if http_server_ip:
+                # write 'httpd_server' hostname mapping in ETC_HOSTS_FILE if the hostname is not present
+                logger.info('write http-server hostname entry to {}'.format(ETC_HOSTS_FILE))
+                with open(ETC_HOSTS_FILE) as f:
+                    if 'http-server' not in f.read():
+                        etc_hosts_data = '{} {}\n'.format(self.http_config['http_server_ip'], 'http-server')
+                        f = open(ETC_HOSTS_FILE, 'a')
+                        f.write(etc_hosts_data)
+        else:
+            logger.info('http config is not available, skip server configuration.')
+
+    def run(self):
+        # HTTP config is provided, configure the service.
+        self.config_http()
+
+def main():
+    parser = argparse.ArgumentParser(description='Chassis configuration service')
+    parser.add_argument('--config-file', help='Chassis config yaml file', default=None)
+    args = parser.parse_args()
+
+    global logger
+    logger = logging.getLogger(__name__)
+
+    ChassisConfig(args).run()
+
+
+if __name__ == '__main__':
+    main()
---
+++ files/image_config/http-server/http-server.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# 
+###########################################################################
+# http-server.py
+# 
+# Python HTTP Server service.
+# This service should run only when /etc/sonic/http_server.yaml file is
+# available.
+#
+# http_server.yaml provides following parameters:
+#     http_server_port: Port to bind to, default: 8000
+#     http_server_dir:  HTTP server home directory path, default: /var/www/
+#
+###########################################################################
+# Copyright (c) 2021 Cisco Systems, Inc. and its affiliates.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###########################################################################
+
+import argparse
+import http.server
+import logging
+import os.path
+import socketserver
+import yaml
+
+from pathlib import Path
+
+HTTP_DEFAULT_CONFIG_FILE = '/etc/sonic/http_config.yaml'
+HTTP_DEFAULT_BIND_PORT = 8000
+HTTP_DEFAULT_DIR_PATH = '/var/www'
+http_dir = None
+
+class HTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=http_dir, **kwargs)
+
+class HttpServer():
+    def __init__(self, args, **kw):
+        self.http_config = None
+        logger.info('HTTP Server config file:{}'.format(args.config_file))
+
+        with open(args.config_file) as f:
+            # Load http server config from config_file
+            self.http_config = yaml.safe_load(f)
+            # Get Server directory path from config
+            global http_dir
+            http_dir = self.http_config.get('http_server_dir', HTTP_DEFAULT_DIR_PATH)
+
+            # Get Server port from config
+            self.http_server_port = self.http_config.get('http_server_port', HTTP_DEFAULT_BIND_PORT)
+
+            # Create HTTP home dir path if not present
+            path = Path(http_dir)
+            path.mkdir(parents=True, exist_ok=True)
+
+    def run(self):
+        if self.http_config:
+            with socketserver.TCPServer(('http-server', self.http_server_port), HTTPRequestHandler) as httpd:
+                logger.info('HTTP Server Port:{} home:{}'.format(self.http_server_port, http_dir))
+                httpd.serve_forever()
+
+def main():
+    parser = argparse.ArgumentParser(description='Python3 HTTP Server')
+    parser.add_argument('--config-file', help='HTTP Server config yaml file', default=HTTP_DEFAULT_CONFIG_FILE)
+    args = parser.parse_args()
+
+    global logger
+    logger = logging.getLogger(__name__)
+
+    HttpServer(args).run()
+
+if __name__ == '__main__':
+    main()
