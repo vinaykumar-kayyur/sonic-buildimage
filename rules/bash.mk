ecf587d6e8563986a8db070fc4471734c9b17c4d
Kalimuthu Velappan <kalimuthu.velappan@broadcom.com>
2021-09-29 20:27:16 -0700

    Changes for building community master in broadcom LSF servers

--- rules/bash.mk
+++ rules/bash.mk
@@ -6,7 +6,7 @@
 # Bash major release-number corresponding to Debian-11 (Bullseye)
 BASH_VERSION_MAJOR = 5.1
 # Bash complete release-number. This image contains all 5.1 fixes  up to patch '2'.
-BASH_VERSION_FULL = $(BASH_VERSION_MAJOR)-2
+BASH_VERSION_FULL = $(BASH_VERSION_MAJOR)-2+deb11u1
 
 export BASH_VERSION_MAJOR BASH_VERSION_FULL
 
