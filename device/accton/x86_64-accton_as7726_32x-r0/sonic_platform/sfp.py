b000cc3c6f6804e24d302538ad64dd2a53bee3dc
Sean Wu <sean_wu@edge-core.com>
2021-10-06 01:49:37 +0000

    Fix is_host() API might make OS stuck
    * The original design will use 'docker' to check operating environment.
      But that command might get stuck occasionally when it is called in the
      context of 'determine-reboot-cause.service'. It is suspetced the
      reason why it gets stucked is related to the service status of
      'containerd'.
    * Change to use 'which systemctl' to check operation environment.
      When systemctl exists, returns 0. (In Host os env)
      When it does not exist, returns 1. (In docker os env)
    
    Signed-off-by: Sean Wu <sean_wu@edge-core.com>

--- device/accton/x86_64-accton_as7726_32x-r0/sonic_platform/sfp.py
+++ device/accton/x86_64-accton_as7726_32x-r0/sonic_platform/sfp.py
@@ -124,7 +124,7 @@ class Sfp(SfpBase):
     # Path to sysfs
     PLATFORM_ROOT_PATH = "/usr/share/sonic/device"
     PMON_HWSKU_PATH = "/usr/share/sonic/hwsku"
-    HOST_CHK_CMD = "docker > /dev/null 2>&1"
+    HOST_CHK_CMD = "which systemctl > /dev/null 2>&1"
         
     PLATFORM = "x86_64-accton_as7726_32x-r0"
     HWSKU = "Accton-AS7726-32X"
