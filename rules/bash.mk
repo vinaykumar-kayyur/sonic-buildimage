ec852b42127c273c34335487c75b684c8d7e5b64
liuh-80 <58683130+liuh-80@users.noreply.github.com>
2021-10-08 16:41:12 +0800

    Change to use 5.1-2 for debian-11 bullseye.

--- rules/bash.mk
+++ rules/bash.mk
@@ -1,12 +1,12 @@
 # bash package
 #
-# Created to patch plugin support in the bash-package included in Debian-10 (Buster)
+# Created to patch plugin support in the bash-package included in Debian-11 (Bullseye)
 # release.
 
-# Bash major release-number corresponding to Debian-10 (Buster)
-BASH_VERSION_MAJOR = 5.0
-# Bash complete release-number. This image contains all 5.0 fixes  up to patch '4'.
-BASH_VERSION_FULL = $(BASH_VERSION_MAJOR)-4
+# Bash major release-number corresponding to Debian-11 (Bullseye)
+BASH_VERSION_MAJOR = 5.1
+# Bash complete release-number. This image contains all 5.1 fixes  up to patch '2'.
+BASH_VERSION_FULL = $(BASH_VERSION_MAJOR)-2
 
 export BASH_VERSION_MAJOR BASH_VERSION_FULL
 
