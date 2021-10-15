6bc00f5e4f9e779141020c07d6a095f1ddbdfac0
Jostar Yang <jostar_yang@accton.com.tw>
2021-10-15 18:10:19 +0800

    Correct the wait and timeout mechanism for better CPU usage

--- device/accton/x86_64-accton_as7816_64x-r0/sonic_platform/sfp.py
+++ device/accton/x86_64-accton_as7816_64x-r0/sonic_platform/sfp.py
@@ -122,7 +122,7 @@ class Sfp(SfpBase):
     # Path to sysfs
     PLATFORM_ROOT_PATH = "/usr/share/sonic/device"
     PMON_HWSKU_PATH = "/usr/share/sonic/hwsku"
-    HOST_CHK_CMD = "docker > /dev/null 2>&1"
+    HOST_CHK_CMD = "which systemctl > /dev/null 2>&1"
         
     PLATFORM = "x86_64-accton_as7816_64x-r0"
     HWSKU = "Accton-AS7816-64X"
