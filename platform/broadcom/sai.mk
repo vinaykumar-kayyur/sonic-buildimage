c7a707749d854aab34eb3406b01f5c2b5ec5c06b
Vineet Mittal <vineetmittal@microsoft.com>
2021-10-19 06:17:00 +0000

    Updated libsaibcm to 6.0 code

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,12 +1,12 @@
-BRCM_SAI = libsaibcm_5.0.0.8_amd64.deb
-$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm_5.0.0.8_amd64.deb?sv=2015-04-05&sr=b&sig=T%2FPesnOIeN9802mClMpgk8XFQbqjFAgCnJbbNHxijHo%3D&se=2035-05-13T21%3A34%3A26Z&sp=r"
-BRCM_SAI_DEV = libsaibcm-dev_5.0.0.8_amd64.deb
+BRCM_SAI = libsaibcm_6.0.0.10_amd64.deb
+$(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/6.0/master/libsaibcm_6.0.0.10_amd64.deb?sv=2020-08-04&st=2021-10-18T20%3A10%3A37Z&se=2030-10-19T20%3A10%3A00Z&sr=b&sp=r&sig=4pIluTLBmfOizMOlk93%2F73hrYH%2B0wnPizinxzgdZeuw%3D"
+BRCM_SAI_DEV = libsaibcm-dev_6.0.0.10_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm-dev_5.0.0.8_amd64.deb?sv=2015-04-05&sr=b&sig=X33hZLhRI3L6f4a5JFSlhJvoaTj%2B3zrmNBM9IzIA%2Bj4%3D&se=2035-05-13T21%3A35%3A58Z&sp=r"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/6.0/master/libsaibcm-dev_6.0.0.10_amd64.deb?sv=2020-08-04&st=2021-10-18T20%3A33%3A23Z&se=2030-10-19T20%3A33%3A00Z&sr=b&sp=r&sig=22zGsALAPStYDs42Ryegtf23zUvy9Hc0F9%2F6FOIdEn0%3D"
 
 # SAI module for DNX Asic family
-BRCM_DNX_SAI = libsaibcm_dnx_5.0.0.8_amd64.deb
-$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/5.0/master/libsaibcm_dnx_5.0.0.8_amd64.deb?sv=2015-04-05&sr=b&sig=uy0OW6ZhWjYntalZunEIIzHUztkOyI7TS3F73Sla9vY%3D&se=2035-05-13T21%3A37%3A06Z&sp=r"
+BRCM_DNX_SAI = libsaibcm_dnx_6.0.0.10_amd64.deb
+$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/6.0/master/libsaibcm_dnx_6.0.0.10_amd64.deb?sv=2020-08-04&st=2021-10-19T05%3A15%3A02Z&se=2030-10-20T05%3A15%3A00Z&sr=b&sp=r&sig=tibPfsZy32BbX%2F7IM4Hhy%2FBRh35yY5F7q9xJA7HaNe4%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 SONIC_ONLINE_DEBS += $(BRCM_DNX_SAI)
