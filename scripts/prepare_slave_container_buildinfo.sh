a57a904f1be2552517e4061c724f864a0002e9c3
xumia <xumia@microsoft.com>
2021-10-20 09:31:33 +0000

    Fix the unexpected python packages installed issue

--- scripts/prepare_slave_container_buildinfo.sh
+++ scripts/prepare_slave_container_buildinfo.sh
@@ -4,6 +4,19 @@ SLAVE_DIR=$1
 ARCH=$2
 DISTRO=$3
 
+# Filter the packages installed in the slave container
+function filter_packages()
+{
+    local file=$1
+    local build_path=$SLAVE_DIR/../files/build/versions/build
+    [ ! -d $build_path ] && return 0
+    while read line; do
+        if grep -q "$line" $build_path -r; then
+            echo $line
+        fi
+    done < $file
+}
+
 # Install the latest debian package sonic-build-hooks in the slave container
 sudo dpkg -i --force-overwrite $SLAVE_DIR/buildinfo/sonic-build-hooks_*.deb > /dev/null
 
@@ -21,4 +34,8 @@ touch ${BUILDINFO_PATH}/versions/versions-deb
 
 rm -f /etc/apt/preferences.d/01-versions-deb
 ([ "$ENABLE_VERSION_CONTROL_DEB" == "y" ] && [ -f $VERSION_DEB_PREFERENCE ]) && cp -f $VERSION_DEB_PREFERENCE /etc/apt/preferences.d/
+
+# Install the python packages if existing, skipping none existing packages
+([ "$ENABLE_VERSION_CONTROL_PY2" == "y" ] && [ -f $BUILDINFO_PATH/versions/versions-py2 ]) && filter_packages $BUILDINFO_PATH/versions/versions-py2 | sudo xargs -n 1 pip2 install >$LOG_PATH/error.log 2>&1 
+([ "$ENABLE_VERSION_CONTROL_PY3" == "y" ] && [ -f $BUILDINFO_PATH/versions/versions-py3 ]) && filter_packages $BUILDINFO_PATH/versions/versions-py3 | sudo xargs -n 1 pip3 install >$LOG_PATH/error.log 2>&1
 exit 0
