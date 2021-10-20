d87e159a6d2d9f81074d4294eb0008deac014e28
xumia <xumia@microsoft.com>
2021-10-20 13:50:22 +0000

    change to install python package without sudo

--- scripts/prepare_slave_container_buildinfo.sh
+++ scripts/prepare_slave_container_buildinfo.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+  
 SLAVE_DIR=$1
 ARCH=$2
 DISTRO=$3
@@ -21,10 +21,10 @@ function filter_packages()
 sudo dpkg -i --force-overwrite $SLAVE_DIR/buildinfo/sonic-build-hooks_*.deb > /dev/null
 
 # Enable the build hooks
-symlink_build_hooks
+sudo symlink_build_hooks
 
 # Build the slave running config
-cp -rf $SLAVE_DIR/buildinfo/* /usr/local/share/buildinfo/
+sudo cp -rf $SLAVE_DIR/buildinfo/* /usr/local/share/buildinfo/
 . /usr/local/share/buildinfo/scripts/buildinfo_base.sh
 
 # Build the slave version config
@@ -32,10 +32,10 @@ cp -rf $SLAVE_DIR/buildinfo/* /usr/local/share/buildinfo/
 scripts/versions_manager.py generate -t "/usr/local/share/buildinfo/versions" -n "build-${SLAVE_DIR}" -d "$DISTRO" -a "$ARCH"
 touch ${BUILDINFO_PATH}/versions/versions-deb
 
-rm -f /etc/apt/preferences.d/01-versions-deb
-([ "$ENABLE_VERSION_CONTROL_DEB" == "y" ] && [ -f $VERSION_DEB_PREFERENCE ]) && cp -f $VERSION_DEB_PREFERENCE /etc/apt/preferences.d/
+sudo rm -f /etc/apt/preferences.d/01-versions-deb
+([ "$ENABLE_VERSION_CONTROL_DEB" == "y" ] && [ -f $VERSION_DEB_PREFERENCE ]) && sudo cp -f $VERSION_DEB_PREFERENCE /etc/apt/preferences.d/
 
 # Install the python packages if existing, skipping none existing packages
-([ "$ENABLE_VERSION_CONTROL_PY2" == "y" ] && [ -f $BUILDINFO_PATH/versions/versions-py2 ]) && filter_packages $BUILDINFO_PATH/versions/versions-py2 | sudo xargs -n 1 pip2 install >$LOG_PATH/error.log 2>&1 
-([ "$ENABLE_VERSION_CONTROL_PY3" == "y" ] && [ -f $BUILDINFO_PATH/versions/versions-py3 ]) && filter_packages $BUILDINFO_PATH/versions/versions-py3 | sudo xargs -n 1 pip3 install >$LOG_PATH/error.log 2>&1
+([ "$ENABLE_VERSION_CONTROL_PY2" == "y" ] && [ -f $BUILDINFO_PATH/versions/versions-py2 ]) && filter_packages $BUILDINFO_PATH/versions/versions-py2 | xargs -n 1 pip2 install >>$LOG_PATH/error.log 2>&1
+([ "$ENABLE_VERSION_CONTROL_PY3" == "y" ] && [ -f $BUILDINFO_PATH/versions/versions-py3 ]) && filter_packages $BUILDINFO_PATH/versions/versions-py3 | xargs -n 1 pip3 install >>$LOG_PATH/error.log 2>&1
 exit 0
