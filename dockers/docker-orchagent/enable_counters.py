03c5b78011b835d2f4dfc5c1d82e995f7ac6e43d
Sudharsan Dhamal Gopalarathnam <sudharsand@nvidia.com>
2021-10-29 22:24:36 +0000

    Disabling counter group tunnel as per review comments

--- dockers/docker-orchagent/enable_counters.py
+++ dockers/docker-orchagent/enable_counters.py
@@ -45,7 +45,6 @@ def enable_counters():
     enable_counter_group(db, 'QUEUE_WATERMARK')
     enable_counter_group(db, 'BUFFER_POOL_WATERMARK')
     enable_counter_group(db, 'PORT_BUFFER_DROP')
-    enable_counter_group(db, 'TUNNEL')
     enable_rates()
 
 
