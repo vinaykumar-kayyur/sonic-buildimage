1e24fe8cea6d946489c72ed373696d37f2b36985
liuh-80 <58683130+liuh-80@users.noreply.github.com>
2021-11-10 14:59:59 +0800

    Fix the bash plugin not installed issue.

--- slave.mk
+++ slave.mk
@@ -922,6 +922,7 @@ $(addprefix $(TARGET_PATH)/, $(SONIC_INSTALLERS)) : $(TARGET_PATH)/% : \
                 $(SONIC_UTILITIES_DATA) \
                 $(SONIC_HOST_SERVICES_DATA) \
                 $(BASH)) \
+                $(BASH_TACPLUS) \
         $$(addprefix $(TARGET_PATH)/,$$($$*_DOCKERS)) \
         $$(addprefix $(TARGET_PATH)/,$$(SONIC_PACKAGES_LOCAL)) \
         $$(addprefix $(FILES_PATH)/,$$($$*_FILES)) \
