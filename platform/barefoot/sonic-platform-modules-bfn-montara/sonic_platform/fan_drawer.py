32e6ecbc1d23cd80f325bba4ba10b70b705467db
Andriy Kokhan <andriyx.kokhan@intel.com>
2021-11-18 10:06:14 -0800

    [BFN] Updated platform APIs impl
    Signed-off-by: Andriy Kokhan <andriyx.kokhan@intel.com>

--- platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/fan_drawer.py
+++ platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/fan_drawer.py
@@ -59,9 +59,10 @@ class Fan(FanBase):
 
 # FanDrawer -> FanDrawerBase -> DeviceBase
 class FanDrawer(FanDrawerBase):
-    def __init__(self):
+    def __init__(self, fantray_index):
         # For now we return only present fans
         self._fan_list = [Fan(i.fan_num) for i in _fan_info_get_all()]
+        self.fantrayindex = fantray_index + 1
 
     # DeviceBase interface methods:
     def get_name(self):
@@ -73,5 +74,68 @@ class FanDrawer(FanDrawerBase):
     def get_status(self):
         return True
 
+    def get_position_in_parent(self):
+        """
+        Retrieves 1-based relative physical position in parent device.
+        Returns:
+            integer: The 1-based relative physical position in parent
+            device or -1 if cannot determine the position
+        """
+        return self.fantrayindex
+
+    def is_replaceable(self):
+        """
+        Indicate whether this fan drawer is replaceable.
+        Returns:
+            bool: True if it is replaceable, False if not
+        """
+        return True
+
+    def get_model(self):
+        """
+        Retrieves the part number of the fan drawer
+        Returns:
+            string: Part number of fan drawer
+        """
+        return 'N/A'
+
+    def get_serial(self):
+        """
+        Retrieves the serial number of the fan drawer
+        Returns:
+            string: Serial number of the fan drawer
+        """
+        return 'N/A'
+
+    def set_status_led(self, color):
+        """
+        Set led to expected color
+        Args:
+            color: A string representing the color with which to set the
+            fan module status LED
+        Returns:
+            bool: True if set success, False if fail.
+        """
+        # Fan tray status LED controlled by BMC
+        # Should return True to avoid thermalctld alarm
+        return False
+
+    def get_status_led(self):
+        """
+        Gets the state of the fan drawer LED
+        Returns:
+            A string, one of the predefined STATUS_LED_COLOR_* strings above
+        """
+        return "N/A"
+
+    def get_maximum_consumed_power(self):
+        """
+        Retrives the maximum power drawn by Fan Drawer
+        Returns:
+            A float, with value of the maximum consumable power of the
+            component.
+        """
+        return 36.0
+
 def fan_drawer_list_get():
-    return [FanDrawer()]
+    return [FanDrawer(0)]
