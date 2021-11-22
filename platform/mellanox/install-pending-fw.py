fb8f44279df3c819f6d7b09348d7e4431f325917
Alexander Allen <arallen@nvidia.com>
2021-11-22 22:21:47 +0000

    Finish implementation

--- platform/mellanox/install-pending-fw.py
+++ platform/mellanox/install-pending-fw.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python
 
-from config.fwutil.lib import PlatformDataProvider, PlatformComponentsParser
+from config.fwutil.lib import ComponentStatusProvider, PlatformComponentsParser
+from sonic_platform.component import ComponentCPLD
+
+#Globals
+CPLD_FLAG = None
 
 # Init platform chassis helper classes
-pdp = PlatformDataProvider()
-pcp = PlatformComponentsParser(pdp.is_modular_chassis())
 csp = ComponentStatusProvider()
+pcp = PlatformComponentsParser(csp.is_modular_chassis())
+
 
 # Parse update status file
 update_status = csp.read_au_status_file_if_exists()
+
+if update_status is None:
+    quit()
 
 # Parse platform components file
 pcp.parse_platform_components()
 
 
 # Iterate each component in the status file
-for key, status in update_status.items():
+for boot_type, components in update_status:
+    for comp in components:
 
-    if len(key.split('/')) == 3:
-        chassis_name, mod_name, comp_name = key.split('/')
-        component = pcp.module_component_map[mod_name][comp_name]
-    else:
-        chassis_name, comp_name = key.split('/')
-        component = pcp.chassis_component_map[chassis_name][comp_name]
+        if comp["info"] != "scheduled": continue
 
+        key = comp['comp']
 
-# Check status file for "installed" firmware
+        if len(key.split('/')) == 3:
+            chassis_name, mod_name, comp_name = key.split('/')
+            comp_fw = pcp.module_component_map[mod_name][comp_name]
+            component = csp.module_component_map[mod_name][comp_name]
+        else:
+            chassis_name, comp_name = key.split('/')
+            comp_fw = pcp.chassis_component_map[chassis_name][comp_name]
+            component = csp.chassis_component_map[chassis_name][comp_name]
 
+        fw_file = comp_fw['firmware']
 
-# Run install_firmware function for these
+        if type(component) == ComponentCPLD:
+            if CPLD_FLAG is not None:
+                print("WARNING: Multiple CPLD firmwares defined. Some CPLD updates may not fully complete.")
+            CPLD_FLAG = (component, fw_filw)
+        else:
+            component.install_firmware(fw_file)
 
-
-# IF CPLD run update last after all other installs. 
+# Run CPLD refresh last if needed
+if CPLD_FLAG is not None:
+    CPLD_FLAG[0].update_firmware(CPLD_FLAG[1])
+    
