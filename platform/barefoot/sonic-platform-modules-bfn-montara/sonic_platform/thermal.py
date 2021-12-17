391a35e25ee9deb2c548bd3336dc0a619da0a483
Sean Wu <sean_wu@edge-core.com>
2021-12-17 10:04:55 +0000

    [Wedge100bf] Add HW-defined thermal thresholds
    Signed-off-by: Sean Wu <sean_wu@edge-core.com>

--- platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/thermal.py
+++ platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/thermal.py
@@ -3,8 +3,14 @@ try:
 
     from sonic_platform.bfn_extensions.platform_sensors import platform_sensors_get
     from sonic_platform_base.thermal_base import ThermalBase
+    from collections import namedtuple
 except ImportError as e:
     raise ImportError (str(e) + "- required module not found")
+
+
+Threshold = namedtuple('Threshold', ['high_crit', 'high_err', 'high_warn',
+                       'low_warn', 'low_err', 'low_crit'], defaults=[0]*6)
+
 
 '''
 data argument is in "sensors -A -u" format, example:
@@ -68,6 +74,22 @@ def _value_get(d: dict, key_prefix, key_suffix=''):
 
 # Thermal -> ThermalBase -> DeviceBase
 class Thermal(ThermalBase):
+    _thresholds = {
+        "com_e_driver-i2c-4-33:memory-temp": Threshold(85.0, 80.75, 76.5),
+        "com_e_driver-i2c-4-33:cpu-temp":    Threshold(105.0, 99.75, 94.5),
+        "pfe1100-i2c-7-59:temp1":            Threshold(50.0, 47.5, 45.0),
+        "pfe1100-i2c-7-59:temp2":            Threshold(90.0, 85.5, 81.0),
+        "pfe1100-i2c-7-5a:temp1":            Threshold(50.0, 47.5, 45.0),
+        "pfe1100-i2c-7-5a:temp2":            Threshold(90.0, 85.5, 81.0),
+        "tmp75-i2c-3-48:outlet-middle-temp": Threshold(80.0, 76.0, 72.0),
+        "tmp75-i2c-3-49:inlet-middle-temp":  Threshold(60.0, 57.0, 54.0),
+        "tmp75-i2c-3-4a:inlet-left-temp":    Threshold(60.0, 57.0, 54.0),
+        "tmp75-i2c-3-4b:switch-temp":        Threshold(105.0, 99.75, 94.5),
+        "tmp75-i2c-3-4c:inlet-right-temp":   Threshold(60.0, 57.0, 54.0),
+        "tmp75-i2c-8-48:outlet-right-temp":  Threshold(80.0, 76.0, 72.0),
+        "tmp75-i2c-8-49:outlet-left-temp":   Threshold(80.0, 76.0, 72.0),
+    }
+
     def __init__(self, chip, label):
         self.__chip = chip
         self.__label = label
@@ -83,11 +105,38 @@ class Thermal(ThermalBase):
     def get_temperature(self) -> float:
         return float(self.__get('temp', 'input'))
 
-    def get_high_threshold(self) -> float:
+    def get_high_warning_threshold(self) -> float:
+        if self.get_name() in self._thresholds:
+            return self._thresholds[self.get_name()].high_warn
         return float(self.__get('temp', 'max'))
 
+    def get_high_threshold(self) -> float:
+        if self.get_name() in self._thresholds:
+            return self._thresholds[self.get_name()].high_err
+        #cpu core case
+        temp_warn = self.get_high_warning_threshold()
+        temp_crit = self.get_high_critical_threshold()
+        return (temp_warn + temp_crit) / 2
+
     def get_high_critical_threshold(self) -> float:
+        if self.get_name() in self._thresholds:
+            return self._thresholds[self.get_name()].high_crit
         return float(self.__get('temp', 'crit'))
+
+    def get_low_warning_threshold(self) -> float:
+        if self.get_name() in self._thresholds:
+            return self._thresholds[self.get_name()].low_warn
+        return 0
+
+    def get_low_threshold(self) -> float:
+        if self.get_name() in self._thresholds:
+            return self._thresholds[self.get_name()].low_err
+        return 0
+
+    def get_low_critical_threshold(self) -> float:
+        if self.get_name() in self._thresholds:
+            return self._thresholds[self.get_name()].low_crit
+        return 0
 
     # DeviceBase interface methods:
     def get_name(self):
