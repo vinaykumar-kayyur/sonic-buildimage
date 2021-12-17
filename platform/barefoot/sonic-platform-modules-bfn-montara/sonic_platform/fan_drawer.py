cbd35ddbb3bd8819bfa340bd4d811318ef918e12
Andriy Kokhan <andriyx.kokhan@intel.com>
2021-12-17 05:32:27 -0800

    Updated fan_drawer.py

--- platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/fan_drawer.py
+++ platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/fan_drawer.py
@@ -1,7 +1,7 @@
 try:
     from sonic_platform_base.fan_drawer_base import FanDrawerBase
     from sonic_py_common import device_info
-    from .fan import Fan
+    from sonic_platform.fan import Fan
 except ImportError as e:
     raise ImportError (str(e) + "- required module not found")
 
