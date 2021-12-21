ede36b97667861385b40d9add430510ef424a1a8
Yong Zhao <yozhao@microsoft.com>
2021-12-20 16:44:28 -0800

    [telemetry] Fix the indent issue in the script `telemetry.sh`.
    Signed-off-by: Yong Zhao <yozhao@microsoft.com>

--- dockers/docker-sonic-telemetry/telemetry.sh
+++ dockers/docker-sonic-telemetry/telemetry.sh
@@ -32,54 +32,54 @@ do
         logger "The file path of Root CA certificate in CONFIG_DB is: $CA_CRT"
 
         if [[ -f $SERVER_CRT && -f $SERVER_KEY && -f $CA_CRT ]]; then
-	        logger "Succeeded in retrieving server certificate, key and Root CA certificate from HwProxy."
-		    TELEMETRY_ARGS+=" --server_crt $SERVER_CRT --server_key $SERVER_KEY --ca_crt $CA_CRT"
-		    break
-		else
-		    logger "Failed to retrieve server certificate, key or Root CA certificate from HwProxy!"
-		fi
+            logger "Succeeded in retrieving server certificate, key and Root CA certificate from HwProxy."
+            TELEMETRY_ARGS+=" --server_crt $SERVER_CRT --server_key $SERVER_KEY --ca_crt $CA_CRT"
+            break
+        else
+            logger "Failed to retrieve server certificate, key or Root CA certificate from HwProxy!"
+        fi
 
         SERVER_CRT_ACMS=$(echo $CERTS | jq -r '.server_crt_acms')
-	    SERVER_KEY_ACMS=$(echo $CERTS | jq -r '.server_key_acms')
-	    CA_CRT_ACMS=$(echo $CERTS | jq -r '.ca_crt_acms')
+        SERVER_KEY_ACMS=$(echo $CERTS | jq -r '.server_key_acms')
+        CA_CRT_ACMS=$(echo $CERTS | jq -r '.ca_crt_acms')
 
         logger "Trying to retrieve server certificate, key and Root CA certificate managed by ACMS ..."
-	    logger "The file path of server certificate in CONFIG_DB is: $SERVER_CRT_ACMS"
-	    logger "The file path of server private key in CONFIG_DB is: $SERVER_KEY_ACMS"
-	    logger "The file path of Root CA certificate in CONFIG_DB is: $CA_CRT_ACMS"
+        logger "The file path of server certificate in CONFIG_DB is: $SERVER_CRT_ACMS"
+        logger "The file path of server private key in CONFIG_DB is: $SERVER_KEY_ACMS"
+        logger "The file path of Root CA certificate in CONFIG_DB is: $CA_CRT_ACMS"
 
         if [[ -f $SERVER_CRT_ACMS && -f $SERVER_KEY_ACMS && -f $CA_CRT_ACMS ]]; then
-	        logger "Succeeded in retrieving the certificate, key and Root CA certificate from ACMS."
-		    continue
+            logger "Succeeded in retrieving the certificate, key and Root CA certificate from ACMS."
+            continue
         else
-	        logger "Failed to retrieve server certificate, key or Root CA certificate from ACMS!"
-		fi
+            logger "Failed to retrieve server certificate, key or Root CA certificate from ACMS!"
+        fi
 	elif [ -n "$X509" ]; then
-	    SERVER_CRT=$(echo $X509 | jq -r '.server_crt')
-	    SERVER_KEY=$(echo $X509 | jq -r '.server_key')
+        SERVER_CRT=$(echo $X509 | jq -r '.server_crt')
+        SERVER_KEY=$(echo $X509 | jq -r '.server_key')
 	    if [ -z $SERVER_CRT  ] || [ -z $SERVER_KEY  ]; then
-	        TELEMETRY_ARGS+=" --insecure"
-		else
-		    TELEMETRY_ARGS+=" --server_crt $SERVER_CRT --server_key $SERVER_KEY "
-		fi
+            TELEMETRY_ARGS+=" --insecure"
+        else
+            TELEMETRY_ARGS+=" --server_crt $SERVER_CRT --server_key $SERVER_KEY "
+        fi
 
         CA_CRT=$(echo $X509 | jq -r '.ca_crt')
-	    if [ ! -z $CA_CRT ]; then
-	        TELEMETRY_ARGS+=" --ca_crt $CA_CRT"
-		fi
-		break
-	else
-	    TELEMETRY_ARGS+=" --noTLS"
-	    break
-	fi
+        if [ ! -z $CA_CRT ]; then
+            TELEMETRY_ARGS+=" --ca_crt $CA_CRT"
+        fi
+        break
+    else
+        TELEMETRY_ARGS+=" --noTLS"
+        break
+    fi
 
-	logger "Sleeping 3600 seconds and checks the existence of secrets again ..."
-	sleep 3600
+    logger "Sleeping 3600 seconds and checks the existence of secrets again ..."
+    sleep 3600
 done
 
 # If no configuration entry exists for TELEMETRY, create one default port
 if [ -z "$GNMI" ]; then
-	PORT=8080
+    PORT=8080
 else
     PORT=$(echo $GNMI | jq -r '.port')
 fi
