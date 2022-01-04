2e0362a502d0e87d318c0df014207480f7c64d1e
Junchao-Mellanox <57339448+Junchao-Mellanox@users.noreply.github.com>
2022-01-04 09:19:21 +0800

    Fix LGTM warning

--- platform/mellanox/mlnx-platform-api/sonic_platform/thermal_manager.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/thermal_manager.py
@@ -1,6 +1,5 @@
 import os
 from sonic_platform_base.sonic_thermal_control.thermal_manager_base import ThermalManagerBase
-from sonic_platform_base.sonic_thermal_control.thermal_policy import ThermalPolicy
 from .thermal_actions import *
 from .thermal_conditions import *
 from .thermal_infos import *
