e7ed71493133582577e18b43187b115c8b8d9a03
Sean Wu <sean_wu@edge-core.com>
2022-01-13 09:09:40 +0000

    [AS5835-54X] Enable pca954x idle_disconnect
    Signed-off-by: Sean Wu <sean_wu@edge-core.com>

--- platform/broadcom/sonic-platform-modules-accton/as5835-54x/utils/pddf_post_device_create.sh
+++ platform/broadcom/sonic-platform-modules-accton/as5835-54x/utils/pddf_post_device_create.sh
@@ -22,6 +22,16 @@ set_system_led_diag_loc_to_off()
     i2cset -y -f 3 0x60 0xa 0x1c &>/dev/null
 }
 
+set_pca_mux_idle_disconnect()
+{
+    echo -2 | tee /sys/bus/i2c/drivers/pca954x/*-00*/idle_state >& /dev/null
+    if [ $? -ne 0 ]; then
+        echo "Fail to set pca954x mux idle disconnect"
+        exit 2
+    fi
+}
+
 set_qsfp_reset_to_normal_state
 set_sfp_tx_disable_to_disable
 set_system_led_diag_loc_to_off
+set_pca_mux_idle_disconnect
