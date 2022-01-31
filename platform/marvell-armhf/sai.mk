5fff2f32ce5507d167375da00d49b9d8f5d995a7
Rajkumar-Marvell <54936542+rajkumar38@users.noreply.github.com>
2022-01-31 11:27:23 +0530

    [Marvell] Update armhf SAI deb version 1.9.1 (#9865)
    Move marvell armhf SAI deb to 1.9.1 to address build failures.
    
    Signed-off-by: Rajkumar Pennadam Ramamoorthy <rpennadamram@marvell.com>

--- platform/marvell-armhf/sai.mk
+++ platform/marvell-armhf/sai.mk
@@ -1,6 +1,6 @@
 # Marvell SAI
 
-export MRVL_SAI_VERSION = 1.8.1-1
+export MRVL_SAI_VERSION = 1.9.1-1
 export MRVL_SAI = mrvllibsai_$(MRVL_SAI_VERSION)_$(PLATFORM_ARCH).deb
 export MRVL_SAI_PACKAGE = mrvllibsai_$(PLATFORM_ARCH)_$(MRVL_SAI_VERSION).deb
 
