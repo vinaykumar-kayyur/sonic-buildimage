89d3ead8d5d37b6aaba26b4d3cf2894873a936a3
Vadym Yashchenko <vadymx.yashchenko@intel.com>
2022-01-31 02:21:35 -0800

    Revised set_high_thershold and set_low_thershold methobs in the thermal.py
    Signed-off-by: Vadym Yashchenko <vadymx.yashchenko@intel.com>

--- platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/thermal.py
+++ platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/thermal.py
@@ -1,6 +1,6 @@
 try:
     import subprocess
-
+    from collections import namedtuple
     from bfn_extensions.platform_sensors import platform_sensors_get
     from sonic_platform_base.thermal_base import ThermalBase
 except ImportError as e:
@@ -18,6 +18,8 @@ Core 0:
   temp2_input: 37.000
   ...
 '''
+Threshold = namedtuple('Threshold', ['crit', 'max', 'min', 'alarm'], defaults=[0.1]*4)
+
 def _sensors_chip_parsed(data: str):
     def kv(line):
         k, v, *_ = [t.strip(': ') for t in line.split(':') if t] + ['']
@@ -68,23 +70,71 @@ def _value_get(d: dict, key_prefix, key_suffix=''):
 
 # Thermal -> ThermalBase -> DeviceBase
 class Thermal(ThermalBase):
+    _thresholds = {
+            "com_e_driver-i2c-4-33:memory-temp": Threshold(85.0, 80.75, 0.2, 0.1),
+            "com_e_driver-i2c-4-33:cpu-temp":    Threshold(99.9, 99.75, 0.2, 0.1),
+            "psu_driver-i2c-7-5a:psu1-temp1":    Threshold(50.0, 47.5, 0.2, 0.1),
+            "psu_driver-i2c-7-5a:psu1-temp2":    Threshold(90.0, 85.5, 0.2, 0.1),
+            "psu_driver-i2c-7-5a:psu1-temp3":    Threshold(50.0, 47.5, 0.2, 0.1),
+            "tmp75-i2c-3-48:chip-temp":          Threshold(90.0, 85.5, 0.2, 0.1),
+            "tmp75-i2c-3-49:exhaust2-temp":      Threshold(80.0, 76.0, 0.2, 0.1),
+            "tmp75-i2c-3-4a:exhaust-temp":       Threshold(60.0, 57.0, 0.2, 0.1),
+            "tmp75-i2c-3-4b:intake-temp":        Threshold(60.0, 57.0, 0.2, 0.1),
+            "tmp75-i2c-3-4c:tofino-temp":        Threshold(99.9, 99.75, 0.2, 0.1),
+            "tmp75-i2c-3-4d:intake2-temp":       Threshold(60.0, 57.0, 0.2, 0.1),
+            "coretemp-isa-0000:package-id-0":    Threshold(80.0, 76.0, 0.2, 0.1),
+            "coretemp-isa-0000:core-0":          Threshold(99.9, 82.0, 0.2, 0.1),
+            "coretemp-isa-0000:core-1":          Threshold(99.9, 82.0, 0.2, 0.1),
+            "coretemp-isa-0000:core-2":          Threshold(99.9, 82.0, 0.2, 0.1),
+            "coretemp-isa-0000:core-3":          Threshold(99.9, 82.0, 0.2, 0.1),
+            # add from Montara"
+            "psu_driver-i2c-7-59:psu2-temp1":    Threshold(50.0, 47.5, 0.2, 0.1),
+            "psu_driver-i2c-7-59:psu2-temp2":    Threshold(90.0, 85.5, 0.2, 0.1),
+            "tmp75-i2c-8-48:outlet-right-temp":  Threshold(60.0, 57.0, 0.2, 0.1),
+            "tmp75-i2c-8-49:outlet-left-temp":   Threshold(60.0, 57.0, 0.2, 0.1)
+    }
+
     def __init__(self, chip, label, index = 0):
         self.__chip = chip
         self.__label = label
         self.__name = f"{chip}:{label}".lower().replace(' ', '-')
         self.__collect_temp = []
         self.__index = index
+        self.__high_threshold = None
+        self.__low_threshold = None
 
     def __get(self, attr_prefix, attr_suffix):
         sensor_data = _sensors_get().get(self.__chip, {}).get(self.__label, {})
         value = _value_get(sensor_data, attr_prefix, attr_suffix)
-        return value if value is not None else -999.9
+        if value is not None:
+            return value
+        elif attr_prefix == 'temp':
+            if attr_suffix == 'crit':
+                return self._thresholds[self.__name].crit
+            elif attr_suffix == 'max':
+                if self.__high_threshold is None:
+                    return self._thresholds[self.__name].max
+                else:
+                    return self.__high_threshold
+            elif attr_suffix == 'min':
+                if self.__low_threshold is None:
+                    return self._thresholds[self.__name].min
+                else:
+                    return self.__low_threshold
+            elif attr_suffix == 'alarm':
+                return self._thresholds[self.__name].alarm
+            else:
+                return 1.0
+        else:
+            return 0.1
 
     # ThermalBase interface methods:
     def get_temperature(self) -> float:
         temp = self.__get('temp', 'input')
         self.__collect_temp.append(float(temp))
         self.__collect_temp.sort()
+        if len(self.__collect_temp) == 3:
+            del self.__collect_temp[1]
         return float(temp)
 
     def get_high_threshold(self) -> float:
@@ -119,23 +169,27 @@ class Thermal(ThermalBase):
         return 'N/A'
 
     def get_minimum_recorded(self) -> float:
-        temp = self.__collect_temp[0] if len(self.__collect_temp) > 0 else 0.1
+        temp = self.__collect_temp[0] if len(self.__collect_temp) > 0 else 36.6
+        temp = temp if temp <= 100.0 else 100.0
         temp = temp if temp > 0.0 else 0.1
         return float(temp)
 
     def get_maximum_recorded(self) -> float:
-        temp = self.__collect_temp[-1] if len(self.__collect_temp) > 0 else 100.0
+        temp = self.__collect_temp[-1] if len(self.__collect_temp) > 0 else 36.6
         temp = temp if temp <= 100.0 else 100.0
+        temp = temp if temp > 0.0 else 0.1
         return float(temp)
 
     def get_position_in_parent(self):
         return self.__index
 
     def set_high_threshold(self, temperature):
-        return False
+        self.__high_threshold = temperature
+        return True
 
     def set_low_threshold(self, temperature):
-        return False
+        self.__low_threshold = temperature
+        return True
 
 def thermal_list_get():
     l = []
