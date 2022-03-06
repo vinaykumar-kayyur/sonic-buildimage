3787651132e380588094d9f21e7be13a48c185e7
shilongliu <shilongliu@microsoft.com>
2022-03-06 23:58:12 +0800

    draft

--- scripts/docker_version_control.sh
+++ scripts/docker_version_control.sh
@@ -15,33 +15,55 @@ mkdir -p target/versions/default
 
 . src/sonic-build-hooks/buildinfo/config/buildinfo.config
 
-image_tag=`grep "^FROM " $DOCKERFILE | awk '{print$2}'`
-image=`echo $image_tag | cut -f1 -d:`
-tag=`echo $image_tag | cut -f2 -d:`
+image_tag=$(grep "^FROM " $DOCKERFILE | awk '{print$2}')
+hash_value=$(grep "${ARCH}:${image_tag}" $version_file 2>/dev/null | awk -F== '{print$2}')
+oldimage=$image_tag
+newimage=$image_tag
+prefix=$SONIC_DEFAULT_CONTAINER_REGISTRY
+
+# if it is loaded from file, exit
+[ -f target/${image_tag}.gz-load.log ] && echo "loaded: $image_tag" >> $new_version_file.log && exit 0
+
+# Debug info to logfile
+echo "=================================================" >> $new_version_file.log
+echo "Input: $@" >> $new_version_file.log
+echo "Image: $image_tag" >> $new_version_file.log
+echo "hash in config: $hash_value" >> $new_version_file.log
+echo "Default ACR: $prefix" >> $new_version_file.log
+cat src/sonic-build-hooks/buildinfo/config/buildinfo.config 2>&1 >> $new_version_file.log
+echo "target/${image_tag}.gz-load.log: $(ls target/${image_tag}.gz-load.log 2>&1)" >> $new_version_file.log
+echo "-------------------------------------------------" >> $new_version_file.log
+exit 1
 
 if [[ ",$SONIC_VERSION_CONTROL_COMPONENTS," == *,all,* ]] || [[ ",$SONIC_VERSION_CONTROL_COMPONENTS," == *,docker,* ]]; then
-    # if docker image not in white list, exit
-    if [[ "$image_tag" != */debian:* ]] && [[ "$image_tag" != multiarch/debian-debootstrap:* ]];then
-        exit 0
-    fi
-    if [ -f $version_file ];then
-        hash_value=`grep "${ARCH}:${image_tag}" $version_file | awk -F== '{print$2}'`
-    fi
-    if [ -z $hash_value ];then
-        hash_value=unknown
-        echo "$image_tag sha256 value is unknown" >> ${new_version_file}.log
-        exit 0
-    fi
-    oldimage=${image_tag//\//\\/}
-    newimage="${oldimage}@$hash_value"
-    echo "sed -i \"s/$oldimage/$newimage/\" $DOCKERFILE" >> ${new_version_file}.log
-    sed -i "s/$oldimage/$newimage/" $DOCKERFILE
+    # version control On
+    i=0
+    for image in $prefix$image_tag@$hash_value $image_tag@$hash_value $prefix$image_tag
+    do
+        ((i++))
+        if docker pull $image 1>/dev/null;then
+            echo "$i: success! pulled image from $image" >> $new_version_file.log
+            newimage=$image
+            break
+        else
+            echo "$i: failed!  pulled image from $image" >> $new_version_file.log
+        fi
+    done
+    echo "replace $oldimage with $newimage" >> $new_version_file.log
+    sed -i "s#$oldimage#$newimage#" $DOCKERFILE
 else
-    hash_value=`docker pull $image_tag 2> ${new_version_file}.log | grep Digest | awk '{print$2}'`
-    if [ -z hash_value ];then
-        hash_value=unknown
-    fi
-fi
-if [[ "$hash_value" != "unknown" ]];then
-    echo -e "${ARCH}:${image_tag}==$hash_value" >> $new_version_file
+    # version control OFF
+    i=0
+    for image in $prefix$image_tag $image_tag
+    do
+        ((i++))
+        if docker pull $image 1>/dev/null;then
+            echo "$i: success! pulled image from $image" >> $new_version_file.log
+            echo -e "${ARCH}:${image_tag}==$(docker pull $image | grep Digest | awk '{print$2}')" >> $new_version_file
+            sort -u $new_version_file -o $new_version_file
+            break
+        else
+            echo "$i: failed!  pulled image from $image" >> $new_version_file.log
+        fi
+    done
 fi
