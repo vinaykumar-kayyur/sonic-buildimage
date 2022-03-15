6b26385d411d230872f5018012c10b84c0d5f8ba
Shlomi Bitton <shlomibi@nvidia.com>
2022-03-15 12:37:27 +0000

    Optimize pmon init bash script for checking existence of python2 and python3 platform package. Optimize LLDP service start up by calling sonic-cfggen at the beginning script and not as part of supervisord. These optimizations are for execution of create_switch function on syncd with less CPU consumers, mainly for fast-reboot but good to have in general
    Signed-off-by: Shlomi Bitton <shlomibi@nvidia.com>

--- dockers/docker-lldp/docker-lldp-init.sh
+++ dockers/docker-lldp/docker-lldp-init.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+. /usr/bin/start.sh
+
 #Generate supervisord.conf based on device metadata
 mkdir -p /etc/supervisor/conf.d/
 sonic-cfggen -d -t /usr/share/sonic/templates/supervisord.conf.j2 > /etc/supervisor/conf.d/supervisord.conf
