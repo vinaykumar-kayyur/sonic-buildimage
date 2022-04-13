f451d433ecdd862d31a748bbaca7c46c75400d0e
Saikrishna Arcot <sarcot@microsoft.com>
2022-04-13 17:16:32 +0000

    Merged PR 5890172: [202012] Update dnsmasq host update script
    - Support generating addresses for multiple VLANs and adjust update time
    
    Read the VLANs present (and their configuration) from app DB and generate the host addresses based on that, instead of having a hardcoded network/subnet.
    
    - Update `/etc/dnsmasq.hosts` only when there's new changes

--- dockers/docker-dhcp-relay/updateHostAddresses.py
+++ dockers/docker-dhcp-relay/updateHostAddresses.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import argparse
+import ipaddress
 import psutil
 import os
 import signal
@@ -8,13 +9,13 @@ import syslog
 import time
 
 from swsscommon.swsscommon import SubscriberStateTable, TableConsumable, Select
-from swsscommon.swsscommon import DBConnector, Table, STATE_FDB_TABLE_NAME, APP_PORT_TABLE_NAME
+from swsscommon.swsscommon import DBConnector, Table, STATE_FDB_TABLE_NAME, APP_PORT_TABLE_NAME, APP_VLAN_MEMBER_TABLE_NAME, APP_INTF_TABLE_NAME
 
 from swsscommon import swsscommon
 
 class DnsmasqStaticHostMonitor(object):
 
-    def __init__(self, hosts_update_delay=300):
+    def __init__(self, hosts_update_delay):
         self.appl_db = DBConnector("APPL_DB", 0)
         self.state_db = DBConnector("STATE_DB", 0)
 
@@ -24,21 +25,10 @@ class DnsmasqStaticHostMonitor(object):
 
         self.next_hosts_update = None
         self.hosts_update_delay = hosts_update_delay
-        self.min_index = 1
-
-        port_table = Table(self.appl_db, APP_PORT_TABLE_NAME)
-        ports = port_table.getKeys()
-        for port in ports:
-            (status, index) = port_table.hget(port, "index")
-            if not status:
-                syslog.syslog(syslog.LOG_WARNING,
-                        f"Unable to get port index for {port}")
-                continue
-            index = int(index)
-            if self.min_index is None or index < self.min_index:
-                self.min_index = index
+        self.changes_since_last_update = False
 
     def new_host_update(self, key, op, data):
+        self.changes_since_last_update = True
         current_time = time.clock_gettime(time.CLOCK_MONOTONIC)
         if self.next_hosts_update is None or current_time > self.next_hosts_update:
             self.update_static_dnsmasq_hosts_file()
@@ -52,6 +42,27 @@ class DnsmasqStaticHostMonitor(object):
 
         fdb_table = Table(self.state_db, STATE_FDB_TABLE_NAME)
         port_table = Table(self.appl_db, APP_PORT_TABLE_NAME)
+        vlan_member_table = Table(self.appl_db, APP_VLAN_MEMBER_TABLE_NAME)
+
+        # Build a lookup dictionary from VLAN to VLAN members, with the members being sorted
+        # by their port index
+        vlan_members = vlan_member_table.getKeys()
+        vlan_member_lookup = {}
+        for vlan_member in vlan_members:
+            (vlan, port) = vlan_member.split(":", maxsplit=1)
+
+            # Get the index that corresponds to this ethernet port
+            (status, port_index) = port_table.hget(port, 'index')
+            if not status:
+                syslog.syslog(syslog.LOG_WARNING, f"Unable to get port index for {port}")
+                continue
+            port_index = int(port_index)
+            if vlan not in vlan_member_lookup:
+                vlan_member_lookup[vlan] = []
+            vlan_member_lookup[vlan].append((port, port_index))
+
+        for vlan in vlan_member_lookup.keys():
+            vlan_member_lookup[vlan].sort(key=lambda a: a[1])
 
         with open("/etc/dnsmasq.hosts", "w") as f:
             # Get all known MAC addresses
@@ -62,16 +73,24 @@ class DnsmasqStaticHostMonitor(object):
                 if not status:
                     continue
 
-                # Get the index that corresponds to this ethernet port
-                (status, port_index) = port_table.hget(port, 'index')
-                port_index = int(port_index)
-                if not status:
-                    syslog.syslog(syslog.LOG_WARNING,
-                            f"Unable to get port index for {port}")
-                    continue
+                for i in range(len(vlan_member_lookup[vlan])):
+                    if vlan_member_lookup[vlan][i][0] != port:
+                        continue
 
-                # Write a host entry
-                f.write(f"{mac_address},172.17.0.{(port_index - self.min_index) * 4 + 1},24h\n")
+                    vlan_ip_addresses = self.appl_db.keys(f"{APP_INTF_TABLE_NAME}:{vlan}:*")
+                    ipv4_network = None
+                    for vlan_ip_address in vlan_ip_addresses:
+                        ipv4_network = ipaddress.ip_interface(vlan_ip_address.split(":", maxsplit=2)[2])
+                        if ipv4_network.version != 4:
+                            continue
+
+                    if not ipv4_network:
+                        syslog.syslog(syslog.LOG_INFO, f"No IPv4 addresses found for {vlan}, skipping")
+                        continue
+
+                    # Write a host entry
+                    f.write(f"{mac_address},{ipv4_network.network[i * 4 + 1]},15m\n")
+                    break
 
         # Update the next time we'll update /etc/dnsmasq.hosts
         current_time = time.clock_gettime(time.CLOCK_MONOTONIC)
@@ -81,6 +100,8 @@ class DnsmasqStaticHostMonitor(object):
            if proc.info["name"] == "dnsmasq":
                os.kill(proc.info["pid"], signal.SIGHUP)
                break
+
+        self.changes_since_last_update = False
 
     def subscribe(self, db, table, callback, pri):
         try:
@@ -96,14 +117,18 @@ class DnsmasqStaticHostMonitor(object):
 
     def start(self):
         while True:
-            select_timeout = 60
-            state, selectable_ = self.selector.select(select_timeout)
+            select_timeout_msec = 60 * 1000
+            hosts_update_delay_msec = self.hosts_update_delay * 1000
+            if hosts_update_delay_msec < select_timeout_msec:
+                select_timeout_msec = self.hosts_update_delay_msec
+            state, selectable_ = self.selector.select(select_timeout_msec)
             if state == self.selector.TIMEOUT:
-                # Add a bit of a hack here where if we need to update the hosts file,
-                # but there's no new change, then manually do the update here
-                current_time = time.clock_gettime(time.CLOCK_MONOTONIC)
-                if self.next_hosts_update is None or current_time >= self.next_hosts_update:
-                    self.update_static_dnsmasq_hosts_file()
+                if self.changes_since_last_update:
+                    # Add a bit of a hack here where if we need to update the hosts file,
+                    # but there's no new change, then manually do the update here
+                    current_time = time.clock_gettime(time.CLOCK_MONOTONIC)
+                    if self.next_hosts_update is None or current_time >= self.next_hosts_update:
+                        self.update_static_dnsmasq_hosts_file()
                 continue
             elif state == self.selector.ERROR:
                 syslog.syslog(syslog.LOG_ERR,
@@ -129,7 +154,7 @@ class DnsmasqStaticHostMonitor(object):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Start a script to update /etc/dnsmasq.hosts with the currently-known MAC addresses.')
-    parser.add_argument('--update-every', type=int, default=300, help='Set the frequency (in seconds) to update /etc/dnsmasq.hosts. (default: 300 seconds, or 5 minutes)')
+    parser.add_argument('--update-every', type=int, default=60, help='Set the frequency (in seconds) to update /etc/dnsmasq.hosts. (default: 60 seconds, or 1 minute)')
 
     args = parser.parse_args()
 
