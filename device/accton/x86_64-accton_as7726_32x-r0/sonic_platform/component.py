7c9d00853c5dd8aecf6095d5f58c166f7f2fd1fd
Jostar Yang <jostar_yang@accton.com.tw>
2022-04-15 15:29:21 +0800

    Fix read cpld_ver from hex to dec.

--- device/accton/x86_64-accton_as7726_32x-r0/sonic_platform/component.py
+++ device/accton/x86_64-accton_as7726_32x-r0/sonic_platform/component.py
@@ -73,7 +73,7 @@ class Component(ComponentBase):
             try:
                 cpld_path = "{}{}{}".format(SYSFS_PATH, CPLD_ADDR_MAPPING[cpld_name], '/version')
                 cpld_version_raw= self._api_helper.read_txt_file(cpld_path)
-                cpld_version[cpld_name] = "{}".format(int(cpld_version_raw,16))
+                cpld_version[cpld_name] = "{}".format(int(cpld_version_raw,10))
             except Exception as e:
                 print('Get exception when read cpld')
                 cpld_version[cpld_name] = 'None'
