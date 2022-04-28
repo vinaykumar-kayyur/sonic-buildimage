ec8f3f39213aea4c10e3c74c4210557782648ce6
stormliang <stormliang@microsoft.com>
2022-04-28 12:50:05 +0000

    refine code and format

--- src/sonic-bgpcfgd/bgpcfgd/managers_advertise_rt.py
+++ src/sonic-bgpcfgd/bgpcfgd/managers_advertise_rt.py
@@ -7,7 +7,8 @@ from .log import log_info, log_err
 
 
 class AdvertiseRouteMgr(Manager):
-    """ This class Advertises routes when ADVERTISE_NETWORK_TABLE in STATE_DB is updated """
+    """This class Advertises routes when ADVERTISE_NETWORK_TABLE in STATE_DB is updated"""
+
     def __init__(self, common_objs, db, table):
         """
         Initialize the object
@@ -21,14 +22,17 @@ class AdvertiseRouteMgr(Manager):
             db,
             table,
         )
-        
-        self.directory.subscribe([("CONFIG_DB", swsscommon.CFG_DEVICE_METADATA_TABLE_NAME, "localhost/bgp_asn"),], self.on_bgp_asn_change)
+
+        self.directory.subscribe(
+            [
+                ("CONFIG_DB", swsscommon.CFG_DEVICE_METADATA_TABLE_NAME, "localhost/bgp_asn"),
+            ],
+            self.on_bgp_asn_change,
+        )
         self.advertised_routes = dict()
 
-
-    OP_DELETE = 'DELETE'
-    OP_ADD = 'ADD'
-
+    OP_DELETE = "DELETE"
+    OP_ADD = "ADD"
 
     def set_handler(self, key, data):
         log_info("AdvertiseRouteMgr:: set handler")
@@ -39,28 +43,31 @@ class AdvertiseRouteMgr(Manager):
 
         return True
 
-
     def del_handler(self, key):
         log_info("AdvertiseRouteMgr:: del handler")
         if not self._del_handler_validate(key):
-            return True
+            return
         vrf, ip_prefix = self.split_key(key)
         self.remove_route_advertisement(vrf, ip_prefix)
 
     def _ip_addr_validate(self, key):
         if key:
             _, ip_prefix = self.split_key(key)
-            ip_prefix = ip_prefix.split('/')
+            ip_prefix = ip_prefix.split("/")
             if len(ip_prefix) != 2:
                 log_err("BGPAdvertiseRouteMgr:: No valid ip prefix for advertised route %s" % key)
                 return False
             try:
                 ip = ipaddress.ip_address(ip_prefix[0])
                 if ip.version == 4 and int(ip_prefix[1]) not in range(0, 33):
-                    log_err("BGPAdvertiseRouteMgr:: ipv4 prefix %s is illegal for advertised route %s" % (ip_prefix[1], key))
+                    log_err(
+                        "BGPAdvertiseRouteMgr:: ipv4 prefix %s is illegal for advertised route %s" % (ip_prefix[1], key)
+                    )
                     return False
                 if ip.version == 6 and int(ip_prefix[1]) not in range(0, 129):
-                    log_err("BGPAdvertiseRouteMgr:: ipv6 prefix %s is illegal for advertised route %s" % (ip_prefix[1], key))
+                    log_err(
+                        "BGPAdvertiseRouteMgr:: ipv6 prefix %s is illegal for advertised route %s" % (ip_prefix[1], key)
+                    )
                     return False
             except ValueError:
                 log_err("BGPAdvertiseRouteMgr:: No valid ip %s for advertised route %s" % (ip_prefix[0], key))
@@ -69,21 +76,18 @@ class AdvertiseRouteMgr(Manager):
             return False
         return True
 
-
     def _set_handler_validate(self, key, data):
         if data:
-            if 'profile' in data and data['profile'] not in ROUTE_MAPS:
+            if "profile" in data and data["profile"] not in ROUTE_MAPS:
                 log_err("BGPAdvertiseRouteMgr:: No valid profile for advertised route %s" % data)
                 return False
-            elif data != {'':''}:
+            elif data != {"": ""}:
                 log_err("BGPAdvertiseRouteMgr:: Invalid data for advertised route %s" % data)
                 return False
         return self._ip_addr_validate(key)
 
-
     def _del_handler_validate(self, key):
         return self._ip_addr_validate(key)
-
 
     def add_route_advertisement(self, vrf, ip_prefix, data):
         if self.directory.path_exist("CONFIG_DB", swsscommon.CFG_DEVICE_METADATA_TABLE_NAME, "localhost/bgp_asn"):
@@ -91,8 +95,7 @@ class AdvertiseRouteMgr(Manager):
                 self.bgp_network_import_check_commands(vrf, self.OP_ADD)
             self.advertise_route_commands(ip_prefix, vrf, self.OP_ADD, data)
 
-        self.advertised_routes.setdefault(vrf, dict()).update({ip_prefix:data})
-
+        self.advertised_routes.setdefault(vrf, dict()).update({ip_prefix: data})
 
     def remove_route_advertisement(self, vrf, ip_prefix):
         if ip_prefix not in self.advertised_routes.get(vrf, dict()):
@@ -107,41 +110,47 @@ class AdvertiseRouteMgr(Manager):
                 self.bgp_network_import_check_commands(vrf, self.OP_DELETE)
             self.advertise_route_commands(ip_prefix, vrf, self.OP_DELETE)
 
-
-    def advertise_route_commands(self, ip_prefix, vrf, op, data = None):
+    def advertise_route_commands(self, ip_prefix, vrf, op, data=None):
         is_ipv6 = TemplateFabric.is_ipv6(ip_prefix)
-        bgp_asn = self.directory.get_slot("CONFIG_DB", swsscommon.CFG_DEVICE_METADATA_TABLE_NAME)["localhost"]["bgp_asn"]
+        bgp_asn = self.directory.get_slot("CONFIG_DB", swsscommon.CFG_DEVICE_METADATA_TABLE_NAME)["localhost"][
+            "bgp_asn"
+        ]
         cmd_list = []
-        if vrf == 'default':
+        if vrf == "default":
             cmd_list.append("router bgp %s" % bgp_asn)
         else:
             cmd_list.append("router bgp %s vrf %s" % (bgp_asn, vrf))
 
         cmd_list.append(" address-family %s unicast" % ("ipv6" if is_ipv6 else "ipv4"))
 
-        if data and 'profile' in data and data['profile'] != '':
-            cmd_list.append("  network %s route-map %s" % (ip_prefix, "%s_RM" % data['profile']))
-            log_info("BGPAdvertiseRouteMgr:: Update bgp %s network %s with route-map %s" %
-                     (bgp_asn, vrf + '|' + ip_prefix, "%s_RM" % data['profile']))
+        if data and "profile" in data:
+            cmd_list.append("  network %s route-map %s" % (ip_prefix, "%s_RM" % data["profile"]))
+            log_info(
+                "BGPAdvertiseRouteMgr:: Update bgp %s network %s with route-map %s"
+                % (bgp_asn, vrf + "|" + ip_prefix, "%s_RM" % data["profile"])
+            )
         else:
-            cmd_list.append("  %snetwork %s" % ('no ' if op == self.OP_DELETE else '', ip_prefix))
-            log_info("BGPAdvertiseRouteMgr:: %sbgp %s network %s" % 
-                     ('Remove ' if op == self.OP_DELETE else 'Update ', bgp_asn, vrf + '|' + ip_prefix))
+            cmd_list.append("  %snetwork %s" % ("no " if op == self.OP_DELETE else "", ip_prefix))
+            log_info(
+                "BGPAdvertiseRouteMgr:: %sbgp %s network %s"
+                % ("Remove " if op == self.OP_DELETE else "Update ", bgp_asn, vrf + "|" + ip_prefix)
+            )
 
         self.cfg_mgr.push_list(cmd_list)
         log_info("BGPAdvertiseRouteMgr::Done")
 
     def bgp_network_import_check_commands(self, vrf, op):
-        bgp_asn = self.directory.get_slot("CONFIG_DB", swsscommon.CFG_DEVICE_METADATA_TABLE_NAME)["localhost"]["bgp_asn"]
+        bgp_asn = self.directory.get_slot("CONFIG_DB", swsscommon.CFG_DEVICE_METADATA_TABLE_NAME)["localhost"][
+            "bgp_asn"
+        ]
         cmd_list = []
-        if vrf == 'default':
+        if vrf == "default":
             cmd_list.append("router bgp %s" % bgp_asn)
         else:
             cmd_list.append("router bgp %s vrf %s" % (bgp_asn, vrf))
-        cmd_list.append(" %sbgp network import-check" % ('' if op == self.OP_DELETE else 'no '))
+        cmd_list.append(" %sbgp network import-check" % ("" if op == self.OP_DELETE else "no "))
 
         self.cfg_mgr.push_list(cmd_list)
-
 
     def on_bgp_asn_change(self):
         if self.directory.path_exist("CONFIG_DB", swsscommon.CFG_DEVICE_METADATA_TABLE_NAME, "localhost/bgp_asn"):
@@ -150,7 +159,6 @@ class AdvertiseRouteMgr(Manager):
                 for ip_prefix in ip_prefixes:
                     self.add_route_advertisement(vrf, ip_prefix, ip_prefixes[ip_prefix])
 
-
     @staticmethod
     def split_key(key):
         """
@@ -158,7 +166,7 @@ class AdvertiseRouteMgr(Manager):
         :param key: key to split
         :return: vrf name extracted from the key, ip prefix extracted from the key
         """
-        if '|' not in key:
-            return 'default', key
+        if "|" not in key:
+            return "default", key
         else:
-            return tuple(key.split('|', 1))
+            return tuple(key.split("|", 1))
--- src/sonic-bgpcfgd/bgpcfgd/managers_rm.py
+++ src/sonic-bgpcfgd/bgpcfgd/managers_rm.py
@@ -1,11 +1,12 @@
 from .manager import Manager
 from .log import log_info, log_err
 
-ROUTE_MAPS = ['FROM_SDN_SLB_ROUTES', '']
+ROUTE_MAPS = ["FROM_SDN_SLB_ROUTES", ""]
 
 
 class RouteMapMgr(Manager):
-    """ This class add route-map when ROUTE_MAP_TABLE in APPL_DB is updated """
+    """This class add route-map when BGP_PROFILE_TABLE in APPL_DB is updated"""
+
     def __init__(self, common_objs, db, table):
         """
         Initialize the object
@@ -20,16 +21,14 @@ class RouteMapMgr(Manager):
             table,
         )
 
-
     def set_handler(self, key, data):
         log_info("BGPRouteMapMgr:: set handler")
-        '''Only need a name as the key, and community id as the data'''
+        """Only need a name as the key, and community id as the data"""
         if not self._set_handler_validate(key, data):
             return True
-        
+
         self._update_rm(key, data)
         return True
-
 
     def del_handler(self, key):
         log_info("BGPRouteMapMgr:: del handler")
@@ -37,33 +36,34 @@ class RouteMapMgr(Manager):
             return
         self._remove_rm(key)
 
-
     def _remove_rm(self, rm):
-        cmds = ['no route-map %s permit 100' % ("%s_RM" % rm)]
+        cmds = ["no route-map %s permit 100" % ("%s_RM" % rm)]
         log_info("BGPRouteMapMgr:: remove route-map %s" % ("%s_RM" % rm))
         self.cfg_mgr.push_list(cmds)
         log_info("BGPRouteMapMgr::Done")
-
 
     def _set_handler_validate(self, key, data):
         if key not in ROUTE_MAPS:
             log_err("BGPRouteMapMgr:: Invalid key for route-map %s" % key)
             return False
-        
+
         if not data:
             log_err("BGPRouteMapMgr:: data is None")
             return False
-        community_ids = data['community_id'].split(':')
+        community_ids = data["community_id"].split(":")
         try:
-            if len(community_ids) != 2 or int(community_ids[0]) not in range(0, 65536) or int(community_ids[1]) not in range(0, 65536):
+            if (
+                len(community_ids) != 2
+                or int(community_ids[0]) not in range(0, 65536)
+                or int(community_ids[1]) not in range(0, 65536)
+            ):
                 log_err("BGPRouteMapMgr:: data %s doesn't include valid community id %s" % (data, community_ids))
                 return False
         except ValueError:
-            log_err("BGPRouteMapMgr:: data %s doesn't include valid community id %s" % (data, community_ids))
+            log_err("BGPRouteMapMgr:: data %s includes illegal input" % (data))
             return False
 
         return True
-
 
     def _del_handler_validate(self, key):
         if key not in ROUTE_MAPS:
@@ -71,12 +71,8 @@ class RouteMapMgr(Manager):
             return False
         return True
 
-
     def _update_rm(self, rm, data):
-        cmds = [
-            'route-map %s permit 100' % ("%s_RM" % rm),
-            ' set community %s' % data['community_id']
-        ]
-        log_info("BGPRouteMapMgr:: update route-map %s community %s" % ("%s_RM" % rm, data['community_id']))
+        cmds = ["route-map %s permit 100" % ("%s_RM" % rm), " set community %s" % data["community_id"]]
+        log_info("BGPRouteMapMgr:: update route-map %s community %s" % ("%s_RM" % rm, data["community_id"]))
         self.cfg_mgr.push_list(cmds)
         log_info("BGPRouteMapMgr::Done")
