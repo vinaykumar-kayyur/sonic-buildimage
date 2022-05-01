3b2e947c363d67d97f94bae52197e92598ccb4b8
Andriy Kokhan <andriyx.kokhan@intel.com>
2022-05-01 01:59:50 -0700

    [BFN] Canceling PSU platform API calls on SIGTERM
    Signed-off-by: Andriy Kokhan <andriyx.kokhan@intel.com>

--- platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/platform_utils.py
+++ platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/platform_utils.py
@@ -3,6 +3,8 @@
 try:
     import os
     import subprocess
+    import signal
+    from functools import wraps
 
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
@@ -20,3 +22,24 @@ def file_create(path, mode=None):
         run_cmd(['touch', path])
     if (mode is not None):    
         run_cmd(['chmod', mode, path])
+
+def cancel_on_sigterm(func):
+    """
+    Wrapper for a function which has to be cancel on SIGTERM
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        def handler(sig, frame):
+            if sigterm_handler:
+                sigterm_handler(sig, frame)
+            raise Exception("Canceling {}() execution...".format(func.__name__))
+
+        sigterm_handler = signal.getsignal(signal.SIGTERM)
+        signal.signal(signal.SIGTERM, handler)
+        result = None
+        try:
+            result = func(*args, **kwargs)
+        finally:
+            signal.signal(signal.SIGTERM, sigterm_handler)
+        return result
+    return wrapper
