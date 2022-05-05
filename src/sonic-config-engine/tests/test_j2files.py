4adb6bd1e6ff315de9e5d9f82484775f27b72b9a
Stephen Sun <stephens@nvidia.com>
2022-05-05 06:16:35 +0000

    Support t1 in dual ToR scenario
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- src/sonic-config-engine/tests/test_j2files.py
+++ src/sonic-config-engine/tests/test_j2files.py
@@ -343,7 +343,9 @@ class TestJ2Files(TestCase):
             ('arista', 'x86_64-arista_7260cx3_64', 'Arista-7260CX3-D108C8', 'sample-arista-7260-dualtor-minigraph.xml', 'buffers.json.j2', 'buffer-arista7260-dualtor.json'),
             ('arista', 'x86_64-arista_7260cx3_64', 'Arista-7260CX3-D108C8', 'sample-arista-7260-dualtor-minigraph-remap-disabled.xml', 'buffers.json.j2', 'buffer-arista7260-dualtor-remap-disabled.json'),
             ('arista', 'x86_64-arista_7260cx3_64', 'Arista-7260CX3-C64', 'sample-arista-7260-t1-minigraph.xml', 'buffers.json.j2', 'buffer-arista7260-t1.json'),
-            ('arista', 'x86_64-arista_7260cx3_64', 'Arista-7260CX3-C64', 'sample-arista-7260-t1-minigraph-remap-disabled.xml', 'buffers.json.j2', 'buffer-arista7260-t1-remap-disabled.json')
+            ('arista', 'x86_64-arista_7260cx3_64', 'Arista-7260CX3-C64', 'sample-arista-7260-t1-minigraph-remap-disabled.xml', 'buffers.json.j2', 'buffer-arista7260-t1-remap-disabled.json'),
+            ('mellanox', 'x86_64-mlnx_msn4600c-r0', 'Mellanox-SN4600C-C64', 'sample-mellanox-4600c-t1-minigraph.xml', 'buffers_dynamic.json.j2', 'buffers-mellanox4600c-t1-dynamic.json'),
+            ('mellanox', 'x86_64-mlnx_msn4600c-r0', 'Mellanox-SN4600C-C64', 'sample-mellanox-4600c-t1-minigraph.xml', 'buffers.json.j2', 'buffers-mellanox4600c-t1.json')
         ]
 
         for test_data in TEST_DATA:
@@ -365,7 +367,8 @@ class TestJ2Files(TestCase):
             '../../../device/arista/x86_64-arista_7260cx3_64/Arista-7260CX3-C64',
             '../../../device/arista/x86_64-arista_7050cx3_32s/Arista-7050CX3-32S-D48C8',
             '../../../device/arista/x86_64-arista_7260cx3_64/Arista-7260CX3-D108C8',
-            '../../../device/arista/x86_64-arista_7260cx3_64/Arista-7260CX3-C64'
+            '../../../device/arista/x86_64-arista_7260cx3_64/Arista-7260CX3-C64',
+            '../../../device/mellanox/x86_64-mlnx_msn4600c-r0/Mellanox-SN4600C-C64'
             ]
         sample_outputs = [
             'qos-arista7050cx3-dualtor.json',
@@ -373,7 +376,8 @@ class TestJ2Files(TestCase):
             'qos-arista7260-t1.json',
             'qos-arista7050cx3-dualtor-remap-disabled.json',
             'qos-arista7260-dualtor-remap-disabled.json',
-            'qos-arista7260-t1-remap-disabled.json'
+            'qos-arista7260-t1-remap-disabled.json',
+            'qos-mellanox4600c-c64.json'
         ]
         sample_minigraph_files = [
             'sample-arista-7050cx3-dualtor-minigraph.xml',
@@ -381,8 +385,10 @@ class TestJ2Files(TestCase):
             'sample-arista-7260-t1-minigraph.xml',
             'sample-arista-7050cx3-dualtor-minigraph-remap-disabled.xml',
             'sample-arista-7260-dualtor-minigraph-remap-disabled.xml',
-            'sample-arista-7260-t1-minigraph-remap-disabled.xml'
+            'sample-arista-7260-t1-minigraph-remap-disabled.xml',
+            'sample-mellanox-4600c-t1-minigraph.xml'
         ]
+
         for i, path in enumerate(dir_paths):
             device_template_path = os.path.join(self.test_dir, path)
             sample_output = sample_outputs[i]
