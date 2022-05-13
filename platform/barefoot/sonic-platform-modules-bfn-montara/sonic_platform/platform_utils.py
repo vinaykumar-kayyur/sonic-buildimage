4341a4ad91262f96543f5613dd38f8d40b8e5af1
Taras Keryk <tarasx.keryk@intel.com>
2022-05-13 16:57:22 +0300

    [BFN] Fixed SONiC fwutil exec time (#31)
    Signed-off-by: Taras Keryk <tarasx.keryk@intel.com>

--- platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/platform_utils.py
+++ platform/barefoot/sonic-platform-modules-bfn-montara/sonic_platform/platform_utils.py
@@ -10,6 +10,12 @@ except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
 def file_create(path, mode=None):
+    """
+    Ensure that file is created with the appropriate permissions
+    Args:
+        path: full path of a file
+        mode: file permission in octal representation  
+    """
     def run_cmd(cmd):
         if os.geteuid() != 0:
             cmd.insert(0, 'sudo')
@@ -20,7 +26,7 @@ def file_create(path, mode=None):
         run_cmd(['mkdir', '-p', file_path])
     if not os.path.isfile(path):
         run_cmd(['touch', path])
-    if (mode is not None):    
+    if (mode is not None):
         run_cmd(['chmod', mode, path])
 
 def cancel_on_sigterm(func):
@@ -43,3 +49,32 @@ def cancel_on_sigterm(func):
             signal.signal(signal.SIGTERM, sigterm_handler)
         return result
     return wrapper
+
+def limit_execution_time(execution_time_secs: int):
+    """
+    Wrapper for a function whose execution time must be limited
+    Args:
+        execution_time_secs: maximum execution time in seconds,
+        after which the function execution will be stopped
+    """
+    def wrapper(func):
+        @wraps(func)
+        def execution_func(*args, **kwargs):
+            def handler(sig, frame):
+                if sigalrm_handler:
+                    sigalrm_handler(sig, frame)
+                raise Exception("Canceling {}() execution...".format(func.__name__))
+
+            sigalrm_handler = signal.getsignal(signal.SIGALRM)
+            signal.signal(signal.SIGALRM, handler)
+            signal.alarm(execution_time_secs)
+            result = None
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+
+            return result
+        return execution_func
+    return wrapper
+
