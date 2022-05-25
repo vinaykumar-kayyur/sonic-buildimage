a5cc43d3a8d0098909159ab04862a3a0ee774118
Blueve <jika@microsoft.com>
2022-05-25 23:53:06 +0800

    Update make files

--- rules/openssh.mk
+++ rules/openssh.mk
@@ -9,7 +9,7 @@ OPENSSH_SERVER = openssh-server_$(OPENSSH_VERSION_FULL)_$(CONFIGURED_ARCH).deb
 $(OPENSSH_SERVER)_SRC_PATH = $(SRC_PATH)/openssh
 SONIC_MAKE_DEBS += $(OPENSSH_SERVER)
 
-# The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
-# are archived into debug one image to facilitate debugging.
-#
-DBG_SRC_ARCHIVE += openssh
+OPENSSH_SERVER_DBG = openssh-server-dbgsym_$(OPENSSH_VERSION_FULL)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(OPENSSH_SERVER),$(OPENSSH_SERVER_DBG)))
+
+export OPENSSH_SERVER OPENSSH_SERVER_DBG
