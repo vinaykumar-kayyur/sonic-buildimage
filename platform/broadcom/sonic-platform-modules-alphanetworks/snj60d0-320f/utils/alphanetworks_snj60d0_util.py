0f8da583861f08f0a092dd23b88dd6e33ed4f14a
juntseng62 <juntseng62@gmail.com>
2022-05-27 10:35:37 +0800

    Add SCG60D0-484T
    Signed-off-by: juntseng62 <juntseng62@gmail.com>

--- platform/broadcom/sonic-platform-modules-alphanetworks/snj60d0-320f/utils/alphanetworks_snj60d0_util.py
+++ platform/broadcom/sonic-platform-modules-alphanetworks/snj60d0-320f/utils/alphanetworks_snj60d0_util.py
@@ -116,12 +116,12 @@ def log_os_system(cmd, show):
     return  status, output
 
 def driver_check():
-    ret, lsmod = log_os_system("lsmod | grep " + DRIVER_NAME, 0)
+    ret, lsmod = log_os_system("ls /sys/module/*" + DRIVER_NAME + "*", 0)
     logging.info('mods:'+lsmod)
-    if len(lsmod) ==0:
-        return False   
-    return True
-
+    if ret:
+        return False
+    else:
+        return True
 
 kos = [
 'modprobe i2c_ismt',
