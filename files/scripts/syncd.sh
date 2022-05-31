e4ffbbcd07d5a2bf4eac69943e6011333281fb29
shlomibitton <60430976+shlomibitton@users.noreply.github.com>
2022-05-31 10:41:43 +0300

    Make the if condition consistent

--- files/scripts/syncd.sh
+++ files/scripts/syncd.sh
@@ -41,7 +41,7 @@ function waitplatform() {
     if [[ x"$sonic_asic_platform" == x"mellanox" ]]; then
         if [[ x"$BOOT_TYPE" = @(x"fast"|x"warm"|x"fastfast") ]]; then
             PMON_TIMER_STATUS=$(systemctl is-active pmon.timer)
-            if [[ "$PMON_TIMER_STATUS" = "inactive" ]]; then
+            if [[ x"$PMON_TIMER_STATUS" = x"inactive" ]]; then
                 systemctl start pmon.timer
             else
                 debug "PMON service is delayed by a timer for better fast/warm boot performance"
