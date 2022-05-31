77d70e602907c9c02861607cb2f4e35b0bcb9aea
Boyang Yu <byu@arista.com>
2022-05-31 09:25:51 -0700

    [phy-credo]: update deb for speed change and bug fix

--- rules/phy-credo.mk
+++ rules/phy-credo.mk
@@ -1,3 +1,3 @@
 PHY_CREDO = phy-credo_1.0_amd64.deb
-$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/d227160f981f27df020ab50b9e0f16b1bd9166ea/phy/phy-credo_1.0_amd64.deb"
+$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/0c1359ce68bbccb301269e77884de5643055d1de/phy/phy-credo_1.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(PHY_CREDO)
