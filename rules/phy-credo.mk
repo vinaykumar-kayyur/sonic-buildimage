499450c2336cde3b303aacc601bc400d7ed62b01
Boyang Yu <byu@arista.com>
2022-06-02 15:03:58 -0700

    [phy-credo]: update deb to not apply to SmartsvilleBkMs

--- rules/phy-credo.mk
+++ rules/phy-credo.mk
@@ -1,3 +1,3 @@
 PHY_CREDO = phy-credo_1.0_amd64.deb
-$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/0c1359ce68bbccb301269e77884de5643055d1de/phy/phy-credo_1.0_amd64.deb"
+$(PHY_CREDO)_URL = "https://github.com/aristanetworks/sonic-firmware/raw/e89a1696954fd381e1e95edf208cffc97caf15d4/phy/phy-credo_1.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(PHY_CREDO)
