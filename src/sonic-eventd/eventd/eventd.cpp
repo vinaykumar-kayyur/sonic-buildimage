acffaa16c68c251a220ca0840837282c0342bc13
Renuka Manavalan <remanava@microsoft.com>
2022-06-02 18:22:51 +0000

    First cut

--- src/sonic-eventd/eventd/eventd.cpp
+++ src/sonic-eventd/eventd/eventd.cpp
@@ -21,87 +21,95 @@
 /* Count of elements returned in each read */
 #define READ_SET_SIZE 100
 
-/*
- * Max count of possible concurrent event publishers
- * A rough estimate only, more for mem reserve.
- * So this does not limit any usage
- */
-#define MAX_PUBLISHERS_COUNT  1000
-
-
-eventd_server::eventd_server() : m_capture(NULL)
-{
-    m_ctx = zmq_ctx_new();
-    RET_ON_ERR(m_ctx != NULL, "Failed to get zmq ctx");
-    m_cache_max = get_config_data(CACHE_MAX_CNT, (uint32_t)MAX_CACHE_SIZE);
-
-out:
-    return;
-}
-
-
-eventd_server::~eventd_server()
-{
-    close();
-}
-
-
 int
-eventd_server::zproxy_service()
+eventd_proxy::init()
 {
     int ret = -1;
     SWSS_LOG_INFO("Start xpub/xsub proxy");
 
-    void *frontend = zmq_socket(m_ctx, ZMQ_XSUB);
-    RET_ON_ERR(frontend != NULL, "failing to get ZMQ_XSUB socket");
+    m_frontend = zmq_socket(m_ctx, ZMQ_XSUB);
+    RET_ON_ERR(m_frontend != NULL, "failing to get ZMQ_XSUB socket");
 
-    int rc = zmq_bind(frontend, get_config(XSUB_END_KEY));
+    int rc = zmq_bind(m_frontend, get_config(XSUB_END_KEY));
     RET_ON_ERR(rc == 0, "Failing to bind XSUB to %s", get_config(XSUB_END_KEY));
 
-    void *backend = zmq_socket(m_ctx, ZMQ_XPUB);
-    RET_ON_ERR(backend != NULL, "failing to get ZMQ_XPUB socket");
+    m_backend = zmq_socket(m_ctx, ZMQ_XPUB);
+    RET_ON_ERR(m_backend != NULL, "failing to get ZMQ_XPUB socket");
 
-    rc = zmq_bind(backend, get_config(XPUB_END_KEY));
+    rc = zmq_bind(m_backend, get_config(XPUB_END_KEY));
     RET_ON_ERR(rc == 0, "Failing to bind XPUB to %s", get_config(XPUB_END_KEY));
 
-    void *capture = zmq_socket(m_ctx, ZMQ_PUB);
-    RET_ON_ERR(capture != NULL, "failing to get ZMQ_PUB socket for capture");
+    m_capture = zmq_socket(m_ctx, ZMQ_PUB);
+    RET_ON_ERR(m_capture != NULL, "failing to get ZMQ_PUB socket for capture");
 
-    rc = zmq_bind(capture, get_config(CAPTURE_END_KEY));
+    rc = zmq_bind(m_capture, get_config(CAPTURE_END_KEY));
     RET_ON_ERR(rc == 0, "Failing to bind capture PUB to %s", get_config(CAPTURE_END_KEY));
 
-    m_thread_proxy = thread(&eventd_server::zproxy_service_run, this, frontend, 
-            backend, capture);
+    m_thr = thread(&eventd_proxy::run, this);
     ret = 0;
 out:
     return ret;
 }
 
-
 void
-eventd_server::zproxy_service_run(void *frontend, void *backend, void *capture)
+eventd_proxy::run()
 {
     SWSS_LOG_INFO("Running xpub/xsub proxy");
 
     /* runs forever until zmq context is terminated */
-    zmq_proxy(frontend, backend, capture);
-
-    zmq_close(frontend);
-    zmq_close(backend);
-    zmq_close(capture);
-
-    SWSS_LOG_ERR("Terminating xpub/xsub proxy");
+    zmq_proxy(m_frontend, m_backend, m_capture);
 
     return 0;
 }
 
 
-int
-eventd_server::capture_events(events_data_lst_t &lst)
+capture_service::~capture_service()
+{
+    stop_capture();
+}
+
+capture_service::stop_capture()
+{
+    if (m_socket != NULL) {
+        zmq_close(m_socket);
+        m_socket = NULL;
+    }
+    if (m_thr.joinable()) {
+        m_thr.join();
+    }
+}
+
+static bool
+validate_event(const internal_event_t &event, string &rid, string &seq)
+{
+    bool ret = false;
+
+    internal_event_t::const_iterator itc_r, itc_s, itc_e;
+    itc_r = event.find(EVENT_RUNTIME_ID);
+    itc_s = event.find(EVENT_SEQUENCE);
+    itc_e = event.find(EVENT_STR_DATA);
+
+    if ((itc_r != event.end()) && (itc_s != event.end()) && (itc_e != event.end())) {
+        invalid_evt = true;
+
+        rid = itc_r->second;
+        seq = itc_s->second;
+    }
+    else {
+        SWSS_LOG_ERROR("Invalid evt: %s", map_to_str(event).str());
+    }
+
+    return ret;
+}
+        
+
+void
+capture_service::init_capture_cache(events_data_lst_t &lst)
 {
     /* clean any pre-existing cache */
     int ret = -1;
     int i;
+    string rid, seq;
 
     /*
      * Reserve a MAX_PUBLISHERS_COUNT entries for last events, as we use it only
@@ -114,73 +122,80 @@ eventd_server::capture_events(events_data_lst_t &lst)
     }
 
     /* Cache last 
-    typedef map<runtime_id_t, sequence_t> pre_exist_id_t;
-    pre_exist_id_t pre_exist_id;
+    for (events_data_lst_t::it = lst.begin(); it != lst.end(); ++it) {
+        internal_event_t event;
 
-    RET_ON_ERR(m_capture != NULL, "capture sock is not initialized yet");
-
-    if (!lst.empty()) {
-        for (events_data_lst_t::it = lst.begin(); it != lst.end(); ++it) {
-            internal_event_t event;
-
-            if (deserialize(*itc, event) == 0) {
-                pre_exist_id[event[EVENT_RUNTIME_ID]] = events_base::str_to_seq(event[EVENT_SEQUENCE]);
-            }
-            else {
-                SWSS_LOG_ERROR("failed to serialize cache message from subscriber; DROP");
+        if (deserialize(*itc, event) == 0) {
+            if (validate_event(event, rid, seq)) {
+                m_pre_exist_id[event[EVENT_RUNTIME_ID]] = rid;
+                m_events.push_back(*itc);
             }
         }
-        m_events.swap(lst);
+        else {
+            SWSS_LOG_ERROR("failed to serialize cache message from subscriber; DROP");
+        }
     }
+}
 
-    if (!pre_exist_id.empty()) {
-        /* Check read events against provided cache for 2 seconds to skip */
-        chrono::steady_clock::timepoint start = chrono::steady_clock::now();
-        while(!pre_exist_id.empty()) {
-            internal_event_t event;
-            string source, evt_str;
 
-            RET_ON_ERR(zmq_message_read(m_socket, 0, source, evt_str) == 0,
-                    "Failed to read from capture socket");
+void
+capture_service::do_capture()
+{
+    /* clean any pre-existing cache */
+    int ret = -1;
+    int i;
+    string rid, seq;
 
-            if (deserialize(evt_str, event) == 0) {
-                pre_exist_id_t::iterator it = pre_exist_id.find(event[EVENT_RUNTIME_ID]);
-                if (it != pre_exist_id.end()) {
-                    seq = events_base::str_to_seq(event[EVENT_SEQUENCE]);
-                    if (seq > it->second) {
-                        m_events.push_back(evt_str);
-                    }
-                    if (seq >= it->second) {
-                        pre_exist_id.erase(it);
-                    }
+    /* Check read events against provided cache for 2 seconds to skip */
+    chrono::steady_clock::timepoint start = chrono::steady_clock::now();
+    while(!m_pre_exist_id.empty()) {
+        internal_event_t event;
+        string source, evt_str;
+
+        RET_ON_ERR(zmq_message_read(m_socket, 0, source, event) == 0,
+                "Failed to read from capture socket");
+
+        if (validate_event(event, rid, seq)) {
+
+            serialize(event, evt_str);
+            m_pre_exist_id_t::iterator it = m_pre_exist_id.find(rid);
+
+            if (it != m_pre_exist_id.end()) {
+                seq = events_base::str_to_seq(seq);
+                if (seq > it->second) {
+                    m_events.push_back(evt_str);
+                }
+                if (seq >= it->second) {
+                    m_pre_exist_id.erase(it);
                 }
             }
-            else {
-                SWSS_LOG_ERROR("failed to serialize received event from publisher. DROP");
-            }
-            if(chrono::steady_clock::now() - start > chrono::seconds(2))
-                break;
         }
-        pre_exist_id_t().swap(pre_exist_id);
+        if(chrono::steady_clock::now() - start > chrono::seconds(2))
+            break;
     }
+    pre_exist_id_t().swap(m_pre_exist_id);
 
     /* Save until max allowed */
     while(m_events.size() < m_cache_max) {
+        internal_event_t event;
         string source, evt_str;
 
-        RET_ON_ERR(zmq_message_read(m_socket, 0, source, evt_str) == 0,
+        RET_ON_ERR(zmq_message_read(m_socket, 0, source, event) == 0,
                 "Failed to read from capture socket");
-        try
-        {
-            m_events.push_back(evt_str);
-        }
-        catch (exception& e)
-        {
-            stringstream ss;
-            ss << e.what();
-            SWSS_LOG_ERROR("Cache save event failed with %s events:size=%d",
-                    ss.str().c_str(), m_events.size());
-            break;
+        if (validate_event(event, rid, seq)) {
+            serialize(event, evt_str);
+            try
+            {
+                m_events.push_back(evt_str);
+            }
+            catch (exception& e)
+            {
+                stringstream ss;
+                ss << e.what();
+                SWSS_LOG_ERROR("Cache save event failed with %s events:size=%d",
+                        ss.str().c_str(), m_events.size());
+                break;
+            }
         }
     }
 
@@ -190,14 +205,12 @@ eventd_server::capture_events(events_data_lst_t &lst)
         internal_event_t event;
         string source, evt_str;
 
-        RET_ON_ERR(zmq_message_read(m_socket, 0, source, evt_str) == 0,
+        RET_ON_ERR(zmq_message_read(m_socket, 0, source, event) == 0,
                 "Failed to read from capture socket");
 
-        if (deserialize(evt_str, event) == 0) {
-            m_last_events[event[EVENT_RUNTIME_ID]] = evt_str;
-        }
-        else {
-            SWSS_LOG_ERROR("FAILED to serialize received event from publisher. DROP");
+        if (validate_event(event, rid, seq)) {
+            serialize(event, evt_str);
+            m_last_events[rid] = evt_str;
         }
     }
 out:
@@ -208,15 +221,109 @@ out:
     return 0;
 }
 
+int
+capture_service::set_control(capture_control_t ctrl, events_data_lst_t *lst)
+{
+    int ret = -1;
+
+    /* Can go in single step only. */
+    RET_ON_ERR((ctrl - m_ctrl) == 1, "m_ctrl(%d) > ctrl(%d)", m_ctrl, ctrl);
+    m_ctrl = ctrl;
+
+    switch(m_ctrl) {
+        case INIT_CAPTURE:
+            {
+            void *sock = NULL;
+            sock = zmq_socket(m_ctx, ZMQ_SUB);
+            RET_ON_ERR(sock != NULL, "failing to get ZMQ_SUB socket");
+
+            rc = zmq_connect(sock, get_config(CAPTURE_END_KEY));
+            RET_ON_ERR(rc == 0, "Failing to bind capture SUB to %s", get_config(CAPTURE_END_KEY));
+
+            rc = zmq_setsockopt(sock, ZMQ_SUBSCRIBE, "", 0);
+            RET_ON_ERR(rc == 0, "Failing to ZMQ_SUBSCRIBE");
+
+            m_socket = sock;
+            }
+            ret = 0;
+            break;
+
+        case START_CAPTURE:
+            
+            /*
+             * Reserve a MAX_PUBLISHERS_COUNT entries for last events, as we use it only
+             * upon m_events/vector overflow, which might block adding new entries in map
+             * if overall mem consumption is too high. Clearing the map just before use
+             * is likely to help.
+             */
+            for (i=0; i<MAX_PUBLISHERS_COUNT; ++i) {
+                m_last_events[to_string(i)] = "";
+            }
+            
+            if ((lst != NULL) && (!lst->empty())) {
+                init_capture_cache(*lst);
+            }
+
+            m_thr = thread(&capture_service::do_capture, this);
+            RET_ON_ERR(m_thr.joinable(), "Capture thread not running");
+            ret = 0;
+            break;
+
+
+        case STOP_CAPTURE:
+            /*
+             * Caller would have initiated SUBS channel.
+             * Read for CACHE_DRAIN_IN_MILLISECS to drain off cache
+             * before stopping.
+             */
+            this_thread::sleep_for(chrono::milliseconds(CACHE_DRAIN_IN_MILLISECS));
+            stop_capture();
+            break;
+
+        default:
+            SWSS_LOG_ERROR("Unexpected code=%d", m_ctrl);
+            break;
+    }
+out:
+    return ret;
+}
 
 int
-eventd_server::eventd_service()
+capture_service::read_cache(events_data_lst_t &lst_fifo,
+        last_events_t &lst_last)
+{
+    lst_fifo.swap(m_events);
+    lst_last.swap(m_last_events);
+    last_events_t().swap(m_last_events);
+    events_data_lst_t().swap(m_events);
+    return 0;
+}
+
+
+void
+run_eventd_service()
 {
     event_service service;
+    eventd_proxy *proxy = NULL;
+    capture_service *capture = NULL;
 
-    RET_ON_ERR(zproxy_service() == 0, "Failed to start zproxy_service");
+    events_data_lst_t capture_fifo_events;
+    last_events_t capture_last_events;
 
-    RET_ON_ERR(service.init_server(m_ctx) == 0, "Failed to init service");
+    SWSS_LOG_ERROR("Eventd service starting\n");
+
+    void *zctx = zmq_ctx_new();
+    RET_ON_ERR(ctx != NULL, "Failed to get zmq ctx");
+
+    cache_max = get_config_data(CACHE_MAX_CNT, (uint32_t)MAX_CACHE_SIZE);
+    RET_ON_ERR(cache_max > 0, "Failed to get CACHE_MAX_CNT");
+
+    proxy = new eventd_proxy(zctx);
+    RET_ON_ERR(proxy != NULL, "Failed to create proxy");
+
+    RET_ON_ERR(proxy->init() == 0, "Failed to init proxy");
+
+    RET_ON_ERR(service.init_server(zctx) == 0, "Failed to init service");
 
     while(true) {
         int code, resp = -1; 
@@ -228,87 +335,79 @@ eventd_server::eventd_service()
         switch(code) {
             case EVENT_CACHE_INIT:
                 /* connect only*/
-                if (m_capture != NULL) {
-                    resp_code = 1;
-                    break;
+                if (capture != NULL) {
+                    delete capture;
                 }
-                m_capture = zmq_socket(m_ctx, ZMQ_SUB);
-                RET_ON_ERR(capture != NULL, "failing to get ZMQ_SUB socket");
+                events_data_lst_t().swap(capture_fifo_events);
+                last_events_t().swap(capture_last_events);
 
-                rc = zmq_connect(capture, get_config(CAPTURE_END_KEY));
-                RET_ON_ERR(rc == 0, "Failing to bind capture SUB to %s", get_config(CAPTURE_END_KEY));
-
-                rc = zmq_setsockopt(sub_read, ZMQ_SUBSCRIBE, "", 0);
-                RET_ON_ERR(rc == 0, "Failing to ZMQ_SUBSCRIBE");
-
-                resp_code = 0;
+                capture = new capture_service(zctx, cache_max);
+                if (capture != NULL) {
+                    resp = capture->set_control(INIT_CAPTURE);
+                }
                 break;
 
                 
             case EVENT_CACHE_START:
-                if (m_capture == NULL) {
-                    resp_code = -1;
+                if (capture == NULL) {
+                    SWSS_LOG_ERROR("Cache is not initialized to start");
+                    resp = -1;
                     break;
                 }
-                events_data_lst_t.swap(m_events);
-                last_events_t.swap(m_last_events);
-
-                /* Kick off the service */
-                m_thread_capture = thread(&eventd_server::capture_events, this, req_data);
-
-                resp_code = 0;
+                resp = capture->set_control(START_CAPTURE, req_data);
                 break;
 
                 
             case EVENT_CACHE_STOP:
-                if (m_capture != NULL) {
-                    /*
-                     * Caller would have initiated SUBS channel.
-                     * Read for CACHE_DRAIN_IN_MILLISECS to drain off cache
-                     * before stopping.
-                     */
-                    this_thread::sleep_for(chrono::milliseconds(CACHE_DRAIN_IN_MILLISECS));
-                    close(m_capture);
-                    m_capture = NULL;
-
-                    /* Wait for thread to end */
-                    m_thread_capture.join();
-                    resp_code = 0;
+                if (capture == NULL) {
+                    SWSS_LOG_ERROR("Cache is not initialized to stop");
+                    resp = -1;
+                    break;
                 }
-                else {
-                    resp_code = -1;
+                resp = capture->set_control(STOP_CAPTURE);
+                if (resp == 0) {
+                    resp = capture->read_cache(capture_fifo_events, capture_last_events);
                 }
+                delete capture;
+                capture = NULL;
                 break;
 
 
             case EVENT_CACHE_READ:
-                resp_code = 0;
+                if (capture != NULL) {
+                    SWSS_LOG_ERROR("Cache is not stopped yet.");
+                    resp = -1;
+                    break;
+                }
+                resp = 0;
 
-                if (m_events.empty()) {
-                    for (last_events_t::iterator it = m_last_events.begin();
-                            it != m_last_events.end(); ++it) {
-                        m_events.push_back(it->second);
+                if (capture_fifo_events.empty()) {
+                    for (last_events_t::iterator it = capture_last_events.begin();
+                            it != capture_last_events.end(); ++it) {
+                        capture_fifo_events.push_back(it->second);
                     }
-                    last_events_t().swap(m_last_events);
+                    last_events_t().swap(capture_last_events);
                 }
 
-                int sz = m_events.size() < READ_SET_SIZE ? m_events.size() : READ_SET_SIZE;
+                int sz = capture_fifo_events.size() < READ_SET_SIZE ?
+                    capture_fifo_events.size() : READ_SET_SIZE;
 
                 if (sz != 0) {
-                    auto it = std::next(m_events.begin(), sz);
-                    move(m_events.begin(), m_events.end(), back_inserter(resp_data));
+                    auto it = std::next(capture_fifo_events.begin(), sz);
+                    move(capture_fifo_events.begin(), capture_fifo_events.end(),
+                            back_inserter(resp_data));
 
-                    if (sz == m_events.size()) {
-                        events_data_lst_t().swap(m_events);
+                    if (sz == capture_fifo_events.size()) {
+                        events_data_lst_t().swap(capture_fifo_events);
                     } else {
-                        m_events.erase(m_events.begin(), it);
+                        events.erase(capture_fifo_events.begin(), it);
                     }
                 }
                 break;
 
 
             case EVENT_ECHO:
-                resp_code = 0;
+                resp = 0;
                 resp_data.swap(req_data);
 
             default:
@@ -320,22 +419,18 @@ eventd_server::eventd_service()
                 "Failed to write response back");
     }
 out:
-    /* Breaks here on fatal failure */
-    if (m_capture != NULL) {
-        close(m_capture);
-        m_capture = NULL;
+    m_service.close();
+    if (proxy != NULL) {
+        delete proxy;
     }
-    close();
-    m_thread_proxy.join();
-    m_thread_capture.join();
-    return 0;
-}
+    if (capture != NULL) {
+        delete capture;
+    }
+    if (zctx != NULL) {
+        zmq_ctx_term(zctx);
+    }
+    SWSS_LOG_ERROR("Eventd service exiting\n");
 
-
-
-void eventd_server::close()
-{
-    zmq_ctx_term(m_ctx); m_ctx = NULL;
-
+    meturn 0;
 }
 
--- src/sonic-eventd/eventd/eventd.h
+++ src/sonic-eventd/eventd/eventd.h
@@ -5,95 +5,143 @@
 
 typedef map<runtime_id_t, events_data_type_t> last_events_t;
 
-class eventd_server {
+/*
+ *  Started by eventd_service.
+ *  Creates XPUB & XSUB end points.
+ onicanalytics.azurecr.io  Bind the same
+ *  Create a PUB socket end point for capture and bind.
+ *  Call run_proxy method with sockets in a dedicated thread.
+ *  Thread runs forever until the zmq context is terminated.
+ */
+class eventd_proxy
+{
     public:
-        /* Creates the zmq context */
-        eventd_server();
+        eventd_proxy(void *ctx) : m_ctx(ctx), m_frontend(NULL), m_backend(NULL),
+            m_capture(NULL) {};
 
-        ~eventd_server();
+        ~eventd_proxy() {
+            zmq_close(m_frontend);
+            zmq_close(m_backend);
+            zmq_close(m_capture);
 
-        /*
-         *  Main eventd service loop that honors event_req_type_t
-         *
-         *  For echo, it just echoes
-         *
-         *  FOr cache start, create the SUB end of capture and kick off
-         *  capture_events thread. Upon cache stop command, close the handle
-         *  which will stop the caching thread with read failure.
-         *
-         *  for cache read, returns the collected events as subset of 
-         *  strings.
-         *
-         */
-        int eventd_service();
+            if (m_thr.joinable)
+                m_thr.join();
+        }
 
-
-        /*
-         * For any fatal failure, terminate the entire run across threads
-         * by deleting the zmq context.
-         */
-        void close();
+        int init();
 
     private:
-        /*
-         *  Started by eventd_service.
-         *  Creates XPUB & XSUB end points.
-         *  Bind the same
-         *  Create a PUB socket end point for capture and bind.
-         *  Call run_proxy method with sockets in a dedicated thread.
-         *  Thread runs forever until the zmq context is terminated.
-         */
-        int zproxy_service();
-        int zproxy_service_run(void *front, void *back, void *capture);
-
-
-        /*
-         *  Capture/Cache service
-         *
-         *  The service started in a dedicted thread upon demand.
-         *  It expects SUB end of capture created & connected to the PUB
-         *  capture end in zproxy service.
-         *
-         *  It goes in a forever loop, until the zmq read fails, which will happen
-         *  if the capture/SUB end is closed. The stop cache service will close it,
-         *  while start cache service creates & connects.
-         *
-         *  Hence this thread/function is active between cache start & stop.
-         *
-         *  Each event is 2 parts. It drops the first part, which is
-         *  more for filtering events. It creates string from second part
-         *  and saves it.
-         *
-         *  The string is the serialized version of internal_event_ref
-         *
-         *  It keeps two sets of data
-         *      1) List of all events received in vector in same order as received
-         *      2) Map of last event from each runtime id upon list overflow max size.
-         *
-         *  We add to the vector as much as allowed by vector and max limit,
-         *  whichever comes first.
-         *  
-         *  The sequence number in internal event will help assess the missed count
-         *  by the consumer of the cache data.
-         *
-         *  Thread is started upon creating SUB end of capture socket.
-         */
-        int capture_events(events_data_lst_t &lst);
-
-
-    private:
-        uint32_t m_cache_max;
+        int run();
 
         void *m_ctx;
+        void *m_frontend;
+        void *m_backend;
+        void *m_capture;
+        thread m_thr;
+};
+
+
+/*
+ *  Capture/Cache service
+ *
+ *  The service started in a dedicted thread upon demand.
+ *  It is controlled by the caller.
+ *  On cache init, the thread is created.
+ *      Upon create, it creates a SUB socket to PUB end point of capture.
+ *      PUB end point is maintained by zproxy service.
+ *
+ *  On Cache start, the thread is signalled to start reading.
+ *
+ *  On cache stop, it is signalled to stop reading and exit. Caller waits
+ *  for thread to exit, before starting to read cached data, to ensure
+ *  that the data is not handled by two threads concurrently.
+ *
+ *  This thread maintains its own copy of cache. Reader, does a swap
+ *  after thread exits.
+ *  This thread ensures the cache is empty at the init.
+ *
+ *  Upon cache start, the thread is blocked in receive call with timeout.
+ *  Only upon receive/timeout, it would notice stop signal. Hence stop
+ *  is not synchronous. The caller may wait for thread to terminate
+ *  via thread.join().
+ *
+ *  Each event is 2 parts. It drops the first part, which is
+ *  more for filtering events. It creates string from second part
+ *  and saves it.
+ *
+ *  The string is the serialized version of internal_event_ref
+ *
+ *  It keeps two sets of data
+ *      1) List of all events received in vector in same order as received
+ *      2) Map of last event from each runtime id upon list overflow max size.
+ *
+ *  We add to the vector as much as allowed by vector and max limit,
+ *  whichever comes first.
+ *  
+ *  The sequence number in internal event will help assess the missed count
+ *  by the consumer of the cache data.
+ *
+ */
+typedef enum {
+    NEED_INIT = 0, 
+    INIT_CAPTURE,
+    START_CAPTURE,
+    STOP_CAPTURE
+} capture_control_t;
+
+
+int capture_status;
+
+class capture_service
+{
+    public:
+        capture_service(void *ctx, int cache_max) : m_ctx(ctx), m_socket(NULL),
+            m_ctl(NEED_INIT), m_status(0), m_cache_max(cache_max)
+        {}
+
+        ~capture_service();
+
+        int set_control(capture_control_t ctrl, events_data_lst_t *p=NULL);
+
+        int read_cache(events_data_lst_t &lst_fifo,
+                last_events_t &lst_last);
+
+    private:
+        void init_capture_cache(events_data_lst_t &lst);
+        void do_capture();
+
+        void stop_capture();
+
+        void *m_ctx;
+        void *m_socket;
+        capture_control_t m_ctrl;
+        thread m_thr;
+
+        uint32_t m_cache_max;
 
         events_data_lst_t m_events;
 
         last_events_t m_last_events;
 
-        void *m_capture;
+        typedef map<runtime_id_t, sequence_t> pre_exist_id_t;
+        pre_exist_id_t m_pre_exist_id;
 
-        thread m_thread_proxy;
-        thread m_thread_capture;
 };
+
+
+/*
+ * Main server, that starts the zproxy service and honor
+ * eventd service requests event_req_type_t
+ *
+ *  For echo, it just echoes
+ *
+ *  FOr cache start, create the SUB end of capture and kick off
+ *  capture_events thread. Upon cache stop command, close the handle
+ *  which will stop the caching thread with read failure.
+ *
+ *  for cache read, returns the collected events in chunks.
+ *
+ */
+void run_eventd_service();
 
 
