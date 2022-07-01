4947c8e2a30998513a4dd6326ec3cde7cc316e51
Dmytro Lytvynenko <dmytrox.lytvynenko@intel.com>
2022-07-01 05:56:49 -0700

    draft upgrade to deb11 of syncd and syncd-rpc

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,5 +1,6 @@
-BFN_PLATFORM = bfnplatform_20220512_sai_1.10.2_deb10.deb
-$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
+BFN_PLATFORM = bfnplatform_1.0.0_amd64.deb
+$(BFN_PLATFORM)_PATH = /sonic
 
-SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
+# SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
+SONIC_COPY_DEBS += $(BFN_PLATFORM)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
