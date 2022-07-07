4e13fa030d86ff83e85b5bbe81df464e1a79d3d1
Dmytro Lytvynenko <dmytrox.lytvynenko@intel.com>
2022-07-07 07:11:41 -0700

    Provide urls for sai and platform debs

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -1,6 +1,5 @@
-BFN_PLATFORM = bfnplatform_1.0.0_amd64.deb
-$(BFN_PLATFORM)_PATH = /sonic
+BFN_PLATFORM = bfnplatform_20220704_sai_1.10.2_deb11.deb
+$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_PLATFORM)"
 
-# SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
-SONIC_COPY_DEBS += $(BFN_PLATFORM)
+SONIC_ONLINE_DEBS += $(BFN_PLATFORM)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_PLATFORM)
--- platform/barefoot/bfn-sai.mk
+++ platform/barefoot/bfn-sai.mk
@@ -1,10 +1,9 @@
-BFN_SAI = bfnsdk_1.0.0_amd64.deb
-$(BFN_SAI)_PATH = /sonic
+BFN_SAI = bfnsdk_20220704_sai_1.10.2_deb11.deb
+$(BFN_SAI)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/dev/$(BFN_SAI)"
 
 $(BFN_SAI)_DEPENDS += $(LIBNL_GENL3_DEV)
 $(eval $(call add_conflict_package,$(BFN_SAI),$(LIBSAIVS_DEV)))
 $(BFN_SAI)_RDEPENDS += $(LIBNL_GENL3)
 
-# SONIC_ONLINE_DEBS += $(BFN_SAI)
-SONIC_COPY_DEBS +=$(BFN_SAI)
+SONIC_ONLINE_DEBS += $(BFN_SAI)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_SAI)
