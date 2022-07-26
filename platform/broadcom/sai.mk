1eee58fe2fe4a94c9482951197743899cab7426e
Judy Joseph <jujoseph@microsoft.com>
2022-07-26 21:09:12 +0000

    Merged PR 6376951: Update the DNX with macsec fixes in internal branch
    Update the DNX with macsec fixes in internal branch

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -1,5 +1,5 @@
 LIBSAIBCM_XGS_VERSION = 7.1.0.0-6
-LIBSAIBCM_DNX_VERSION = 7.1.0.0-2
+LIBSAIBCM_DNX_VERSION = 7.1.0.0-7
 LIBSAIBCM_BRANCH_NAME = REL_7.0
 LIBSAIBCM_XGS_URL_PREFIX = "https://sonicstorage.blob.core.windows.net/public/sai/bcmsai/$(LIBSAIBCM_BRANCH_NAME)/$(LIBSAIBCM_XGS_VERSION)"
 LIBSAIBCM_DNX_URL_PREFIX = "https://sonicstorage.blob.core.windows.net/public/sai/bcmsai/$(LIBSAIBCM_BRANCH_NAME)/$(LIBSAIBCM_DNX_VERSION)"
@@ -12,7 +12,7 @@ $(BRCM_XGS_SAI_DEV)_URL = "$(LIBSAIBCM_XGS_URL_PREFIX)/$(BRCM_XGS_SAI_DEV)"
 
 # SAI module for DNX Asic family
 BRCM_DNX_SAI = libsaibcm_dnx_$(LIBSAIBCM_DNX_VERSION)_amd64.deb
-$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/sai/bcmsai/REL_7.0/7.1.0.0-2/libsaibcm_dnx_7.1.0.0-2_amd64.deb?sv=2020-10-02&st=2022-07-04T21%3A20%3A04Z&se=2025-01-05T22%3A20%3A00Z&sr=b&sp=r&sig=MzkvQWdnQCIfu1pUNIKuJkqqI%2BEDLLxUJi84qpZsEm4%3D"
+$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/7.0/7.1.0.0-7/libsaibcm_dnx_7.1.0.0-7_amd64.deb?sv=2020-10-02&st=2022-07-23T02%3A00%3A55Z&se=2026-07-24T02%3A00%3A00Z&sr=b&sp=r&sig=AEgooC3VpB3ee%2FcxtzUIWtoaFpWbdoYltuqEXGkrPwQ%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_XGS_SAI)
 SONIC_ONLINE_DEBS += $(BRCM_DNX_SAI)
