f7d90d0e6fa15e596ec50c50ab7782cdd41e78a4
maipbui <maibui@microsoft.com>
2022-09-15 19:10:28 +0000

    Fix getstatusoutput and add check_output_pipe
    Signed-off-by: maipbui <maibui@microsoft.com>

--- src/sonic-py-common/sonic_py_common/general.py
+++ src/sonic-py-common/sonic_py_common/general.py
@@ -1,5 +1,5 @@
 import sys
-import subprocess
+from subprocess import Popen, STDOUT, PIPE, CalledProcessError, check_output
 
 
 def load_module_from_source(module_name, file_path):
@@ -31,11 +31,15 @@ def getstatusoutput_noshell(cmd):
     This function implements getstatusoutput API from subprocess module
     but using shell=False to prevent shell injection.
     """
-    p = subprocess.run(cmd, capture_output=True, universal_newlines=True)
-    status, output = p.returncode, p.stdout
+    try:
+        output = check_output(cmd, text=True, stderr=STDOUT)
+        exitcode = 0
+    except CalledProcessError as ex:
+        output = ex.output
+        exitcode = ex.returncode
     if output[-1:] == '\n':
         output = output[:-1]
-    return (status, output)
+    return exitcode, output
 
 
 def getstatusoutput_noshell_pipe(cmd1, cmd2):
@@ -44,13 +48,36 @@ def getstatusoutput_noshell_pipe(cmd1, cmd2):
     but using shell=False to prevent shell injection. Input command includes
     pipe command.
     """
-    status = 0
-    with subprocess.Popen(cmd1, universal_newlines=True, stdout=subprocess.PIPE) as p1:
-        with subprocess.Popen(cmd2, universal_newlines=True, stdin=p1.stdout, stdout=subprocess.PIPE) as p2:
+    cmd = ' '.join(cmd1) + ' | ' + ' '.join(cmd2)
+    try:
+        with Popen(cmd1, universal_newlines=True, stdout=PIPE) as p1:
+            with Popen(cmd2, universal_newlines=True, stdin=p1.stdout, stdout=PIPE) as p2:
+                output = p2.communicate()[0]
+                p1.wait()
+                if p1.returncode != 0 and p2.returncode != 0:
+                    returncode = p1.returncode if p2.returncode == 0 else p2.returncode
+                    raise CalledProcessError(returncode=returncode, cmd=cmd, output=output)
+        exitcode = 0
+    except CalledProcessError as e:
+        output = e.output
+        exitcode = e.returncode
+    if output[-1:] == '\n':
+        output = output[:-1]
+    return exitcode, output
+
+
+def check_output_pipe(cmd1, cmd2):
+    """
+    This function implements check_output API from subprocess module.
+    Input command includes pipe command.
+    """
+    cmd = ' '.join(cmd1) + ' | ' + ' '.join(cmd2)
+    with Popen(cmd1, universal_newlines=True, stdout=PIPE) as p1:
+        with Popen(cmd2, universal_newlines=True, stdin=p1.stdout, stdout=PIPE) as p2:
             output = p2.communicate()[0]
             p1.wait()
             if p1.returncode != 0 and p2.returncode != 0:
-                status = 1
-    if output[-1:] == '\n':
-        output = output[:-1]
-    return (status, output)
+                returncode = p1.returncode if p2.returncode == 0 else p2.returncode
+                raise CalledProcessError(returncode=returncode, cmd=cmd, output=output)
+    return output
+
