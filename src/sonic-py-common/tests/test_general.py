cd007de797ad336912b5b3c0e89addb78471b027
maipbui <maibui@microsoft.com>
2022-09-18 01:09:52 +0000

    Fix comments
    Signed-off-by: maipbui <maibui@microsoft.com>

--- src/sonic-py-common/tests/test_general.py
+++ src/sonic-py-common/tests/test_general.py
@@ -11,14 +11,14 @@ def test_getstatusoutput_noshell(tmp_path):
     exitcode, output = getstatusoutput_noshell([sys.executable, "-c", "import sys; sys.exit(6)"])
     assert exitcode != 0
 
-def test_getstatusoutput_noshell_pipes():
+def test_getstatusoutput_noshell_pipe():
     exitcode, output = getstatusoutput_noshell_pipe(['echo', 'sonic'], ['awk', '{print $1}'])
     assert (exitcode, output) == ([0, 0], 'sonic')
 
     exitcode, output = getstatusoutput_noshell_pipe([sys.executable, "-c", "import sys; sys.exit(6)"], [sys.executable, "-c", "import sys; sys.exit(8)"])
     assert exitcode == [6, 8]
 
-def test_check_output_pipes():
+def test_check_output_pipe():
     output = check_output_pipe(['echo', 'sonic'], ['awk', '{print $1}'])
     assert output == 'sonic\n'
 
