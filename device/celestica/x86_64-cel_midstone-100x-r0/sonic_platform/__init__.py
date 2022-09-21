8dffdbb581fd6f13f1bbfb5c9e2869cf803e617d
praveenraja1 <praveenrajawork@gmail.com>
2022-09-21 02:46:19 +0000

    Resolving LGTM errors

--- device/celestica/x86_64-cel_midstone-100x-r0/sonic_platform/__init__.py
+++ device/celestica/x86_64-cel_midstone-100x-r0/sonic_platform/__init__.py
@@ -1,3 +1,8 @@
-__all__ = ["platform", "chassis"]
-from sonic_platform import *
-
+__all__ = ['chassis', 'eeprom', 'platform', 'psu', 'sfp', 'thermal', 'fan']
+from . import platform
+from . import chassis
+from . import eeprom
+from . import psu
+from . import sfp
+from . import thermal
+from . import fan
