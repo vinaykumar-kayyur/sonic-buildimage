69e88c2875f8b22193c1a5bcc22b0bafd1919825
timsftian <timsftian@tencent.com>
2022-09-28 21:07:50 +0800

    two platforms supporting S3IP sysfs
    Signed-off-by: timsftian <timsftian@tencent.com>

---
+++ platform/broadcom/sonic-platform-modules-tencent/common/script/ruijiecommon.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+# -------------------------------------------------------------------------------
+# Author:      sonic_rd@ruijie.com.cn
+#
+# Created:     02/07/2018
+# Copyright:   2001-2022 Ruijie Network. All rights reserved.
+# -------------------------------------------------------------------------------
+DEVICE             = "DEVICE"
+GLOBALCONFIG       = "GLOBALCONFIG"
+DRIVERLISTS        = "DRIVERLISTS"
+INIT_PARAM         = "INIT_PARAM"
+STARTMODULE = {}
+MAC_AVS_PARAM = {}
+MAC_DEFAULT_PARAM = {}
+DEV_MONITOR_PARAM = {}
+MANUINFO_CONF = {}
+AVS_VOUT_MODE_PARAM = {}
+PMON_SYSLOG_STATUS = {}
+OPTOE = []
+BLACKLIST_DRIVERS = []
+INIT_PARAM_PRE = []
+INIT_COMMAND_PRE = []
+INIT_PARAM = []
+INIT_COMMAND = []
+DRIVERLISTS = []
+DEVICE = []
---
+++ platform/broadcom/sonic-platform-modules-tencent/tcs9400/config/x86_64_tencent_tcs9400_r0_port_config.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+
+PLATFORM_INTF_OPTOE = {
+    "port_num": 128,
+    "optoe_start_bus": 124,
+}
