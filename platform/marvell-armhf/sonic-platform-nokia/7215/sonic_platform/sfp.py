8b979ec4c97513056616b7e4798c3a87d7c85727
maipbui <maibui@microsoft.com>
2022-10-05 18:58:19 +0000

    Change run to call
    Signed-off-by: maipbui <maibui@microsoft.com>

--- platform/marvell-armhf/sonic-platform-nokia/7215/sonic_platform/sfp.py
+++ platform/marvell-armhf/sonic-platform-nokia/7215/sonic_platform/sfp.py
@@ -181,7 +181,7 @@ class Sfp(SfpBase):
             return 'N/A'
 
     def __is_host(self):
-        return subprocess.run(self.HOST_CHK_CMD).returncode == 0
+        return subprocess.call(self.HOST_CHK_CMD) == 0
 
     def __get_path_to_port_config_file(self):
         platform_path = "/".join([self.PLATFORM_ROOT_PATH, self.PLATFORM])
