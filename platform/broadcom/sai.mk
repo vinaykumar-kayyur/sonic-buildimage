4b477d4fc489f0fae366046fca536b91fd0b2faa
Judy Joseph <jujoseph@microsoft.com>
2022-10-12 16:50:32 +0000

    Merged PR 6898455: Update the internal SAI debian package to the one which has macsec support.
    Update the internal SAI debian package to teh correct one which has macsec support also.
    The one shared in sonic-buildimage public branches don't have macsec enabled.
    
    **This needs to be cherry-picked to internal-202205, internal-202205-chassis**

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -12,7 +12,7 @@ $(BRCM_XGS_SAI_DEV)_URL = "$(LIBSAIBCM_XGS_URL_PREFIX)/$(BRCM_XGS_SAI_DEV)"
 
 # SAI module for DNX Asic family
 BRCM_DNX_SAI = libsaibcm_dnx_$(LIBSAIBCM_DNX_VERSION)_amd64.deb
-$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/7.0/7.1.0.0-7/libsaibcm_dnx_7.1.0.0-7_amd64.deb?sv=2020-10-02&st=2022-07-23T02%3A00%3A55Z&se=2026-07-24T02%3A00%3A00Z&sr=b&sp=r&sig=AEgooC3VpB3ee%2FcxtzUIWtoaFpWbdoYltuqEXGkrPwQ%3D"
+$(BRCM_DNX_SAI)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/7.0/7.1.7.2/libsaibcm_dnx_7.1.7.2_amd64.deb?sv=2020-10-02&st=2022-10-11T21%3A52%3A06Z&se=2040-10-12T21%3A52%3A00Z&sr=b&sp=r&sig=xv6%2BqN%2FAywjZpw5QM9MR%2BDNZPoch7W8hb36loOIkIJI%3D"
 
 SONIC_ONLINE_DEBS += $(BRCM_XGS_SAI)
 SONIC_ONLINE_DEBS += $(BRCM_DNX_SAI)
