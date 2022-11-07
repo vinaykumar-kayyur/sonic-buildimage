50eebf4020ab98dc26c9d8d0000573a4b9ab2394
xumia <xumia@microsoft.com>
2022-11-07 01:49:42 +0000

    Pass the mirror url relative parameters to slave

--- scripts/build_mirror_config.sh
+++ scripts/build_mirror_config.sh
@@ -12,7 +12,7 @@ DEFAULT_MIRROR_SECURITY_URLS=http://debian-archive.trafficmanager.net/debian-sec
 # The debian-archive.trafficmanager.net does not support armhf, use debian.org instead
 if [ "$ARCHITECTURE" == "armhf" ]; then
     DEFAULT_MIRROR_URLS=http://deb.debian.org/debian/,http://packages.trafficmanager.net/debian/debian/
-    DEFAULT_MIRROR_SECURITY_URLS=http://security.debian.org/,http://packages.trafficmanager.net/debian/debian-security/
+    DEFAULT_MIRROR_SECURITY_URLS=http://deb.debian.org/debian-security/,http://packages.trafficmanager.net/debian/debian-security/
 fi
 
 [ -z "$MIRROR_URLS" ] && MIRROR_URLS=$DEFAULT_MIRROR_URLS
