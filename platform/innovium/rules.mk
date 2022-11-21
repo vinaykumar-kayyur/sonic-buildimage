ab47ab2539ea770dc6daa3e99b09a954cb2bba87
skannan <skannan@celestica.com>
2022-11-21 06:23:17 +0000

    Silverstonex and Midstone100x support in Master

--- platform/innovium/rules.mk
+++ platform/innovium/rules.mk
@@ -1,13 +1,12 @@
 include $(PLATFORM_PATH)/invm-sai.mk
 include $(PLATFORM_PATH)/platform-modules-cel.mk
-include $(PLATFORM_PATH)/platform-modules-delta.mk
-include $(PLATFORM_PATH)/platform-modules-wistron.mk
-include $(PLATFORM_PATH)/platform-modules-netberg.mk
 include $(PLATFORM_PATH)/docker-syncd-invm.mk
 include $(PLATFORM_PATH)/docker-syncd-invm-rpc.mk
 include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/libsaithrift-dev.mk
 include $(PLATFORM_PATH)/python-saithrift.mk
+
+INVM_CONFLICT_DEB = innovium-sai-headers
 
 SONIC_ALL += $(SONIC_INVM_ONE_IMAGE) \
              $(DOCKER_FPM) \
@@ -16,7 +15,7 @@ SONIC_ALL += $(SONIC_INVM_ONE_IMAGE) \
 
 # Inject invm sai into syncd
 $(SYNCD)_DEPENDS += $(INVM_HSAI) $(INVM_LIBSAI) $(LIBSAITHRIFT_DEV_INVM)
-$(SYNCD)_UNINSTALLS += $(INVM_HSAI)
+$(SYNCD)_UNINSTALLS += $(INVM_CONFLICT_DEB)
 
 # Runtime dependency on invm sai is set only for syncd
 $(SYNCD)_RDEPENDS += $(INVM_HSAI)
