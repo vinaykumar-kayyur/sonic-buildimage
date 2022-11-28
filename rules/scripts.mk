67a59e63ad66f1d2ffecdb624ee8f2f3531b3a2c
Rajendra Dendukuri <rajendra.dendukuri@broadcom.com>
2022-11-28 07:04:04 -0800

    Heartbeat monitor for SONiC - Proof of concept do not merge
    Note: This is a proof of concept implemented as part of the Hackathon event for the ONE Summit 2022
    
    - Heartbeat monitor daemon which polls registered applications to check if they are active and responsive
    - Makefile and container configuration file changes to include the heart beat
      monitor as part of the swss container
    - Future improvements
      - Improved heartbeat message
      - Better polling interval and dead interval management to avoid clock skew
      - Recording additional data like average response time
      - Robust recovery mechanism if the heartbeat monitor crashes or restarts

--- rules/scripts.mk
+++ rules/scripts.mk
@@ -20,6 +20,9 @@ $(CBF_CONFIG_TEMPLATE)_PATH = files/build_templates
 SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT = supervisor-proc-exit-listener
 $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT)_PATH = files/scripts
 
+HEART_BEAT_MONITORD_SCRIPT = hbmonitord
+$(HEART_BEAT_MONITORD_SCRIPT)_PATH = files/scripts
+
 SYSCTL_NET_CONFIG = sysctl-net.conf
 $(SYSCTL_NET_CONFIG)_PATH = files/image_config/sysctl
 
@@ -41,6 +44,7 @@ SONIC_COPY_FILES += $(CONFIGDB_LOAD_SCRIPT) \
                     $(BUFFERS_CONFIG_TEMPLATE) \
                     $(QOS_CONFIG_TEMPLATE) \
                     $(CBF_CONFIG_TEMPLATE) \
+                    $(HEART_BEAT_MONITORD_SCRIPT) \
                     $(SUPERVISOR_PROC_EXIT_LISTENER_SCRIPT) \
                     $(SYSCTL_NET_CONFIG) \
                     $(UPDATE_CHASSISDB_CONFIG_SCRIPT) \
