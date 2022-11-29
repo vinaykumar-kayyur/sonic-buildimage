50f03bfded672c8e2774b10ca6e2976816f0c306
liora <liora@nvidia.com>
2022-11-29 11:31:13 +0000

    Remove unused object from packet scheme

--- platform/mellanox/docker-syncd-mlnx/ecmp_calculator/packet_scheme.py
+++ platform/mellanox/docker-syncd-mlnx/ecmp_calculator/packet_scheme.py
@@ -4,141 +4,134 @@ PACKET_SCHEME = {
     "type" : "object",
     "properties" :
     {
-        "ecmp_hash" : 
+        "packet_info" : 
         {
             "type" : "object",
             "properties" : 
             {
-                "packet_info" : 
+                "outer" : 
                 {
                     "type" : "object",
                     "properties" : 
                     {
-                        "outer" : 
+                        "layer2" : 
                         {
                             "type" : "object",
                             "properties" : 
                             {
-                                "layer2" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "smac": {"type": "string"},
-                                        "dmac": {"type": "string"},
-                                        "ethertype": {"type": "number"},
-                                        "outer_vid": {"type": "number"},
-                                        "outer_pcp": {"type": "number"},
-                                        "outer_dei": {"type": "number"},
-                                        "inner_vid": {"type": "number"},
-                                        "inner_pcp": {"type": "number"},
-                                        "inner_dei": {"type": "number"}
-                                    }
-                                },
-                                "arp" :
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "spa": {"type": "string"},
-                                        "tpa": {"type": "string"}
-                                    }
-                                },
-                                "ipv4" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "sip": {"type": "string"},
-                                        "dip": {"type": "string"},
-                                        "proto": {"type": "number"}
-                                    },
-                                    "required": ["dip"]
-                                },
-                                "ipv6" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "sip": {"type": "string"},
-                                        "dip": {"type": "string"},
-                                        "mflag": {"type": "number"},
-                                        "next_header": {"type": "number"},
-                                        "dscp": {"type": "number"},
-                                        "ecn": {"type": "number"},
-                                        "l3_length": {"type": "number"},
-                                        "flow_label": {"type": "number"}
-                                    },
-                                    "required": ["dip"]
-                                },
-                                "tcp_udp" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "sport": {"type": "number"},
-                                        "dport": {"type": "number"}
-                                    }
-                                },
-                                "vxlan_nvgre" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "vni": {"type": "number"}
-                                    }
-                                }
+                                "smac": {"type": "string"},
+                                "dmac": {"type": "string"},
+                                "ethertype": {"type": "number"},
+                                "outer_vid": {"type": "number"},
+                                "outer_pcp": {"type": "number"},
+                                "outer_dei": {"type": "number"},
+                                "inner_vid": {"type": "number"},
+                                "inner_pcp": {"type": "number"},
+                                "inner_dei": {"type": "number"}
                             }
-                        },                      
-                        "inner" : 
+                        },
+                        "arp" :
                         {
                             "type" : "object",
                             "properties" : 
                             {
-                                "layer2" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "smac": {"type": "string"},
-                                        "dmac": {"type": "string"},
-                                        "ethertype": {"type": "number"}
-                                    }
-                                },
-                                "ipv4" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "sip": {"type": "string"},
-                                        "dip": {"type": "string"},
-                                        "mflag": {"type": "number"},
-                                        "proto": {"type": "number"}
-                                    }
-                                },
-                                "ipv6" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "sip": {"type": "string"},
-                                        "dip": {"type": "string"},
-                                        "mflag": {"type": "number"},
-                                        "next_header": {"type": "number"},
-                                        "flow_label": {"type": "number"}
-                                    }
-                                },
-                                "tcp_udp" : 
-                                {
-                                    "type" : "object",
-                                    "properties" : 
-                                    {
-                                        "sport": {"type": "number"},
-                                        "dport": {"type": "number"}
-                                    }
-                                }
+                                "spa": {"type": "string"},
+                                "tpa": {"type": "string"}
                             }
-                        }                        
+                        },
+                        "ipv4" : 
+                        {
+                            "type" : "object",
+                            "properties" : 
+                            {
+                                "sip": {"type": "string"},
+                                "dip": {"type": "string"},
+                                "proto": {"type": "number"}
+                            },
+                            "required": ["dip"]
+                        },
+                        "ipv6" : 
+                        {
+                            "type" : "object",
+                            "properties" : 
+                            {
+                                "sip": {"type": "string"},
+                                "dip": {"type": "string"},
+                                "mflag": {"type": "number"},
+                                "next_header": {"type": "number"},
+                                "dscp": {"type": "number"},
+                                "ecn": {"type": "number"},
+                                "l3_length": {"type": "number"},
+                                "flow_label": {"type": "number"}
+                            },
+                            "required": ["dip"]
+                        },
+                        "tcp_udp" : 
+                        {
+                            "type" : "object",
+                            "properties" : 
+                            {
+                                "sport": {"type": "number"},
+                                "dport": {"type": "number"}
+                            }
+                        },
+                        "vxlan_nvgre" : 
+                        {
+                            "type" : "object",
+                            "properties" : 
+                            {
+                                "vni": {"type": "number"}
+                            }
+                        }
+                    }
+                },                      
+                "inner" : 
+                {
+                    "type" : "object",
+                    "properties" : 
+                    {
+                        "layer2" : 
+                        {
+                            "type" : "object",
+                            "properties" : 
+                            {
+                                "smac": {"type": "string"},
+                                "dmac": {"type": "string"},
+                                "ethertype": {"type": "number"}
+                            }
+                        },
+                        "ipv4" : 
+                        {
+                            "type" : "object",
+                            "properties" : 
+                            {
+                                "sip": {"type": "string"},
+                                "dip": {"type": "string"},
+                                "mflag": {"type": "number"},
+                                "proto": {"type": "number"}
+                            }
+                        },
+                        "ipv6" : 
+                        {
+                            "type" : "object",
+                            "properties" : 
+                            {
+                                "sip": {"type": "string"},
+                                "dip": {"type": "string"},
+                                "mflag": {"type": "number"},
+                                "next_header": {"type": "number"},
+                                "flow_label": {"type": "number"}
+                            }
+                        },
+                        "tcp_udp" : 
+                        {
+                            "type" : "object",
+                            "properties" : 
+                            {
+                                "sport": {"type": "number"},
+                                "dport": {"type": "number"}
+                            }
+                        }
                     }
                 }                        
             }
