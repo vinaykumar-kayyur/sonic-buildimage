fde7bf85bc3ea8f6b6f5e5a781a41bce9c7f1782
Santhosh Kumar T <Santhosh_Kumar_T@dell.com>
2022-12-12 17:51:05 +0530

    Removing comments

--- platform/broadcom/sonic-platform-modules-dell/z9264f/sonic_platform/chassis.py
+++ platform/broadcom/sonic-platform-modules-dell/z9264f/sonic_platform/chassis.py
@@ -84,9 +84,6 @@ class Chassis(ChassisBase):
 
         for port_num in range(self.PORT_START, (self.PORT_END + 1)):
             #presence = self.get_sfp(port_num).get_presence()
-            #if presence:
-            #    self._global_port_pres_dict[port_num] = '1'
-            #else:
             self._global_port_pres_dict[port_num] = '0'
 
     def __del__(self):
