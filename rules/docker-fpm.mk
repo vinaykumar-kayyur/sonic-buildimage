32485c8dd8cd0d22e32b1d65dc780b74e330c74c
Maksym Hedeon <maksym@githedgehog.com>
2022-12-27 11:16:47 +0200

    Rename INCLUDE_BGP flag to INCLUDE_ROUTING_STACK
    Signed-off-by: Maksym Hedeon <maksym@githedgehog.com>

--- rules/docker-fpm.mk
+++ rules/docker-fpm.mk
@@ -1,6 +1,6 @@
 # Docker-fpm rule-file is simply a wrapper containing routing-stack selection logic.
 
-ifeq ($(INCLUDE_BGP), y)
+ifeq ($(INCLUDE_ROUTING_STACK), y)
 ifeq ($(SONIC_ROUTING_STACK), frr)
 SONIC_INSTALL_DOCKER_IMAGES += $(DOCKER_FPM_FRR)
 SONIC_INSTALL_DOCKER_DBG_IMAGES += $(DOCKER_FPM_FRR_DBG)
