8e631076186dbe1bddc2eae32c979859f77b71e2
pettershao-ragilenetworks <81281940+pettershao-ragilenetworks@users.noreply.github.com>
2023-01-07 11:38:49 +0800

    Update sai.mk

--- platform/centec-arm64/sai.mk
+++ platform/centec-arm64/sai.mk
@@ -3,8 +3,8 @@
 export CENTEC_SAI_VERSION = 1.10.2-1
 export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
-$(CENTEC_SAI)_URL = https://github.com/pettershao-ragilenetworks/sonic-binaries/blob/main/centec-arm64/sai/libsai_1.10.2-1_arm64.deb
-
+$(CENTEC_SAI)_URL = https://github.com/pettershao-ragilenetworks/sonic-binaries/raw/main/centec-arm64/sai/libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
+$(CENTEC_SAI)_DEPENDS += $(LIBNL3) $(LIBNL3_DEV) $(LIBNL_GENL3) $(LIBNL_GENL3_DEV)
 
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
 
