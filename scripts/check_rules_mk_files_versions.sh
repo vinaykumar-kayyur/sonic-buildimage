b46d2b96a733200aa69f65e3e0bb76ba3d4eef62
Guillaume Lambert <guillaume.lambert@orange.com>
2023-01-12 13:57:56 +0100

    [build] Add kdump-tools and lm-sensors automatic version check and configuration
    - declare kdump-tools and lm-sensors
      in scripts/check_rules_mk_files_versions.sh
    - adapt consequently corresponding rules/*.mk files
    
    Signed-off-by: Guillaume Lambert <guillaume.lambert@orange.com>

--- scripts/check_rules_mk_files_versions.sh
+++ scripts/check_rules_mk_files_versions.sh
@@ -22,6 +22,8 @@ for pkgsrcname in \
        bash \
        debootstrap \
        isc-dhcp \
+       kdump-tools \
+       lm-sensors \
        ;do
     basevarname=$(echo $pkgsrcname"_VERSION" | tr [a-z\-] [A-Z_])
 
@@ -35,9 +37,9 @@ for pkgsrcname in \
 
     # next, use eval and awk to parse 'apt-cache showsrc' output and retrieve available versions from debian repositories
     # pipe result to a unique and version sort (-u and -V options) and tail to get the last version
-    eval $(echo "version=\$(apt-cache -c ../../alt_apt_confdir/etc/apt/apt.conf showsrc $pkgsrcname | awk  -F\"[' ]\" '/^Version: $versionpattern[^0-9]*/{print \$2}' | sort -uV |tail -1)")
+    eval $(echo "version=\$(apt-cache -c ../../alt_apt_confdir/etc/apt/apt.conf showsrc $pkgsrcname | awk  -F\"[' ]\" '/^Version: (1:)*$versionpattern[^0-9]*/{print \$2}' | cut -d: -f2- | sort -uV | tail -1)")
     # previous line will be interpreted as follow (modulo package name and version changes)
-    # version=$(apt-cache -c ../../alt_apt_confdir/etc/apt/apt.conf showsrc isc-dhcp | awk  -F"[' ]" '/^Version: 4\.4\.1[^0-9]/{print $2}' | sort -uV |tail -1)
+    # version=$(apt-cache -c ../../alt_apt_confdir/etc/apt/apt.conf showsrc isc-dhcp | awk  -F"[' ]" '/^Version: (1:)*4\.4\.1[^0-9]*/{print $2}' | cut -d: -f2- | sort -uV |tail -1)
     if [ -z "$version" ]; then
         eval echo "ERROR No candidate version in apt sources for $pkgsrcname \$$basevarname. Exiting" >&2
         exit 3
@@ -45,7 +47,7 @@ for pkgsrcname in \
     if [ "$version" != "$(eval echo \$$basevarname\_FULL)" ]; then
        echo "WARNING $(eval echo \$$basevarname\_FULL) and latest version $version differs." >&2
        eval backslashedfullversion=$(echo "\$(echo \$$basevarname\_FULL| sed 's/\./\\\\./g')")
-       if [ -z "$(eval "apt-cache -c ../../alt_apt_confdir/etc/apt/apt.conf showsrc $pkgsrcname | grep '^Version: $backslashedfullversion$'")" ];then
+       if [ -z "$(eval "apt-cache -c ../../alt_apt_confdir/etc/apt/apt.conf showsrc $pkgsrcname | grep '^Version: \(1:\)*$backslashedfullversion$'")" ];then
            echo "WARNING No candidate version in apt sources for $(eval echo \$$basevarname\_FULL). Updating rules/$pkgsrcname.mk to use latest version $version instead." >&2
            sed -i_ "s/^$basevarname\_FULL = .*/$basevarname\_FULL = $version/g" ../../rules/$pkgsrcname.mk
        fi
