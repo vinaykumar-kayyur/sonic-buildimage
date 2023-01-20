c139450d50e85354a8a736dd202b130d3eca9db6
pettershao-ragilenetworks <pettershao@ragilenetworks.com>
2023-01-20 19:19:44 +0800

    Changes: fix redfish_api
    Signed-off-by: pettershao@ragilenetworks.com

--- platform/centec-arm64/sonic-platform-modules-ragile/ra-b6010-48gt4x/sonic_platform/chassis.py
+++ platform/centec-arm64/sonic-platform-modules-ragile/ra-b6010-48gt4x/sonic_platform/chassis.py
@@ -2,6 +2,7 @@
 #!/usr/bin/env python
 
 try:
+    import time
     import os
     from sonic_platform_base.chassis_base import ChassisBase
     from sonic_platform.eeprom import Eeprom
@@ -21,6 +22,10 @@ NUM_PORT    = 52
 NUM_PSU = 2
 
 class Chassis(ChassisBase):
+
+    port_dict = {}
+    STATUS_INSERTED = "1"
+    STATUS_REMOVED = "0"
 
     def __init__(self):
         ChassisBase.__init__(self)
@@ -42,6 +47,10 @@ class Chassis(ChassisBase):
         for index in range(0, NUM_PORT + 1):
             sfp = Sfp(index)
             self._sfp_list.append(sfp)
+            if sfp.get_presence():
+                self.port_dict[index] = self.STATUS_INSERTED
+            else:
+                self.port_dict[index] = self.STATUS_REMOVED
         # Initialize PSU
         for index in range(0, NUM_PSU):
             psu = Psu(index)
@@ -146,7 +155,7 @@ class Chassis(ChassisBase):
         """
         return (None, None)
 
-    def get_change_event(self, timeout=2000):
+    def get_change_event(self, timeout=0):
         """
         Returns a nested dictionary containing all devices which have
         experienced a change at chassis level
@@ -157,21 +166,117 @@ class Chassis(ChassisBase):
 
         Returns:
             (bool, dict):
-                - True if call successful, False if not;
-                - A nested dictionary where key is a device type,
-                  value is a dictionary with key:value pairs in the
-                  format of {'device_id':'device_event'},
-                  where device_id is the device ID for this device and
-                        device_event,
-                             status='1' represents device inserted,
-                             status='0' represents device removed.
-                  Ex. {'fan':{'0':'0', '2':'1'}, 'sfp':{'11':'0'}}
-                      indicates that fan 0 has been removed, fan 2
-                      has been inserted and sfp 11 has been removed.
+                - bool: True if call successful, False if not;
+                - dict: A nested dictionary where key is a device type,
+                        value is a dictionary with key:value pairs in the format of
+                        {'device_id':'device_event'}, where device_id is the device ID
+                        for this device and device_event.
+                        The known devices's device_id and device_event was defined as table below.
+                         -----------------------------------------------------------------
+                         device   |     device_id       |  device_event  |  annotate
+                         -----------------------------------------------------------------
+                         'fan'          '<fan number>'     '0'              Fan removed
+                                                           '1'              Fan inserted
+
+                         'sfp'          '<sfp number>'     '0'              Sfp removed
+                                                           '1'              Sfp inserted
+                                                           '2'              I2C bus stuck
+                                                           '3'              Bad eeprom
+                                                           '4'              Unsupported cable
+                                                           '5'              High Temperature
+                                                           '6'              Bad cable
+
+                         'voltage'      '<monitor point>'  '0'              Vout normal
+                                                           '1'              Vout abnormal
+                         --------------------------------------------------------------------
+                  Ex. {'fan':{'0':'0', '2':'1'}, 'sfp':{'11':'0', '12':'1'},
+                       'voltage':{'U20':'0', 'U21':'1'}}
+                  Indicates that:
+                     fan 0 has been removed, fan 2 has been inserted.
+                     sfp 11 has been removed, sfp 12 has been inserted.
+                     monitored voltage U20 became normal, voltage U21 became abnormal.
+                  Note: For sfp, when event 3-6 happened, the module will not be avalaible,
+                        XCVRD shall stop to read eeprom before SFP recovered from error status.
         """
-        ret, port_dict = self._sfp_list[0].get_transceiver_change_event(timeout)
-        ret_dict = {"sfp": port_dict}
-        return ret, ret_dict
+
+        change_event_dict = {"fan": {}, "sfp": {}, "voltage": {}}
+
+        start_time = time.time()
+        forever = False
+
+        if timeout == 0:
+            forever = True
+        elif timeout > 0:
+            timeout = timeout / float(1000)  # Convert to secs
+        else:
+            print("get_change_event:Invalid timeout value", timeout)
+            return False, change_event_dict
+
+        end_time = start_time + timeout
+        if start_time > end_time:
+            print(
+                "get_change_event:" "time wrap / invalid timeout value",
+                timeout,
+            )
+            return False, change_event_dict  # Time wrap or possibly incorrect timeout
+        try:
+            while timeout >= 0:
+                # check for sfp
+                sfp_change_dict = self.get_transceiver_change_event()
+                # check for fan
+                # fan_change_dict = self.get_fan_change_event()
+                # check for voltage
+                # voltage_change_dict = self.get_voltage_change_event()
+
+                if sfp_change_dict:
+                    change_event_dict["sfp"] = sfp_change_dict
+                    # change_event_dict["fan"] = fan_change_dict
+                    # change_event_dict["voltage"] = voltage_change_dict
+                    return True, change_event_dict
+                if forever:
+                    time.sleep(1)
+                else:
+                    timeout = end_time - time.time()
+                    if timeout >= 1:
+                        time.sleep(1)  # We poll at 1 second granularity
+                    else:
+                        if timeout > 0:
+                            time.sleep(timeout)
+                        return True, change_event_dict
+        except Exception as e:
+            print(e)
+        print("get_change_event: Should not reach here.")
+        return False, change_event_dict
+
+    def get_transceiver_change_event(self):
+        current_port_dict = {}
+        ret_dict = {}
+
+        # Check for OIR events and return ret_dict
+        for index in range(0, NUM_PORT + 1):
+            if self._sfp_list[index].get_presence():
+                current_port_dict[index] = self.STATUS_INSERTED
+            else:
+                current_port_dict[index] = self.STATUS_REMOVED
+
+        if len(self.port_dict) == 0:       # first time
+            self.port_dict = current_port_dict
+            return {}
+
+        if current_port_dict == self.port_dict:
+            return {}
+
+        # Update reg value
+        for index, status in current_port_dict.items():
+            if self.port_dict[index] != status:
+                ret_dict[index] = status
+                #ret_dict[str(index)] = status
+        self.port_dict = current_port_dict
+        for index, status in ret_dict.items():
+            if int(status) == 1:
+                pass
+                #self._sfp_list[int(index)].check_sfp_optoe_type()
+        return ret_dict
 
     def get_num_psus(self):
         return len(self._psu_list)
