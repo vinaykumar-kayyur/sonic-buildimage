d15e8442cba9ddae4b7aafb8b7c49596011d3b64
pettershao-ragilenetworks <pettershao@ragilenetworks.com>
2023-01-27 23:04:37 +0800

    fix psu fan led status Signed-off-by: pettershao-ragilenetworks <pettershao@ragilenetworks.com>

--- platform/broadcom/sonic-platform-modules-ragile/ra-b6510-48v8c/sonic_platform/fan.py
+++ platform/broadcom/sonic-platform-modules-ragile/ra-b6510-48v8c/sonic_platform/fan.py
@@ -9,7 +9,7 @@ class Fan(PddfFan):
     """PDDF Platform-Specific Fan class"""
 
     def __init__(self, tray_idx, fan_idx=0, pddf_data=None, pddf_plugin_data=None, is_psu_fan=False, psu_index=0):
-        # idx is 0-based 
+        # idx is 0-based
         PddfFan.__init__(self, tray_idx, fan_idx, pddf_data, pddf_plugin_data, is_psu_fan, psu_index)
         self.pddf_obj = api.newapi()
     # Provide the functions/variables below for which implementation is to be overwritten
@@ -40,4 +40,24 @@ class Fan(PddfFan):
             return None
 
         return super().get_target_speed()
+
+    def get_status_led(self):
+        if self.is_psu_fan:
+            return "N/A"
+
+        led_device_name = "FANTRAY{}".format(self.fantray_index) + "_LED"
+
+        if led_device_name not in self.pddf_obj.data.keys():
+            # Implement a generic status_led color scheme
+            if self.get_status():
+                return self.STATUS_LED_COLOR_GREEN
+            else:
+                return self.STATUS_LED_COLOR_OFF
+
+        device_name = self.pddf_obj.data[led_device_name]['dev_info']['device_name']
+        self.pddf_obj.create_attr('device_name', device_name,  self.pddf_obj.get_led_path())
+        self.pddf_obj.create_attr('index', str(self.fantray_index-1), self.pddf_obj.get_led_path())
+        self.pddf_obj.create_attr('dev_ops', 'get_status',  self.pddf_obj.get_led_path())
+        color = self.pddf_obj.get_led_color()
+        return (color)
 
