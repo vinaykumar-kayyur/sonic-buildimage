66974d7154f6b411076b7b341b05797b0809ced4
shilongliu <shilongliu@microsoft.com>
2023-01-31 13:30:54 +0800

    [build] use pip install of later setup.py to install test dependencies

---
+++ scripts/get_py_depend.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import setuptools
+from distutils import core
+import os
+import json
+
+def setup(**kwargs):
+        ret=[]
+        a=kwargs.get('setup_requires')
+        b=kwargs.get('tests_require')
+        c=kwargs.get('install_requires')
+        if a:
+            ret+=a
+        if b:
+            ret+=b
+        if c:
+            ret+=c
+        for i,x in enumerate(ret):
+            print("'"+x+"'")
+
+
+if __name__ == "__main__":
+  setuptools.setup=setup
+  core.setup=setup
+  content = open('setup.py').read()
+  exec(content)
