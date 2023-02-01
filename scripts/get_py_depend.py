4f0086661fbcfb4d4673297a2b8694fb8af56e34
shilongliu <shilongliu@microsoft.com>
2023-02-01 15:10:58 +0800

    fix

--- scripts/get_py_depend.py
+++ scripts/get_py_depend.py
@@ -4,18 +4,10 @@ import setuptools
 from distutils import core
 
 def setup(**kwargs):
-        ret=[]
-        a=kwargs.get('setup_requires')
-        b=kwargs.get('tests_require')
-        c=kwargs.get('install_requires')
-        if a:
-            ret+=a
-        if b:
-            ret+=b
-        if c:
-            ret+=c
-        for i,x in enumerate(ret):
-            print("'"+x+"'")
+  ret=kwargs.get('tests_require')
+  if ret:
+    for i,x in enumerate(ret):
+      print("'"+x+"'")
 
 
 if __name__ == "__main__":
