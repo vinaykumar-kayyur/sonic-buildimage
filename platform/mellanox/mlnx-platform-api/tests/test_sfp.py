f1c529965b485c9371b7ce0797ced133b8493d76
junchao <junchao@nvidia.com>
2023-02-07 12:38:59 +0800

    [Mellanox] Use sysfs for sfp reset/LPM/presence

--- platform/mellanox/mlnx-platform-api/tests/test_sfp.py
+++ platform/mellanox/mlnx-platform-api/tests/test_sfp.py
@@ -121,7 +121,6 @@ class TestSfp:
             handle.write.side_effect = OSError('')
             assert not sfp.write_eeprom(0, 1, bytearray([1]))
 
-    @mock.patch('sonic_platform.sfp.SFP.get_mst_pci_device', mock.MagicMock(return_value = None))
     @mock.patch('sonic_platform.sfp.SFP._get_page_and_page_offset')
     def test_sfp_read_eeprom(self, mock_get_page):
         sfp = SFP(0)
@@ -142,14 +141,6 @@ class TestSfp:
 
             handle.read.side_effect = OSError('')
             assert sfp.read_eeprom(0, 1) is None
-
-    @mock.patch('sonic_platform.sfp.SFP._fetch_port_status')
-    def test_is_port_admin_status_up(self, mock_port_status):
-        mock_port_status.return_value = (0, True)
-        assert SFP.is_port_admin_status_up(None, None)
-
-        mock_port_status.return_value = (0, False)
-        assert not SFP.is_port_admin_status_up(None, None)
 
     @mock.patch('sonic_platform.sfp.SFP._get_eeprom_path', mock.MagicMock(return_value = None))
     @mock.patch('sonic_platform.sfp.SFP._get_sfp_type_str')
@@ -216,13 +207,14 @@ class TestSfp:
         assert page == '/tmp/1/data'
         assert page_offset is 0
 
-    @mock.patch('sonic_platform.sfp.SFP._read_eeprom')
-    def test_get_presence(self, mock_read_eeprom):
+    @mock.patch('sonic_platform.utils.read_int_from_file')
+    def test_get_presence(self, mock_read_int):
         sfp = SFP(0)
-        mock_read_eeprom.return_value = None
+        mock_read_int.return_value = 0
         assert not sfp.get_presence()
+        mock_read_int.assert_called_with('/sys/module/sx_core/asic0/module0/present')
 
-        mock_read_eeprom.return_value = bytearray([1])
+        mock_read_int.return_value = 1
         assert sfp.get_presence()
 
     @mock.patch('sonic_platform.sfp.SFP.get_xcvr_api')
@@ -238,3 +230,31 @@ class TestSfp:
         mock_get_xcvr_api.return_value = None
         assert sfp.get_rx_los() is None
         assert sfp.get_tx_fault() is None
+
+    @mock.patch('sonic_platform.utils.write_file')
+    def test_reset(self, mock_write):
+        sfp = SFP(0)
+        mock_write.return_value = True
+        assert sfp.reset()
+        mock_write.assert_called_with('/sys/module/sx_core/asic0/module0/reset', '1')
+
+    @mock.patch('sonic_platform.utils.read_int_from_file')
+    def test_get_lpmode(self, mock_read_int):
+        sfp = SFP(0)
+        mock_read_int.return_value = 1
+        assert sfp.get_lpmode()
+        mock_read_int.assert_called_with('/sys/module/sx_core/asic0/module0/power_mode')
+
+        mock_read_int.return_value = 2
+        assert not sfp.get_lpmode()
+
+    @mock.patch('sonic_platform.utils.write_file')
+    @mock.patch('sonic_platform.utils.read_int_from_file')
+    def test_set_lpmode(self, mock_read_int, mock_write):
+        sfp = SFP(0)
+        mock_read_int.return_value = 1
+        assert sfp.set_lpmode(False)
+        assert mock_write.call_count == 0
+
+        assert sfp.set_lpmode(True)
+        mock_write.assert_called_with('/sys/module/sx_core/asic0/module0/power_mode_policy', '2')
