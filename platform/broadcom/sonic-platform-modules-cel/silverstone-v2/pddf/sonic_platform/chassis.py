bfd2607ed181692e6776af50b91e4a7e8bedde80
Yagami-Jiang <119273528+Yagami-Jiang@users.noreply.github.com>
2023-02-10 11:18:58 +0800

    Update chassis.py
    add fun: get_reboot_cause

--- platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/chassis.py
+++ platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/chassis.py
@@ -6,8 +6,16 @@
 
 try:
     from sonic_platform_pddf_base.pddf_chassis import PddfChassis
+    import sys
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
+
+HOST_REBOOT_CAUSE_PATH = "/host/reboot-cause/"
+REBOOT_CAUSE_FILE = "reboot-cause.txt"
+PREV_REBOOT_CAUSE_FILE = "previous-reboot-cause.txt"
+GETREG_PATH = "/sys/devices/platform/sys_cpld/getreg"
+RESET_REGISTER = "0xA106"
+
 
 class Chassis(PddfChassis):
     """
@@ -31,12 +39,50 @@ class Chassis(PddfChassis):
         sfp = None
 
         try:
-            if (index == 0):
+            if index == 0:
                 raise IndexError
-            sfp = self._sfp_list[index-1]
+            sfp = self._sfp_list[index - 1]
         except IndexError:
             sys.stderr.write("override: SFP index {} out of range (1-{})\n".format(
                 index, len(self._sfp_list)))
 
         return sfp
-    # Provide the functions/variables below for which implementation is to be overwritten
+
+    def get_reboot_cause(self):
+        """
+        Retrieves the cause of the previous reboot
+        Returns:
+            A tuple (string, string) where the first element is a string
+            containing the cause of the previous reboot. This string must be
+            one of the predefined strings in this class. If the first string
+            is "REBOOT_CAUSE_HARDWARE_OTHER", the second string can be used
+            to pass a description of the reboot cause.
+            REBOOT_CAUSE_POWER_LOSS = "Power Loss"
+            REBOOT_CAUSE_THERMAL_OVERLOAD_CPU = "Thermal Overload: CPU"
+            REBOOT_CAUSE_THERMAL_OVERLOAD_ASIC = "Thermal Overload: ASIC"
+            REBOOT_CAUSE_THERMAL_OVERLOAD_OTHER = "Thermal Overload: Other"
+            REBOOT_CAUSE_INSUFFICIENT_FAN_SPEED = "Insufficient Fan Speed"
+            REBOOT_CAUSE_WATCHDOG = "Watchdog"
+            REBOOT_CAUSE_HARDWARE_OTHER = "Hardware - Other"
+            REBOOT_CAUSE_NON_HARDWARE = "Non-Hardware"
+        """
+        reboot_cause_path = (HOST_REBOOT_CAUSE_PATH + REBOOT_CAUSE_FILE)
+        sw_reboot_cause = self._api_helper.read_txt_file(
+            reboot_cause_path) or "Unknown"
+        hw_reboot_cause = self._api_helper.get_cpld_reg_value(
+            GETREG_PATH, RESET_REGISTER)
+
+        prev_reboot_cause = {
+            '0x11': (self.REBOOT_CAUSE_POWER_LOSS, "The last reset is Power on reset"),
+            '0x22': (self.REBOOT_CAUSE_HARDWARE_OTHER, "The last reset is soft-set CPU warm reset"),
+            '0x33': (self.REBOOT_CAUSE_NON_HARDWARE, "The last reset is CPU cold reset"),
+            '0x44': (self.REBOOT_CAUSE_NON_HARDWARE, "The last reset is CPU warm reset"),
+            '0x66': (self.REBOOT_CAUSE_WATCHDOG, "The last reset is watchdog reset"),
+
+        }.get(hw_reboot_cause, (self.REBOOT_CAUSE_HARDWARE_OTHER, 'Unknown reason'))
+
+        if sw_reboot_cause != 'Unknown':
+            prev_reboot_cause = (
+                self.REBOOT_CAUSE_NON_HARDWARE, sw_reboot_cause)
+
+        return prev_reboot_cause
