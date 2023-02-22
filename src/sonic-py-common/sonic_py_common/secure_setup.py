6fbafa1bc2b663c3f80911bd5be565303a2051b3
shilongliu <shilongliu@microsoft.com>
2023-02-22 07:01:35 +0000

    fix

--- src/sonic-py-common/sonic_py_common/secure_setup.py
+++ src/sonic-py-common/sonic_py_common/secure_setup.py
@@ -11,8 +11,8 @@ def secure_setup(**kwargs):
                 proc = subprocess.Popen([sys.executable, '-m', 'pip', 'show', package.split("=")[0].split(">")[0].split("<")[0]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 stdout, stderr = proc.communicate()
                 if proc.returncode != 0:
-                    sys.stdout.write(stdout)
-                    sys.stderr.write(stderr)
+                    sys.stdout.write(str(stdout))
+                    sys.stderr.write(str(stderr))
                     sys.stderr.write("Please build and install SONiC python wheels dependencies from github.com/sonic-net/sonic-buildimage\n")
                     sys.stderr.write("Then install other dependencies from Pypi\n")
                     exit(1)
