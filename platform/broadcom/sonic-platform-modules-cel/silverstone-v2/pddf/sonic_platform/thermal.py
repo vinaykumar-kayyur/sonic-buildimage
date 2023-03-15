748b1dbd8adaeafa0f2f3b065f175057a0734507
Yagami-Jiang <119273528+Yagami-Jiang@users.noreply.github.com>
2023-03-15 16:35:58 +0800

    Update thermal.py

--- platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/thermal.py
+++ platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/thermal.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# @Company ：Celestica
+# @Time    : 2023/3/6 09:32
+# @Mail    : J_Talong@163.com yajiang@celestica.com
+# @Author  : jiang tao
+
 try:
     from sonic_platform_pddf_base.pddf_thermal import PddfThermal
     from . import helper
