d92e10def9698e35e499fc8b4640cfa82207a75a
Neetha John <nejo@microsoft.com>
2023-03-22 18:38:12 -0700

    Update 6100 qos test as well to prevent dirty check failure
    Signed-off-by: Neetha John <nejo@microsoft.com>

--- src/sonic-config-engine/tests/test_j2files.py
+++ src/sonic-config-engine/tests/test_j2files.py
@@ -323,14 +323,18 @@ class TestJ2Files(TestCase):
         self._test_qos_render_template('dell', 'x86_64-dellemc_z9332f_d1508-r0', 'DellEMC-Z9332f-O32', 'sample-dell-9332-t1-minigraph.xml', 'qos-dell9332.json')
 
     def test_qos_dell6100_render_template(self):
-        self._test_qos_render_template('dell', 'x86_64-dell_s6100_c2538-r0', 'Force10-S6100', 'sample-dell-6100-t0-minigraph.xml', 'qos-dell6100.json')
+        self._test_qos_render_template('dell', 'x86_64-dell_s6100_c2538-r0', 'Force10-S6100', 'sample-dell-6100-t0-minigraph.xml', 'qos-dell6100.json', copy_files=True)
 
     def test_qos_arista7260_render_template(self):
         self._test_qos_render_template('arista', 'x86_64-arista_7260cx3_64', 'Arista-7260CX3-D96C16', 'sample-arista-7260-t1-minigraph-remap-disabled.xml', 'qos-arista7260.json')
 
-    def _test_qos_render_template(self, vendor, platform, sku, minigraph, expected):
+    def _test_qos_render_template(self, vendor, platform, sku, minigraph, expected, copy_files=False):
         file_exist, dir_exist = self.create_machine_conf(platform, vendor)
         dir_path = os.path.join(self.test_dir, '..', '..', '..', 'device', vendor, platform, sku)
+
+        if copy_files:
+            self.copy_mmu_templates(dir_path, revert=False)
+
         qos_file = os.path.join(dir_path, 'qos.json.j2')
         port_config_ini_file = os.path.join(dir_path, 'port_config.ini')
 
@@ -345,6 +349,8 @@ class TestJ2Files(TestCase):
         # cleanup
         qos_config_file_new = os.path.join(dir_path, 'qos_config.j2')
         os.remove(qos_config_file_new)
+        if copy_files:
+            self.copy_mmu_templates(dir_path, revert=True)
 
         self.remove_machine_conf(file_exist, dir_exist)
 
