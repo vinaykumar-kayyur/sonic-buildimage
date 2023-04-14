702012554948c6ea96cbe3bc17d52cdacf2f710f
tianguangqing <tianguangqing@ruijie.com.cn>
2023-04-14 10:49:51 +0800

    [Changes]: support centec b6010
      Signed-off-by: pettershao@ragilenetworks.com

--- platform/centec-arm64/sai.mk
+++ platform/centec-arm64/sai.mk
@@ -1,13 +1,14 @@
 # Centec SAI
 
-export CENTEC_SAI_VERSION = 1.11.0-1
+export CENTEC_SAI_VERSION = 1.10.2-1
 export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
-export CENTEC_SAI_DEV = libsai-dev_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
+#export CENTEC_SAI_DEV = libsai-dev_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
-$(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI)
-$(CENTEC_SAI_DEV)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI_DEV)
-$(eval $(call add_conflict_package,$(CENTEC_SAI_DEV),$(LIBSAIVS_DEV)))
+#$(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI)
+#$(CENTEC_SAI_DEV)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI_DEV)
+#$(eval $(call add_conflict_package,$(CENTEC_SAI_DEV),$(LIBSAIVS_DEV)))
+$(CENTEC_SAI)_URL = https://github.com/pettershao-ragilenetworks/sonic-binaries/raw/main/centec-arm64/sai/$(CENTEC_SAI)
 
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
-SONIC_ONLINE_DEBS += $(CENTEC_SAI_DEV)
+#SONIC_ONLINE_DEBS += $(CENTEC_SAI_DEV)
 
