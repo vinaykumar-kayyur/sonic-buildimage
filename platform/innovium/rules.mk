e89657d621210dc41b3edb9d1b95751f1de510cc
praveenraja1 <praveenrajawork@gmail.com>
2023-04-17 13:59:42 +0000

    Adding back changes done by Marvell in master

--- platform/innovium/rules.mk
+++ platform/innovium/rules.mk
@@ -1,5 +1,8 @@
 include $(PLATFORM_PATH)/invm-sai.mk
 include $(PLATFORM_PATH)/platform-modules-cel.mk
+#include $(PLATFORM_PATH)/platform-modules-delta.mk
+include $(PLATFORM_PATH)/platform-modules-wistron.mk
+#include $(PLATFORM_PATH)/platform-modules-netberg.mk
 include $(PLATFORM_PATH)/docker-syncd-invm.mk
 include $(PLATFORM_PATH)/docker-syncd-invm-rpc.mk
 include $(PLATFORM_PATH)/one-image.mk
@@ -14,8 +17,8 @@ SONIC_ALL += $(SONIC_INVM_ONE_IMAGE) \
 INVM_CONFLICT_DEB = innovium-sai-headers
 
 # Inject invm sai into syncd
-$(SYNCD)_DEPENDS += $(INVM_HSAI) $(INVM_LIBSAI) $(LIBSAITHRIFT_DEV_INVM)
-$(SYNCD)_UNINSTALLS += $(INVM_CONFLICT_DEB)
+$(SYNCD)_DEPENDS += $(INVM_HSAI) $(INVM_LIBSAI) $(LIBSAITHRIFT_DEV_INVM) $(INVM_SHELL)
+$(SYNCD)_UNINSTALLS += $(INVM_CONFLICT_DEB) $(INVM_HSAI)
 
 # Runtime dependency on invm sai is set only for syncd
 $(SYNCD)_RDEPENDS += $(INVM_HSAI)
