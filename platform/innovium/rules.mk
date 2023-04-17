df63d6470cb897c05e6405a33e45dc6c10108d4f
praveenraja1 <praveenrajawork@gmail.com>
2023-04-17 10:27:39 +0000

    [Clestica] support for Midstone100x and SilverstoneX - Adding files

--- platform/innovium/rules.mk
+++ platform/innovium/rules.mk
@@ -1,8 +1,5 @@
 include $(PLATFORM_PATH)/invm-sai.mk
 include $(PLATFORM_PATH)/platform-modules-cel.mk
-#include $(PLATFORM_PATH)/platform-modules-delta.mk
-include $(PLATFORM_PATH)/platform-modules-wistron.mk
-#include $(PLATFORM_PATH)/platform-modules-netberg.mk
 include $(PLATFORM_PATH)/docker-syncd-invm.mk
 include $(PLATFORM_PATH)/docker-syncd-invm-rpc.mk
 include $(PLATFORM_PATH)/one-image.mk
@@ -14,9 +11,11 @@ SONIC_ALL += $(SONIC_INVM_ONE_IMAGE) \
              $(DOCKER_PTF_INVM) \
              $(DOCKER_SYNCD_INVM_RPC)
 
+INVM_CONFLICT_DEB = innovium-sai-headers
+
 # Inject invm sai into syncd
-$(SYNCD)_DEPENDS += $(INVM_HSAI) $(INVM_LIBSAI) $(LIBSAITHRIFT_DEV_INVM) $(INVM_SHELL)
-$(SYNCD)_UNINSTALLS += $(INVM_HSAI)
+$(SYNCD)_DEPENDS += $(INVM_HSAI) $(INVM_LIBSAI) $(LIBSAITHRIFT_DEV_INVM)
+$(SYNCD)_UNINSTALLS += $(INVM_CONFLICT_DEB)
 
 # Runtime dependency on invm sai is set only for syncd
 $(SYNCD)_RDEPENDS += $(INVM_HSAI)
