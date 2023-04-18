c1764eb1c08571447424d82fc60df7321890e7ac
nmoray <nikhil@aviznetworks.com>
2023-04-18 05:19:25 -0700

    Addressed the comments

--- rules/docker-macsec.mk
+++ rules/docker-macsec.mk
@@ -40,6 +40,8 @@ $(DOCKER_MACSEC)_VERSION = 1.0.0
 $(DOCKER_MACSEC)_PACKAGE_NAME = macsec
 $(DOCKER_MACSEC)_RUN_OPT += --privileged -t
 $(DOCKER_MACSEC)_RUN_OPT += -v /etc/sonic:/etc/sonic:ro
+$(DOCKER_MACSEC)_RUN_OPT += -v /etc/timezone:/etc/timezone:ro 
+$(DOCKER_MACSEC)_RUN_OPT += -v /etc/localtime:/etc/localtime:ro 
 $(DOCKER_MACSEC)_RUN_OPT += -v /host/warmboot:/var/warmboot
 
 $(DOCKER_MACSEC)_SERVICE_REQUIRES = updategraph
