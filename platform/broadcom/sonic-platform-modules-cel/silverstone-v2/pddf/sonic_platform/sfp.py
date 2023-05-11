fc61fe8d9ad49d103bbac0e967dff38b07b32aac
Yagami-Jiang <119273528+Yagami-Jiang@users.noreply.github.com>
2023-05-11 13:45:00 +0800

    Update sfp.py

--- platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/sfp.py
+++ platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/sfp.py
@@ -9,12 +9,6 @@ try:
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
-I2C_EEPROM_PATH = '/sys/bus/i2c/devices/i2c-{0}/{0}-0050/eeprom'
-BUS_PORT_DICT = {'1': '16', '2': '13', '3': '12', '4': '10', '5': '9', '6': '14', '7': '11', '8': '7', '9': '20',
-                 '10': '17', '11': '22', '12': '25', '13': '19', '14': '21', '15': '24', '16': '18', '17': '3',
-                 '18': '4', '19': '1', '20': '2', '21': '8', '22': '6', '23': '5', '24': '15', '25': '27', '26': '32',
-                 '27': '29', '28': '31', '29': '30', '30': '23', '31': '26', '32': '28'}
-
 
 class Sfp(PddfSfp):
     """
@@ -23,13 +17,3 @@ class Sfp(PddfSfp):
 
     def __init__(self, index, pddf_data=None, pddf_plugin_data=None):
         PddfSfp.__init__(self, index, pddf_data, pddf_plugin_data)
-
-    # Provide the functions/variables below for which implementation is to be overwritten
-    def get_eeprom_path(self):
-        """
-        Overwrite this function cause BUS-Port isn't Ordered
-        """
-        for bus, port in BUS_PORT_DICT.items():
-            if port == str(self.port_index):
-                eeprom_path = I2C_EEPROM_PATH.format(bus, port)
-                return eeprom_path
