e9a1816489b3479318f1f3c04488f437c4fde71f
Yagami-Jiang <119273528+Yagami-Jiang@users.noreply.github.com>
2023-05-24 10:18:49 +0800

    Update pddf_sensor_list_refresh.py

--- platform/broadcom/sonic-platform-modules-cel/silverstone-v2/utils/pddf_sensor_list_refresh.py
+++ platform/broadcom/sonic-platform-modules-cel/silverstone-v2/utils/pddf_sensor_list_refresh.py
@@ -8,32 +8,4 @@
 import os
 import time
 
-try:
-    from sonic_platform import sensor_list_config
-    from sonic_py_common import logger
-except ImportError as e:
-    raise ImportError(str(e) + "- required module not found")
-
-sensor_info_path = sensor_list_config.Sensor_List_Info
-refresh_interval = sensor_list_config.Sensor_Info_Update_Period_Secs
-
-log = logger.Logger()
-
-
-def write_sensor_list_info():
-    """
-    Write the log of the command 'ipmitool sensor list' in sensor_info.log
-    """
-    try:
-        info = os.popen("ipmitool sensor list").read()
-        if "System_Event" in info:
-            with open(sensor_info_path, "w") as f:
-                f.write(info)
-    except Exception as E:
-        log.log_error(str(E))
-
-
-if __name__ == '__main__':
-    while 1:
-        write_sensor_list_info()
-        time.sleep(refresh_interval)
+print("wait for overwrite)
