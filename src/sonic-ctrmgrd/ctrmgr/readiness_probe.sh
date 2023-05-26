4b53a1293e942a0c1a931b7a2633920abbd2523e
Yun Li <yunli1@microsoft.com>
2023-05-26 13:29:42 +0800

    Update

--- src/sonic-ctrmgrd/ctrmgr/readiness_probe.sh
+++ src/sonic-ctrmgrd/ctrmgr/readiness_probe.sh
@@ -8,7 +8,14 @@
 # 2: supervisor start service doesn't exit normally
 #### other: return code of post_check_script, define in the script, should not include 1,2
 
-if [ $(supervisorctl status start |awk '{print $2}') != 'EXITED' ]; then
+# supervisorctl exit code
+# 0: Running
+# 3: Exited
+# 4: ERROR (no such process)
+pre_check_service_name="start"
+supervisorctl status |awk '{print $1}' |grep -w $pre_check_service_name > /dev/null
+start_check_result=$?
+if [ $start_check_result = 0 ] && [ $(supervisorctl status $pre_check_service_name |awk '{print $2}') != 'EXITED' ]; then
     exit 2
 fi
 
@@ -16,9 +23,9 @@ fi
 post_check_script="/usr/bin/readiness_probe.py"
 if [ -f $post_check_script ]; then
     python3 $post_check_script
-    check_result=$?
-    if [ $check_result != 0 ]; then
-        exit $check_result
+    post_check_result=$?
+    if [ $post_check_result != 0 ]; then
+        exit $post_check_result
     fi
 fi
 
