a74b4083381ca063ac1ba25d5b356bb67260b763
Yun Li <yunli1@microsoft.com>
2023-05-26 17:13:36 +0800

    Update comments

--- src/sonic-ctrmgrd/ctrmgr/readiness_probe.sh
+++ src/sonic-ctrmgrd/ctrmgr/readiness_probe.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-# This script is used to check the readiness of containers
-# Check if the container is ready or not
+# This script is used by k8s to check the readiness of containers
+# Check if the container is readiness or not, exit code 0 means readiness, others mean not readiness
 
-#### exit code
-# 0: ready
+#### exit code contract, k8s only cares zero or not none-zero, but we want to use none-zero code to indicate different error
+# 0: readiness
 # 1: python script crach exit code
 # 2: supervisor start service doesn't exit normally
-#### other: return code of post_check_script, define in the script, should not include 1,2
+# other exit code: returned by post_check_script, define in the post_check_script, should not include 1,2
 
-# supervisorctl exit code
-# 0: Running
-# 3: Exited
-# 4: ERROR (no such process)
+# check if the start service exists
+# if the start service exists, check if it exits normally
+# if the start service doesn't exist normally, exit with code 2
 pre_check_service_name="start"
 supervisorctl status |awk '{print $1}' |grep -w $pre_check_service_name > /dev/null
 start_check_result=$?
@@ -20,6 +19,9 @@ if [ $start_check_result = 0 ] && [ $(supervisorctl status $pre_check_service_na
 fi
 
 # feature owner can add their own readiness check script
+# check if the post_check_script exists
+# if the post_check_script exists, run it
+# if the post_check_script exits with non-zero code, exit with the code
 post_check_script="/usr/bin/readiness_probe.py"
 if [ -f $post_check_script ]; then
     python3 $post_check_script
