86967f31b3a9dd56857552778af7f37738a53e5a
Pavan Naregundi <pnaregundi@marvell.com>
2023-06-05 09:51:21 +0000

    marvell-arm64: Add platform support db98cx8580
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell-arm64/rules.mk
+++ platform/marvell-arm64/rules.mk
@@ -8,6 +8,7 @@ include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/platform-nokia.mk
 include $(PLATFORM_PATH)/platform-rd98dx35xx.mk
 include $(PLATFORM_PATH)/platform-db98cx8540-16cd.mk
+include $(PLATFORM_PATH)/platform-db98cx8580-32cd.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM) 	\
