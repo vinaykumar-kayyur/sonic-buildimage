864aace2f029399523646b97fb51253374421ffc
Pavan Naregundi <pnaregundi@marvell.com>
2023-06-05 09:33:47 +0000

    marvell-arm64: Add platform support for rd98DX35xx
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell-arm64/rules.mk
+++ platform/marvell-arm64/rules.mk
@@ -6,6 +6,7 @@ include $(PLATFORM_PATH)/docker-saiserver-mrvl.mk
 include $(PLATFORM_PATH)/libsaithrift-dev.mk
 include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/platform-nokia.mk
+include $(PLATFORM_PATH)/platform-rd98dx35xx.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM) 	\
