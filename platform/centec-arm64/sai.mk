15851cfbb84b936e9da52ae2ca8be86a99fe7540
philo <philo@micasnetworks.com>
2023-06-08 11:17:34 +0800

    Add new platform m2-b6010
    Signed-off-by: philo <philo@micasnetworks.com>

--- platform/centec-arm64/sai.mk
+++ platform/centec-arm64/sai.mk
@@ -1,9 +1,8 @@
 # Centec SAI
 
-export CENTEC_SAI_VERSION = 1.9.1-0
+export CENTEC_SAI_VERSION = 1.10.2-1
 export CENTEC_SAI = libsai_$(CENTEC_SAI_VERSION)_$(PLATFORM_ARCH).deb
 
-$(CENTEC_SAI)_URL = https://github.com/CentecNetworks/sonic-binaries/raw/master/$(PLATFORM_ARCH)/sai/$(CENTEC_SAI)
-$(eval $(call add_conflict_package,$(CENTEC_SAI),$(LIBSAIVS_DEV)))
+$(CENTEC_SAI)_URL = https://raw.githubusercontent.com/philo-micas/sonic-binaries/main/centec-arm64/sai/$(CENTEC_SAI)
 SONIC_ONLINE_DEBS += $(CENTEC_SAI)
 
