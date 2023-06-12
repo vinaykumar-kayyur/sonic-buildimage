e9aba9aad5c9f2d045cc14271caba4a82d59b887
Abhishek Dosi <abdosi@microsoft.com>
2023-06-12 21:16:30 +0000

    Changes in LLDP Configuration to handle this: 1. Enable LLDP only on front panel interfaces 2. Remove System hostname and System Management IP configuration from lldpd.conf since it is now already handle by lldpmgrd as part of Config Db event Set handling 3. For multi-asic do not generate lldpd.conf with eth0 related configuration 4. remove lldpd pause command from lldpd.conf.j2 but instead add it as part of waitfor_lldp_ready.sh
    Signed-off-by: Abhishek Dosi <abdosi@microsoft.com>

--- dockers/docker-lldp/waitfor_lldp_ready.sh
+++ dockers/docker-lldp/waitfor_lldp_ready.sh
@@ -5,10 +5,8 @@
 # But then it execute 'lldpcli resume' to configure and unpause itself.
 # When lldpd execute lldpcli, it doesn't check the return code
 # Sometimes lldpcli returns failure, but lldpd doesn't catch it
-# and keeps working paused and unconfigured
-#
-# The fix below addresses the issue.
-#
+# and keeps working paused and unconfigured. The fix below addresses the issue and
+# pause lldpd operations until all interfaces are well configured, resume command will run in lldpmgrd
 
 # wait until lldpd started
 until [[ -e /var/run/lldpd.socket ]];
@@ -19,6 +17,6 @@ done
 # Manually try to resume lldpd, until it's successful
 while /bin/true;
 do
-    lldpcli -u /var/run/lldpd.socket -c /etc/lldpd.conf -c /etc/lldpd.d resume > /dev/null && break
+    lldpcli -u /var/run/lldpd.socket -c /etc/lldpd.conf -c /etc/lldpd.d pause > /dev/null && break
     sleep 1
 done
