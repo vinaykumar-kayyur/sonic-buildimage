91b483469b214050be7c060f63b7ea50455069f7
FS <sec@feisu.com>
2023-06-19 14:21:43 +0800

    Add new FS platform
    Signed-off-by: FS <sec@feisu.com>

---
+++ device/fs/x86_64-fs_n8560w_32c-r0/N8560W-32C/sai.profile
@@ -0,0 +1,1 @@
+SAI_INIT_CONFIG_FILE=/usr/share/sonic/hwsku/td3-n8560w-32c-32x100G.config.bcm
---
+++ platform/broadcom/sonic-platform-modules-fs/debian/rule.mk
@@ -0,0 +1,6 @@
+currentdir = $(shell pwd)
+
+MODULE_DIRS += n8560w-32c
+MODULE_DIRS += n8560w-48bc
+
+export MODULE_DIRS
