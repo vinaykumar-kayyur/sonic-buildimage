af3a9656957fc3e4701c15f37098dcc341f45952
Pavan Naregundi <pnaregundi@marvell.com>
2023-06-19 11:57:04 +0000

    Unified sdk driver changes
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell-arm64/prestera.mk
+++ platform/marvell-arm64/prestera.mk
@@ -1,3 +1,3 @@
 # Marvell Prestera
 export MRVL_PRESTERA_SRC_URL = https://github.com/Marvell-switching/mrvl-prestera.git
-export MRVL_PRESTERA_SRC_TAG = MRVL_PRESTERA_DRIVER_1.5
+export MRVL_PRESTERA_SRC_TAG = MRVL_PRESTERA_DRIVER_1.6
