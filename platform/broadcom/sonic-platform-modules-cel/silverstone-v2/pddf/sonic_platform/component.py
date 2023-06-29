9e144bb0833a574e21ace6152b2eca3e218eb5fc
Yagami-Jiang <119273528+Yagami-Jiang@users.noreply.github.com>
2023-06-29 17:02:38 +0800

    Update component.py

--- platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/component.py
+++ platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/component.py
@@ -11,26 +11,22 @@ except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
 
 FPGA_VERSION_PATH = "/sys/devices/platform/fpga-sys/version"
-Check_Bios_Boot = "0x3a 0x25 0x02"
+Check_Bios_Boot = "i2cget -y -f 100 0x0d 0x70 | tr a-z A-Z | cut -d 'X' -f 2"
 Bios_Version_Cmd = "dmidecode -t bios | grep Version"
-Fan_CPLD_Cmd = "ipmitool raw 0x3a 0x64 02 01 00"
-COME_CPLD_Cmd = "ipmitool raw 0x3a 0x3e 1 0x1a 1 0xe0"
-Sys_Cpld_Cmd = "ipmitool raw 0x3a 0x64 0x00 0x01 0x00"
-Sw_Cpld1_Cmd = "i2cget -y -f 108 0x30 0 | tr a-z A-Z | cut -d 'X' -f 2"
-Sw_Cpld2_Cmd = "i2cget -y -f 108 0x31 0 | tr a-z A-Z | cut -d 'X' -f 2"
-Main_BMC_Cmd = "0x32 0x8f 0x08 0x01"
-Backup_BMC_Cmd = "0x32 0x8f 0x08 0x01"
+Fan_CPLD_Cmd = "i2cget -y -f 107 0x0d 0x00 | tr a-z A-Z | cut -d 'X' -f 2"
+COME_CPLD_Cmd = "i2cget -y -f 104 0x0d 0xe0 | tr a-z A-Z | cut -d 'X' -f 2"
+Sys_Cpld_Cmd = "i2cget -y -f 100 0x0d 0x00 | tr a-z A-Z | cut -d 'X' -f 2"
+Sw_Cpld1_Cmd = "i2cget -y -f 108 0x30 0x00 | tr a-z A-Z | cut -d 'X' -f 2"
+Sw_Cpld2_Cmd = "i2cget -y -f 108 0x31 0x00 | tr a-z A-Z | cut -d 'X' -f 2"
 
 COMPONENT_NAME_LIST = ["FPGA", "COME_CPLD", "SWCPLD1", "SWCPLD2", "FANCPLD", "SYSCPLD",
-                       "Main_BMC", "Backup_BMC", "Main_BIOS", "Backup_BIOS"]
+                       "Main_BIOS", "Backup_BIOS"]
 COMPONENT_DES_LIST = ["Used for managering the CPU and expanding I2C channels",
                       "Used for managing the CPU",
                       "Used for managing QSFP+ ports (1-16)",
                       "Used for managing QSFP+ ports (17-32)",
                       "Used for managing fans",
                       "Used for managing control the system power & reset",
-                      "Main Baseboard Management Controller",
-                      "Backup Baseboard Management Controller",
                       "Main basic Input/Output System",
                       "Backup basic Input/Output System"]
 
@@ -51,7 +47,7 @@ class Component(ComponentBase):
         Get Bios version by command 'dmidecode -t bios | grep Version'
         return: Bios Version
         """
-        status, result = self.helper.ipmi_raw(Check_Bios_Boot)
+        status, result = self.helper.run_command(Check_Bios_Boot)
         bios_version = "N/A"
         if not status:
             print("Fail! Unable to get the current Main bios or backup bios!")
@@ -68,7 +64,7 @@ class Component(ComponentBase):
         elif result.strip() == "03" and self.name == "Backup_BIOS":
             return bios_version.strip()
         else:
-            return bios_version
+            return "N/A"
 
     def __get_cpld_version(self):
         """
@@ -101,21 +97,6 @@ class Component(ComponentBase):
         if not status:
             return "N/A"
         return fpga_version.replace("0x", "")
-
-    def __get_bmc_version(self):
-        """
-        Get main/backup bmc version
-        """
-        version = "N/A"
-        cmd = Main_BMC_Cmd if self.name == "Main_BMC" else Backup_BMC_Cmd
-        status, result = self.helper.ipmi_raw(cmd)
-        if not status:
-            print("Fail! Can't get the %s version by command:%s" % (self.name, cmd))
-            return version
-        str_1 = str(int(result.strip().split(" ")[0]))
-        str_2 = str(int(result.strip().split(" ")[1], 16))
-        version = "%s.%s" % (str_1, str_2)
-        return version
 
     def get_name(self):
         """
@@ -151,7 +132,8 @@ class Component(ComponentBase):
             fw_version = self.__get_bmc_version()
         return fw_version
 
-    def install_firmware(self, image_path):
+    @staticmethod
+    def install_firmware(image_path):
         """
         Install firmware to module
         Args:
@@ -161,6 +143,7 @@ class Component(ComponentBase):
         """
         return False
 
-    def update_firmware(self, image_path):
+    @staticmethod
+    def update_firmware(image_path):
         # Not support
         return False
