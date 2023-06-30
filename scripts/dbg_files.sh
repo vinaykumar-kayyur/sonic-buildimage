c19867d5d91647bff6f7baa925d1207583cfe86c
Renuka Manavalan <47282725+renukamanavalan@users.noreply.github.com>
2023-06-30 13:28:21 -0700

    Integrate Device Health into SONiC (#40)
    Integrate device-health as SONiC service as submod.

--- scripts/dbg_files.sh
+++ scripts/dbg_files.sh
@@ -6,7 +6,7 @@ if [ "$DEBUG_IMG" == "y" ]
 then
     for i in $DEBUG_SRC_ARCHIVE_DIRS
     do
-        find src/$i/ -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -type f
+        find src/$i/ -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" -o -name "*.go" -o -name "*.json" -type f
     done | tar -czf $DEBUG_SRC_ARCHIVE_FILE -T -
 fi
 
