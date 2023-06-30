6f9c632250d3375724b5c71c47e0be2a67f1650d
Renuka Manavalan <remanava@microsoft.com>
2023-06-30 20:23:37 +0000

    Added debug docker

--- rules/device-health.mk
+++ rules/device-health.mk
@@ -9,6 +9,9 @@ $(SONIC_DEVICE_HEALTH)_DEPENDS  = $(LIBSWSSCOMMON_DEV) $(LIBSWSSCOMMON)
 
 SONIC_DPKG_DEBS += $(SONIC_DEVICE_HEALTH)
 
+SONIC_DEVICE_HEALTH_DBG = sonic-$(SONIC_DEVICE_HEALTH_PKG_NAME)-dbgsym_$(SONIC_DEVICE_HEALTH_VERSION)_$(CONFIGURED_ARCH).deb
+$(eval $(call add_derived_package,$(SONIC_DEVICE_HEALTH),$(SONIC_DEVICE_HEALTH_DBG)))
+
 # The .c, .cpp, .h & .hpp files under src/{$DBG_SRC_ARCHIVE list}
 # are archived into debug one image to facilitate debugging.
 #
