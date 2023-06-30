2c8cb8cb4c868a4d03f153203191833bb8f491ba
Zhaohui Sun <zhaohuisun@microsoft.com>
2023-06-30 08:05:39 +0000

    Fix UT test
    Signed-off-by: Zhaohui Sun <zhaohuisun@microsoft.com>

--- src/sonic-host-services/tests/caclmgrd/caclmgrd_scale_test.py
+++ src/sonic-host-services/tests/caclmgrd/caclmgrd_scale_test.py
@@ -9,7 +9,7 @@ from pyfakefs.fake_filesystem_unittest import patchfs
 
 from .test_scale_vectors import CACLMGRD_SCALE_TEST_VECTOR
 from tests.common.mock_configdb import MockConfigDb
-from unittest.mock import MagicMock, patch
+
 
 DBCONFIG_PATH = '/var/run/redis/sonic-db/database_config.json'
 
@@ -28,24 +28,24 @@ class TestCaclmgrdScale(TestCase):
 
     @parameterized.expand(CACLMGRD_SCALE_TEST_VECTOR)
     @patchfs
-    def test_caclmgrd_scale(self, test_name, test_data, fs):
+    def test_caclmgrd_scale(self, test_name, test_data, fs): 
         if not os.path.exists(DBCONFIG_PATH):
             fs.create_file(DBCONFIG_PATH) # fake database_config.json
 
         MockConfigDb.set_config_db(test_data["config_db"])
 
-        with mock.patch("caclmgrd.ControlPlaneAclManager.run_commands", return_value='sonic'):
-            with mock.patch("caclmgrd.subprocess") as mocked_subprocess:
-                popen_mock = mock.Mock()
-                popen_attrs = test_data["popen_attributes"]
-                popen_mock.configure_mock(**popen_attrs)
-                mocked_subprocess.Popen.return_value = popen_mock
-                mocked_subprocess.PIPE = -1
+        with mock.patch("caclmgrd.subprocess") as mocked_subprocess:
+            popen_mock = mock.Mock()
+            popen_attrs = test_data["popen_attributes"]
+            popen_mock.configure_mock(**popen_attrs)
+            mocked_subprocess.Popen.return_value = popen_mock
+            mocked_subprocess.PIPE = -1
 
-                call_rc = test_data["call_rc"]
-                mocked_subprocess.call.return_value = call_rc
+            call_rc = test_data["call_rc"]
+            mocked_subprocess.call.return_value = call_rc
 
-                caclmgrd_daemon = self.caclmgrd.ControlPlaneAclManager("caclmgrd")
-                caclmgrd_daemon.num_changes[''] = 150
-                caclmgrd_daemon.check_and_update_control_plane_acls('', 150)
-                mocked_subprocess.Popen.assert_has_calls(test_data["expected_subprocess_calls"], any_order=True)
+            caclmgrd_daemon = self.caclmgrd.ControlPlaneAclManager("caclmgrd")
+            caclmgrd_daemon.num_changes[''] = 150
+            caclmgrd_daemon.check_and_update_control_plane_acls('', 150)
+
+            mocked_subprocess.Popen.assert_has_calls(test_data["expected_subprocess_calls"], any_order=True)
