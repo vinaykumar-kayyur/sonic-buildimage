30b4c0644016bd7fb119cd298e79b4bd2c7bd0f8
Yun Li <yunli1@microsoft.com>
2023-07-03 18:52:04 +0800

    Update the hook script to an executable file

--- src/sonic-ctrmgrd/ctrmgr/readiness_probe.sh
+++ src/sonic-ctrmgrd/ctrmgr/readiness_probe.sh
@@ -4,7 +4,7 @@
 
 #### exit code contract, k8s only cares zero or not none-zero, but we want to use none-zero code to indicate different error
 # 0: readiness
-# 1: python script crash exit code
+# 1: if the hook script is python code, the default crash exit code is 1
 # 2: supervisor start service doesn't exit normally
 # other exit code: returned by post_check_script, define in the post_check_script, should not include 1,2
 
@@ -22,9 +22,9 @@ fi
 # check if the post_check_script exists
 # if the post_check_script exists, run it
 # if the post_check_script exits with non-zero code, exit with the code
-post_check_script="/usr/bin/readiness_probe.py"
-if [ -f $post_check_script ]; then
-    python3 $post_check_script
+post_check_script="/usr/bin/readiness_probe_hook"
+if [ -x $post_check_script ]; then
+    $post_check_script
     post_check_result=$?
     if [ $post_check_result != 0 ]; then
         exit $post_check_result
