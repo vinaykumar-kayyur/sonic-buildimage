61ccb382afbe9589f5215582be8f75f4ebe29908
Stephen Sun <stephens@nvidia.com>
2023-07-03 15:35:07 +0800

    Remove PSU name from the description
    Signed-off-by: Stephen Sun <stephens@nvidia.com>

--- src/system-health/health_checker/hardware_checker.py
+++ src/system-health/health_checker/hardware_checker.py
@@ -261,9 +261,9 @@ class HardwareChecker(HealthChecker):
                     try:
                         power = data_dict['power']
                         power_critical_threshold = data_dict['power_critical_threshold']
-                        self.set_object_not_ok('PSU', name, 'System power exceeds {} threshold ({}w)'.format(name, power_critical_threshold))
+                        self.set_object_not_ok('PSU', name, 'System power exceeds threshold ({}w)'.format(power_critical_threshold))
                     except KeyError:
-                        self.set_object_not_ok('PSU', name, 'System power exceeds {} threshold but power_critical_threshold is invalid'.format(name))
+                        self.set_object_not_ok('PSU', name, 'System power exceeds threshold but power_critical_threshold is invalid')
                     continue
 
             self.set_object_ok('PSU', name)
