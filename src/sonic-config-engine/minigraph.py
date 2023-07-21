1cbe8fc5ae9cb6a3a0494d13ae7e3c3a70121c41
vaibhav-dahiya <vdahiya@microsoft.com>
2023-07-21 00:30:45 +0000

    fix the files
    Signed-off-by: vaibhav-dahiya <vdahiya@microsoft.com>

--- src/sonic-config-engine/minigraph.py
+++ src/sonic-config-engine/minigraph.py
@@ -1771,6 +1771,8 @@ def parse_xml(filename, platform=None, port_config_file=None, asic_name=None, hw
         port_default_speed =  port_speeds_default.get(port_name, None)
         port_png_speed = port_speed_png[port_name]
 
+        ports.setdefault(port_name, {})['speed'] = port_speed_png[port_name]
+
         # when the port speed is changes from 400g to 100g/40g
         # update the port lanes, use the first 4 lanes of the 400G port to support 100G/40G port
         if port_default_speed == '400000' and (port_png_speed == '100000' or port_png_speed == '40000'):
@@ -1779,7 +1781,6 @@ def parse_xml(filename, platform=None, port_config_file=None, asic_name=None, hw
             updated_lanes = ",".join(port_lanes[:4])
             ports[port_name]['lanes'] = updated_lanes
 
-        ports.setdefault(port_name, {})['speed'] = port_speed_png[port_name]
 
     for port_name, port in list(ports.items()):
         # get port alias from port_config.ini
