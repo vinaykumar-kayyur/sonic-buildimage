49906725b9aa823c625b2c517316f3f95aa34c7e
Yagami-Jiang <119273528+Yagami-Jiang@users.noreply.github.com>
2023-07-27 22:42:27 +0800

    Update psu.py

--- platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/psu.py
+++ platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/psu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # @Company ：Celestica
-# @Time    : 2023/7/20  13:46
+# @Time    : 2023/6/13 13:46
 # @Mail    : yajiang@celestica.com
 # @Author  : jiang tao
 try:
@@ -8,12 +8,8 @@ try:
     import re
     import os
     from . import helper
-    from . import bmc_present_config
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
-
-BMC_EXIST = bmc_present_config.get_bmc_status()
-PSU_STATUS_INFO_CMD = "i2cget -y -f 100 0x0d 0x60"
 
 
 class Psu(PddfPsu):
@@ -22,11 +18,6 @@ class Psu(PddfPsu):
     def __init__(self, index, pddf_data=None, pddf_plugin_data=None):
         PddfPsu.__init__(self, index, pddf_data, pddf_plugin_data)
         self.helper = helper.APIHelper()
-        if BMC_EXIST:
-            from . import sensor_list_config
-            if not os.path.exists(sensor_list_config.Sensor_List_Info):
-                cmd = "ipmitool sensor list > %s" % sensor_list_config.Sensor_List_Info
-                self.helper.run_command(cmd)
 
     @staticmethod
     def get_capacity():
@@ -36,37 +27,10 @@ class Psu(PddfPsu):
     def get_type():
         return 'AC'
 
-    def get_status(self):
+    @staticmethod
+    def get_revision():
         """
-        Get PSU1/2 power ok status by iic command
-        return: True or False
+        Get PSU HW Revision by read psu eeprom data.
+        return: HW Revision or 'N/A'
         """
-        if BMC_EXIST:
-            return super().get_status()
-        else:
-            status, res = self.helper.run_command(PSU_STATUS_INFO_CMD)
-            if not status:
-                return False
-            psu_status = (bin(int(res, 16)))[-2:]
-            psu_power_ok_index = 0 if self.psu_index == 1 else 1
-            psu_power_ok = self.plugin_data['PSU']['psu_power_good']["i2c"]['valmap'][psu_status[psu_power_ok_index]]
-            return psu_power_ok
-
-    def get_presence(self):
-        """
-        Get PSU1/2 present status by iic command
-        return: True or False
-        """
-        if BMC_EXIST:
-            return super().get_presence()
-        else:
-            status, res = self.helper.run_command(PSU_STATUS_INFO_CMD)
-            if not status:
-                return False
-            psu_status = (bin(int(res, 16)))[-4:-2]
-            psu_present_index = 0 if self.psu_index == 1 else 1
-            psu_present = self.plugin_data['PSU']['psu_present']["i2c"]['valmap'][psu_status[psu_present_index]]
-            return psu_present
-
-
-
+        return "N/A"
