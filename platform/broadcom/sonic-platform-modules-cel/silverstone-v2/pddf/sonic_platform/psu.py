375a502c7ea9e671800f3cde36139e35718f8d70
Yagami-Jiang <119273528+Yagami-Jiang@users.noreply.github.com>
2023-07-27 22:43:29 +0800

    Update psu.py

--- platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/psu.py
+++ platform/broadcom/sonic-platform-modules-cel/silverstone-v2/pddf/sonic_platform/psu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # @Company ：Celestica
-# @Time    : 2023/6/13 13:46
+# @Time    : 2023/7/20  13:46
 # @Mail    : yajiang@celestica.com
 # @Author  : jiang tao
 try:
@@ -8,8 +8,11 @@ try:
     import re
     import os
     from . import helper
+    from . import bmc_present_config
 except ImportError as e:
     raise ImportError(str(e) + "- required module not found")
+
+BMC_EXIST = bmc_present_config.get_bmc_status()
 
 
 class Psu(PddfPsu):
@@ -18,6 +21,11 @@ class Psu(PddfPsu):
     def __init__(self, index, pddf_data=None, pddf_plugin_data=None):
         PddfPsu.__init__(self, index, pddf_data, pddf_plugin_data)
         self.helper = helper.APIHelper()
+        if BMC_EXIST:
+            from . import sensor_list_config
+            if not os.path.exists(sensor_list_config.Sensor_List_Info):
+                cmd = "ipmitool sensor list > %s" % sensor_list_config.Sensor_List_Info
+                self.helper.run_command(cmd)
 
     @staticmethod
     def get_capacity():
@@ -34,3 +42,6 @@ class Psu(PddfPsu):
         return: HW Revision or 'N/A'
         """
         return "N/A"
+
+
+
