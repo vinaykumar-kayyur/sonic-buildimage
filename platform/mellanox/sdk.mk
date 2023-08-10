6963d3bf6ac4711f2e38a9b3c008e419445098d3
Kebo Liu <kebol@nvidia.com>
2023-08-10 22:08:43 +0800

    [Mellanox] Update SDK/FW to 4.6.1018/2012.1018
    Signed-off-by: Kebo Liu <kebol@nvidia.com>

--- platform/mellanox/sdk.mk
+++ platform/mellanox/sdk.mk
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-MLNX_SDK_VERSION = 4.5.5142
+MLNX_SDK_VERSION = 4.6.1018
 MLNX_SDK_ISSU_VERSION = 101
 
 MLNX_SDK_DRIVERS_GITHUB_URL = https://github.com/Mellanox/Spectrum-SDK-Drivers
