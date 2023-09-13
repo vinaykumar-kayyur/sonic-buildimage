84201a2ee857617acd3186b5b8332535d91e4a0d
shilongliu <shilongliu@microsoft.com>
2023-09-13 15:07:21 +0800

    fix

--- rules/docker-dpu.mk
+++ rules/docker-dpu.mk
@@ -4,6 +4,7 @@ DOCKER_DPU_STEM = docker-dpu
 DOCKER_DPU = $(DOCKER_DPU_STEM).gz
 
 $(DOCKER_DPU)_PACKAGE_NAME = dpu
+$(DOCKER_DPU)_CONTAINER_NAME = dpu
 $(DOCKER_DPU)_URL = https://sonicstoragepublic.blob.core.windows.net/public/ubuntu.gz
 $(DOCKER_DPU)_MACHINE = vs
 $(DOCKER_DPU)_VERSION = 1.0.0
