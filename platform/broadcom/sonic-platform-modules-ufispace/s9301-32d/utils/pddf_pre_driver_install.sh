0cb357797f7582be069d7427b83ce5de48403232
linyutsung <linyutsung6502@gmail.com>
2023-09-22 15:25:18 +0800

    [Ufispace][PDDF] Add support for S9301-32D platform

---
+++ platform/broadcom/sonic-platform-modules-ufispace/s9301-32d/utils/pddf_pre_driver_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#rmmod gpio_ich
+if [ ! -f /tmp/._pddf_pre_driver_init_completion ]; then
+    # make sure igb/i40e init in correct order
+    rmmod i40e
+    rmmod igb
+    modprobe igb
+    modprobe i40e
+    # disable bmc watchdog
+    ipmitool mc watchdog off
+    date > /tmp/._pddf_pre_driver_init_completion
+fi
+echo "PDDF driver pre-install completed"
