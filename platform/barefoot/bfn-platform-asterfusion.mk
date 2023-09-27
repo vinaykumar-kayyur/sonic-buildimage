162353363c80f24ea139892b1589a3fc7603ab88
SunZheng <sunzheng@asterfusion.com>
2023-09-27 14:34:25 +0800

    [Asterfusion][platform]Update bfnplatform from 23.08-rc2 to 23.09
    Signed-off-by: SunZheng <sunzheng@asterfusion.com>

--- platform/barefoot/bfn-platform-asterfusion.mk
+++ platform/barefoot/bfn-platform-asterfusion.mk
@@ -1,5 +1,5 @@
 BFN_ASTERFUSION_PLATFORM = bfnplatform-asterfusion_1.0.0_amd64.deb
-$(BFN_ASTERFUSION_PLATFORM)_URL = "https://github.com/asterfusion/bf-bsp-lts/releases/download/23.08-rc2/bfnplatform-asterfusion_1.0.0_amd64.deb"
+$(BFN_ASTERFUSION_PLATFORM)_URL = "https://github.com/asterfusion/bf-bsp-lts/releases/download/23.09/bfnplatform-asterfusion_1.0.0_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BFN_ASTERFUSION_PLATFORM)
 $(BFN_SAI_DEV)_DEPENDS += $(BFN_ASTERFUSION_PLATFORM)
