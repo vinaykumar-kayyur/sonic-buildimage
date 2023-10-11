d8c71aa3ec1bce5229bece0abbee5b8d2e64764e
Junchao-Mellanox <junchao@nvidia.com>
2023-10-11 09:31:05 +0300

    [Mellanox] implement platform wait in python code

--- platform/mellanox/mlnx-platform-api/sonic_platform/utils.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/utils.py
@@ -289,6 +289,30 @@ def wait_until(predict, timeout, interval=1, *args, **kwargs):
     return False
 
 
+def wait_until_conditions(conditions, timeout, interval=1):
+    """
+    Wait until all the conditions become true
+    Args:
+        conditions (list): a list of callable which generate True|False
+        timeout (int): wait time in seconds
+        interval (int, optional):  interval to check the predict. Defaults to 1.
+
+    Returns:
+        bool: True if wait success else False
+    """
+    while timeout > 0:
+        pending_conditions = []
+        for condition in conditions:
+            if not condition():
+                pending_conditions.append(condition)
+        if not pending_conditions:
+            return True
+        conditions = pending_conditions
+        time.sleep(interval)
+        timeout -= interval
+    return False
+
+
 class DbUtils:
     lock = threading.Lock()
     db_instances = threading.local()
--- platform/mellanox/mlnx-platform-api/tests/test_sfp.py
+++ platform/mellanox/mlnx-platform-api/tests/test_sfp.py
@@ -333,3 +333,28 @@ class TestSfp:
         assert not sfp.set_lpmode(True)
         assert sfp.set_lpmode(False)
         assert not sfp.set_lpmode(False)
+
+    @mock.patch('sonic_platform.utils.read_int_from_file')
+    @mock.patch('sonic_platform.device_data.DeviceDataManager.is_independent_mode')
+    @mock.patch('sonic_platform.utils.DbUtils.get_db_instance')
+    def test_is_sw_control(self, mock_get_db, mock_mode, mock_read):
+        sfp = SFP(0)
+        mock_mode.return_value = False
+        assert not sfp.is_sw_control()
+        mock_mode.return_value = True
+
+        mock_db = mock.MagicMock()
+        mock_get_db.return_value = mock_db
+        mock_db.get = mock.MagicMock(return_value=None)
+        with pytest.raises(Exception):
+            sfp.is_sw_control()
+
+        mock_read.return_value = 0
+        mock_db.get.return_value = 'FW_CONTROL'
+        assert not sfp.is_sw_control()
+        mock_read.return_value = 1
+        mock_db.get.return_value = 'SW_CONTROL'
+        assert sfp.is_sw_control()
+        mock_read.return_value = 0
+        with pytest.raises(Exception):
+            sfp.is_sw_control()
