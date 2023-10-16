f18d03f5c96efc61e998b5435c4644a18c1274e7
Pavan Naregundi <pnaregundi@marvell.com>
2023-10-16 07:23:49 +0000

    [Marvell-arm64] Create submodule for sdk drivers
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell-arm64/rules.mk
+++ platform/marvell-arm64/rules.mk
@@ -5,7 +5,7 @@ include $(PLATFORM_PATH)/docker-syncd-mrvl-rpc.mk
 include $(PLATFORM_PATH)/docker-saiserver-mrvl.mk
 include $(PLATFORM_PATH)/libsaithrift-dev.mk
 include $(PLATFORM_PATH)/one-image.mk
-include $(PLATFORM_PATH)/linux-kernel-arm64.mk
+include $(PLATFORM_PATH)/mrvl-prestera.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM) 	\
