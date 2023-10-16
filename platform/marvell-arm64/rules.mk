7c0e9ecebf6446c8d5500fc154aad8239190ffa1
Pavan Naregundi <pnaregundi@marvell.com>
2023-10-16 07:27:47 +0000

    [Marvell-arm64] Add platform support for rd98DX35xx
    This change adds following two variants of rd98DX35xx board to arm64
    build.
    
    Board with CPU integrated into the 98DX35xx switching chip:
    
     Platform: arm64-marvell_rd98DX35xx-r0
     HwSKU: rd98DX35xx
     ASIC: marvell
     Port Config: 32x1G + 16x2.5G + 6x25G
    
    Board with external CN9131 CPU connected over PCI to 98DX35xx
    switching chip:
    
     Platform: arm64-marvell_rd98DX35xx_cn9131-r0
     HwSKU: rd98DX35xx_cn9131
     ASIC: marvell
     Port Config: 32x1G + 16x2.5G + 6x25G
    
    Signed-off-by: Pavan Naregundi <pnaregundi@marvell.com>

--- platform/marvell-arm64/rules.mk
+++ platform/marvell-arm64/rules.mk
@@ -6,6 +6,7 @@ include $(PLATFORM_PATH)/docker-saiserver-mrvl.mk
 include $(PLATFORM_PATH)/libsaithrift-dev.mk
 include $(PLATFORM_PATH)/one-image.mk
 include $(PLATFORM_PATH)/mrvl-prestera.mk
+include $(PLATFORM_PATH)/platform-marvell.mk
 
 SONIC_ALL += $(SONIC_ONE_IMAGE) \
              $(DOCKER_FPM) 	\
