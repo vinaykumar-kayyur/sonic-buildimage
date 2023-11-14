4ff9a17f76d9eff25c1edefad119eb2953ff9517
Xichen Lin <lukelin0907@gmail.com>
2023-11-14 05:22:47 +0000

    fix bug

--- dockers/docker-dhcp-server/cli-plugin-tests/test_show_dhcp_server.py
+++ dockers/docker-dhcp-server/cli-plugin-tests/test_show_dhcp_server.py
@@ -3,7 +3,6 @@ from unittest import mock
 
 from click.testing import CliRunner
 
-from utilities_common.db import Db
 
 sys.path.append('../cli/show/plugins/')
 import show_dhcp_server
@@ -15,21 +14,21 @@ class TestShowDHCPServer(object):
 
     def test_show_dhcp_server_ipv4_lease_without_dhcpintf(self, mock_db):
         runner = CliRunner()
-        db = Db()
+        db = Mock()
         db.db = mock_db()
         result = runner.invoke(show_dhcp_server.dhcp_server.ipv4.lease, [])
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
 
     def test_show_dhcp_server_ipv4_lease_with_dhcpintf(self, mock_db):
         runner = CliRunner()
-        db = Db()
+        db = Mock()
         db.db = mock_db()
         result = runner.invoke(show_dhcp_server.dhcp_server.ipv4.lease, ["Vlan1000"])
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
 
     def test_show_dhcp_server_ipv4_lease_client_not_in_fdb(self, mock_db):
         runner = CliRunner()
-        db = Db()
+        db = Mock()
         db.db = mock_db()
         result = runner.invoke(show_dhcp_server.dhcp_server.ipv4.lease, ["Vlan1001"])
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
