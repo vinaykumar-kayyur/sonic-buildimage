e681d60feae6635acf0d21dd772603ba1b224d04
nmoray <nikhil@aviznetworks.com>
2023-11-16 09:45:12 -0800

    Added test cases to test security cipher module

---
+++ src/sonic-py-common/tests/test_security_cipher.py
@@ -0,0 +1,18 @@
+from sonic_py_common.secure_cipher import secure_cipher
+
+temp = security_cipher()
+
+class TestSecurityCipher(object):
+    def test_passkey_encryption(self):
+        encrypt, err = temp.encrypt_passkey("TACPLUS", "passkey1", "TEST1")
+        assert encrypt !=  "passkey1"
+        assert err == None 
+
+    def test_passkey_decryption(self):
+        encrypt, err = temp.encrypt_passkey("RADIUS", "passkey2", "TEST2")
+        assert err == None 
+        decrypt, err = temp.decrypt_passkey("TACPLUS", encrypt)
+        assert err == None 
+        assert decrypt == "passkey2"
+
+
