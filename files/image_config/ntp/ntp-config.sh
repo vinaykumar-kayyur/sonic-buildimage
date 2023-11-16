3e6379a0c6ee590a105b82a0c086030a57d16cc4
Saravanan <saravan@celestica.com>
2023-11-16 06:42:55 +0000

    Issue Description: sonic device clock stays far away from actual clock time even when NTP is synchronized to it Root-Cause: NTP long jump is disabled in the NTP daemon What I did: Enabled NTP longjump so that sonic device clock is synchronized with NTP server

--- files/image_config/ntp/ntp-config.sh
+++ files/image_config/ntp/ntp-config.sh
@@ -26,7 +26,7 @@ function modify_ntp_default
 sonic-cfggen -d -t /usr/share/sonic/templates/ntp.conf.j2 >/etc/ntp.conf
 
 get_database_reboot_type
-echo "Disabling NTP long jump for reboot type ${reboot_type} ..."
-modify_ntp_default "s/NTPD_OPTS='-g'/NTPD_OPTS='-x'/"
+echo "Enabling NTP long jump for reboot type ${reboot_type} ..."
+modify_ntp_default "s/NTPD_OPTS='-x'/NTPD_OPTS='-g'/"
 
 systemctl --no-block restart ntp
