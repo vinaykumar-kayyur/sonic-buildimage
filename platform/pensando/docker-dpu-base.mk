23ce5f4239b6f6d12744b03a5c0ff3253183fbf7
Shantanu Shrivastava <shanshri@amd.com>
2023-11-16 11:56:12 +0000

    Adding dummy docker-dpu-base URL

--- platform/pensando/docker-dpu-base.mk
+++ platform/pensando/docker-dpu-base.mk
@@ -4,5 +4,7 @@ DOCKER_DPU_BASE_STEM = docker-dpu-base
 
 DOCKER_DPU_BASE = $(DOCKER_DPU_BASE_STEM).gz
 
+$(DOCKER_DPU_BASE)_URL = https://sonicstorage.blob.core.windows.net/public/docker-dpu-base.gz
+
 DOWNLOADED_DOCKER_IMAGES += $(DOCKER_DPU_BASE)
 
