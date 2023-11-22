4efb99dd13e5a9811acef2d00525d63ea0a358a9
nmoray <nikhil@aviznetworks.com>
2023-11-22 09:21:07 -0800

    Added support of mock class in existing AUT

--- src/sonic-py-common/tests/test_security_cipher.py
+++ src/sonic-py-common/tests/test_security_cipher.py
@@ -5,25 +5,36 @@ if sys.version_info.major == 3:
 else:
     import mock
 
+import pytest
 from sonic_py_common.security_cipher import security_cipher
 from .mock_swsscommon import ConfigDBConnector
 
+EXPECTED_PASSWD = "TEST2" 
 
 class TestSecurityCipher(object):
     def test_passkey_encryption(self):
         with mock.patch("sonic_py_common.security_cipher.ConfigDBConnector", new=ConfigDBConnector):
             temp = security_cipher()
-            encrypt, err = temp.encrypt_passkey("TACPLUS", "passkey1", "TEST1")
-            assert encrypt !=  "passkey1"
-            assert err == None 
+
+            # Use patch to replace the built-in 'open' function with a mock
+            with mock.patch("builtins.open", mock_open()) as mock_file:
+                encrypt, err = temp.encrypt_passkey("TACPLUS", "passkey1", "TEST1")
+                assert encrypt !=  "passkey1"
+                assert err == None 
 
     def test_passkey_decryption(self):
         with mock.patch("sonic_py_common.security_cipher.ConfigDBConnector", new=ConfigDBConnector):
             temp = security_cipher()
-            encrypt, err = temp.encrypt_passkey("RADIUS", "passkey2", "TEST2")
-            assert err == None 
-            decrypt, err = temp.decrypt_passkey("RADIUS", encrypt)
-            assert err == None 
-            assert decrypt == "passkey2"
+
+            # Use patch to replace the built-in 'open' function with a mock
+            with mock.patch("builtins.open", mock_open()) as mock_file:
+                encrypt, err = temp.encrypt_passkey("RADIUS", "passkey2", EXPECTED_PASSWD)
+                assert err == None
+
+            # Use patch to replace the built-in 'open' function with a mock
+            with mock.patch("builtins.open", mock_open(read_data=EXPECTED_PASSWD)) as mock_file: 
+                decrypt, err = temp.decrypt_passkey("RADIUS", encrypt)
+                assert err == None 
+                assert decrypt == "passkey2"
 
 
