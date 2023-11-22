4b3ebbdacedbfa732dfb003be605242de03390c6
nmoray <nikhil@aviznetworks.com>
2023-11-22 03:12:53 -0800

    Fixed AUT issues

--- src/sonic-py-common/tests/test_security_cipher.py
+++ src/sonic-py-common/tests/test_security_cipher.py
@@ -1,18 +1,22 @@
 from sonic_py_common.security_cipher import security_cipher
+from .mock_swsscommon import ConfigDBConnector 
 
-temp = security_cipher()
 
 class TestSecurityCipher(object):
     def test_passkey_encryption(self):
-        encrypt, err = temp.encrypt_passkey("TACPLUS", "passkey1", "TEST1")
-        assert encrypt !=  "passkey1"
-        assert err == None 
+        with mock.patch("sonic_py_common.security_cipher.ConfigDBConnector", new=ConfigDBConnector):
+            temp = security_cipher()
+            encrypt, err = temp.encrypt_passkey("TACPLUS", "passkey1", "TEST1")
+            assert encrypt !=  "passkey1"
+            assert err == None 
 
     def test_passkey_decryption(self):
-        encrypt, err = temp.encrypt_passkey("RADIUS", "passkey2", "TEST2")
-        assert err == None 
-        decrypt, err = temp.decrypt_passkey("RADIUS", encrypt)
-        assert err == None 
-        assert decrypt == "passkey2"
+        with mock.patch("sonic_py_common.security_cipher.ConfigDBConnector", new=ConfigDBConnector):
+            temp = security_cipher()
+            encrypt, err = temp.encrypt_passkey("RADIUS", "passkey2", "TEST2")
+            assert err == None 
+            decrypt, err = temp.decrypt_passkey("RADIUS", encrypt)
+            assert err == None 
+            assert decrypt == "passkey2"
 
 
