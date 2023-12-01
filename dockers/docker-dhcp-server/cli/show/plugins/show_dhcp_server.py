fa233206f356dda8ebc8ff30cac5acd89ca54dc8
Xichen Lin <lukelin0907@gmail.com>
2023-12-01 18:54:18 +0000

    add change

--- dockers/docker-dhcp-server/cli/show/plugins/show_dhcp_server.py
+++ dockers/docker-dhcp-server/cli/show/plugins/show_dhcp_server.py
@@ -12,9 +12,14 @@ def ts_to_str(ts):
 
 
 @click.group(cls=clicommon.AliasedGroup)
-def dhcp_server():
+@clicommon.pass_db
+def dhcp_server(db):
     """Show dhcp_server related info"""
-    pass
+    ctx = click.get_current_context()
+    dbconn = db.db
+    if dbconn.get("CONFIG_DB", "FEATURE|dhcp_server", "state") != "enabled":
+        ctx.fail("Feature dhcp_server is not enabled")
+    ctx.dbconn = dbconn
 
 
 @dhcp_server.group(cls=clicommon.AliasedGroup)
@@ -25,13 +30,13 @@ def ipv4():
 
 @ipv4.command()
 @click.argument('dhcp_interface', required=False)
-@clicommon.pass_db
-def lease(db, dhcp_interface):
+def lease(dhcp_interface):
+    ctx = click.get_current_context()
     if not dhcp_interface:
         dhcp_interface = "*"
     headers = ["Interface", "MAC Address", "IP", "Lease Start", "Lease End"]
     table = []
-    dbconn = db.db
+    dbconn = ctx.dbconn
     for key in dbconn.keys("STATE_DB", "DHCP_SERVER_IPV4_LEASE|" + dhcp_interface + "|*"):
         entry = dbconn.get_all("STATE_DB", key)
         interface, mac = key.split("|")[1:]
@@ -50,13 +55,13 @@ def count_ipv4(start, end):
 
 @ipv4.command()
 @click.argument('range_name', required=False)
-@clicommon.pass_db
 def range(db, range_name):
+    ctx = click.get_current_context()
     if not range_name:
         range_name = "*"
     headers = ["Range", "IP Start", "IP End", "IP Count"]
     table = []
-    dbconn = db.db
+    dbconn = ctx.dbconn
     for key in dbconn.keys("CONFIG_DB", "DHCP_SERVER_IPV4_RANGE|" + range_name):
         name = key.split("|")[1]
         entry = dbconn.get_all("CONFIG_DB", key)
