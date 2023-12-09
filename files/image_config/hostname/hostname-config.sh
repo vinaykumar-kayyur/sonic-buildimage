9a868b4844463c2e3c3b9467033e1a4e901aeb8a
PinghaoQu <qu.pinghao@h3c.com>
2023-12-09 10:12:50 +0800

    Write the default hostname ‘sonic’ into config_db
    hostname-config/lldp: Optimize
    
    * Fixing the “RemoteDevice” displayed in "show lldp tables" does not match the remote device's hostname.
    
      Signed-off-by: qu.pinghao@h3c.com

--- files/image_config/hostname/hostname-config.sh
+++ files/image_config/hostname/hostname-config.sh
@@ -6,6 +6,7 @@ HOSTNAME=`sonic-cfggen -d -v DEVICE_METADATA[\'localhost\'][\'hostname\']`
 if [ -z "$HOSTNAME" ] ; then
        echo "Missing hostname in the config file, setting to default 'sonic'"
        HOSTNAME='sonic'
+       sonic-cfggen -a '{"DEVICE_METADATA":{"localhost":{"hostname":"sonic"}}}' -w
 fi
 
 echo $HOSTNAME > /etc/hostname
