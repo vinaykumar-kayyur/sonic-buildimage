29d75e18dadf3f2cb02a87983041c2da411afab0
Xichen Lin <lukelin0907@gmail.com>
2023-12-13 17:35:04 +0000

    add test dup gw nm

--- dockers/docker-dhcp-server/cli-plugin-tests/test_config_dhcp_server.py
+++ dockers/docker-dhcp-server/cli-plugin-tests/test_config_dhcp_server.py
@@ -30,3 +30,19 @@ class TestConfigDHCPServer(object):
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
         assert mock_db.get_all("CONFIG_DB", "DHCP_SERVER_IPV4|Vlan200") == expected_value
 
+    def test_config_dhcp_server_ipv4_add_dup_gw_nm(self, mock_db):
+        expected_value = {
+            "gateway": "100.10.10.10",
+            "lease_time": "1000",
+            "mode": "PORT",
+            "netmask": "255.255.255.0",
+            "state": "disabled"
+        }
+        runner = CliRunner()
+        db = clicommon.Db()
+        db.db = mock_db
+        result = runner.invoke(dhcp_server.dhcp_server.commands["ipv4"].commands["add"], \
+                ["Vlan200", "--mode=PORT", "--lease_time=1000", "--dup_gw_nm"], obj=db)
+        assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
+        assert mock_db.get_all("CONFIG_DB", "DHCP_SERVER_IPV4|Vlan200") == expected_value
+
