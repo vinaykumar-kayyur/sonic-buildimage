93e40c451975a0e272718e953db5626078d092f9
Xichen Lin <lukelin0907@gmail.com>
2023-12-13 15:19:43 +0000

    fix bug

--- dockers/docker-dhcp-server/cli-plugin-tests/test_config_dhcp_server.py
+++ dockers/docker-dhcp-server/cli-plugin-tests/test_config_dhcp_server.py
@@ -6,13 +6,13 @@ from click.testing import CliRunner
 import utilities_common.cli as clicommon
 
 sys.path.append('../cli/config/plugins/')
-import config_dhcp_server
+import dhcp_server
 
 
 class TestConfigDHCPServer(object):
     def test_plugin_registration(self):
         cli = mock.MagicMock()
-        config_dhcp_server.register(cli)
+        dhcp_server.register(cli)
 
     def test_config_dhcp_server_ipv4_add(self, mock_db):
         expected_value = {
@@ -25,7 +25,7 @@ class TestConfigDHCPServer(object):
         runner = CliRunner()
         db = clicommon.Db()
         db.db = mock_db
-        result = runner.invoke(config_dhcp_server.dhcp_server.commands["ipv4"].commands["add"], \
+        result = runner.invoke(dhcp_server.dhcp_server.commands["ipv4"].commands["add"], \
                 ["Vlan200", "--mode=PORT", "--lease_time=1000", "--gateway=10.10.10.10", "--netmask=255.255.254.0"], obj=db)
         assert result.exit_code == 0, "exit code: {}, Exception: {}, Traceback: {}".format(result.exit_code, result.exception, result.exc_info)
         assert mock_db.get_all("CONFIG_DB", "DHCP_SERVER_IPV4|Vlan200") == expected_value
