ada3e1886069d4f1a600b88ffeead991046911bf
Rida Hanif <61490193+ridahanif96@users.noreply.github.com>
2023-12-24 11:05:39 +0000

    Fix for Elastic test

--- src/sonic-config-engine/minigraph.py
+++ src/sonic-config-engine/minigraph.py
@@ -1880,6 +1880,13 @@ def parse_xml(filename, platform=None, port_config_file=None, asic_name=None, hw
     for port in ports.values():
         port['mtu'] = '9100'
         port['tpid'] = '0x8100'
+    # mode check for vlan membership in PORT table default mode is routed and for vlan membership it is trunk
+    for port_name, port in ports.items():
+        if 'mode' not in port:
+            if port_name in [key[1] for key in vlan_members.keys()]:
+                port['mode'] = 'trunk'
+            else:
+                port['mode'] = 'routed'
 
     # asymmetric PFC is disabled by default
     for port in ports.values():
