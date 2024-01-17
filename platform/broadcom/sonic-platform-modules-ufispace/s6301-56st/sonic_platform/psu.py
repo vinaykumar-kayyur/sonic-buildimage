78217c6f14ff0798f29469d127d556c592d77b35
nonodark <ef67891@yahoo.com.tw>
2024-01-17 15:59:30 +0800

    [Ufispace] Use default pddf api to get psu capacity on s6301-56st
    Signed-off-by: nonodark <ef67891@yahoo.com.tw>

--- platform/broadcom/sonic-platform-modules-ufispace/s6301-56st/sonic_platform/psu.py
+++ platform/broadcom/sonic-platform-modules-ufispace/s6301-56st/sonic_platform/psu.py
@@ -10,23 +10,12 @@ except ImportError as e:
 
 class Psu(PddfPsu):
     """PDDF Platform-Specific PSU class"""
-    
-    PLATFORM_PSU_CAPACITY = 750
 
     def __init__(self, index, pddf_data=None, pddf_plugin_data=None):
         PddfPsu.__init__(self, index, pddf_data, pddf_plugin_data)
         self.psu_fru = PsuFru(self.psu_index)
                
     # Provide the functions/variables below for which implementation is to be overwritten
-    def get_maximum_supplied_power(self):
-        """
-        Retrieves the maximum supplied power by PSU (or PSU capacity)
-        Returns:
-            A float number, the maximum power output in Watts.
-            e.g. 1200.1
-        """
-        return float(self.PLATFORM_PSU_CAPACITY)
-
     def get_power(self):
         """
         Retrieves current energy supplied by PSU
