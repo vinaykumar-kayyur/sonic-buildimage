dca3f69275803ed9ab5357e3ddcfceaedb160244
Sophie Kravitz <skravitz@nvidia.com>
2024-02-04 09:02:08 +0000

    Added the ability to see a summary of all component versions on a switch. Added a compiled versions file to /etc/mlnx/ that contains only the  versions available in compilation (FW, SDK, MFT, SAI, HW-MGMT, Kernel) and a script that outputs a table with all the versions (internal + platform). The table contains three columns: component name, compiled version(only available for intarnal components), actual version(taken from the switch itself).
    example:
    
      Component      |       compilation      |     actual
    -----------------------------------------------------------------
    SDK              |  4.6.2134              |  4.6.2134
    FW               |  2012.2134             |  2012.2134
    SAI              |  SAIBuild2311.26.0.28  |  SAIBuild2311.26.0.28
    HW-MGMT          |  7.0030.2008           |  7.0030.2008
    MFT              |  4.25.0                |  4.25.0
    Kernel           |  5.10.0-23-2-amd64     |  5.10.0-23-2-amd64
    BIOS             |                        |  5.6.5
    SSD              |                        |  0202-000
    CPLD1            |                        |  CPLD000087_REV0600
    CPLD2            |                        |  CPLD000075_REV0600
    ONIE             |                        |  2022.08-5.3.0010-9600

--- platform/mellanox/rules.mk
+++ platform/mellanox/rules.mk
@@ -18,7 +18,6 @@ include $(PLATFORM_PATH)/sdk.mk
 include $(PLATFORM_PATH)/cpld.mk
 include $(PLATFORM_PATH)/fw.mk
 include $(PLATFORM_PATH)/mft.mk
-include $(PLATFORM_PATH)/mft-fwtrace-cfg.mk
 include $(PLATFORM_PATH)/mlnx-sai.mk
 include $(PLATFORM_PATH)/hw-management.mk
 include $(PLATFORM_PATH)/mlnx-platform-api.mk
@@ -31,7 +30,6 @@ include $(PLATFORM_PATH)/mlnx-ffb.mk
 include $(PLATFORM_PATH)/issu-version.mk
 include $(PLATFORM_PATH)/mlnx-onie-fw-update.mk
 include $(PLATFORM_PATH)/mlnx-ssd-fw-update.mk
-include $(PLATFORM_PATH)/iproute2.mk
 include $(PLATFORM_PATH)/install-pending-fw.mk
 include $(PLATFORM_PATH)/integration-scripts.mk
 include $(PLATFORM_PATH)/component-versions.mk
@@ -59,8 +57,5 @@ $(DOCKER_PLATFORM_MONITOR)_DEPENDS += $(APPLIBS) $(SX_COMPLIB) $(SXD_LIBS) $(SX_
 
 # Force the target bootloader for mellanox platforms to grub regardless of arch
 TARGET_BOOTLOADER = grub
-
-# location for the platform specific external kernel patches
-override EXTERNAL_KERNEL_PATCH_LOC := $(BUILD_WORKDIR)/$(PLATFORM_PATH)/non-upstream-patches/
 
 export SONIC_BUFFER_MODEL=dynamic
