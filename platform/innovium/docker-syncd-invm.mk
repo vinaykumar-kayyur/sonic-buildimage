4ded5ca4a0aff6d108e92f2dd741f8870105423f
Keshav Gupta <keshavg@marvell.com>
2024-02-09 02:34:41 -0800

    Fix Marvell innovium build for SONiC202211

--- platform/innovium/docker-syncd-invm.mk
+++ platform/innovium/docker-syncd-invm.mk
@@ -1,7 +1,7 @@
 # docker image for innovium syncd
 
 DOCKER_SYNCD_PLATFORM_CODE = invm
-include $(PLATFORM_PATH)/../template/docker-syncd-base.mk
+include $(PLATFORM_PATH)/../template/docker-syncd-bullseye.mk
 
 $(DOCKER_SYNCD_BASE)_DEPENDS += $(SYNCD) $(PYTHON_SDK_API) $(INVM_LIBSAI)
 
@@ -10,10 +10,8 @@ $(DOCKER_SYNCD_BASE)_DBG_DEPENDS += $(SYNCD_DBG) \
                                 $(LIBSAIMETADATA_DBG) \
                                 $(LIBSAIREDIS_DBG)
 
-SONIC_STRETCH_DOCKERS += $(DOCKER_SYNCD_BASE)
-SONIC_STRETCH_DBG_DOCKERS += $(DOCKER_SYNCD_BASE_DBG)
-
 $(DOCKER_SYNCD_BASE)_VERSION = 1.0.0
 $(DOCKER_SYNCD_BASE)_PACKAGE_NAME = syncd
+$(DOCKER_SYNCD_BASE)_MACHINE = innovium
 
 $(DOCKER_SYNCD_BASE)_RUN_OPT += -v /host/warmboot:/var/warmboot
