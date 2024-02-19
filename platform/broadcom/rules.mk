4cd3708fd6ba956bd07455bd4f94e3a5e987a5f6
philo <philo@micasnetworks.com>
2024-02-19 10:22:01 +0800

    update rules.mk
    Signed-off-by: philo <philo@micasnetworks.com>

--- platform/broadcom/rules.mk
+++ platform/broadcom/rules.mk
@@ -15,6 +15,7 @@ include $(PLATFORM_PATH)/platform-modules-alphanetworks.mk
 include $(PLATFORM_PATH)/platform-modules-cel.mk
 #include $(PLATFORM_PATH)/platform-modules-delta.mk
 include $(PLATFORM_PATH)/platform-modules-quanta.mk
+include $(PLATFORM_PATH)/platform-modules-micas.mk
 ##include $(PLATFORM_PATH)/platform-modules-mitac.mk
 #include $(PLATFORM_PATH)/platform-modules-juniper.mk
 #include $(PLATFORM_PATH)/platform-modules-brcm-xlr-gts.mk
@@ -22,7 +23,6 @@ include $(PLATFORM_PATH)/platform-modules-quanta.mk
 #include $(PLATFORM_PATH)/platform-modules-ragile.mk
 #include $(PLATFORM_PATH)/platform-modules-tencent.mk
 #include $(PLATFORM_PATH)/platform-modules-ufispace.mk
-include $(PLATFORM_PATH)/platform-modules-micas.mk
 include $(PLATFORM_PATH)/docker-syncd-brcm.mk
 include $(PLATFORM_PATH)/docker-syncd-brcm-rpc.mk
 include $(PLATFORM_PATH)/docker-saiserver-brcm.mk
