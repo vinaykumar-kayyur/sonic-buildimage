6462dcad5a3c66bdc8f0c8dafd3ce1b5e0b1a42b
shilongliu <shilongliu@microsoft.com>
2024-02-29 10:41:18 +0800

    fix

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -3,7 +3,7 @@ $(BRCM_SAI)_URL = "https://sonicstorage.blob.core.windows.net/public/bcmsai/3.5/
 
 BRCM_SAI_DEV = libsaibcm-dev_3.5.3.8_amd64.deb
 $(eval $(call add_derived_package,$(BRCM_SAI),$(BRCM_SAI_DEV)))
-$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/packages/bcmsai/3.5/jessie/libsaibcm-dev_3.5.3.8_amd64.deb"
+$(BRCM_SAI_DEV)_URL = "https://sonicstorage.blob.core.windows.net/public/bcmsai/3.5/jessie/libsaibcm-dev_3.5.3.8_amd64.deb"
 
 SONIC_ONLINE_DEBS += $(BRCM_SAI)
 $(BRCM_SAI_DEV)_DEPENDS += $(BRCM_SAI)
