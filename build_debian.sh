5acfb78d581404a6fd6c44559e6b76b4b91befbf
gpunathilell <gpunathilell@nvidia.com>
2024-03-06 20:00:10 +0000

    Added new package minicom in the switch image

--- build_debian.sh
+++ build_debian.sh
@@ -392,6 +392,7 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
     jq                      \
     auditd                  \
     linux-perf              \
+    minicom                 \
     resolvconf              \
 	lsof                    \
 	sysstat                 \
