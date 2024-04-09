1f70c4c7689619d15d763a7e4df33da3fe56caae
FSSec <sec@feisu.com>
2024-04-09 01:45:29 +0000

    add n9550-32d rule

--- platform/broadcom/sonic-platform-modules-fs/n5850-48s6c/sonic_platform_setup.py
+++ platform/broadcom/sonic-platform-modules-fs/n5850-48s6c/sonic_platform_setup.py
@@ -1,34 +1,16 @@
-from setuptools import setup
+#!/usr/bin/env pytho
 
-DEVICE_NAME = 'fs'
-HW_SKU = 'x86_64-fs_n5850_48s6c-r0'  
+import os
+import sys
+from setuptools import setup
+os.listdir
 
 setup(
-    name='sonic-platform',
-    version='1.0',
-    description='SONiC platform API implementation on FS Platforms',
-    license='Apache 2.0',
-    author='SONiC Team',
-    author_email='linuxnetdev@microsoft.com',
-    url='https://github.com/Azure/sonic-buildimage',
-    maintainer='Jostar Yang',
-    maintainer_email='jostar_yang@fs.com',
-    packages=[
-        'sonic_platform',
-    ],
-    package_dir={
-        'sonic_platform': '../../../../device/{}/{}/sonic_platform'.format(DEVICE_NAME, HW_SKU)},
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Plugins',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Utilities',
-    ],
-    keywords='sonic SONiC platform PLATFORM',
+   name='sonic_platform',
+   version='1.0',
+   description='SONiC platform API implementation on FS Platforms',
+   
+   packages=['sonic-platform'],
+   package_dir={'sonic_platform': 'n8550-48b8c/sonic_platform'},
 )
+
--- platform/broadcom/sonic-platform-modules-fs/n9550-32d/sonic_platform_setup.py
+++ platform/broadcom/sonic-platform-modules-fs/n9550-32d/sonic_platform_setup.py
@@ -1,25 +1,16 @@
+#!/usr/bin/env pytho
+
+import os
+import sys
 from setuptools import setup
+os.listdir
 
 setup(
-    name='sonic-platform',
-    version='1.0',
-    description='SONiC platform API implementation on FS Platforms',
-    license='Apache 2.0',
-    author='SONiC Team',
-    author_email='linuxnetdev@microsoft.com',
-    url='https://github.com/Azure/sonic-buildimage',
-    packages=['sonic_platform'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Plugins',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Utilities',
-    ],
-    keywords='sonic SONiC platform PLATFORM',
+   name='sonic_platform',
+   version='1.0',
+   description='SONiC platform API implementation on FS Platforms',
+   
+   packages=['sonic-platform'],
+   package_dir={'sonic_platform': 'n9550-32d/sonic_platform'},
 )
+
