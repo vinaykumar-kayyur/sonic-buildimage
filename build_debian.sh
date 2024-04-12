fac1073add5ed3cad7cf514fe03b1ecf5b12f9f8
Justin Wong <51811017+justin-wong-ce@users.noreply.github.com>
2024-04-11 22:54:00 -0700

    Fixed indents
    Applying the patch did something weird to the indents

--- build_debian.sh
+++ build_debian.sh
@@ -402,11 +402,11 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
     auditd                  \
     linux-perf              \
     resolvconf              \
-	lsof                    \
-	sysstat                 \
-	xxd                     \
+    lsof                    \
+    sysstat                 \
+    xxd                     \
     wireless-regdb          \
-	zstd
+    zstd
 
 # Have systemd create the auditd log directory
 sudo mkdir -p ${FILESYSTEM_ROOT}/etc/systemd/system/auditd.service.d
