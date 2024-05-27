347addaaea50e5ee54364e1483c575bac8f4b43c
tshalvi <tshalvi@nvidia.com>
2024-05-27 16:54:53 +0300

    Refactor to validate module presence before accessing its EEPROM

--- platform/mellanox/mlnx-platform-api/sonic_platform/sfp.py
+++ platform/mellanox/mlnx-platform-api/sonic_platform/sfp.py
@@ -507,6 +507,11 @@ class SFP(NvidiaSFPCommon):
         Returns:
             bytearray: the content of EEPROM
         """
+        presence_sysfs = f'/sys/module/sx_core/asic0/module{self.sdk_index}/hw_present' if self.is_sw_control() else f'/sys/module/sx_core/asic0/module{self.sdk_index}/present'
+        if utils.read_int_from_file(presence_sysfs) != 1:
+            logger.log_warning(f'Failed attempt to read EEPROM for sfp={self.sdk_index}: No module connected')
+            return None
+
         result = bytearray(0)
         while num_bytes > 0:
             _, page, page_offset = self._get_page_and_page_offset(offset)
