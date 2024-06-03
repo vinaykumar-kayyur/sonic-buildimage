ae6a93365e0a2375f2c768872043ac2c4af0407f
peter005_wong <peter005_wong@edge-core.com>
2024-06-03 09:34:27 +0800

    Fix 'requests' library version lower than 2.32.0
    Description:
    In the issue#19028, the new version of requests library caused building sonic image got failed.
    The purpose of this PR is to fix the requests library verison to lower than 2.32.0 to let the building pass.

--- build_debian.sh
+++ build_debian.sh
@@ -543,7 +543,7 @@ sudo LANG=C DEBIAN_FRONTEND=noninteractive chroot $FILESYSTEM_ROOT apt-get -y in
 
 # docker Python API package is needed by Ansible docker module as well as some SONiC applications
 sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'docker==6.1.1'
-
+sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'requests<2.32.0'
 # Install scapy
 sudo https_proxy=$https_proxy LANG=C chroot $FILESYSTEM_ROOT pip3 install 'scapy==2.4.4'
 
