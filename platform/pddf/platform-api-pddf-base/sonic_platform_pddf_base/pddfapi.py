d4c53076730e6c0a2fb922449350464430e4c999
Jemston Fernando <sjemstonirudayam.fernando@celestica.com>
2024-06-30 18:17:29 +0000

    Enhance IPMI raw cmd and custom pmon object names
    - Add some post processing parameters for IPMI raw command
    - Use custom names for the PMON objects - Fan, Fan drawer, Thermal
    - Attempt to get LPMode from the transceiver only when presence is true

--- platform/pddf/platform-api-pddf-base/sonic_platform_pddf_base/pddfapi.py
+++ platform/pddf/platform-api-pddf-base/sonic_platform_pddf_base/pddfapi.py
@@ -904,16 +904,40 @@ class PddfApi():
         return str(value)
 
     def raw_ipmi_get_request(self, bmc_attr):
-        value = 'N/A'
+        value = None
         cmd = bmc_attr['bmc_cmd'] + " 2>/dev/null"
         if bmc_attr['type'] == 'raw':
             try:
                 value = subprocess.check_output(cmd, shell=True, universal_newlines=True).strip()
+
+                if 'offset' in bmc_attr.keys():
+                    offset = int(bmc_attr['offset'])
+                    #Assuming value is a string of bytes seperated by space
+                    value = value[offset * 3 : (offset * 3) + 2]
+
+                value = int(value, 16)
+
+                if 'multiplier' in bmc_attr.keys():
+                    mult = float(bmc_attr['multiplier'])
+                    value = int(value * mult)
+
+                if 'decrementor' in bmc_attr.keys():
+                    decr = float(bmc_attr['decrementor'])
+                    value = int(value - decr)
+
+                if 'mask' in bmc_attr.keys():
+                    mask = int(bmc_attr['mask'], 16)
+                    value = value & mask
+
+
             except Exception as e:
                 pass
 
-            if value != 'N/A':
-                value = str(int(value, 16))
+            if value != None:
+                value = str(value)
+            else:
+                value = 'N/A'
+
             return value
 
         if bmc_attr['type'] == 'mask':
