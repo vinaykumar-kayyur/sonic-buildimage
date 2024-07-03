7edc98750cc32e1ed4032504bd36904fca1d9522
Ghulam-Bahoo <ghulam.bahoo@xflowresearch.com>
2024-07-03 14:23:16 +0500

    Kdump Network setup

--- build_debian.sh
+++ build_debian.sh
@@ -431,8 +431,8 @@ sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "mkdir -p /etc/initramfs-tools/
 sudo LANG=C chroot $FILESYSTEM_ROOT /bin/bash -c "echo 'MODULES=most' >> /etc/initramfs-tools/conf.d/driver-policy"
 
 # Ensure the relevant directories exist
-mkdir -p /etc/initramfs-tools/scripts/init-premount
-mkdir -p /etc/initramfs-tools/hooks
+sudo mkdir -p /etc/initramfs-tools/scripts/init-premount
+sudo mkdir -p /etc/initramfs-tools/hooks
 
 # Copy the network setup scriptgit
 sudo cp files/scripts/network_setup.sh /etc/initramfs-tools/scripts/init-premount/network_setup.sh
