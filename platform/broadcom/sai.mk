7e1a7eb28882df699781cdd1dbfbc81f01988b0f
wenyiz2021 <91497961+wenyiz2021@users.noreply.github.com>
2024-08-19 17:32:26 -0700

    Update sai.mk

--- platform/broadcom/sai.mk
+++ platform/broadcom/sai.mk
@@ -12,7 +12,7 @@ $(eval $(call add_derived_package,$(BRCM_XGS_SAI),$(BRCM_XGS_SAI_DEV)))
 $(BRCM_XGS_SAI_DEV)_URL = "$(LIBSAIBCM_XGS_URL_PREFIX)/$(BRCM_XGS_SAI_DEV)"
 
 # SAI module for DNX Asic family
-BRCM_DNX_SAI = libsaibcm_$(LIBSAIBCM_DNX_VERSION)_amd64.deb
+BRCM_DNX_SAI = libsaibcm_dnx_$(LIBSAIBCM_DNX_VERSION)_amd64.deb
 $(BRCM_DNX_SAI)_URL = "$(LIBSAIBCM_DNX_URL_PREFIX)/$(BRCM_DNX_SAI)"
 
 SONIC_ONLINE_DEBS += $(BRCM_XGS_SAI)
