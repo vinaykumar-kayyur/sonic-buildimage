61be6bc0e88cdfa25cd315892d009806e3fcce8c
Saikrishna Arcot <sarcot@microsoft.com>
2024-08-31 18:02:28 -0700

    Make sure environment variables are present in the second (final) stage
    Signed-off-by: Saikrishna Arcot <sarcot@microsoft.com>

--- platform/vs/syncd-vs.mk
+++ platform/vs/syncd-vs.mk
@@ -3,13 +3,6 @@ $(LIBSAIREDIS)_DEB_BUILD_PROFILES += syncd vs
 SYNCD_VS = syncd-vs_1.0.0_$(CONFIGURED_ARCH).deb
 $(SYNCD_VS)_RDEPENDS += $(LIBSAIREDIS) $(LIBSAIMETADATA) $(LIBSAIVS)
 
-ifeq ($(BLDENV),bookworm)
-  $(LIBSAIREDIS)_DEB_BUILD_PROFILES += dashsai
-  $(LIBSAIREDIS)_DEPENDS += $(DASH_SAI)
-  $(SYNCD_VS)_RDEPENDS += $(DASH_SAI)
-else
-  $(warning DASH_SAI cannot support this build environment $(BLDENV))
-endif
 
 
 $(eval $(call add_derived_package,$(LIBSAIREDIS),$(SYNCD_VS)))
